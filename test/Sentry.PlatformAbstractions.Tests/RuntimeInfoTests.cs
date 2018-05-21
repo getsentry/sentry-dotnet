ae4bc88cec59c3fba97e7ff6861cdadeddd6c11a
Bruno Garcia <bruno@brunogarcia.com>
2018-05-21 10:59:07 +0200

    Fixed parsing when name is known and provided

--- test/Sentry.PlatformAbstractions.Tests/RuntimeInfoTests.cs
+++ test/Sentry.PlatformAbstractions.Tests/RuntimeInfoTests.cs
@@ -17,11 +17,19 @@ namespace Sentry.PlatformAbstractions.Tests
         [Theory, TestCaseSource(typeof(RuntimeInfoTests), nameof(ParseTestCases))]
         public void Parse_TestCases(ParseTestCase parseTestCase)
         {
-            var actual = RuntimeInfo.Parse(parseTestCase.Raw);
+            var actual = RuntimeInfo.Parse(parseTestCase.Raw,
+                    parseTestCase.NameProvided);
 
             if (parseTestCase.Raw == null)
             {
-                Assert.Null(actual);
+                if (parseTestCase.NameProvided == null)
+                {
+                    Assert.Null(actual);
+                }
+                else
+                {
+                    Assert.AreEqual(parseTestCase.NameProvided, actual.Name);
+                }
             }
             else
             {
@@ -92,6 +100,13 @@ namespace Sentry.PlatformAbstractions.Tests
             };
             yield return new ParseTestCase
             {
+                Raw = "5.10.1.47 (2017-12/8eb8f7d5e74 Fri Apr 13 20:18:12 EDT 2018)",
+                NameProvided = "Mono",
+                ExpectedName = "Mono",
+                ExpectedVersion = "5.10.1.47"
+            };
+            yield return new ParseTestCase
+            {
                 Raw = "Mono 5.10.0 (Visual Studio built mono)",
                 ExpectedName = "Mono",
                 ExpectedVersion = "5.10.0"
@@ -104,7 +119,21 @@ namespace Sentry.PlatformAbstractions.Tests
             };
             yield return new ParseTestCase
             {
+                Raw = "4.0.30319.17020",
+                NameProvided = ".NET Framework",
+                ExpectedName = ".NET Framework",
+                ExpectedVersion = "4.0.30319.17020"
+            };
+            yield return new ParseTestCase
+            {
                 Raw = ".NET Core 1.0",
+                ExpectedName = ".NET Core",
+                ExpectedVersion = "1.0"
+            };
+            yield return new ParseTestCase
+            {
+                Raw = "1.0",
+                NameProvided = ".NET Core",
                 ExpectedName = ".NET Core",
                 ExpectedVersion = "1.0"
             };
@@ -116,17 +145,22 @@ namespace Sentry.PlatformAbstractions.Tests
             };
             yield return new ParseTestCase
             {
-                Raw = ".NET Framework For Windows Mobile"
+                Raw = ".NET Framework For Windows Mobile 10"
             };
             yield return new ParseTestCase
             {
-                Raw = ".NET Framework For Windows Mobile 10",
+                Raw = ".NET Framework For Windows Mobile 10.1",
                 ExpectedName = ".NET Framework For Windows Mobile",
-                ExpectedVersion = "10"
+                ExpectedVersion = "10.1"
             };
             yield return new ParseTestCase
             {
                 Raw = "web"
+            };
+            yield return new ParseTestCase
+            {
+                NameProvided = "web",
+                ExpectedName = "web"
             };
             yield return new ParseTestCase
             {
@@ -139,6 +173,7 @@ namespace Sentry.PlatformAbstractions.Tests
         public class ParseTestCase
         {
             public string Raw { get; set; }
+            public string NameProvided { get; set; }
             public string ExpectedName { get; set; }
             public string ExpectedVersion { get; set; }
         }
