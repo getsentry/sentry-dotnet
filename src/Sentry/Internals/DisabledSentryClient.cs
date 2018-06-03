f7c1ee0ca2ae4ae2152313905b5fde9f84b2cf46
Bruno Garcia <bruno@brunogarcia.com>
2018-06-03 15:05:05 -0700

    Removed redundant members of DisabledClient

--- src/Sentry/Internals/DisabledSentryClient.cs
+++ src/Sentry/Internals/DisabledSentryClient.cs
@@ -1,8 +1,6 @@
 using System;
-using System.Collections.Generic;
 using System.Threading.Tasks;
 using Sentry.Extensibility;
-using Sentry.Infrastructure;
 using Sentry.Protocol;
 
 namespace Sentry.Internals
@@ -14,6 +12,8 @@ namespace Sentry.Internals
 
         public static DisabledSentryClient Disabled = new DisabledSentryClient();
 
+        public bool IsEnabled => false;
+
         private DisabledSentryClient() { }
 
         public void ConfigureScope(Action<Scope> configureScope) { }
@@ -22,39 +22,9 @@ namespace Sentry.Internals
         public IDisposable PushScope() => this;
         public IDisposable PushScope<TState>(TState state) => this;
 
-        public void AddBreadcrumb(
-            string message,
-            string type,
-            string category = null,
-            IDictionary<string, string> data = null,
-            BreadcrumbLevel level = default)
-        { }
-
-        public void AddBreadcrumb(
-            ISystemClock clock,
-            string message,
-            string type = null,
-            string category = null,
-            IDictionary<string, string> data = null,
-            BreadcrumbLevel level = default)
-        { }
-
-        public bool IsEnabled => false;
-
         public SentryResponse CaptureEvent(SentryEvent evt) => DisabledResponse;
-
         public Task<SentryResponse> CaptureEventAsync(SentryEvent evt) => DisabledResponseTask;
-
-        public SentryResponse CaptureException(Exception exception) => DisabledResponse;
-
-        public Task<SentryResponse> CaptureExceptionAsync(Exception exception) => DisabledResponseTask;
-
-        public SentryResponse WithClientAndScope(Func<ISentryClient, Scope, SentryResponse> handler) => DisabledResponse;
-
-        public Task<SentryResponse> WithClientAndScopeAsync(Func<ISentryClient, Scope, Task<SentryResponse>> handler) => DisabledResponseTask;
-
         public SentryResponse CaptureEvent(Func<SentryEvent> eventFactory) => DisabledResponse;
-
         public Task<SentryResponse> CaptureEventAsync(Func<Task<SentryEvent>> eventFactory) => DisabledResponseTask;
 
         public void Dispose() { }
