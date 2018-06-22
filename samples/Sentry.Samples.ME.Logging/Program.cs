57a026f8c42cda5f989950d9287b98991618abba
Bruno Garcia <bruno@brunogarcia.com>
2018-06-22 21:09:59 +0200

    Better comment of expected event/breadcrumb

--- samples/Sentry.Samples.ME.Logging/Program.cs
+++ samples/Sentry.Samples.ME.Logging/Program.cs
@@ -40,20 +40,20 @@ class Program
             {
                 logger.LogWarning("4 - Breadcrumb that only exists inside this scope");
 
-                logger.LogError("5 - An event that includes the scoped key-value (A, B) above and also the breadcrumbs: (2, 4)");
+                logger.LogError("5 - An event that includes the scoped key-value (A, B) above and also the breadcrumbs: (2, 4) and event (3)");
 
                 using (logger.BeginScope("C - Inner most scope, with single string state"))
                 {
                     logger.LogInformation("6 - Inner most breadcrumb");
 
-                    logger.LogError("7 - An event that includes the scope key-value (A, B, C) and also the breadcrumbs: (2, 4, 6)");
+                    logger.LogError("7 - An event that includes the scope key-value (A, B, C) and also the breadcrumbs: (2, 4, 6) and events (3, 5)");
 
                 } // Dispose scope C, drops state C and breadcrumb 6
 
                 // An exception that will go unhandled and crash the app:
                 // Even though it's not caught nor logged, this error is captured by Sentry!
                 // It will include all the scope data available up to this point
-                throw new Exception("8 - This unhandled exception is captured and includes Scope (A, B) and crumbs: (2, 4)");
+                throw new Exception("8 - This unhandled exception is captured and includes Scope (A, B) and crumbs: (2, 4, 5) and event (3) ");
             }
         }
         // Disposing the LoggerFactory will close the SDK since it was initialized through
