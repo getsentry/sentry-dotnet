6fcaf69715d1d9e65aa2cb23b153a22dc358bb52
Bruno Garcia <bruno@brunogarcia.com>
2018-06-24 10:40:57 +0200

    Synchronize environment variable dependant tests

--- test/Sentry.Tests/Helpers/EnvironmentVariableGuard.cs
+++ test/Sentry.Tests/Helpers/EnvironmentVariableGuard.cs
@@ -4,16 +4,22 @@ namespace Sentry.Tests.Helpers
 {
     internal static class EnvironmentVariableGuard
     {
+        // To allow different xunit colletions use of this
+        private static readonly object Lock = new object();
+
         public static void WithVariable(string key, string value, Action action)
         {
-            Environment.SetEnvironmentVariable(key, value, EnvironmentVariableTarget.Process);
-            try
+            lock (Lock)
             {
-                action();
-            }
-            finally
-            {
-                Environment.SetEnvironmentVariable(key, null, EnvironmentVariableTarget.Process);
+                Environment.SetEnvironmentVariable(key, value, EnvironmentVariableTarget.Process);
+                try
+                {
+                    action();
+                }
+                finally
+                {
+                    Environment.SetEnvironmentVariable(key, null, EnvironmentVariableTarget.Process);
+                }
             }
         }
     }
