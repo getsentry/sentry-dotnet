02097b8ef89c42b96ad7ae6c28f8936f5571e90f
Bruno Garcia <bruno@brunogarcia.com>
2018-06-27 22:18:31 +0200

    Optionally enable request rewind

--- src/Sentry.AspNetCore/DefaultRequestPayloadExtractor.cs
+++ src/Sentry.AspNetCore/DefaultRequestPayloadExtractor.cs
@@ -8,16 +8,26 @@ namespace Sentry.AspNetCore
     {
         public object ExtractPayload(HttpRequest request)
         {
-            // https://github.com/dotnet/corefx/blob/master/src/Common/src/CoreLib/System/IO/StreamReader.cs#L186
-            // Default parameters other than 'leaveOpen'
-            using (var reader = new StreamReader(request.Body, Encoding.UTF8, true, 1024,
-                // Make sure StreamReader does not close the stream
-                leaveOpen: true))
+            var originalPosition = request.Body.Position;
+            try
             {
-                var body = reader.ReadToEnd();
-                return body.Length == 0
-                    ? null
-                    : body;
+                request.Body.Position = 0;
+
+                // https://github.com/dotnet/corefx/blob/master/src/Common/src/CoreLib/System/IO/StreamReader.cs#L186
+                // Default parameters other than 'leaveOpen'
+                using (var reader = new StreamReader(request.Body, Encoding.UTF8, true, 1024,
+                    // Make sure StreamReader does not close the stream:
+                    leaveOpen: true))
+                {
+                    var body = reader.ReadToEnd();
+                    return body.Length == 0
+                        ? null
+                        : body;
+                }
+            }
+            finally
+            {
+                request.Body.Position = originalPosition;
             }
         }
     }
