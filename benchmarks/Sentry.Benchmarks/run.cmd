298077e4cfe02a83eee9ccf5a1bb288648e8278b
Bruno Garcia <bruno@brunogarcia.com>
2018-06-27 10:04:03 +0200

    Run scripts dont specify target

--- benchmarks/Sentry.Benchmarks/run.cmd
+++ benchmarks/Sentry.Benchmarks/run.cmd
@@ -1,6 +1,4 @@
-set framework=netcoreapp2.0
-
-dotnet build -c Release -f %framework%
+dotnet build -c Release -o bin/Release
 if not errorlevel 0 exit /b -1
 
-dotnet bin\Release\%framework%\Sentry.Benchmarks.dll
+dotnet bin/Release/Sentry.Benchmarks.dll
--- benchmarks/Sentry.Benchmarks/run.sh
+++ benchmarks/Sentry.Benchmarks/run.sh
@@ -1,6 +1,5 @@
 ﻿#!/bin/sh
 set -e errexit
 
-framework=netcoreapp2.0
-dotnet build -c Release -f $framework 
-dotnet bin/Release/$framework/Sentry.Benchmarks.dll
+dotnet build -c Release -o bin/Release
+dotnet bin/Release/Sentry.Benchmarks.dll
