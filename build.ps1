98a95e47305affccdba94372702633331f9ef8c5
Bruno Garcia <bruno@brunogarcia.com>
2018-06-27 11:07:16 +0200

    Code coverage reporting to codecov

--- build.ps1
+++ build.ps1
@@ -1,6 +1,6 @@
 $ErrorActionPreference = "Stop"
 
-dotnet test -c Release
+dotnet test -c Release /p:CollectCoverage=true /p:CoverletOutputFormat=opencover
  if ($LASTEXITCODE -ne 0) { exit 1 }
 
 dotnet pack -c Release --no-build --include-symbols
