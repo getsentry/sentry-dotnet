46396e32ad836541de7b214e61b82dbae3a3f3a4
Bruno Garcia <bruno@brunogarcia.com>
2018-06-27 11:53:56 +0200

    Don't include Sentry.Test* in coverage

--- build.ps1
+++ build.ps1
@@ -1,6 +1,6 @@
 $ErrorActionPreference = "Stop"
 
-dotnet test -c Release /p:CollectCoverage=true /p:CoverletOutputFormat=opencover
+dotnet test -c Release /p:CollectCoverage=true /p:CoverletOutputFormat=opencover /p:Exclude="[Sentry.Test*]*"
  if ($LASTEXITCODE -ne 0) { exit 1 }
 
 dotnet pack -c Release --no-build --include-symbols
