d425cf9d28d32c7b1e6cf67098c9de03491c7712
Bruno Garcia <bruno@brunogarcia.com>
2018-06-28 12:24:03 +0200

    No need for --include symbols Pdbs are already included in the main package

--- build.ps1
+++ build.ps1
@@ -3,5 +3,5 @@ $ErrorActionPreference = "Stop"
 dotnet test -c Release
  if ($LASTEXITCODE -ne 0) { exit 1 }
 
-dotnet pack -c Release --no-build --include-symbols
+dotnet pack -c Release --no-build
  if ($LASTEXITCODE -ne 0) { exit 1 }
