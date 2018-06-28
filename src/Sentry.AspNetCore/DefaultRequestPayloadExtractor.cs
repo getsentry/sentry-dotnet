7985929e4e21d7fd908c59e4a68ffa4ed9bc1aa9
Bruno Garcia <bruno@brunogarcia.com>
2018-06-28 11:20:31 +0200

    Test payload reading

--- src/Sentry.AspNetCore/DefaultRequestPayloadExtractor.cs
+++ src/Sentry.AspNetCore/DefaultRequestPayloadExtractor.cs
@@ -8,6 +8,11 @@ namespace Sentry.AspNetCore
     {
         public object ExtractPayload(HttpRequest request)
         {
+            if (!request.Body.CanSeek || !request.Body.CanRead)
+            {
+                return null;
+            }
+
             var originalPosition = request.Body.Position;
             try
             {
@@ -19,6 +24,9 @@ namespace Sentry.AspNetCore
                     // Make sure StreamReader does not close the stream:
                     leaveOpen: true))
                 {
+                    // This could be turned into async but at this point the stream should be buffered so no real value
+                    // A custom serializer that would take the stream and read from it into the output stream would add more value
+                    // as it would avoid the need to create the following (possibly huge) string
                     var body = reader.ReadToEnd();
                     return body.Length == 0
                         ? null
