36f276b19abbde3cf2e3d181802b06726929ea4d
Bruno Garcia <bruno@brunogarcia.com>
2018-07-02 10:42:41 +0200

    Test AppDomain adapter

--- src/Sentry/Integrations/AppDomainAdapter.cs
+++ src/Sentry/Integrations/AppDomainAdapter.cs
@@ -7,18 +7,15 @@ namespace Sentry.Integrations
         event UnhandledExceptionEventHandler UnhandledException;
     }
 
-    internal class AppDomainAdapter : IAppDomain
+    internal sealed class AppDomainAdapter : IAppDomain
     {
         public static AppDomainAdapter Instance { get; } = new AppDomainAdapter();
 
-        private AppDomainAdapter()
-        {
-            AppDomain.CurrentDomain.UnhandledException += OnUnhandledException;
-        }
+        private AppDomainAdapter() => AppDomain.CurrentDomain.UnhandledException += OnUnhandledException;
 
         public event UnhandledExceptionEventHandler UnhandledException;
 
-        protected virtual void OnUnhandledException(object sender, UnhandledExceptionEventArgs e)
+        private void OnUnhandledException(object sender, UnhandledExceptionEventArgs e)
             => UnhandledException?.Invoke(this, e);
     }
 }
