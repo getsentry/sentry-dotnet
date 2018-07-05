98ad4f095f3375b55b98a75ae706aa161bb7c20c
Bruno Garcia <bruno@brunogarcia.com>
2018-07-05 17:24:03 +0200

    MemoryQueueOptions under Extensibility

--- src/Sentry/SentryOptions.cs
+++ src/Sentry/SentryOptions.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
+using Sentry.Extensibility;
 using Sentry.Http;
 using Sentry.Integrations;
 using static Sentry.Internal.Constants;
