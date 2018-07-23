6ef9787ec2c83ec49a8defc03503cbba5c800728
Bruno Garcia <bruno@brunogarcia.com>
2018-07-23 20:42:53 +0200

    Copy Exception.Data to SentryEvent.Data

--- test/Sentry.Tests/Internals/MainExceptionProcessorTests.cs
+++ test/Sentry.Tests/Internals/MainExceptionProcessorTests.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using Sentry.Internal;
 using Xunit;
 
@@ -16,6 +17,24 @@ namespace Sentry.Tests.Internals
 
             Assert.Null(evt.Exception);
             Assert.Null(evt.SentryExceptionValues);
+        }
+
+        [Fact]
+        public void Process_ExceptionAndEventWithoutExtra_ExtraIsNull()
+        {
+            var evt = new SentryEvent();
+            Sut.Process(new Exception(), evt);
+
+            Assert.Null(evt.InternalExtra);
+        }
+
+        [Fact]
+        public void Process_ExceptionsWithoutData_ExtraIsNull()
+        {
+            var evt = new SentryEvent();
+            Sut.Process(new Exception("ex", new Exception()), evt);
+
+            Assert.Null(evt.InternalExtra);
         }
 
         [Fact]
@@ -51,7 +70,7 @@ namespace Sentry.Tests.Internals
 
             var actual = Assert.Single(evt.Extra);
 
-            Assert.Equal($"System.Exception.Data[{expectedKey}]", actual.Key);
+            Assert.Equal($"Exception[0][{expectedKey}]", actual.Key);
             Assert.Equal(expectedValue, (int)actual.Value);
         }
 
@@ -67,6 +86,25 @@ namespace Sentry.Tests.Internals
             Sut.Process(ex, evt);
 
             Assert.Same(expected, evt.Extra);
+        }
+
+        [Fact]
+        public void Process_TwoExceptionsWithData_DataOnEventExtra()
+        {
+            var evt = new SentryEvent();
+
+            var first = new Exception();
+            var firstValue = new object();
+            first.Data.Add("first", firstValue);
+            var second = new Exception("second", first);
+            var secondValue = new object();
+            second.Data.Add("second", secondValue);
+
+            Sut.Process(second, evt);
+
+            Assert.Equal(2, evt.Extra.Count);
+            Assert.Contains(evt.Extra, e => e.Key == "Exception[0][first]" && e.Value == firstValue);
+            Assert.Contains(evt.Extra, e => e.Key == "Exception[1][second]" && e.Value == secondValue);
         }
 
         // TODO: Test when the approach for parsing is finalized
