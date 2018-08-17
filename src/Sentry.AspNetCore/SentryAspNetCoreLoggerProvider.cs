05e9af53d2522cc6ecb83219e0fa54fa6de4ce7d
Bruno Garcia <bruno@brunogarcia.com>
2018-08-17 15:06:45 +0200

    fix: bind config on aspnet

--- src/Sentry.AspNetCore/SentryAspNetCoreLoggerProvider.cs
+++ src/Sentry.AspNetCore/SentryAspNetCoreLoggerProvider.cs
@@ -1,8 +1,10 @@
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Sentry.Extensions.Logging;
 
 namespace Sentry.AspNetCore
 {
+    [ProviderAlias("Sentry")]
     internal class SentryAspNetCoreLoggerProvider : SentryLoggerProvider
     {
         public SentryAspNetCoreLoggerProvider(IOptions<SentryAspNetCoreOptions> options)
