fc3324118b69ce778746b63a580ba9fdf3c9d0c5
Bruno Garcia <bruno@brunogarcia.com>
2018-09-04 15:31:53 +0200

    ref: centralize bootstrap in IApplicationBuilder

--- src/Sentry.AspNetCore/SentryStartupFilter.cs
+++ src/Sentry.AspNetCore/SentryStartupFilter.cs
@@ -1,11 +1,6 @@
 using System;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
-using Sentry.Extensions.Logging;
-using Sentry.Infrastructure;
 
 namespace Sentry.AspNetCore
 {
@@ -15,18 +10,6 @@ namespace Sentry.AspNetCore
         public Action<IApplicationBuilder> Configure(Action<IApplicationBuilder> next)
             => e =>
             {
-                // Container is built so resolve a logger and modify the SDK internal logger
-                var options = e.ApplicationServices.GetRequiredService<IOptions<SentryAspNetCoreOptions>>().Value;
-                if (options.Debug && (options.DiagnosticLogger == null || options.DiagnosticLogger.GetType() == typeof(ConsoleDiagnosticLogger)))
-                {
-                    var logger = e.ApplicationServices.GetRequiredService<ILogger<ISentryClient>>();
-                    options.DiagnosticLogger = new MelDiagnosticLogger(logger, options.DiagnosticsLevel);
-                }
-
-
-                var lifetime = e.ApplicationServices.GetService<IApplicationLifetime>();
-                lifetime?.ApplicationStopped.Register(SentrySdk.Close);
-
                 e.UseSentry();
 
                 next(e);
