6b7d741fc0c154ea89be9fdcdf8a7c67cfc6be72
Marcin Trocha <cinekmar21@gmail.com>
2018-11-17 18:53:44 +0100

    Added SentryId struct

---
+++ src/Sentry/Internal/SentryId.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sentry.Internal
+{
+   public struct SentryId
+    {
+       public Guid eventID;
+
+        public override string ToString()
+        {
+          return eventID.ToString("n");
+        }
+
+        public static implicit operator Guid(SentryId d)
+        {
+            return d.eventID;
+        }
+    }
+}
