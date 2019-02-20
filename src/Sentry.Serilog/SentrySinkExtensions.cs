eff16aef7662ab819e404eb8b0c00293dc0446c9
Sean O'Connor <potatman@gmail.com>
2019-02-19 19:51:07 -0800

    Enable configuration for Environment and Release when Sentry Sink is configured via appsettings, and include intelligent defaults.

--- src/Sentry.Serilog/SentrySinkExtensions.cs
+++ src/Sentry.Serilog/SentrySinkExtensions.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Reflection;
 using Sentry;
 using Sentry.Serilog;
 using Serilog.Configuration;
@@ -17,13 +18,17 @@ namespace Serilog
         /// <param name="minimumBreadcrumbLevel">Minimum log level to record a breadcrumb.</param>
         /// <param name="minimumEventLevel">Minimum log level to send an event.</param>
         /// <param name="formatProvider">The Serilog format provider.</param>
+        /// <param name="environment">The environment the event occured in.</param>
+        /// <param name="release">The release version the event occured in.</param>
         /// <returns></returns>
         public static LoggerConfiguration Sentry(
             this LoggerSinkConfiguration loggerConfiguration,
             string dsn = null,
             LogEventLevel minimumBreadcrumbLevel = LogEventLevel.Information,
             LogEventLevel minimumEventLevel = LogEventLevel.Error,
-            IFormatProvider formatProvider = null)
+            IFormatProvider formatProvider = null,
+            string environment = null,
+            string release = null)
             => loggerConfiguration.Sentry(o =>
                 {
                     if (dsn != null)
@@ -33,6 +38,27 @@ namespace Serilog
                     o.MinimumBreadcrumbLevel = minimumBreadcrumbLevel;
                     o.MinimumEventLevel = minimumEventLevel;
                     o.FormatProvider = formatProvider;
+
+                    var environmentToUse = string.IsNullOrWhiteSpace(release)
+                        ? Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT",
+                              EnvironmentVariableTarget.Machine) ??
+                          Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT",
+                              EnvironmentVariableTarget.Process)
+                        : environment;
+
+                    var releaseToUse = string.IsNullOrWhiteSpace(release)
+                        ? (Assembly.GetEntryAssembly() ?? Assembly.GetExecutingAssembly()).GetName().Version?.ToString()
+                        : release;
+
+                    if (environmentToUse != null)
+                    {
+                        o.Environment = environmentToUse;
+                    }
+
+                    if (releaseToUse != null)
+                    {
+                        o.Release = releaseToUse;
+                    }
                 });
 
         public static LoggerConfiguration Sentry(
