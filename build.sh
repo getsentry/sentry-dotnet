31144866ee5f6937cc99995629030e9a6a262513
Bruno Garcia <bruno@brunogarcia.com>
2019-05-30 13:04:20 +0200

    fix: Code coverage reports

--- build.sh
+++ build.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 set -e
 
-dotnet test -c Release /p:CollectCoverage=true /p:CoverletOutputFormat=opencover /p:Exclude="[Sentry.Test*]*"
+dotnet test -c Release \
+    /p:CollectCoverage=true \
+    /p:CoverletOutputFormat=opencover \
+    /p:Exclude=\"[Sentry.Test*]*,[xunit.*]*\" \
+    /p:UseSourceLink=true
 
 # Docs
 pushd docs
