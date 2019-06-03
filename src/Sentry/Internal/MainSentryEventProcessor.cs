153296fa4003e0a71fea7dc15ff2b9d5486b597c
Bruno Garcia <bruno@brunogarcia.com>
2019-06-03 22:10:56 +0200

    feat: Use stacktrace from calling thread (#222)

--- src/Sentry/Internal/MainSentryEventProcessor.cs
+++ src/Sentry/Internal/MainSentryEventProcessor.cs
@@ -1,6 +1,9 @@
 using System;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
 using System.Runtime.InteropServices;
+using System.Threading;
 using Sentry.Extensibility;
 using Sentry.PlatformAbstractions;
 using Sentry.Protocol;
@@ -108,7 +111,18 @@ namespace Sentry.Internal
                 var stackTrace = SentryStackTraceFactoryAccessor().Create(@event.Exception);
                 if (stackTrace != null)
                 {
-                    @event.Stacktrace = stackTrace;
+                    var thread = new SentryThread
+                    {
+                        Crashed = false,
+                        Current = true,
+                        Name = Thread.CurrentThread.Name,
+                        Id = Thread.CurrentThread.ManagedThreadId,
+                        Stacktrace = stackTrace
+                    };
+
+                    @event.SentryThreads = @event.SentryThreads.Any()
+                        ? new List<SentryThread>(@event.SentryThreads) { thread }
+                        : new[] { thread } as IEnumerable<SentryThread>;
                 }
             }
 
