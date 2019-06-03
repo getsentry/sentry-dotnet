8ac07577a5e1fcf87d16255e0f60afb10844438f
Bruno Garcia <bruno@brunogarcia.com>
2019-06-03 16:07:00 +0200

    fix: docfx

--- build.sh
+++ build.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 set -e
 
-dotnet test -c Release /p:CollectCoverage=true /p:CoverletOutputFormat=opencover /p:Exclude="[Sentry.Protocol.Test*]*"
+dotnet test -c Release \
+    /p:CollectCoverage=true \
+    /p:CoverletOutputFormat=opencover \
+    /p:Exclude=\"[Sentry.Protocol.Test*]*,[xunit.*]*\" \
+    /p:UseSourceLink=true
 
 # Docs
 pushd docs
