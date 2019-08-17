c654b4339044eae7d881b1abbb771ed27baa458e
Bruno Garcia <bruno@brunogarcia.com>
2019-08-18 00:26:58 +0200

    fix: Don't include Sentry.Testing in coverage report

--- build.ps1
+++ build.ps1
@@ -3,8 +3,7 @@ $ErrorActionPreference = "Stop"
 dotnet test -c Release `
     /p:CollectCoverage=true `
     /p:CoverletOutputFormat=opencover `
-    /p:Exclude="[Sentry.Test*]*" `
-    /p:Exclude="[xunit.*]*"
+    /p:Exclude='"""[Sentry.Test*]*,[xunit.*]*"""'
 if ($LASTEXITCODE -ne 0) { exit 1 }
 
 dotnet pack -c Release --no-build
