2541f2ea983b02bea75a2af70c33a2698fd0c95b
Anton Ovchinnikov <anton@tonyo.info>
2019-11-04 16:04:22 +0100

    feat: Move all Zeus operations to the script

--- scripts/upload-zeus-artifacts.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-# Redirect stderr to stdout to avoid weird Powershell errors
-exec 2>&1
-set -eux
-
-zeus upload -t "application/zip+nupkg" ./src/*/bin/Release/*.nupkg || [[ ! "$APPVEYOR_REPO_BRANCH" =~ ^release/ ]]
-
-zeus job update --status=passed || [[ ! "$APPVEYOR_REPO_BRANCH" =~ ^release/ ]]
