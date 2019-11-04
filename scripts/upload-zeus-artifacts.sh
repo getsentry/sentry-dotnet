55df806b78af70ed67accfb4dbd30db863c0061f
Anton Ovchinnikov <anton@tonyo.info>
2019-11-04 15:05:46 +0100

    fix: Redirect stderr to stdout

--- scripts/upload-zeus-artifacts.sh
+++ scripts/upload-zeus-artifacts.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 set -eux
 
+# Redirect stderr to stdout to avoid weird Powershell errors
+exec 2>&1
+
 zeus upload -t "application/zip+nupkg" ./src/*/bin/Release/*.nupkg || [[ ! "$APPVEYOR_REPO_BRANCH" =~ ^release/ ]]
 
 zeus job update --status=passed || [[ ! "$APPVEYOR_REPO_BRANCH" =~ ^release/ ]]
