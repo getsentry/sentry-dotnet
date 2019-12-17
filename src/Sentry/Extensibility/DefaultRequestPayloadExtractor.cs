845a559339f5aeb57d15953da39dcb667d423017
Bruno Garcia <bruno@brunogarcia.com>
2019-12-17 23:18:50 +0100

    fix: Reading request body throws on ASP.NET Core 3 (#324)

--- src/Sentry/Extensibility/DefaultRequestPayloadExtractor.cs
+++ src/Sentry/Extensibility/DefaultRequestPayloadExtractor.cs
@@ -4,7 +4,7 @@ using System.Text;
 namespace Sentry.Extensibility
 {
     /// <summary>
-    /// Default request payload extractor tat will read the body as a string.
+    /// Default request payload extractor that will read the body as a string.
     /// </summary>
     public class DefaultRequestPayloadExtractor : BaseRequestPayloadExtractor
     {
@@ -28,10 +28,13 @@ namespace Sentry.Extensibility
                 // Make sure StreamReader does not close the stream:
                 leaveOpen: true))
             {
-                // This could be turned into async but at this point the stream should be buffered so no real value
+                // This can't be turned into async because it's called by sync API, i.e: _logger.LogError()
+                // But at this point the stream should already be buffered: Model binding happened,
+                // request is buffered so data is still in memory, no blocking call is done below.
                 // A custom serializer that would take the stream and read from it into the output stream would add more value
                 // as it would avoid the need to create the following (possibly huge) string
-                var body = reader.ReadToEnd();
+                // Note: Using ReadToEndAsync instead of ReadToEnd because in ASP.NET Core 3 sync calls will throw.
+                var body = reader.ReadToEndAsync().GetAwaiter().GetResult();
                 return body.Length == 0
                     ? null
                     : body;
