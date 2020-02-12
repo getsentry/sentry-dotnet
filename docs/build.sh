16ded1b5d079891ff84a25dbe373f4a2f8789ba2
Bruno Garcia <bruno@brunogarcia.com>
2020-02-11 22:48:12 -0500

    fic docs

--- docs/build.sh
+++ docs/build.sh
@@ -3,7 +3,7 @@ set -e
 
 if [ ! -f ./docfx/docfx.exe ]; then
     echo "Installing docfx"
-    curl -sL https://github.com/dotnet/docfx/releases/download/v2.42.4/docfx.zip -o docfx.zip
+    curl -sL https://github.com/dotnet/docfx/releases/download/v2.48.1/docfx.zip -o docfx.zip
     unzip -oq docfx.zip -d docfx
 fi
 
