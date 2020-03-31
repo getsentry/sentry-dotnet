b43a7cd8bad195537427159699be612f56c6f941
Jim Hume <17392609+JimHume@users.noreply.github.com>
2020-03-30 19:40:48 -0700

    Serilog Sentry sink does not load all options from IConfiguration (#290) (#380)

--- src/Sentry.Serilog/SentrySinkExtensions.cs
+++ src/Sentry.Serilog/SentrySinkExtensions.cs
@@ -1,5 +1,8 @@
 using System;
+using System.IO.Compression;
+using System.Net;
 using Sentry;
+using Sentry.Protocol;
 using Sentry.Serilog;
 using Serilog.Configuration;
 using Serilog.Events;
@@ -28,15 +31,295 @@ namespace Serilog
             LogEventLevel minimumEventLevel = LogEventLevel.Error,
             IFormatProvider formatProvider = null)
             => loggerConfiguration.Sentry(o =>
+            {
+                if (dsn != null)
                 {
-                    if (dsn != null)
-                    {
-                        o.Dsn = new Dsn(dsn);
-                    }
-                    o.MinimumBreadcrumbLevel = minimumBreadcrumbLevel;
-                    o.MinimumEventLevel = minimumEventLevel;
-                    o.FormatProvider = formatProvider;
-                });
+                    o.Dsn = new Dsn(dsn);
+                }
+                o.MinimumBreadcrumbLevel = minimumBreadcrumbLevel;
+                o.MinimumEventLevel = minimumEventLevel;
+                o.FormatProvider = formatProvider;
+            });
+
+        /// <summary>
+        /// Add Sentry Serilog Sink.
+        /// </summary>
+        /// <param name="loggerConfiguration">The logger configuration .<seealso cref="LoggerSinkConfiguration"/></param>
+        /// <param name="sendDefaultPii">Whether to include default Personal Identifiable information. <seealso cref="SentryOptions.SendDefaultPii"/></param>
+        /// <param name="isEnvironmentUser">Whether to report the <see cref="System.Environment.UserName"/> as the User affected in the event. <seealso cref="SentryOptions.IsEnvironmentUser"/></param>
+        /// <param name="serverName">Gets or sets the name of the server running the application. <seealso cref="SentryOptions.ServerName"/></param>
+        /// <param name="attachStackTrace">Whether to send the stack trace of a event captured without an exception. <seealso cref="SentryOptions.AttachStacktrace"/></param>
+        /// <param name="maxBreadcrumbs">Gets or sets the maximum breadcrumbs. <seealso cref="SentryOptions.MaxBreadcrumbs"/></param>
+        /// <param name="sampleRate">The rate to sample events. <seealso cref="SentryOptions.SampleRate"/></param>
+        /// <param name="release">The release version of the application. <seealso cref="SentryOptions.Release"/></param>
+        /// <param name="environment">The environment the application is running. <seealso cref="SentryOptions.Environment"/></param>
+        /// <param name="dsn">The Sentry DSN. <seealso cref="SentryOptions.Dsn"/></param>
+        /// <param name="maxQueueItems">The maximum number of events to keep while the worker attempts to send them. <seealso cref="SentryOptions.MaxQueueItems"/></param>
+        /// <param name="shutdownTimeout">How long to wait for events to be sent before shutdown. <seealso cref="SentryOptions.ShutdownTimeout"/></param>
+        /// <param name="decompressionMethods">Decompression methods accepted. <seealso cref="SentryOptions.DecompressionMethods"/></param>
+        /// <param name="requestBodyCompressionLevel">The level of which to compress the <see cref="SentryEvent"/> before sending to Sentry. <seealso cref="SentryOptions.RequestBodyCompressionLevel"/></param>
+        /// <param name="requestBodyCompressionBuffered">Whether the body compression is buffered and the request 'Content-Length' known in advance. <seealso cref="SentryOptions.RequestBodyCompressionBuffered"/></param>
+        /// <param name="debug">Whether to log diagnostics messages. <seealso cref="SentryOptions.Debug"/></param>
+        /// <param name="diagnosticsLevel">The diagnostics level to be used. <seealso cref="SentryOptions.DiagnosticsLevel"/></param>
+        /// <param name="reportAssemblies">Whether or not to include referenced assemblies in each event sent to sentry. Defaults to <see langword="true"/>. <seealso cref="SentryOptions.ReportAssemblies"/></param>
+        /// <param name="deduplicateMode">What modes to use for event automatic de-duplication. <seealso cref="SentryOptions.DeduplicateMode"/></param>
+        /// <param name="initializeSdk">Whether to initialize this SDK through this integration. <seealso cref="SentrySerilogOptions.InitializeSdk"/></param>
+        /// <param name="minimumEventLevel">Minimum log level to send an event. <seealso cref="SentrySerilogOptions.MinimumEventLevel"/></param>
+        /// <param name="minimumBreadcrumbLevel">Minimum log level to record a breadcrumb. <seealso cref="SentrySerilogOptions.MinimumBreadcrumbLevel"/></param>
+        /// <param name="formatProvider">The Serilog format provider. <seealso cref="IFormatProvider"/></param>
+        /// <returns><see cref="LoggerConfiguration"/></returns>
+        /// <example>This sample shows how each item may be set from within a configuration file:
+        /// <code>
+        /// {
+        ///     "Serilog": {
+        ///         "Using": [
+        ///             "Serilog",
+        ///             "Sentry",
+        ///         ],
+        ///         "WriteTo": [{
+        ///                 "Name": "Sentry",
+        ///                 "Args": {
+        ///                     "sendDefaultPii": false,
+        ///                     "isEnvironmentUser": false,
+        ///                     "serverName": "MyServerName",
+        ///                     "attachStackTrace": false,
+        ///                     "maxBreadcrumbs": 20,
+        ///                     "sampleRate": 0.5,
+        ///                     "release": "0.0.1",
+        ///                     "environment": "staging",
+        ///                     "dsn": "https://MY-DSN@sentry.io",
+        ///                     "maxQueueItems": 100,
+        ///                     "shutdownTimeout": "00:00:05",
+        ///                     "decompressionMethods": "GZip",
+        ///                     "requestBodyCompressionLevel": "NoCompression",
+        ///                     "requestBodyCompressionBuffered": false,
+        ///                     "debug": false,
+        ///                     "diagnosticsLevel": "Debug",
+        ///                     "reportAssemblies": false,
+        ///                     "deduplicateMode": "All",
+        ///                     "initializeSdk": true,
+        ///                     "minimumBreadcrumbLevel": "Verbose",
+        ///                     "minimumEventLevel": "Error",
+        ///                     "outputTemplate": "{Timestamp:o} [{Level:u3}] ({Application}/{MachineName}/{ThreadId}) {Message}{NewLine}{Exception}"
+        ///                 }
+        ///             }
+        ///         ]
+        ///     }
+        /// }
+        /// </code>
+        /// </example>
+        public static LoggerConfiguration Sentry(
+            this LoggerSinkConfiguration loggerConfiguration,
+            bool? sendDefaultPii = null,
+            bool? isEnvironmentUser = null,
+            string serverName = null,
+            bool? attachStackTrace = null,
+            int? maxBreadcrumbs = null,
+            float? sampleRate = null,
+            string release = null,
+            string environment = null,
+            string dsn = null,
+            int? maxQueueItems = null,
+            TimeSpan? shutdownTimeout = null,
+            DecompressionMethods? decompressionMethods = null,
+            CompressionLevel? requestBodyCompressionLevel = null,
+            bool? requestBodyCompressionBuffered = null,
+            bool? debug = null,
+            SentryLevel? diagnosticsLevel = null,
+            bool? reportAssemblies = null,
+            DeduplicateMode? deduplicateMode = null,
+            bool? initializeSdk = null,
+            LogEventLevel? minimumEventLevel = null,
+            LogEventLevel? minimumBreadcrumbLevel = null,
+            IFormatProvider formatProvider = null)
+        {
+            return loggerConfiguration.Sentry(o => ConfigureSentrySerilogOptions(o,
+                sendDefaultPii,
+                isEnvironmentUser,
+                serverName,
+                attachStackTrace,
+                maxBreadcrumbs,
+                sampleRate,
+                release,
+                environment,
+                dsn,
+                maxQueueItems,
+                shutdownTimeout,
+                decompressionMethods,
+                requestBodyCompressionLevel,
+                requestBodyCompressionBuffered,
+                debug,
+                diagnosticsLevel,
+                reportAssemblies,
+                deduplicateMode,
+                initializeSdk,
+                minimumEventLevel,
+                minimumBreadcrumbLevel,
+                formatProvider));
+        }
+
+        /// <summary>
+        /// Configure the Sentry Serilog Sink.
+        /// </summary>
+        /// <param name="sentrySerilogOptions">The logger configuration to configure with the given parameters.</param>
+        /// <param name="sendDefaultPii">Whether to include default Personal Identifiable information. <seealso cref="SentryOptions.SendDefaultPii"/></param>
+        /// <param name="isEnvironmentUser">Whether to report the <see cref="System.Environment.UserName"/> as the User affected in the event. <seealso cref="SentryOptions.IsEnvironmentUser"/></param>
+        /// <param name="serverName">Gets or sets the name of the server running the application. <seealso cref="SentryOptions.ServerName"/></param>
+        /// <param name="attachStackTrace">Whether to send the stack trace of a event captured without an exception. <seealso cref="SentryOptions.AttachStacktrace"/></param>
+        /// <param name="maxBreadcrumbs">Gets or sets the maximum breadcrumbs. <seealso cref="SentryOptions.MaxBreadcrumbs"/></param>
+        /// <param name="sampleRate">The rate to sample events. <seealso cref="SentryOptions.SampleRate"/></param>
+        /// <param name="release">The release version of the application. <seealso cref="SentryOptions.Release"/></param>
+        /// <param name="environment">The environment the application is running. <seealso cref="SentryOptions.Environment"/></param>
+        /// <param name="dsn">The Sentry DSN. <seealso cref="SentryOptions.Dsn"/></param>
+        /// <param name="maxQueueItems">The maximum number of events to keep while the worker attempts to send them. <seealso cref="SentryOptions.MaxQueueItems"/></param>
+        /// <param name="shutdownTimeout">How long to wait for events to be sent before shutdown. <seealso cref="SentryOptions.ShutdownTimeout"/></param>
+        /// <param name="decompressionMethods">Decompression methods accepted. <seealso cref="SentryOptions.DecompressionMethods"/></param>
+        /// <param name="requestBodyCompressionLevel">The level of which to compress the <see cref="SentryEvent"/> before sending to Sentry. <seealso cref="SentryOptions.RequestBodyCompressionLevel"/></param>
+        /// <param name="requestBodyCompressionBuffered">Whether the body compression is buffered and the request 'Content-Length' known in advance. <seealso cref="SentryOptions.RequestBodyCompressionBuffered"/></param>
+        /// <param name="debug">Whether to log diagnostics messages. <seealso cref="SentryOptions.Debug"/></param>
+        /// <param name="diagnosticsLevel">The diagnostics level to be used. <seealso cref="SentryOptions.DiagnosticsLevel"/></param>
+        /// <param name="reportAssemblies">Whether or not to include referenced assemblies in each event sent to sentry. Defaults to <see langword="true"/>. <seealso cref="SentryOptions.ReportAssemblies"/></param>
+        /// <param name="deduplicateMode">What modes to use for event automatic de-duplication. <seealso cref="SentryOptions.DeduplicateMode"/></param>
+        /// <param name="initializeSdk">Whether to initialize this SDK through this integration. <seealso cref="SentrySerilogOptions.InitializeSdk"/></param>
+        /// <param name="minimumEventLevel">Minimum log level to send an event. <seealso cref="SentrySerilogOptions.MinimumEventLevel"/></param>
+        /// <param name="minimumBreadcrumbLevel">Minimum log level to record a breadcrumb. <seealso cref="SentrySerilogOptions.MinimumBreadcrumbLevel"/></param>
+        /// <param name="formatProvider">The Serilog format provider. <seealso cref="IFormatProvider"/></param>
+        public static void ConfigureSentrySerilogOptions(
+            SentrySerilogOptions sentrySerilogOptions,
+            bool? sendDefaultPii = null,
+            bool? isEnvironmentUser = null,
+            string serverName = null,
+            bool? attachStackTrace = null,
+            int? maxBreadcrumbs = null,
+            float? sampleRate = null,
+            string release = null,
+            string environment = null,
+            string dsn = null,
+            int? maxQueueItems = null,
+            TimeSpan? shutdownTimeout = null,
+            DecompressionMethods? decompressionMethods = null,
+            CompressionLevel? requestBodyCompressionLevel = null,
+            bool? requestBodyCompressionBuffered = null,
+            bool? debug = null,
+            SentryLevel? diagnosticsLevel = null,
+            bool? reportAssemblies = null,
+            DeduplicateMode? deduplicateMode = null,
+            bool? initializeSdk = null,
+            LogEventLevel? minimumEventLevel = null,
+            LogEventLevel? minimumBreadcrumbLevel = null,
+            IFormatProvider formatProvider = null)
+        {
+            if (sendDefaultPii.HasValue)
+            {
+                sentrySerilogOptions.SendDefaultPii = sendDefaultPii.Value;
+            }
+
+            if (isEnvironmentUser.HasValue)
+            {
+                sentrySerilogOptions.IsEnvironmentUser = isEnvironmentUser.Value;
+            }
+
+            if (!string.IsNullOrWhiteSpace(serverName))
+            {
+                sentrySerilogOptions.ServerName = serverName;
+            }
+
+            if (attachStackTrace.HasValue)
+            {
+                sentrySerilogOptions.AttachStacktrace = attachStackTrace.Value;
+            }
+
+            if (maxBreadcrumbs.HasValue)
+            {
+                sentrySerilogOptions.MaxBreadcrumbs = maxBreadcrumbs.Value;
+            }
+
+            if (sampleRate.HasValue)
+            {
+                sentrySerilogOptions.SampleRate = sampleRate;
+            }
+
+            if (!string.IsNullOrWhiteSpace(release))
+            {
+                sentrySerilogOptions.Release = release;
+            }
+
+            if (!string.IsNullOrWhiteSpace(environment))
+            {
+                sentrySerilogOptions.Environment = environment;
+            }
+
+            if (!string.IsNullOrWhiteSpace(dsn))
+            {
+                sentrySerilogOptions.Dsn = new Dsn(dsn);
+            }
+
+            if (maxQueueItems.HasValue)
+            {
+                sentrySerilogOptions.MaxQueueItems = maxQueueItems.Value;
+            }
+
+            if (shutdownTimeout.HasValue)
+            {
+                sentrySerilogOptions.ShutdownTimeout = shutdownTimeout.Value;
+            }
+
+            if (decompressionMethods.HasValue)
+            {
+                sentrySerilogOptions.DecompressionMethods = decompressionMethods.Value;
+            }
+
+            if (requestBodyCompressionLevel.HasValue)
+            {
+                sentrySerilogOptions.RequestBodyCompressionLevel = requestBodyCompressionLevel.Value;
+            }
+
+            if (requestBodyCompressionBuffered.HasValue)
+            {
+                sentrySerilogOptions.RequestBodyCompressionBuffered = requestBodyCompressionBuffered.Value;
+            }
+
+            if (debug.HasValue)
+            {
+                sentrySerilogOptions.Debug = debug.Value;
+            }
+
+            if (diagnosticsLevel.HasValue)
+            {
+                sentrySerilogOptions.DiagnosticsLevel = diagnosticsLevel.Value;
+            }
+
+            if (reportAssemblies.HasValue)
+            {
+                sentrySerilogOptions.ReportAssemblies = reportAssemblies.Value;
+            }
+
+            if (deduplicateMode.HasValue)
+            {
+                sentrySerilogOptions.DeduplicateMode = deduplicateMode.Value;
+            }
+
+            // Serilog-specific items
+            if (initializeSdk.HasValue)
+            {
+                sentrySerilogOptions.InitializeSdk = initializeSdk.Value;
+            }
+
+            if (minimumEventLevel.HasValue)
+            {
+                sentrySerilogOptions.MinimumEventLevel = minimumEventLevel.Value;
+            }
+
+            if (minimumBreadcrumbLevel.HasValue)
+            {
+                sentrySerilogOptions.MinimumBreadcrumbLevel = minimumBreadcrumbLevel.Value;
+            }
+
+            if (formatProvider != null)
+            {
+                sentrySerilogOptions.FormatProvider = formatProvider;
+            }
+        }
 
         /// <summary>
         /// Add Sentry sink to Serilog.
---
+++ test/Sentry.Serilog.Tests/SentrySerilogSinkExtensionsTests.cs
@@ -0,0 +1,161 @@
+using System;
+using System.IO.Compression;
+using System.Net;
+using Newtonsoft.Json;
+using Sentry.Protocol;
+using Serilog;
+using Serilog.Events;
+using Xunit;
+
+namespace Sentry.Serilog.Tests
+{
+
+    public class SentrySerilogSinkExtensionsTests
+    {
+        private class Fixture
+        {
+            public SentrySerilogOptions Options { get; } = new SentrySerilogOptions();
+
+            // Parameter values that are NOT set to the default values in SentryOptions or SentrySerilogOptions
+            public bool SendDefaultPii { get; } = true;
+            public bool IsEnvironmentUser { get; } = false;
+            public string ServerName { get; } = nameof(ConfigureSentrySerilogOptions_WithAllParameters_MakesAppropriateChangesToObject);
+            public bool AttachStackTrace { get; } = true;
+            public int MaxBreadcrumbs { get; } = 9;
+            public float SampleRate { get; } = 0.4f;
+            public string Release { get; } = nameof(ConfigureSentrySerilogOptions_WithAllParameters_MakesAppropriateChangesToObject);
+            public string Environment { get; } = nameof(ConfigureSentrySerilogOptions_WithAllParameters_MakesAppropriateChangesToObject);
+            public string Dsn { get; } = DsnSamples.ValidDsnWithSecret;
+            public int MaxQueueItems { get; } = 17;
+            public TimeSpan ShutdownTimeout { get; } = TimeSpan.FromDays(1.3);
+            public DecompressionMethods DecompressionMethods { get; } = DecompressionMethods.Deflate & DecompressionMethods.GZip;
+            public CompressionLevel RequestBodyCompressionLevel { get; } = CompressionLevel.NoCompression;
+            public bool RequestBodyCompressionBuffered { get; } = false;
+            public bool Debug { get; } = true;
+            public SentryLevel DiagnosticsLevel { get; } = SentryLevel.Warning;
+            public bool ReportAssemblies { get; } = false;
+            public DeduplicateMode DeduplicateMode { get; } = DeduplicateMode.SameExceptionInstance;
+            public bool InitializeSdk { get; } = false;
+            public LogEventLevel MinimumEventLevel { get; } = LogEventLevel.Verbose;
+            public LogEventLevel MinimumBreadcrumbLevel { get; } = LogEventLevel.Fatal;
+
+            public SentrySerilogOptions GetSut() => new SentrySerilogOptions();
+        }
+
+        private readonly SentrySerilogSinkExtensionsTests.Fixture _fixture = new SentrySerilogSinkExtensionsTests.Fixture();
+
+        [Fact]
+        public void ConfigureSentrySerilogOptions_WithNoParameters_MakesNoChangesToObject()
+        {
+            var sut = _fixture.GetSut();
+
+            // Make the call with only the required parameter
+            SentrySinkExtensions.ConfigureSentrySerilogOptions(sut);
+
+            // Compare. I'm not sure how to deep compare--I don't see a nuget ref to that type
+            // of functionality and I'm hesitant to introduce new technologies with such a
+            // small commit.
+            AssertEqualDeep(_fixture.Options, sut);
+        }
+
+        [Fact]
+        public void ConfigureSentrySerilogOptions_WithOneParameter_MakesAppropriateChangeToObject()
+        {
+            var sut = _fixture.GetSut();
+
+            // Make the call with only the required parameter
+            const bool sendDefaultPii = true;
+            SentrySinkExtensions.ConfigureSentrySerilogOptions(sut, sendDefaultPii);
+
+            // Compare. I'm not sure how to deep compare--I don't see a nuget ref to that type
+            // of functionality and I'm hesitant to introduce new technologies with such a
+            // small commit.
+            AssertNotEqualDeep(_fixture.Options, sut);
+
+            Assert.Equal(sendDefaultPii, sut.SendDefaultPii);
+        }
+
+        [Fact]
+        public void ConfigureSentrySerilogOptions_WithMultipleParameters_MakesAppropriateChangesToObject()
+        {
+            var sut = _fixture.GetSut();
+
+            SentrySinkExtensions.ConfigureSentrySerilogOptions(sut, _fixture.SendDefaultPii,
+                decompressionMethods: _fixture.DecompressionMethods, reportAssemblies: _fixture.ReportAssemblies, sampleRate: _fixture.SampleRate);
+
+            // Fail early
+            AssertNotEqualDeep(_fixture.Options, sut);
+
+            // Compare individual properties
+            Assert.Equal(_fixture.SendDefaultPii, sut.SendDefaultPii);
+            Assert.Equal(_fixture.DecompressionMethods, sut.DecompressionMethods);
+            Assert.Equal(_fixture.ReportAssemblies, sut.ReportAssemblies);
+            Assert.Equal(_fixture.SampleRate, sut.SampleRate);
+        }
+
+        [Fact]
+        public void ConfigureSentrySerilogOptions_WithAllParameters_MakesAppropriateChangesToObject()
+        {
+            var sut = _fixture.GetSut();
+
+            SentrySinkExtensions.ConfigureSentrySerilogOptions(sut, _fixture.SendDefaultPii,
+                _fixture.IsEnvironmentUser, _fixture.ServerName, _fixture.AttachStackTrace, _fixture.MaxBreadcrumbs,
+                _fixture.SampleRate, _fixture.Release, _fixture.Environment, _fixture.Dsn, _fixture.MaxQueueItems,
+                _fixture.ShutdownTimeout, _fixture.DecompressionMethods, _fixture.RequestBodyCompressionLevel,
+                _fixture.RequestBodyCompressionBuffered, _fixture.Debug, _fixture.DiagnosticsLevel,
+                _fixture.ReportAssemblies, _fixture.DeduplicateMode, _fixture.InitializeSdk,
+                _fixture.MinimumEventLevel, _fixture.MinimumBreadcrumbLevel);
+
+            // Fail early
+            AssertNotEqualDeep(_fixture.Options, sut);
+
+            // Compare individual properties
+            Assert.Equal(_fixture.SendDefaultPii, sut.SendDefaultPii);
+            Assert.Equal(_fixture.IsEnvironmentUser, sut.IsEnvironmentUser);
+            Assert.Equal(_fixture.ServerName, sut.ServerName);
+            Assert.Equal(_fixture.AttachStackTrace, sut.AttachStacktrace);
+            Assert.Equal(_fixture.MaxBreadcrumbs, sut.MaxBreadcrumbs);
+            Assert.Equal(_fixture.SampleRate, sut.SampleRate);
+            Assert.Equal(_fixture.Release, sut.Release);
+            Assert.Equal(_fixture.Environment, sut.Environment);
+            Assert.Equal(_fixture.Dsn, sut.Dsn.ToString());
+            Assert.Equal(_fixture.MaxQueueItems, sut.MaxQueueItems);
+            Assert.Equal(_fixture.ShutdownTimeout, sut.ShutdownTimeout);
+            Assert.Equal(_fixture.DecompressionMethods, sut.DecompressionMethods);
+            Assert.Equal(_fixture.RequestBodyCompressionLevel, sut.RequestBodyCompressionLevel);
+            Assert.Equal(_fixture.RequestBodyCompressionBuffered, sut.RequestBodyCompressionBuffered);
+            Assert.Equal(_fixture.Debug, sut.Debug);
+            Assert.Equal(_fixture.DiagnosticsLevel, sut.DiagnosticsLevel);
+            Assert.Equal(_fixture.ReportAssemblies, sut.ReportAssemblies);
+            Assert.Equal(_fixture.DeduplicateMode, sut.DeduplicateMode);
+            Assert.Equal(_fixture.InitializeSdk, sut.InitializeSdk);
+            Assert.Equal(_fixture.MinimumEventLevel, sut.MinimumEventLevel);
+            Assert.Equal(_fixture.MinimumBreadcrumbLevel, sut.MinimumBreadcrumbLevel);
+        }
+
+        private static void AssertEqualDeep(object expected, object actual)
+        {
+            AssertDeep(expected, actual, true);
+        }
+
+        private static void AssertNotEqualDeep(object left, object right)
+        {
+            AssertDeep(left, right, false);
+        }
+
+        private static void AssertDeep(object left, object right, bool shouldCheckEqual)
+        {
+            var serializedLeftObject = JsonConvert.SerializeObject(left);
+            var serializedRightObject = JsonConvert.SerializeObject(right);
+
+            if (shouldCheckEqual)
+            {
+                Assert.Equal(serializedLeftObject, serializedRightObject);
+            }
+            else
+            {
+                Assert.NotEqual(serializedLeftObject, serializedRightObject);
+            }
+        }
+    }
+}
