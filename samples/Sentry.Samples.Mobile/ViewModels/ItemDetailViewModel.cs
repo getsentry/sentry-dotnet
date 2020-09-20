bf4a0ef4ea9037f86aa8fc533e53c0b8b4e88fd6
Bruno Garcia <bruno@brunogarcia.com>
2020-09-20 15:22:31 -0400

    new XF sample, removed strong naming

---
+++ samples/Sentry.Samples.Mobile/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Sentry.Samples.Mobile.Models;
+
+namespace Sentry.Samples.Mobile.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
