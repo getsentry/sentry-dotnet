c080779575e80c8e7c1582a1cd5fc1d74bac61ee
Bruno Garcia <bruno@brunogarcia.com>
2020-09-20 15:33:34 -0400

    clean

--- samples/Sentry.Samples.Mobile.iOS/Main.cs
+++ samples/Sentry.Samples.Mobile.iOS/Main.cs
@@ -9,11 +9,8 @@ namespace Sentry.Samples.Mobile.iOS
 {
     public static class Application
     {
-        // This is the main entry point of the application.
         static void Main(string[] args)
         {
-            // if you want to use a different Application Delegate class from "AppDelegate"
-            // you can specify it here.
             UIApplication.Main(args, null, "AppDelegate");
         }
     }
