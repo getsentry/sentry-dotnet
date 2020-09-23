d7125afde3dbf6d696f116a300b410fe5b60f4ba
Lucas Zimerman Fraulob <lucas-zimerman1@hotmail.com>
2020-09-22 23:11:54 -0300

    Project Cleanup

--- samples/Sentry.Samples.Mobile/App.xaml.cs
+++ samples/Sentry.Samples.Mobile/App.xaml.cs
@@ -1,8 +1,4 @@
-﻿using System;
 using Xamarin.Forms;
-using Xamarin.Forms.Xaml;
-using Sentry.Samples.Mobile.Services;
-using Sentry.Samples.Mobile.Views;
 
 namespace Sentry.Samples.Mobile
 {
@@ -11,16 +7,14 @@ namespace Sentry.Samples.Mobile
 
         public App()
         {
-            SentrySdk.Init(o =>
+            _ = SentrySdk.Init(o =>
             {
-                o.Debug = false;
-                o.Dsn = new Dsn("https://80aed643f81249d4bed3e30687b310ab@o447951.ingest.sentry.io/5428537");
+                  o.Debug = false;
+                  o.Dsn = new Dsn("https://80aed643f81249d4bed3e30687b310ab@o447951.ingest.sentry.io/5428537");
             });
 
             InitializeComponent();
-
-            DependencyService.Register<MockDataStore>();
-            MainPage = new MainPage();
+            MainPage = new NavigationPage(new MainPage());
         }
 
         protected override void OnStart() => SentrySdk.AddBreadcrumb("OnStart",  "app.lifecycle", "event");
