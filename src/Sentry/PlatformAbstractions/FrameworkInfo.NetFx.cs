c8e014ee9ce7a49420881f513d45b48c69de4de3
Bruno Garcia <bruno@brunogarcia.com>
2020-09-24 14:16:40 -0400

    Framework and runtime info (#526)
    Co-authored-by: Joao Grassi <joaop.grassi@gmail.com>
    Co-authored-by: Anton Ovchinnikov <anton@tonyo.info>

---
+++ src/Sentry/PlatformAbstractions/FrameworkInfo.NetFx.cs
@@ -0,0 +1,184 @@
+#if NETFX
+using System;
+using System.Collections.Generic;
+using Microsoft.Win32;
+
+namespace Sentry.PlatformAbstractions
+{
+    /// <summary>
+    /// Information about .NET Framework in the running machine
+    /// </summary>
+    public static partial class FrameworkInfo
+    {
+        /// <summary>
+        /// Get the latest Framework installation for the specified CLR
+        /// </summary>
+        /// <remarks>
+        /// Supports the current 3 CLR versions:
+        /// CLR 1 => .NET 1.0, 1.1
+        /// CLR 2 => .NET 2.0, 3.0, 3.5
+        /// CLR 4 => .NET 4.0, 4.5.x, 4.6.x, 4.7.x
+        /// </remarks>
+        /// <param name="clrVersion">The CLR version: 1, 2 or 4</param>
+        /// <returns>The framework installation or null if none is found.</returns>
+        public static FrameworkInstallation? GetLatest(int clrVersion)
+        {
+            // CLR versions
+            // https://docs.microsoft.com/en-us/dotnet/standard/clr
+            if (clrVersion != 1 && clrVersion != 2 && clrVersion != 4)
+            {
+                return null;
+            }
+
+            if (clrVersion == 4)
+            {
+                var release = Get45PlusLatestInstallationFromRegistry();
+                if (release != null)
+                {
+                    return new FrameworkInstallation
+                    {
+                        Version = GetNetFxVersionFromRelease(release.Value),
+                        Release = release
+                    };
+                }
+            }
+
+            FrameworkInstallation latest = null;
+            foreach (var installation in GetInstallations())
+            {
+                latest ??= installation;
+
+                if (clrVersion == 2)
+                {
+                    // CLR 2 runs .NET 2 to 3.5
+                    if ((installation.Version.Major == 2 || installation.Version.Major == 3)
+                        && installation.Version >= latest.Version)
+                    {
+                        latest = installation;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                else if (clrVersion == 4)
+                {
+                    if (installation.Version.Major == 4
+                        && installation.Version >= latest.Version)
+                    {
+                        latest = installation;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+
+            return latest;
+        }
+
+        /// <summary>
+        /// Get all .NET Framework installations in this machine
+        /// </summary>
+        /// <seealso href="https://docs.microsoft.com/en-us/dotnet/framework/migration-guide/how-to-determine-which-versions-are-installed#to-find-net-framework-versions-by-querying-the-registry-in-code-net-framework-1-4"/>
+        /// <returns>Enumeration of installations</returns>
+        public static IEnumerable<FrameworkInstallation> GetInstallations()
+        {
+            using var ndpKey = RegistryKey.OpenRemoteBaseKey(RegistryHive.LocalMachine, string.Empty)
+                .OpenSubKey(@"SOFTWARE\Microsoft\NET Framework Setup\NDP\");
+            if (ndpKey == null)
+            {
+                yield break;
+            }
+
+            foreach (var versionKeyName in ndpKey.GetSubKeyNames())
+            {
+                if (!versionKeyName.StartsWith("v") || !(ndpKey.OpenSubKey(versionKeyName) is RegistryKey versionKey))
+                {
+                    continue;
+                }
+
+                var version = versionKey.GetString("Version");
+                if (version != null && versionKey.GetInt("Install") == 1)
+                {
+                    // 1.0 to 3.5
+                    Version.TryParse(version, out var parsed);
+                    yield return new FrameworkInstallation
+                    {
+                        ShortName = versionKeyName,
+                        Version = parsed,
+                        ServicePack = versionKey.GetInt("SP")
+                    };
+
+                    continue;
+                }
+
+                // 4.0+
+                foreach (var subKeyName in versionKey.GetSubKeyNames())
+                {
+                    var subKey = versionKey.OpenSubKey(subKeyName);
+                    if (subKey?.GetInt("Install") != 1)
+                    {
+                        continue;
+                    }
+
+                    yield return GetFromV4(subKey, subKeyName);
+                }
+            }
+        }
+
+        private static FrameworkInstallation GetFromV4(RegistryKey subKey, string subKeyName)
+        {
+            var hasRelease = int.TryParse(
+                subKey.GetValue("Release", null)?.ToString(), out var release);
+
+            Version version = null;
+            if (hasRelease)
+            {
+                // 4.5+
+                var displayableVersion = GetNetFxVersionFromRelease(release);
+                if (displayableVersion != null)
+                {
+                    version = displayableVersion;
+                }
+            }
+
+            if (version == null)
+            {
+                Version.TryParse(subKey.GetString("Version"), out var parsed);
+                version = parsed;
+            }
+
+            return new FrameworkInstallation
+            {
+                Profile = subKeyName switch
+                {
+                    "Full" => FrameworkProfile.Full,
+                    "Client" => FrameworkProfile.Client,
+                    _ => null
+                },
+                Version = version,
+                ServicePack = subKey.GetInt("SP"),
+                Release = hasRelease ? release : null as int?
+            };
+        }
+
+        // https://docs.microsoft.com/en-us/dotnet/framework/migration-guide/how-to-determine-which-versions-are-installed#to-find-net-framework-versions-by-querying-the-registry-in-code-net-framework-45-and-later
+        internal static int? Get45PlusLatestInstallationFromRegistry()
+        {
+            using var ndpKey = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry32)
+                .OpenSubKey(@"SOFTWARE\Microsoft\NET Framework Setup\NDP\v4\Full\");
+            return ndpKey?.GetInt("Release");
+        }
+
+        internal static Version GetNetFxVersionFromRelease(int release)
+        {
+            NetFxReleaseVersionMap.TryGetValue(release, out var version);
+            Version.TryParse(version, out var parsed);
+            return parsed;
+        }
+    }
+}
+
+#endif
---
+++ src/Sentry/PlatformAbstractions/RuntimeInfo.cs
@@ -0,0 +1,137 @@
+using System;
+using System.Reflection;
+using System.Text.RegularExpressions;
+using System.Runtime.InteropServices;
+
+namespace Sentry.PlatformAbstractions
+{
+    // https://github.com/dotnet/corefx/issues/17452
+    internal static class RuntimeInfo
+    {
+        private static readonly Regex RuntimeParseRegex = new Regex("^(?<name>[^\\d]*)(?<version>(\\d+\\.)+[^\\s]+)",
+            RegexOptions.Compiled | RegexOptions.CultureInvariant);
+
+        /// <summary>
+        /// Gets the current runtime.
+        /// </summary>
+        /// <returns>A new instance for the current runtime</returns>
+        internal static Runtime GetRuntime()
+        {
+            var runtime = GetFromRuntimeInformation();
+            runtime ??= GetFromMonoRuntime();
+
+            runtime ??= GetFromEnvironmentVariable();
+
+#if NETFX
+            SetNetFxReleaseAndVersion(runtime);
+#elif NETSTANDARD || NETCOREAPP // Possibly .NET Core
+            SetNetCoreVersion(runtime);
+#endif
+            return runtime;
+        }
+
+        internal static Runtime? Parse(string rawRuntimeDescription, string? name = null)
+        {
+            if (rawRuntimeDescription == null)
+            {
+                return name == null
+                    ? null
+                    : new Runtime(name);
+            }
+
+            var match = RuntimeParseRegex.Match(rawRuntimeDescription);
+            if (match.Success)
+            {
+                return new Runtime(
+                    name ?? (match.Groups["name"].Value == string.Empty ? null : match.Groups["name"].Value.Trim()),
+                    match.Groups["version"].Value == string.Empty ? null : match.Groups["version"].Value.Trim(),
+                    raw: rawRuntimeDescription
+                );
+            }
+
+            return new Runtime(name, raw: rawRuntimeDescription);
+        }
+
+#if NETFX
+        internal static void SetNetFxReleaseAndVersion(Runtime runtime)
+        {
+            if (runtime?.IsNetFx() == true)
+            {
+                var latest = FrameworkInfo.GetLatest(Environment.Version.Major);
+
+                runtime.FrameworkInstallation = latest;
+                if (latest.Version?.Major < 4)
+                {
+                    // prior to 4, user-friendly versions are always 2 digit: 1.0, 1.1, 2.0, 3.0, 3.5
+                    runtime.Version = latest.ServicePack == null
+                        ? $"{latest.Version.Major}.{latest.Version.Minor}"
+                        : $"{latest.Version.Major}.{latest.Version.Minor} SP {latest.ServicePack}";
+                }
+                else
+                {
+                    runtime.Version = latest.Version?.ToString();
+                }
+            }
+        }
+#endif
+
+#if NETSTANDARD || NETCOREAPP // Possibly .NET Core
+        // Known issue on Docker: https://github.com/dotnet/BenchmarkDotNet/issues/448#issuecomment-361027977
+        internal static void SetNetCoreVersion(Runtime runtime)
+        {
+            if (runtime?.IsNetCore() == true)
+            {
+                // https://github.com/dotnet/BenchmarkDotNet/issues/448#issuecomment-308424100
+                var assembly = typeof(System.Runtime.GCSettings).GetTypeInfo().Assembly;
+                var assemblyPath = assembly.CodeBase.Split(new[] { '/', '\\' }, StringSplitOptions.RemoveEmptyEntries);
+                var netCoreAppIndex = Array.IndexOf(assemblyPath, "Microsoft.NETCore.App");
+                if (netCoreAppIndex > 0
+                    && netCoreAppIndex < assemblyPath.Length - 2)
+                {
+                    runtime.Version = assemblyPath[netCoreAppIndex + 1];
+                }
+            }
+        }
+#endif
+
+        internal static Runtime? GetFromRuntimeInformation()
+        {
+            // Prefered API: netstandard2.0
+            // https://github.com/dotnet/corefx/blob/master/src/System.Runtime.InteropServices.RuntimeInformation/src/System/Runtime/InteropServices/RuntimeInformation/RuntimeInformation.cs
+            // https://github.com/mono/mono/blob/90b49aa3aebb594e0409341f9dca63b74f9df52e/mcs/class/corlib/System.Runtime.InteropServices.RuntimeInformation/RuntimeInformation.cs
+            // e.g: .NET Framework 4.7.2633.0, .NET Native, WebAssembly
+            var frameworkDescription = RuntimeInformation.FrameworkDescription;
+
+            return Parse(frameworkDescription);
+        }
+
+        internal static Runtime? GetFromMonoRuntime()
+            => Type.GetType("Mono.Runtime", false)
+                ?.GetMethod("GetDisplayName", BindingFlags.NonPublic | BindingFlags.Static)
+                ?.Invoke(null, null) is string monoVersion
+                // The implementation of Mono to RuntimeInformation:
+                // https://github.com/mono/mono/blob/90b49aa3aebb594e0409341f9dca63b74f9df52e/mcs/class/corlib/System.Runtime.InteropServices.RuntimeInformation/RuntimeInformation.cs#L40
+                // Examples:
+                // Mono 5.10.1.47 (2017-12/8eb8f7d5e74 Fri Apr 13 20:18:12 EDT 2018)
+                // Mono 5.10.1.47 (tarball Tue Apr 17 09:23:16 UTC 2018)
+                // Mono 5.10.0 (Visual Studio built mono)
+                ? Parse(monoVersion, "Mono")
+                : null;
+
+        // This should really only be used on .NET 1.0, 1.1, 2.0, 3.0, 3.5 and 4.0
+        internal static Runtime GetFromEnvironmentVariable()
+        {
+            // Environment.Version: NET Framework 4, 4.5, 4.5.1, 4.5.2 = 4.0.30319.xxxxx
+            // .NET Framework 4.6, 4.6.1, 4.6.2, 4.7, 4.7.1 =  4.0.30319.42000
+            // Not recommended on NET45+ (which has RuntimeInformation)
+            var version = Environment.Version;
+
+            var friendlyVersion = version.Major switch
+            {
+                1 => "",
+                _ => version.ToString()
+            };
+            return new Runtime(".NET Framework", friendlyVersion, raw: "Environment.Version=" + version);
+        }
+    }
+}
