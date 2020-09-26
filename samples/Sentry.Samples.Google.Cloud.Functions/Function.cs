6c50139a7625f985d1384dbb3b00e4d5b61afbc1
Bruno Garcia <bruno@brunogarcia.com>
2020-09-26 14:15:03 -0400

    GCP functions

--- samples/Sentry.Samples.Google.Cloud.Functions/Function.cs
+++ samples/Sentry.Samples.Google.Cloud.Functions/Function.cs
@@ -1,19 +1,26 @@
 using Google.Cloud.Functions.Framework;
 using Microsoft.AspNetCore.Http;
 using System.Threading.Tasks;
+using Google.Cloud.Functions.Hosting;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
-namespace Sentry.Samples.Google.Cloud.Functions
+[assembly: FunctionsStartup(typeof(SentryCloudStartup))]
+
+public class Function : IHttpFunction
 {
-    public class Function : IHttpFunction
+    /// <summary>
+    /// Logic for your function goes here.
+    /// </summary>
+    /// <param name="context">The HTTP context, containing the request and the response.</param>
+    /// <returns>A task representing the asynchronous operation.</returns>
+    public async Task HandleAsync(HttpContext context)
     {
-        /// <summary>
-        /// Logic for your function goes here.
-        /// </summary>
-        /// <param name="context">The HTTP context, containing the request and the response.</param>
-        /// <returns>A task representing the asynchronous operation.</returns>
-        public async Task HandleAsync(HttpContext context)
-        {
-            await context.Response.WriteAsync("Hello, Functions Framework.");
-        }
+        await context.Response.WriteAsync("Hello, Functions Framework.");
     }
 }
+
+
---
+++ src/Sentry.Google.Cloud.Functions/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Google.Cloud.Functions.Hosting;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace Sentry.Google.Cloud.Functions
+{
+    public class Startup : FunctionsStartup
+    {
+
+        // Hook as we do in:
+        // SentryWebHostBuilderExtensions
+
+        public override void ConfigureLogging(WebHostBuilderContext context, ILoggingBuilder logging)
+        {
+            base.ConfigureLogging(context, logging);
+        }
+
+
+        public override void ConfigureServices(WebHostBuilderContext context, IServiceCollection services)
+        {
+            base.ConfigureServices(context, services);
+        }
+    }
+}
