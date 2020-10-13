fd2aacd07b953430f2c0bda3fe03d2da80095984
Alexey Golub <tyrrrz@gmail.com>
2020-10-13 17:30:38 +0300

    wip

--- src/Sentry/Protocol/ISerializable.cs
+++ src/Sentry/Protocol/ISerializable.cs
@@ -1,5 +1,4 @@
 using System.IO;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -14,17 +13,5 @@ namespace Sentry.Protocol
         /// Serializes the object to a stream.
         /// </summary>
         Task SerializeAsync(Stream stream, CancellationToken cancellationToken = default);
-    }
-
-    public static class SerializableExtensions
-    {
-        public static async Task<string> SerializeToStringAsync(
-            this ISerializable serializable,
-            CancellationToken cancellationToken = default)
-        {
-            using var stream = new MemoryStream();
-            await serializable.SerializeAsync(stream, cancellationToken).ConfigureAwait(false);
-            return Encoding.UTF8.GetString(stream.ToArray());
-        }
     }
 }
