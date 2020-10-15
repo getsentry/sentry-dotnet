4769395c9e13b6eff6f17d4ccfaf5718f41ed7b1
Alexey Golub <tyrrrz@gmail.com>
2020-10-15 17:27:47 +0300

    Rework HubTests

--- test/Sentry.Tests/HubTests.cs
+++ test/Sentry.Tests/HubTests.cs
@@ -1,5 +1,8 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
+using FluentAssertions;
 using NSubstitute;
 using Sentry;
 using Sentry.Extensibility;
@@ -13,112 +16,159 @@ namespace NotSentry.Tests
 {
     public class HubTests
     {
-        private class Fixture
+        private class FakeBackgroundWorker : IBackgroundWorker
         {
-            public SentryOptions SentryOptions { get; set; } = new SentryOptions();
-            public IBackgroundWorker Worker { get; set; } = Substitute.For<IBackgroundWorker>();
+            public List<Envelope> Queue { get; } = new List<Envelope>();
 
-            public Fixture()
+            public int QueuedItems => Queue.Count;
+
+            public bool EnqueueEnvelope(Envelope envelope)
             {
-                SentryOptions.Dsn = DsnSamples.ValidDsnWithSecret;
-                SentryOptions.BackgroundWorker = Worker;
+                Queue.Add(envelope);
+                return true;
             }
 
-            public Hub GetSut() => new Hub(SentryOptions);
+            public Task FlushAsync(TimeSpan timeout) => Task.CompletedTask;
         }
-
-        private readonly Fixture _fixture = new Fixture();
 
         [Fact]
         public void PushScope_BreadcrumbWithinScope_NotVisibleOutside()
         {
-            var sut = _fixture.GetSut();
-
-            using (sut.PushScope())
+            // Arrange
+            var hub = new Hub(new SentryOptions
             {
-                sut.ConfigureScope(s => s.AddBreadcrumb("test"));
-                _ = Assert.Single(sut.ScopeManager.GetCurrent().Key.Breadcrumbs);
+                Dsn = DsnSamples.ValidDsnWithSecret,
+                BackgroundWorker = new FakeBackgroundWorker()
+            });
+
+            // Act & assert
+            using (hub.PushScope())
+            {
+                hub.ConfigureScope(s => s.AddBreadcrumb("test"));
+                Assert.Single(hub.ScopeManager.GetCurrent().Key.Breadcrumbs);
             }
 
-            Assert.Empty(sut.ScopeManager.GetCurrent().Key.Breadcrumbs);
+            Assert.Empty(hub.ScopeManager.GetCurrent().Key.Breadcrumbs);
         }
 
         [Fact]
         public void PushAndLockScope_DoesNotAffectOuterScope()
         {
-            var sut = _fixture.GetSut();
-
-            sut.ConfigureScope(s => Assert.False(s.Locked));
-            using (sut.PushAndLockScope())
+            // Arrange
+            var hub = new Hub(new SentryOptions
             {
-                sut.ConfigureScope(s => Assert.True(s.Locked));
+                Dsn = DsnSamples.ValidDsnWithSecret,
+                BackgroundWorker = new FakeBackgroundWorker()
+            });
+
+            // Act & assert
+            hub.ConfigureScope(s => Assert.False(s.Locked));
+            using (hub.PushAndLockScope())
+            {
+                hub.ConfigureScope(s => Assert.True(s.Locked));
             }
-            sut.ConfigureScope(s => Assert.False(s.Locked));
+
+            hub.ConfigureScope(s => Assert.False(s.Locked));
         }
 
         [Fact]
         public void CaptureMessage_AttachStacktraceTrue_IncludesStackTrace()
         {
-            _fixture.SentryOptions.AttachStacktrace = true;
+            // Arrange
+            var worker = new FakeBackgroundWorker();
 
-            var sut = _fixture.GetSut();
+            var hub = new Hub(new SentryOptions
+            {
+                Dsn = DsnSamples.ValidDsnWithSecret,
+                BackgroundWorker = worker,
+                AttachStacktrace = true
+            });
 
-            _ = sut.CaptureMessage("test");
+            // Act
+            hub.CaptureMessage("test");
 
-            _ = _fixture.Worker.Received(1).EnqueueEnvelope(
-                Arg.Is<Envelope>(e => e
-                    .Items
-                    .Select(i => i.Payload)
-                    .OfType<SentryEvent>()
-                    .Single()
-                    .Exception
-                    .StackTrace != null)
-            );
+            // Assert
+            var envelope = worker.Queue.Single();
+
+            var stackTrace = envelope.Items
+                .Select(i => i.Payload)
+                .OfType<SentryEvent>()
+                .Single()
+                .Exception?
+                .StackTrace;
+
+            stackTrace.Should().NotBeNull();
         }
 
         [Fact]
         public void CaptureMessage_AttachStacktraceFalse_DoesNotIncludeStackTrace()
         {
-            _fixture.SentryOptions.AttachStacktrace = false;
+            // Arrange
+            var worker = new FakeBackgroundWorker();
 
-            var sut = _fixture.GetSut();
+            var hub = new Hub(new SentryOptions
+            {
+                Dsn = DsnSamples.ValidDsnWithSecret,
+                BackgroundWorker = worker,
+                AttachStacktrace = true
+            });
 
-            _ = sut.CaptureMessage("test");
+            // Act
+            hub.CaptureMessage("test");
 
-            _ = _fixture.Worker.Received(1).EnqueueEnvelope(
-                Arg.Is<Envelope>(e => e
-                    .Items
-                    .Select(i => i.Payload)
-                    .OfType<SentryEvent>()
-                    .Single()
-                    .Exception
-                    .StackTrace == null)
-            );
+            // Assert
+            var envelope = worker.Queue.Single();
+
+            var stackTrace = envelope.Items
+                .Select(i => i.Payload)
+                .OfType<SentryEvent>()
+                .Single()
+                .Exception?
+                .StackTrace;
+
+            stackTrace.Should().BeNull();
         }
 
         [Fact]
         public void CaptureMessage_FailedQueue_LastEventIdSetToEmpty()
         {
-            var expectedId = Guid.Empty;
-            _ = _fixture.Worker.EnqueueEnvelope(Arg.Any<Envelope>()).Returns(false);
-            var sut = _fixture.GetSut();
+            // Arrange
+            var worker = Substitute.For<IBackgroundWorker>();
+            worker.EnqueueEnvelope(Arg.Any<Envelope>()).Returns(false);
 
-            var actualId = sut.CaptureMessage("test");
+            var hub = new Hub(new SentryOptions
+            {
+                Dsn = DsnSamples.ValidDsnWithSecret,
+                BackgroundWorker = worker
+            });
 
-            Assert.Equal(expectedId, (Guid)actualId);
-            Assert.Equal(expectedId, (Guid)sut.LastEventId);
+            // Act
+            var actualId = hub.CaptureMessage("test");
+
+            // Assert
+            Assert.Equal(Guid.Empty, (Guid)actualId);
+            Assert.Equal(Guid.Empty, (Guid)hub.LastEventId);
         }
 
         [Fact]
         public void CaptureMessage_SuccessQueued_LastEventIdSetToReturnedId()
         {
-            _ = _fixture.Worker.EnqueueEnvelope(Arg.Any<Envelope>()).Returns(true);
-            var sut = _fixture.GetSut();
+            // Arrange
+            var worker = Substitute.For<IBackgroundWorker>();
+            worker.EnqueueEnvelope(Arg.Any<Envelope>()).Returns(true);
 
-            var actualId = sut.CaptureMessage("test");
+            var hub = new Hub(new SentryOptions
+            {
+                Dsn = DsnSamples.ValidDsnWithSecret,
+                BackgroundWorker = worker
+            });
 
+            // Act
+            var actualId = hub.CaptureMessage("test");
+
+            // Assert
             Assert.NotEqual(default, actualId);
-            Assert.Equal(actualId, sut.LastEventId);
+            Assert.Equal(actualId, hub.LastEventId);
         }
     }
 }
