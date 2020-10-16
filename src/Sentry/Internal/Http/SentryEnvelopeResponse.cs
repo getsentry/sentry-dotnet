f1cbcfd5a9915253033c2ef13902fb801ee4e11d
Alexey Golub <tyrrrz@gmail.com>
2020-10-16 20:00:26 +0300

    Draft start

---
+++ src/Sentry/Internal/Http/SentryEnvelopeResponse.cs
@@ -0,0 +1,12 @@
+﻿using System.Net.Http;
+
+namespace Sentry.Internal.Http
+{
+    internal class SentryEnvelopeResponse
+    {
+        public static SentryEnvelopeResponse FromHttpResponse(HttpResponseMessage response)
+        {
+
+        }
+    }
+}
