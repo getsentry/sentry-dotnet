afbd55ed7ad1d1299c4ff4dadbcabe8e83c4c426
Alexey Golub <tyrrrz@gmail.com>
2020-10-16 14:46:26 +0300

    Fix commented test

--- test/Sentry.Tests/SentrySdkTests.cs
+++ test/Sentry.Tests/SentrySdkTests.cs
@@ -357,9 +357,17 @@ namespace Sentry.Tests
             }))
             {
                 SentrySdk.AddBreadcrumb(expected);
-                _ = SentrySdk.CaptureMessage("message");
+                SentrySdk.CaptureMessage("message");
 
-                //_ = worker.EnqueueEnvelope(Arg.Is<Envelope>(e => e.Breadcrumbs.Single().Message == expected));
+                worker.EnqueueEnvelope(
+                    Arg.Is<Envelope>(e => e.Items
+                            .Select(i => i.Payload)
+                            .OfType<SentryEvent>()
+                            .Single()
+                            .Breadcrumbs
+                            .Single()
+                            .Message == expected)
+                );
             }
         }
 
