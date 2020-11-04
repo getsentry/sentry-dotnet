4c109bf059828aa6b42ca5a604e550471d1ab951
Lucas Zimerman Fraulob <lucas-zimerman1@hotmail.com>
2020-11-04 13:42:02 -0300

    +Lazy

--- src/Sentry/Internal/NetFxInstallationsEventProcessor.cs
+++ src/Sentry/Internal/NetFxInstallationsEventProcessor.cs
@@ -1,4 +1,5 @@
 #if NETFX
+using System;
 using System.Collections.Generic;
 using Sentry.Extensibility;
 using Sentry.PlatformAbstractions;
@@ -9,13 +10,14 @@ namespace Sentry.Internal
     {
         internal static readonly string NetFxInstallationsKey = ".NET Framework";
 
-        private readonly IEnumerable<FrameworkInstallation> _netFxInstallations = FrameworkInfo.GetInstallations();
+        private readonly Lazy<IEnumerable<FrameworkInstallation>> _netFxInstallations =
+            new Lazy<IEnumerable<FrameworkInstallation>>(() => FrameworkInfo.GetInstallations());
 
         public SentryEvent? Process(SentryEvent @event)
         {
             if (!@event.Contexts.ContainsKey(NetFxInstallationsKey))
             {
-                @event.Contexts[NetFxInstallationsKey] = _netFxInstallations;
+                @event.Contexts[NetFxInstallationsKey] = _netFxInstallations.Value;
             }
             return @event;
         }
