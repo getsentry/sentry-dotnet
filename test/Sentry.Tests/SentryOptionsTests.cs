7ebeeb6aff6339678022ca68e1ff005e72ca57de
Bruno Garcia <bruno@brunogarcia.com>
2020-11-07 20:48:19 -0500

    feat: Better Mono stacktraces (#578)

--- test/Sentry.Tests/SentryOptionsTests.cs
+++ test/Sentry.Tests/SentryOptionsTests.cs
@@ -1,5 +1,7 @@
 using System.IO.Compression;
 using System.Net;
+using Sentry.Internal;
+using Sentry.PlatformAbstractions;
 using Xunit;
 
 namespace Sentry.Tests
@@ -19,5 +21,25 @@ namespace Sentry.Tests
             var sut = new SentryOptions();
             Assert.Equal(CompressionLevel.Optimal, sut.RequestBodyCompressionLevel);
         }
+
+#if NETFX
+        [SkippableFact]
+        public void StackTraceFactory_RunningOnMono_HasMonoStackTraceFactory()
+        {
+            Skip.If(!RuntimeInfo.GetRuntime().IsMono());
+
+            var sut = new SentryOptions();
+            Assert.IsType<MonoSentryStackTraceFactory>(sut.SentryStackTraceFactory);
+        }
+
+        [SkippableFact]
+        public void StackTraceFactory_NotRunningOnMono_NotMonoStackTraceFactory()
+        {
+            Skip.If(RuntimeInfo.GetRuntime().IsMono());
+
+            var sut = new SentryOptions();
+            Assert.IsNotType<MonoSentryStackTraceFactory>(sut.SentryStackTraceFactory);
+        }
+#endif
     }
 }
