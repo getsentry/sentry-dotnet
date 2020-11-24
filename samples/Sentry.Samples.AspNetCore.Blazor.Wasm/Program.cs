a18ae4d7a1a098f97d55108f34ed0386bf6cb529
Bruno Garcia <bruno@brunogarcia.com>
2020-11-23 21:28:24 -0500

    net5.0

--- samples/Sentry.Samples.AspNetCore.Blazor.Wasm/Program.cs
+++ samples/Sentry.Samples.AspNetCore.Blazor.Wasm/Program.cs
@@ -1,9 +1,9 @@
 using System;
 using System.Net.Http;
 using System.Threading.Tasks;
-using System.Text;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace Sentry.Samples.AspNetCore.Blazor.Wasm
 {
@@ -12,14 +12,30 @@ namespace Sentry.Samples.AspNetCore.Blazor.Wasm
 #pragma warning disable IDE1006 // Naming Styles
         public static async Task Main(string[] args)
         {
-            using var sdk = SentrySdk.Init("https://80aed643f81249d4bed3e30687b310ab@o447951.ingest.sentry.io/5428537");
+            // Capture blazor bootstrapping errors
+            using var sdk = SentrySdk.Init(o =>
+            {
+                o.Dsn = "https://80aed643f81249d4bed3e30687b310ab@o447951.ingest.sentry.io/5428537";
+                o.Debug = true;
+            });
+            try
+            {
+                var builder = WebAssemblyHostBuilder.CreateDefault(args);
+                builder.RootComponents.Add<App>("#app");
+                builder.Logging.SetMinimumLevel(LogLevel.Debug);
+                // Captures logError and higher as events
+                builder.Logging.AddSentry(o => o.InitializeSdk = false);
 
-            var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<App>("app");
+                builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-            _ = builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
-            await builder.Build().RunAsync();
+                await builder.Build().RunAsync();
+            }
+            catch (Exception e)
+            {
+                SentrySdk.CaptureException(e);
+                await SentrySdk.FlushAsync(TimeSpan.FromSeconds(2));
+                throw;
+            }
         }
     }
 }
