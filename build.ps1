f8a5fd79dca9ecb0996df2e54e0d989e9b4722c1
Alexey Golub <tyrrrrrr@gmail.com>
2020-12-29 19:21:44 +0200

    Fix Windows CI/CD

--- build.ps1
+++ build.ps1
@@ -2,6 +2,9 @@ $ErrorActionPreference = "Stop"
 
 $testLogger = if ($env:GITHUB_ACTIONS -eq "true") {"GitHubActions;report-warnings=false"} else {"console"}
 
+# Fixes an issue with GitHub Actions build on Windows
+dotnet nuget locals all --clear
+
 dotnet test -c Release -l $testLogger `
     /p:CollectCoverage=true `
     /p:CoverletOutputFormat=opencover `
