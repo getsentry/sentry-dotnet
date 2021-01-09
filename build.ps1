6ab1015ed0467e5a55547b4e0d5d116b0474a718
Bruno Garcia <bruno@brunogarcia.com>
2021-01-09 17:59:35 -0500

    lock file for nuget

--- build.ps1
+++ build.ps1
@@ -5,6 +5,9 @@ $testLogger = if ($env:GITHUB_ACTIONS -eq "true") {"GitHubActions;report-warning
 # Fixes an issue with GitHub Actions build on Windows
 dotnet nuget locals all --clear
 
+dotnet restore --locked-mode
+ if ($LASTEXITCODE -ne 0) { exit 1 }
+
 dotnet test -c Release -l $testLogger `
     /p:CollectCoverage=true `
     /p:CoverletOutputFormat=opencover `
--- build.sh
+++ build.sh
@@ -8,7 +8,11 @@ if [ "$GITHUB_ACTIONS" == "true" ]
         testLogger="console"
 fi
 
-dotnet test -c Release -l $testLogger \
+dotnet restore --locked-mode
+
+dotnet test -c Release \
+    -l $testLogger \
+    --no-restore \
     /p:CollectCoverage=true \
     /p:CoverletOutputFormat=opencover \
     /p:Exclude=\"[Sentry.Protocol.Test*]*,[xunit.*]*,[Sentry.Test*]*\" \
