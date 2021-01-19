e0084adf4c5af4a0fed10ac6c4c5a7e27e41c7ba
Tyrrrz <tyrrrz@gmail.com>
2021-01-19 03:00:02 +0200

    wip

--- src/Sentry/Protocol/Context/Trace.cs
+++ src/Sentry/Protocol/Context/Trace.cs
@@ -71,7 +71,7 @@ namespace Sentry.Protocol
             var traceId = json.GetPropertyOrNull("trace_id")?.Pipe(SentryId.FromJson) ?? SentryId.Empty;
             var operation = json.GetPropertyOrNull("op")?.GetString() ?? "unknown";
             var status = json.GetPropertyOrNull("status")?.GetString()?.Pipe(s => s.Replace("_", "").ParseEnum<SpanStatus>());
-            var isSampled = json.GetPropertyOrNull("status")?.GetBoolean() ?? true;
+            var isSampled = json.GetPropertyOrNull("sampled")?.GetBoolean() ?? true;
 
             return new Trace
             {
