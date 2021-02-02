684b0bc9254bb732826fb6f38bf00586d90c9269
Alexey Golub <tyrrrrrr@gmail.com>
2021-02-02 22:32:52 +0200

    Automatically inject 'sentry-trace' on outgoing requests in ASP.NET Core (#784)

---
+++ src/Sentry.AspNetCore/SentryHttpMessageHandlerBuilderFilter.cs
@@ -0,0 +1,22 @@
+﻿#if !NETSTANDARD2_0
+using System;
+using Microsoft.Extensions.Http;
+
+namespace Sentry.AspNetCore
+{
+    // Injects Sentry's HTTP handler into HttpClientFactory
+    internal class SentryHttpMessageHandlerBuilderFilter : IHttpMessageHandlerBuilderFilter
+    {
+        private readonly IHub _hub;
+
+        public SentryHttpMessageHandlerBuilderFilter(IHub hub) => _hub = hub;
+
+        public Action<HttpMessageHandlerBuilder> Configure(Action<HttpMessageHandlerBuilder> next) =>
+            handlerBuilder =>
+            {
+                handlerBuilder.AdditionalHandlers.Add(new SentryHttpMessageHandler(_hub));
+                next(handlerBuilder);
+            };
+    }
+}
+#endif
