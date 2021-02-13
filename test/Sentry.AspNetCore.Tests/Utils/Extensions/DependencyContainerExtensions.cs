67b63198826aed4a35866996827cdd4f12de9818
Alexey Golub <tyrrrrrr@gmail.com>
2021-02-14 00:11:19 +0200

    Validate DI in all ASP.NET Core tests (#816)
    Co-authored-by: Bruno Garcia <bruno@brunogarcia.com>

---
+++ test/Sentry.AspNetCore.Tests/Utils/Extensions/DependencyContainerExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Sentry.AspNetCore.Tests.Utils.Extensions
+{
+    internal static class DependencyContainerExtensions
+    {
+        public static void EnableValidation(this ServiceProviderOptions options, bool enable = true)
+        {
+            options.ValidateScopes = enable;
+
+#if !NET461 && !NETCOREAPP2_1
+            options.ValidateOnBuild = enable;
+#endif
+        }
+    }
+}
