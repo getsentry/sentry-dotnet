afa7161b22fb24127a38bb5d4c32ae4d7fbd1af7
LucasZF <lucas-zimerman1@hotmail.com>
2021-03-15 20:05:23 -0300

    Apply suggestions from code review
    Requested changes,
    
    Co-authored-by: Bruno Garcia <bruno@brunogarcia.com>

--- src/Sentry.AspNet/Internal/SystemWebVersionLocator.cs
+++ src/Sentry.AspNet/Internal/SystemWebVersionLocator.cs
@@ -9,19 +9,20 @@ namespace Sentry.AspNet.Internal
     {
         public static string? GetCurrent() => GetCurrent(ReleaseLocator.GetCurrent());
         internal static string? GetCurrent(string? release)
+
         {
             if (release != null)
             {
                 return release;
             }
-            else if (HttpContext.Current?.ApplicationInstance?.GetType() is Type type)
+            else if (HttpContext.Current?.ApplicationInstance?.GetType() is { } type)
             {
                 //Usually the type is ASP.global_asax and the BaseType is the Web Application.
                 while (type != null && type.Namespace == "ASP")
                 {
                     type = type.BaseType;
                 }
-                if (type?.Assembly is Assembly assembly)
+                if (type?.Assembly is { } assembly)
                 {
                     return ApplicationVersionLocator.GetCurrent(assembly);
                 }
