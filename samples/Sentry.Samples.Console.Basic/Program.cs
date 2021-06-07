5f43b3430b46f420693e642ec9134f3ad25f0c53
Tyrrrz <tyrrrrrr@gmail.com>
2021-06-07 21:27:12 +0300

    revert demo

--- samples/Sentry.Samples.Console.Basic/Program.cs
+++ samples/Sentry.Samples.Console.Basic/Program.cs
@@ -1,16 +1,7 @@
-using System;
-using System.Collections.Generic;
-using System.Threading;
 using Sentry;
 
 using (SentrySdk.Init("https://eb18e953812b41c3aeb042e666fd3b5c@o447951.ingest.sentry.io/5428537"))
 {
-    SentrySdk.StartSession();
-
-    SentrySdk.CaptureMessage("test session");
-
-    SentrySdk.EndSession(SessionEndStatus.Crashed);
-
     // The following exception is captured and sent to Sentry
-    //throw null;
+    throw null;
 }
