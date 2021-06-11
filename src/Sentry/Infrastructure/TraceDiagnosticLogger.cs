7898e855fc57a400d950bc073ce06a9e7e31930a
Lucas Zimerman Fraulob <lucas-zimerman1@hotmail.com>
2021-06-11 16:12:35 -0300

    add constant TRACE so Trace.Write works with othe applications.

--- src/Sentry/Infrastructure/TraceDiagnosticLogger.cs
+++ src/Sentry/Infrastructure/TraceDiagnosticLogger.cs
@@ -1,8 +1,6 @@
 using System;
 using System.Diagnostics;
 using Sentry.Extensibility;
-using System.Linq;
-
 namespace Sentry.Infrastructure
 {
     /// <summary>
@@ -15,18 +13,6 @@ namespace Sentry.Infrastructure
     {
         private readonly SentryLevel _minimalLevel;
 
-        private Lazy<TraceListener?> _traceListener = new Lazy<TraceListener?>(() => GetFirstOrDefaultListener());
-
-        /// <summary>
-        /// Get the first TraceListener, usually it's the process debug output.
-        /// </summary>
-        /// <returns>The first TraceListener, if available</returns>
-        private static TraceListener? GetFirstOrDefaultListener()
-        {
-            var listeners = Trace.Listeners;
-            return listeners.Count > 0 ? listeners[0] : null;
-        }
-
         /// <summary>
         /// Creates a new instance of <see cref="TraceDiagnosticLogger"/>.
         /// </summary>
@@ -35,13 +21,13 @@ namespace Sentry.Infrastructure
         /// <summary>
         /// Whether the logger is enabled to the defined level.
         /// </summary>
-        public bool IsEnabled(SentryLevel level) => level >= _minimalLevel && _traceListener.Value != null;
+        public bool IsEnabled(SentryLevel level) => level >= _minimalLevel;
 
         /// <summary>
         /// Log message with level, exception and parameters.
         /// </summary>
         public void Log(SentryLevel logLevel, string message, Exception? exception = null, params object?[] args)
-            => _traceListener.Value?.Write($@"{logLevel,7}: {string.Format(message, args)}
+            => Trace.Write($@"{logLevel,7}: {string.Format(message, args)}
 {exception}");
     }
 }
