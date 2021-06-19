156f65616202d75ee6ff4314eb2b04aa260f621c
Bruno Garcia <bruno@brunogarcia.com>
2021-06-19 17:43:56 -0400

    ci and offline caching

---
+++ samples/Sentry.Samples.Maui/README.md
@@ -0,0 +1,9 @@
+# .NET MAUI sample
+
+Running:
+
+```
+dotnet build -t:Run -f net6.0-android
+dotnet build -t:Run -f net6.0-ios
+dotnet build -t:Run -f net6.0-maccatalyst
+```
--- samples/Sentry.Samples.Maui/Startup.cs
+++ samples/Sentry.Samples.Maui/Startup.cs
@@ -14,7 +14,9 @@ namespace Sentry.Samples.Maui
                 .UseMauiApp<App>()
                 .UseSentry(o =>
                 {
-                    o.Dsn = "https://80aed643f81249d4bed3e30687b310ab@o447951.ingest.sentry.io/5428537";
+                    o.Dsn = "https://eb18e953812b41c3aeb042e666fd3b5c@o447951.ingest.sentry.io/5428537";
+                    o.Debug = true;
+                    o.CacheDirectoryPath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
                 })
                 .ConfigureFonts(fonts =>
                 {
