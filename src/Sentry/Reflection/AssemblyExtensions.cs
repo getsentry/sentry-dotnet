a02001ab895f006caa962a1754a69c5b84dec356
Geert van Horrik <GeertvanHorrik@users.noreply.github.com>
2021-06-23 20:13:44 +0200

    Add ReportAssembliesMode in favor of ReportAssemblies (#1079)
    * Add ReportAssembliesMode in favor of ReportAssemblies
    
    * Update changelog
    
    * Handle obsolete member in serilog sinks
    
    * AssemblyExtensions.GetNameAndVersion should return null for non-found version
    
    Co-authored-by: Bruno Garcia <bruno@brunogarcia.com>

--- src/Sentry/Reflection/AssemblyExtensions.cs
+++ src/Sentry/Reflection/AssemblyExtensions.cs
@@ -22,11 +22,28 @@ namespace Sentry.Reflection
         {
             var asmName = asm.GetName();
             var name = asmName.Name;
-            var version =
-                asm.GetCustomAttribute<AssemblyInformationalVersionAttribute>()?.InformationalVersion
-                ?? asmName?.Version?.ToString();
+            string? asmVersion = null;
 
-            return new SdkVersion {Name = name, Version = version};
+            // Note: on full .NET FX, checking the AssemblyInformationalVersionAttribute could throw an exception, therefore
+            // this method uses a try/catch to make sure this method always returns a value
+            try
+            {
+                asmVersion = asm.GetCustomAttribute<AssemblyInformationalVersionAttribute>()?.InformationalVersion;
+            }
+            catch
+            {
+            }
+
+            // Note: even though the informational version could be "invalid" (e.g. string.Empty), it should
+            // be used for versioning and the software should not fallback to the assembly version string.
+            //
+            // See https://github.com/getsentry/sentry-dotnet/pull/1079#issuecomment-866992216
+            if (asmVersion is null)
+            {
+                asmVersion = asmName?.Version?.ToString();
+            }
+
+            return new SdkVersion {Name = name, Version = asmVersion };
         }
     }
 }
