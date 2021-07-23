73bc50f57c7b278c67bfae12ec792baa04f0922d
Lucas Zimerman Fraulob <lucas-zimerman1@hotmail.com>
2021-07-23 16:50:13 -0300

    small refactor

--- src/Sentry.Extensions.Logging/SentryEFCoreInterceptor.cs
+++ src/Sentry.Extensions.Logging/SentryEFCoreInterceptor.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading;
 
 namespace Sentry.Extensions.Logging
@@ -36,7 +35,8 @@ namespace Sentry.Extensions.Logging
 
         public SentryEFCoreInterceptor(IHub maHub) => _hub = maHub;
 
-        private void SetSpan(SentryEFSpanType type, ISpan? span) => _spans[type] = span;
+        private void SetSpan(SentryEFSpanType type, string operation, string? description)
+            => _spans[type] = _hub.GetSpan()?.StartChild(operation, description);
 
         private ISpan? GetSpan(SentryEFSpanType type) => _spans.GetValueOrDefault(type);
 
@@ -46,54 +46,50 @@ namespace Sentry.Extensions.Logging
 
         public void OnNext(KeyValuePair<string, object?> value)
         {
-            if (value.Key == "Microsoft.EntityFrameworkCore.ChangeTracking.StartedTracking")
-                return;
-
             if (value.Key == EFContextInitializedKey)
             {
-                SetSpan(SentryEFSpanType.Context, _hub.GetSpan()?.StartChild("ef.core", "opening EF Core context."));
+                SetSpan(SentryEFSpanType.Context, "ef.core", "opening EF Core context.");
             }
 
-            else if (value.Key == EFConnectionOpening)
-            {
-                SetSpan(SentryEFSpanType.Connection, _hub.GetSpan()?.StartChild("db", "connection"));
-            }
-            else if (value.Key == EFCommandExecuting &&
-                _hub.GetSpan()?.StartChild("db", null) is { } querySpanExecuting)
-            {
-                SetSpan(SentryEFSpanType.QueryExecution, querySpanExecuting);
-            }
-            else if (value.Key == EFCommandFailed &&
-                GetSpan(SentryEFSpanType.QueryExecution) is { } errorQuerySpan)
-            {
-                errorQuerySpan.Description = FilterExecutedQueryWithErrorValue(value);
-                errorQuerySpan.Finish(SpanStatus.InternalError);
-            }
-            else if (value.Key == EFCommandExecuted &&
-                GetSpan(SentryEFSpanType.QueryExecution) is { } executedQuerySpan)
-            {
-                executedQuerySpan.Description = value.Value?.ToString();
-                executedQuerySpan.Finish(SpanStatus.Ok);
-            }
-            else if (value.Key == EFConnectionClosed &&
-                     GetSpan(SentryEFSpanType.Connection) is { } connectionSpan)
-            {
-                connectionSpan.Finish(SpanStatus.Ok);
-            }
-            else if (value.Key == EFContextDisposedKey &&
-                GetSpan(SentryEFSpanType.Context) is { } bla)
-            {
-                // We finish it here because the transaction will be dispsed once the context is ended.
-                GetSpan(SentryEFSpanType.Context)?.Finish(SpanStatus.Ok);
-
-            }
+            //Query compiler Span
             else if (value.Key == EFQueryCompiling)
             {
-                SetSpan(SentryEFSpanType.QueryCompiler, _hub.GetSpan()?.StartChild("db", value.Value?.ToString()));
+                SetSpan(SentryEFSpanType.QueryCompiler, "db.query_compiler", FilterNewLineValue(value));
             }
             else if (value.Key == EFQueryCompiled)
             {
                 GetSpan(SentryEFSpanType.QueryCompiler)?.Finish(SpanStatus.Ok);
+            }
+
+            //Connection Span
+            else if (value.Key == EFConnectionOpening)
+            {
+                SetSpan(SentryEFSpanType.Connection, "db.connection", null);
+            }
+            else if (value.Key == EFConnectionClosed)
+            {
+                GetSpan(SentryEFSpanType.Connection)?.Finish(SpanStatus.Ok);
+            }
+
+            //Query Execution Span
+            else if (value.Key == EFCommandExecuting)
+            {
+                SetSpan(SentryEFSpanType.QueryExecution, "db.query", FilterNewLineValue(value));
+            }
+            else if (value.Key == EFCommandFailed)
+            {
+                GetSpan(SentryEFSpanType.QueryExecution)?.Finish(SpanStatus.InternalError);
+            }
+            else if (value.Key == EFCommandExecuted)
+            {
+                GetSpan(SentryEFSpanType.QueryExecution)?.Finish(SpanStatus.Ok);
+            }
+
+            else if (value.Key == EFContextDisposedKey)
+            {
+                // We finish it here because the transaction will be dispsed once the context is ended.
+                GetSpan(SentryEFSpanType.Context)?.Finish(SpanStatus.Ok);
+
             }
         }
 
@@ -101,14 +97,17 @@ namespace Sentry.Extensions.Logging
         /// Get the Query with error message and remove the uneeded values.
         /// </summary>
         /// <example>
-        /// Failed executing DbCommand (8ms) [Parameters=[@__ef_filter__isAuthenticated_0='?' (DbType = Boolean)], CommandType='Text', CommandTimeout='30']
-        /// becomes:  [Parameters=[@__ef_filter__isAuthenticated_0='?' (DbType = Boolean)], CommandType='Text', CommandTimeout='30']
-        /// Executed DbCommand (9ms) [Parameters=[@__ef_filter__isAuthenticated_0='?' (DbType = Boolean)], CommandType='Text', CommandTimeout='30']...
-        /// becomes:  [Parameters=[@__ef_filter__isAuthenticated_0='?' (DbType = Boolean)], CommandType='Text', CommandTimeout='30']...
+        /// Compiling query model: 
+        /// EF Query...
+        /// becomes:
+        /// EF Query...
         /// </example>
         /// <param name="value">the query with error value</param>
         /// <returns>the filtered query</returns>
-        internal string? FilterQueryWithTimeValue(object? value)
-            => value?.ToString()?.Split(')', 1)[1];
+        internal string? FilterNewLineValue(object? value)
+        {
+            var str = value?.ToString();
+            return str?.Substring(str.IndexOf('\n') + 1);
+        }        
     }
 }
