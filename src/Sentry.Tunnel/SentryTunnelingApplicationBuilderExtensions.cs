cd03fe15d833cdcbd9fdf237bcde61c67c0d68d1
kanadaj <kanadaj@users.noreply.github.com>
2021-07-25 16:09:08 +0100

    Add Sentry.Tunnel to add official tunnel middleware (#1133)
    * Add Sentry.Tunnel to add official tunnel middleware
    
    * Add Tunnel version user agent
    
    * Update src/Sentry.Tunnel/SentryTunnelMiddleware.cs
    
    Co-authored-by: Bruno Garcia <bruno@brunogarcia.com>
    
    * Remove .NET testing stuff, cache the assembly version
    
    * Add trailing newline
    
    * Update src/Sentry.Tunnel/SentryTunnelMiddleware.cs
    
    Co-authored-by: LucasZF <lucas-zimerman1@hotmail.com>
    
    * Update src/Sentry.Tunnel/SentryTunnelMiddleware.cs
    
    Co-authored-by: LucasZF <lucas-zimerman1@hotmail.com>
    
    * Update src/Sentry.Tunnel/SentryTunnelMiddleware.cs
    
    Co-authored-by: LucasZF <lucas-zimerman1@hotmail.com>
    
    * Update src/Sentry.Tunnel/SentryTunnelMiddleware.cs
    
    Co-authored-by: LucasZF <lucas-zimerman1@hotmail.com>
    
    * Update src/Sentry.Tunnel/SentryTunnelMiddleware.cs
    
    Co-authored-by: LucasZF <lucas-zimerman1@hotmail.com>
    
    * Update src/Sentry.Tunnel/SentryTunnelMiddleware.cs
    
    Co-authored-by: LucasZF <lucas-zimerman1@hotmail.com>
    
    * Update src/Sentry.Tunnel/SentryTunnelMiddleware.cs
    
    Co-authored-by: LucasZF <lucas-zimerman1@hotmail.com>
    
    * Update src/Sentry.Tunnel/SentryTunnelMiddleware.cs
    
    Co-authored-by: LucasZF <lucas-zimerman1@hotmail.com>
    
    * Update src/Sentry.Tunnel/SentryTunnelMiddleware.cs
    
    Co-authored-by: LucasZF <lucas-zimerman1@hotmail.com>
    
    * Update src/Sentry.Tunnel/SentryTunnelMiddleware.cs
    
    Co-authored-by: LucasZF <lucas-zimerman1@hotmail.com>
    
    Co-authored-by: Bruno Garcia <bruno@brunogarcia.com>
    Co-authored-by: LucasZF <lucas-zimerman1@hotmail.com>

---
+++ src/Sentry.Tunnel/SentryTunnelingApplicationBuilderExtensions.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Sentry.Tunnel
+{
+    public static class SentryTunnelingApplicationBuilderExtensions
+    {
+        /// <summary>
+        /// Adds and configures the Sentry tunneling middleware
+        /// </summary>
+        /// <param name="services"></param>
+        /// <param name="hostnames">The extra hostnames to be allowed for the tunneling. sentry.io is allowed by default; add your own Sentry domain if you use a self-hosted Sentry or Relay.</param>
+        public static void AddSentryTunneling(this IServiceCollection services, params string[] hostnames)
+        {
+            services.AddScoped<SentryTunnelMiddleware>((s) => new SentryTunnelMiddleware(hostnames));
+        }
+        
+        public static void UseSentryTunneling(this IApplicationBuilder builder, string path = "/tunnel")
+        {
+            builder.Map(path, applicationBuilder =>
+            {
+                applicationBuilder.UseMiddleware<SentryTunnelMiddleware>();
+            });
+        }
+    }
+}
