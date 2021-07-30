49956436225b19a1c660017d2e18ca265428e523
Tyrrrz <tyrrrrrr@gmail.com>
2021-07-31 02:31:00 +0300

    zxc

--- src/Sentry/ServerSessionManager.cs
+++ src/Sentry/ServerSessionManager.cs
@@ -6,36 +6,83 @@ using Sentry.Internal;
 
 namespace Sentry
 {
+    // Strategy for future reference:
+    // - Keep only one set of sessions in memory
+    // - Make the timer run on-the-minute
+    // - Just track the exited/error count
+    // - When the timer hits - flush
+
     // AKA server mode
     // https://develop.sentry.dev/sdk/sessions
     internal class ServerSessionManager : ISessionManager, IDisposable
     {
+        private readonly object _lock = new();
+
         private readonly SentryOptions _options;
         private readonly ISentryClient _client;
-        private readonly IInternalScopeManager _scopeManager;
         private readonly ISystemClock _clock;
         private readonly Timer _timer;
+
+        private int _exitedCount;
+        private int _erroredCount;
 
         public ServerSessionManager(
             SentryOptions options,
             ISentryClient client,
-            IInternalScopeManager scopeManager,
             ISystemClock clock)
         {
             _options = options;
             _client = client;
-            _scopeManager = scopeManager;
             _clock = clock;
 
+            // TODO: timer should be synced to run *on the minute*
             _timer = new Timer {Interval = 60 * 1000, Enabled = true, AutoReset = true};
             _timer.Elapsed += (_, _) => Flush();
+        }
+
+        private SessionAggregate? TryAggregate()
+        {
+            lock (_lock)
+            {
+                // If no sessions exited or errored, that means none were started during this period
+                if (_exitedCount == 0 && _erroredCount == 0)
+                {
+                    return null;
+                }
+
+                // Extract release
+                var release = ReleaseLocator.Resolve(_options);
+                if (string.IsNullOrWhiteSpace(release))
+                {
+                    // Release health without release is just health (useless)
+                    _options.DiagnosticLogger?.LogError(
+                        "Failed to flush session aggregate because there is no release information."
+                    );
+
+                    return null;
+                }
+
+                // Extract environment
+                var environment = EnvironmentLocator.Resolve(_options);
+
+                // Get the current minute
+                var now = _clock.GetUtcNow();
+                var startTimestamp = new DateTimeOffset(
+                    now.Year, now.Month, now.Day, now.Hour, now.Minute, 0, now.Offset
+                );
+
+                return new SessionAggregate(startTimestamp, _exitedCount, _erroredCount, release, environment);
+            }
         }
 
         private void Flush()
         {
             try
             {
-                // TODO: aggregate and flush active sessions
+                if (TryAggregate() is { } aggregate)
+                {
+                    _client.CaptureSessionAggregate(aggregate);
+                }
             }
             catch (Exception ex)
             {
@@ -48,12 +95,27 @@ namespace Sentry
 
         public void StartSession()
         {
-            throw new NotImplementedException();
+            // No-op
         }
 
         public void EndSession(SessionEndStatus status)
         {
-            throw new NotImplementedException();
+            lock (_lock)
+            {
+                // Note:
+                // Need to make sure status is correctly set.
+                // Session aggregates don't discern between Crashed and Abnormal.
+                // So we just count them both as errored.
+                // Should we respect ReportError() somehow?
+                if (status == SessionEndStatus.Exited)
+                {
+                    _exitedCount++;
+                }
+                else
+                {
+                    _erroredCount++;
+                }
+            }
         }
 
         public void PauseSession()
@@ -68,7 +130,7 @@ namespace Sentry
 
         public void ReportError()
         {
-            throw new NotImplementedException();
+            // No-op
         }
 
         public void Dispose() => _timer.Dispose();
