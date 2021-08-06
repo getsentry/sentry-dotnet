11cb53efab59bd1e31a9608d20b11a402c8f7210
Lucas Zimerman Fraulob <lucas-zimerman1@hotmail.com>
2021-08-06 14:22:34 -0300

    ext logging cleanup and unsubscripe listener.

--- src/Sentry.Diagnostics.DiagnosticSource/Internals/DiagnosticSource/SentryDiagnosticListenerIntegration.cs
+++ src/Sentry.Diagnostics.DiagnosticSource/Internals/DiagnosticSource/SentryDiagnosticListenerIntegration.cs
@@ -1,13 +1,20 @@
+using System;
 using System.Diagnostics;
-using Sentry.Integrations;
+using Sentry.Internal;
 
 namespace Sentry.Internals.DiagnosticSource
 {
-    internal class SentryDiagnosticListenerIntegration : ISdkIntegration
+    internal class SentryDiagnosticListenerIntegration : IInternalSdkIntegration
     {
+        private IDisposable? _diagnosticListener { get; set; }
         public void Register(IHub hub, SentryOptions options)
         {
-            DiagnosticListener.AllListeners.Subscribe(new SentryDiagnosticSubscriber(hub, options));
+            _diagnosticListener = DiagnosticListener.AllListeners.Subscribe(new SentryDiagnosticSubscriber(hub, options));
+        }
+
+        public void Unregister(IHub hub)
+        {
+            _diagnosticListener?.Dispose();
         }
     }
 }
