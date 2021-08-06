252f103f6303ce4ab7abcced0c7c2bbbcbbacafe
Lucas Zimerman Fraulob <lucas-zimerman1@hotmail.com>
2021-08-06 16:11:21 -0300

    more project fixes.

--- src/Sentry.Diagnostics.DiagnosticSource/SentryOptionsDiagnosticExtensions.cs
+++ src/Sentry.Diagnostics.DiagnosticSource/SentryOptionsDiagnosticExtensions.cs
@@ -2,10 +2,13 @@ using Sentry.Internals.DiagnosticSource;
 
 namespace Sentry
 {
+    /// <summary>
+    /// The additional Sentry Options extensions from Sentry Diagnostic Listener.
+    /// </summary>
     public static class SentryOptionsDiagnosticExtensions
     {
         /// <summary>
-        /// Attaches Sentry to System Diagnostic Listeners events. 
+        /// Attach Sentry to System Diagnostic Listeners events.
         /// </summary>
         /// <param name="options">The Sentry options.</param>
         public static void AddDiagnosticListeners(this SentryOptions options)
