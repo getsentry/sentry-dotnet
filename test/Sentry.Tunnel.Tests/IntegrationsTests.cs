7460accab7e126aa2ff76da77c804398be475568
Simon Cropp <simon.cropp@gmail.com>
2021-09-08 18:43:20 +1000

    redundant braces

--- test/Sentry.Tunnel.Tests/IntegrationsTests.cs
+++ test/Sentry.Tunnel.Tests/IntegrationsTests.cs
@@ -60,31 +60,27 @@ namespace Sentry.Tunnel.Tests
         [Fact]
         public async Task TunnelMiddleware_DoesNotForwardEnvelopeToArbitraryHost()
         {
-            {
-                var requestMessage = new HttpRequestMessage(new HttpMethod("POST"), "/tunnel");
-                requestMessage.Content = new StringContent(
-                    @"{""sent_at"":""2021-01-01T00:00:00.000Z"",""sdk"":{""name"":""sentry.javascript.browser"",""version"":""6.8.0""},""dsn"":""https://dns@evil.com/1""}
+            var requestMessage = new HttpRequestMessage(new HttpMethod("POST"), "/tunnel");
+            requestMessage.Content = new StringContent(
+                @"{""sent_at"":""2021-01-01T00:00:00.000Z"",""sdk"":{""name"":""sentry.javascript.browser"",""version"":""6.8.0""},""dsn"":""https://dns@evil.com/1""}
 {""type"":""session""}
 {""sid"":""fda00e933162466c849962eaea0cfaff""}");
-                var responseMessage = await _server.CreateClient().SendAsync(requestMessage);
+            var responseMessage = await _server.CreateClient().SendAsync(requestMessage);
 
-                Assert.Equal(0, _httpMessageHandler.NumberOfCalls);
-            }
+            Assert.Equal(0, _httpMessageHandler.NumberOfCalls);
         }
 
         [Fact]
         public async Task TunnelMiddleware_CanForwardEnvelopeToWhiteListedHost()
         {
-            {
-                var requestMessage = new HttpRequestMessage(new HttpMethod("POST"), "/tunnel");
-                requestMessage.Content = new StringContent(
-                    @"{""sent_at"":""2021-01-01T00:00:00.000Z"",""sdk"":{""name"":""sentry.javascript.browser"",""version"":""6.8.0""},""dsn"":""https://dns@sentry.mywebsite.com/1""}
+            var requestMessage = new HttpRequestMessage(new HttpMethod("POST"), "/tunnel");
+            requestMessage.Content = new StringContent(
+                @"{""sent_at"":""2021-01-01T00:00:00.000Z"",""sdk"":{""name"":""sentry.javascript.browser"",""version"":""6.8.0""},""dsn"":""https://dns@sentry.mywebsite.com/1""}
 {""type"":""session""}
 {""sid"":""fda00e933162466c849962eaea0cfaff""}");
-                var responseMessage = await _server.CreateClient().SendAsync(requestMessage);
+            var responseMessage = await _server.CreateClient().SendAsync(requestMessage);
 
-                Assert.Equal(1, _httpMessageHandler.NumberOfCalls);
-            }
+            Assert.Equal(1, _httpMessageHandler.NumberOfCalls);
         }
     }
 }
