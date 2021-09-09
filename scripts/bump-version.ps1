30d400372d7214a538bda33058ca05c58a9abfdc
Bruno Garcia <bruno@brunogarcia.com>
2021-09-09 16:39:01 -0400

    bump version cross platform

---
+++ scripts/bump-version.ps1
@@ -0,0 +1,13 @@
+param([string] $newVersion)
+
+$utf8NoBomEncoding = New-Object System.Text.UTF8Encoding($False) 
+function Replace-TextInFile {
+    param([string] $filePath, [string] $pattern, [string] $replacement)
+
+    $content = [IO.File]::ReadAllText($filePath)
+    $content = [Text.RegularExpressions.Regex]::Replace($content, $pattern, $replacement)
+    [IO.File]::WriteAllText($filePath, $content, $utf8NoBomEncoding)
+}
+
+# Version of .NET assemblies:
+Replace-TextInFile "$PSScriptRoot/../Directory.Build.props" '(?<=<Version>)(.*?)(?=</Version>)' $newVersion
