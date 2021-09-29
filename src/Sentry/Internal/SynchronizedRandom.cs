1f8926eeab25d67601b922d71fa807cda3c97c3f
Bruno Garcia <bruno@brunogarcia.com>
2021-09-29 18:45:27 -0400

    random

--- src/Sentry/Internal/SynchronizedRandom.cs
+++ src/Sentry/Internal/SynchronizedRandom.cs
@@ -12,15 +12,43 @@ namespace Sentry.Internal
         };
 
 #if NET6_0_OR_GREATER
+        public static int Next(int minValue, int maxValue) => Random.Shared.Next(minValue, maxValue);
+        public static int Next() => Random.Shared.Next();
         public static double NextDouble() => Random.Shared.NextDouble();
+        public static void NextBytes(byte[] bytes) => Random.Shared.NextBytes(bytes);
 #else
+        // TODO: ThreadLocal instance would avoid contention
         private static readonly Random Random = new();
+
+        public static void NextBytes(byte[] bytes)
+        {
+            lock (Random)
+            {
+                Random.NextBytes(bytes);
+            }
+        }
 
         public static double NextDouble()
         {
             lock (Random)
             {
                 return Random.NextDouble();
+            }
+        }
+
+        public static int Next(int minValue, int maxValue)
+        {
+            lock (Random)
+            {
+                return Random.Next(minValue, maxValue);
+            }
+        }
+
+        public static int Next()
+        {
+            lock (Random)
+            {
+                return Random.Next();
             }
         }
 #endif
