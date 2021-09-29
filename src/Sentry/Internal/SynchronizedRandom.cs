bc8d206c7590d7bd642ebda7d881c278354d8c31
Bruno Garcia <bruno@brunogarcia.com>
2021-09-29 17:34:40 -0400

    shared random

--- src/Sentry/Internal/SynchronizedRandom.cs
+++ src/Sentry/Internal/SynchronizedRandom.cs
@@ -4,6 +4,16 @@ namespace Sentry.Internal
 {
     internal static class SynchronizedRandom
     {
+        public static bool NextBool(double rate) => rate switch
+        {
+            >= 1 => true,
+            <= 0 => false,
+            _ => NextDouble() < rate
+        };
+
+#if NET6_0_OR_GREATER
+        public static double NextDouble() => Random.Shared.NextDouble();
+#else
         private static readonly Random Random = new();
 
         public static double NextDouble()
@@ -13,12 +23,6 @@ namespace Sentry.Internal
                 return Random.NextDouble();
             }
         }
-
-        public static bool NextBool(double rate) => rate switch
-        {
-            >= 1 => true,
-            <= 0 => false,
-            _ => NextDouble() < rate
-        };
+#endif
     }
 }
