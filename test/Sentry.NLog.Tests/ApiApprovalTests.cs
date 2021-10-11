07a3677e933993df54eb990ffc45f8db9a78f0d3
Simon Cropp <simon.cropp@gmail.com>
2021-10-12 09:35:11 +1100

    Use api snapshot to verify api changes (#1248)

---
+++ test/Sentry.NLog.Tests/ApiApprovalTests.cs
@@ -0,0 +1,17 @@
+using System.Threading.Tasks;
+using Sentry.Tests;
+using VerifyXunit;
+using Xunit;
+
+namespace Sentry.NLog.Tests
+{
+    [UsesVerify]
+    public class ApiApprovalTests
+    {
+        [Fact]
+        public Task Run()
+        {
+            return typeof(SentryTarget).Assembly.CheckApproval();
+        }
+    }
+}
