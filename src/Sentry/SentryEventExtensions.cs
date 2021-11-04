cfb2519fe4cb259c43d137e6157bbbb5feb71e5a
Lucas Zimerman Fraulob <lucas-zimerman1@hotmail.com>
2021-11-04 14:47:25 -0300

    rollback Hub CaptureEvent, removed `A` and implemented close unfinished Spans.

--- src/Sentry/SentryEventExtensions.cs
+++ src/Sentry/SentryEventExtensions.cs
@@ -3,14 +3,14 @@ using System.Linq;
 namespace Sentry
 {
     /// <summary>
-    /// Extension methods for <see cref="SentryEvent"/>A
+    /// Extension methods for <see cref="SentryEvent"/>
     /// </summary>
     public static class SentryEventExtensions
     {
         internal static bool IsErrored(this SentryEvent @event)
             => @event.Level >= SentryLevel.Error ||
                @event.Exception is not null ||
-               @event.SentryExceptions?.Any() == true;
+               @event.HasException?.Any() == true;
 
     }
 }
