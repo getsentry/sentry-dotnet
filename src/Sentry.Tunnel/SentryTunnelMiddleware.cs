b1817e8c9f404faf70ef9ff2e0f9c0a24ea2828f
SimonCropp <simon.cropp@gmail.com>
2021-11-09 22:11:10 +1100

    Update SentryTunnelMiddleware.cs

--- src/Sentry.Tunnel/SentryTunnelMiddleware.cs
+++ src/Sentry.Tunnel/SentryTunnelMiddleware.cs
@@ -1,12 +1,7 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Reflection;
 using System.Text.Json;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 
