203b0bb2a2b7a91f067f356c340e32d2ba327248
Simon Cropp <simon.cropp@gmail.com>
2021-12-16 12:28:36 +1100

    android solution filters

---
+++ buildMac.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+
+if [ "$GITHUB_ACTIONS" == "true" ]
+    then
+        testLogger="GitHubActions;report-warnings=false"
+    else
+        testLogger="console"
+fi
+
+dotnet test Sentry_Mac.slnf -c Release -l $testLogger \
+    /p:CollectCoverage=true \
+    /p:CoverletOutputFormat=opencover \
+    /p:Exclude=\"[Sentry.Protocol.Test*]*,[xunit.*]*,[Sentry.Test*]*\" \
+    /p:UseSourceLink=true
