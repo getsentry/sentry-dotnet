c72ec9d6b1fea3e7410dde5befd8e86872e5f728
Simon Cropp <simon.cropp@gmail.com>
2022-01-05 19:59:36 +1100

    Update HttpContextBuilder.cs

--- test/Sentry.AspNet.Tests/HttpContextBuilder.cs
+++ test/Sentry.AspNet.Tests/HttpContextBuilder.cs
@@ -4,7 +4,7 @@ public static class HttpContextBuilder
 {
     public static HttpContext Build(int responseStatusCode = 200)
     {
-        return new HttpContext(new HttpRequest("test", "http://test/the/path", null), new HttpResponse(new StringWriter())
+        return new HttpContext(new HttpRequest("test", "http://test/the/path", null), new HttpResponse(TextWriter.Null)
         {
             StatusCode = responseStatusCode
         });
