6d09af1cb710d4e8e61ca7f793b8be3cf9526f59
Simon Cropp <simon.cropp@gmail.com>
2022-01-06 12:25:04 +1100

    android solution filters (#1396)
    Co-authored-by: Bruno Garcia <bruno@brunogarcia.com>

--- build.sh
+++ build.sh
@@ -8,7 +8,13 @@ if [ "$GITHUB_ACTIONS" == "true" ]
         testLogger="console"
 fi
 
-dotnet test -c Release -l $testLogger \
+case "$OSTYPE" in
+  darwin*)  export Filter=SentryMac.slnf ;;
+  linux*)   export Filter=SentryLinux.slnf ;;
+  *)        echo Unsupported $OSTYPE && exit -1 ;;
+esac
+
+dotnet test $Filter -c Release -l $testLogger \
     /p:CollectCoverage=true \
     /p:CoverletOutputFormat=opencover \
     /p:Exclude=\"[Sentry.Protocol.Test*]*,[xunit.*]*,[Sentry.Test*]*\" \
