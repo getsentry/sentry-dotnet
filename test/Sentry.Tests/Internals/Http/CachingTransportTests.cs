ae31a8b908dd7ad0ede8619d60ea7ae108bce877
Simon Cropp <simon.cropp@gmail.com>
2022-01-14 08:14:16 +1100

    Update CachingTransportTests.cs

--- test/Sentry.Tests/Internals/Http/CachingTransportTests.cs
+++ test/Sentry.Tests/Internals/Http/CachingTransportTests.cs
@@ -35,9 +35,7 @@ public class CachingTransportTests
         await transport.SendEnvelopeAsync(envelope);
 
         // Wait until directory is empty
-        while (
-            Directory.Exists(cacheDirectory.Path) &&
-            Directory.EnumerateFiles(cacheDirectory.Path, "*", SearchOption.AllDirectories).Any())
+        while (Directory.EnumerateFiles(cacheDirectory.Path, "*", SearchOption.AllDirectories).Any())
         {
             await Task.Delay(100);
         }
@@ -228,9 +226,7 @@ public class CachingTransportTests
         // Act
 
         // Wait until directory is empty
-        while (
-            Directory.Exists(cacheDirectory.Path) &&
-            Directory.EnumerateFiles(cacheDirectory.Path, "*", SearchOption.AllDirectories).Any())
+        while (Directory.EnumerateFiles(cacheDirectory.Path, "*", SearchOption.AllDirectories).Any())
         {
             await Task.Delay(100);
         }
