e9e4f011d84be48c512aca721adff1f6645f390e
Simon Cropp <simon.cropp@gmail.com>
2022-02-04 08:27:27 +1100

    make IDE1006 suppression in benchmarks scoped to module

--- benchmarks/Sentry.Benchmarks/Properties/GlobalSuppressions.cs
+++ benchmarks/Sentry.Benchmarks/Properties/GlobalSuppressions.cs
@@ -1,9 +1,3 @@
+using System.Diagnostics.CodeAnalysis;
 
-// This file is used by Code Analysis to maintain SuppressMessage
-// attributes that are applied to this project.
-// Project-level suppressions either have no target or are given
-// a specific target and scoped to a namespace, type, member, etc.
-
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "benchmarks", Scope = "member", Target = "~M:Sentry.Benchmarks.RetryAfterHandlerOverhead.With_RetryAfterHandler_429Response~System.Threading.Tasks.Task")]
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "benchmarks", Scope = "member", Target = "~M:Sentry.Benchmarks.RetryAfterHandlerOverhead.Without_RetryAfterHandler~System.Threading.Tasks.Task")]
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "benchmarks", Scope = "member", Target = "~M:Sentry.Benchmarks.RetryAfterHandlerOverhead.With_RetryAfterHandler_OkResponse~System.Threading.Tasks.Task")]
+[assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "benchmarks", Scope = "module")]
