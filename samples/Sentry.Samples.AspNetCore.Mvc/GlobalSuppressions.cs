291a4d02fe0ae908bef61048bf1a923acb61ef33
Simon Cropp <simon.cropp@gmail.com>
2022-02-04 08:35:17 +1100

    IDE0076	Invalid or missing target for 'SuppressMessageAttribute

--- samples/Sentry.Samples.AspNetCore.Mvc/GlobalSuppressions.cs
+++
@@ -1,3 +0,0 @@
-using System.Diagnostics.CodeAnalysis;
-
-[assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "<Pending>", Scope = "member", Target = "~M:Sentry.Samples.AspNetCore.Mvc.Controllers.HomeController.PostIndex(System.String)~System.Threading.Tasks.Task")]
--- samples/Sentry.Samples.Console.Customized/Properties/GlobalSuppressions.cs
+++
@@ -1,4 +0,0 @@
-using System.Diagnostics.CodeAnalysis;
-
-[assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "App entrypoint", Scope = "member", Target = "~M:Program.Main(System.String[])~System.Threading.Tasks.Task")]
-
