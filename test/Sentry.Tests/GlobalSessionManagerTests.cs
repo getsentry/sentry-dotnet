72958452a65163ffe26f416eaa7379349ffe456f
Simon Cropp <simon.cropp@gmail.com>
2022-02-05 11:41:33 +1100

    Update GlobalSessionManagerTests.cs

--- test/Sentry.Tests/GlobalSessionManagerTests.cs
+++ test/Sentry.Tests/GlobalSessionManagerTests.cs
@@ -303,11 +303,11 @@ public class GlobalSessionManagerTests
         var sessionUpdate = sut.StartSession();
 
         // Act
-        var persistedSessionUpdate = sut.TryRecoverPersistedSession();
+        var persistedSessionUpdate = sut.TryRecoverPersistedSession()!;
 
         // Assert
         sessionUpdate.Should().NotBeNull();
-        persistedSessionUpdate!.EndStatus.Should().Be(SessionEndStatus.Abnormal);
+        persistedSessionUpdate.EndStatus.Should().Be(SessionEndStatus.Abnormal);
         persistedSessionUpdate.Should().NotBeNull();
         persistedSessionUpdate.Should().BeEquivalentTo(sessionUpdate, o =>
         {
@@ -319,10 +319,10 @@ public class GlobalSessionManagerTests
 
             return o;
         });
-        persistedSessionUpdate!.IsInitial.Should().BeFalse();
-        persistedSessionUpdate!.Timestamp.Should().BeAfter(sessionUpdate!.Timestamp);
-        persistedSessionUpdate!.Duration.Should().BeGreaterThan(sessionUpdate!.Duration);
-        persistedSessionUpdate!.SequenceNumber.Should().Be(sessionUpdate!.SequenceNumber + 1);
+        persistedSessionUpdate.IsInitial.Should().BeFalse();
+        persistedSessionUpdate.Timestamp.Should().BeAfter(sessionUpdate!.Timestamp);
+        persistedSessionUpdate.Duration.Should().BeGreaterThan(sessionUpdate!.Duration);
+        persistedSessionUpdate.SequenceNumber.Should().Be(sessionUpdate!.SequenceNumber + 1);
     }
 
     [Fact]
