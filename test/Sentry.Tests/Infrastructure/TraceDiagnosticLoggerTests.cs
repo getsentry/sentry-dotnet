ee409bf8843c4a3633ec0231eff942eca367a27a
Simon Cropp <simon.cropp@gmail.com>
2022-02-07 16:58:17 +1100

    use Trace.WriteLine for TraceDiagnosticLogger

---
+++ test/Sentry.Tests/Infrastructure/TraceDiagnosticLoggerTests.cs
@@ -0,0 +1,40 @@
+using System.Diagnostics;
+using Trace=System.Diagnostics.Trace;
+
+[UsesVerify]
+public class TraceDiagnosticLoggerTests
+{
+    [Fact]
+    public void CanTrace()
+    {
+        var listener = new Listener();
+        Trace.Listeners.Add(listener);
+        try
+        {
+            var logger = new TraceDiagnosticLogger(SentryLevel.Debug);
+            logger.Log(SentryLevel.Debug, "the message {0}", new Exception("the exception"), "arg1");
+            Trace.Flush();
+            Assert.Equal(@"  Debug: the message arg1
+System.Exception: the exception",listener.Messages.Single());
+        }
+        finally
+        {
+            Trace.Listeners.Remove(listener);
+        }
+    }
+
+    class Listener : TraceListener
+    {
+        public List<string> Messages = new();
+
+        public override void Write(string message)
+        {
+            Messages.Add(message);
+        }
+
+        public override void WriteLine(string message)
+        {
+            Messages.Add(message);
+        }
+    }
+}
