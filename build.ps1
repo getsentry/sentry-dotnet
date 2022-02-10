4dbbe43cbc1368c205571a226eebec91d68e94c8
Simon Cropp <simon.cropp@gmail.com>
2022-02-11 06:54:51 +1100

    try no samples in ci (#1478)

--- build.ps1
+++ build.ps1
@@ -2,11 +2,11 @@ $ErrorActionPreference = "Stop"
 
 $testLogger = if ($env:GITHUB_ACTIONS -eq "true") {"GitHubActions;report-warnings=false"} else {"console"}
 
-dotnet test -c Release -l $testLogger `
+dotnet test SentryNoSamples.slnf -c Release -l $testLogger `
     /p:CollectCoverage=true `
     /p:CoverletOutputFormat=opencover `
     /p:Exclude='"""[Sentry.Protocol.Test*]*,[xunit.*]*,[Sentry.Test*]*\"""'
 if ($LASTEXITCODE -ne 0) { exit 1 }
 
-dotnet pack -c Release /p:ContinuousIntegrationBuild=true
+dotnet pack SentryNoSamples.slnf -c Release /p:ContinuousIntegrationBuild=true
 if ($LASTEXITCODE -ne 0) { exit 1 }
