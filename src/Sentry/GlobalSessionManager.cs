4dac7d7df5981dda28d46f096e1e34b414faaf4b
Bruno Garcia <bruno@brunogarcia.com>
2022-02-14 10:53:47 -0500

    fix test

--- src/Sentry/GlobalSessionManager.cs
+++ src/Sentry/GlobalSessionManager.cs
@@ -305,16 +305,18 @@ namespace Sentry
             {
                 // Can't use exception filters because of a Unity 2019.4.35f IL2CPP bug
                 // https://github.com/getsentry/sentry-unity/issues/550
-                if (ex is FileNotFoundException or DirectoryNotFoundException or IOException)
+                if (ex is FileNotFoundException or DirectoryNotFoundException)
                 {
                     // Not a notable error
                     _options.LogDebug("A persisted session does not exist ({0}) at {1}.", ex.GetType().Name, filePath);
-                    return null;
                 }
-                _options.LogError(
-                    "Failed to recover persisted session from the file system '{0}'.",
-                    ex,
-                    filePath);
+                else
+                {
+                    _options.LogError(
+                        "Failed to recover persisted session from the file system '{0}'.",
+                        ex,
+                        filePath);
+                }
 
                 return null;
             }
