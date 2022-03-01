0090e44741e03956eb89ac3bf3b24d83e49b9d11
Simon Cropp <simon.cropp@gmail.com>
2022-03-01 12:02:59 +1100

    Update SentryDiagnosticListenerTests.cs

--- test/Sentry.DiagnosticSource.Tests/Integration/SQLite/SentryDiagnosticListenerTests.cs
+++ test/Sentry.DiagnosticSource.Tests/Integration/SQLite/SentryDiagnosticListenerTests.cs
@@ -6,7 +6,6 @@ using Sentry.Internals.DiagnosticSource;
 
 namespace Sentry.DiagnosticSource.Tests.Integration.SQLite
 {
-    [UsesVerify]
     public class SentryDiagnosticListenerTests
     {
         private class Fixture
@@ -140,11 +139,13 @@ namespace Sentry.DiagnosticSource.Tests.Integration.SQLite
             var itemsList = new ConcurrentBag<List<Item>>();
 
             // Act
-            foreach (var limit in commands)
+            var tasks = commands.Select(async limit =>
             {
                 var command = $"SELECT * FROM Items LIMIT {limit}";
                 itemsList.Add(await _fixture.NewContext().Items.FromSqlRaw(command).ToListAsync());
-            }
+            });
+
+            await Task.WhenAll(tasks);
 
             // Assert
             Assert.Equal(totalCommands, itemsList.Count);
@@ -170,14 +171,17 @@ namespace Sentry.DiagnosticSource.Tests.Integration.SQLite
             var spans = transaction.Spans;
 
             // Act
-            var dbSet = context.Items;
-            var result1 = await dbSet.FromSqlRaw("SELECT * FROM Items WHERE 1=1 LIMIT 10").ToListAsync();
-            await dbSet.FromSqlRaw("SELECT * FROM Items WHERE 1=1 LIMIT 9").ToListAsync();
-            await dbSet.FromSqlRaw("SELECT * FROM Items WHERE 1=1 LIMIT 8").ToListAsync();
-            await dbSet.FromSqlRaw("SELECT * FROM Items WHERE 1=1 LIMIT 7").ToListAsync();
+            var result = new[]
+            {
+                context.Items.FromSqlRaw("SELECT * FROM Items WHERE 1=1 LIMIT 10").ToListAsync(),
+                context.Items.FromSqlRaw("SELECT * FROM Items WHERE 1=1 LIMIT 9").ToListAsync(),
+                context.Items.FromSqlRaw("SELECT * FROM Items WHERE 1=1 LIMIT 8").ToListAsync(),
+                context.Items.FromSqlRaw("SELECT * FROM Items WHERE 1=1 LIMIT 7").ToListAsync(),
+            };
+            await Task.WhenAll(result);
 
             // Assert
-            Assert.Equal(3, result1.Count);
+            Assert.Equal(3, result[0].Result.Count);
             Assert.Equal(4, spans.Count(s => s.Operation == "db.query"));
 #if NET5_0_OR_GREATER
             Assert.Equal(4, spans.Count(s => s.Operation == "db.query_compiler"));
