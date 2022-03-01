b9095d4d7a045dd93094a33553cc36a196aef6aa
Simon Cropp <simon.cropp@gmail.com>
2022-03-02 06:55:19 +1100

    CopyLocalLockFileAssemblies for coverlet (#1505)

--- build.ps1
+++ build.ps1
@@ -5,6 +5,7 @@ $testLogger = if ($env:GITHUB_ACTIONS -eq "true") {"GitHubActions;report-warning
 dotnet test SentryNoSamples.slnf -c Release -l $testLogger `
     /p:CollectCoverage=true `
     /p:CoverletOutputFormat=opencover `
+    /p:CopyLocalLockFileAssemblies=true `
     /p:Exclude='"""[Sentry.Protocol.Test*]*,[xunit.*]*,[Sentry.Test*]*\"""'
 if ($LASTEXITCODE -ne 0) { exit 1 }
 
