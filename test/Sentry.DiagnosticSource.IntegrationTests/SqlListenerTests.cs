c661e0e83e5573e2bfae7663b6533c3e738f391c
Simon Cropp <simon.cropp@gmail.com>
2022-03-10 06:17:21 +1100

    add ef integration test

--- test/Sentry.DiagnosticSource.IntegrationTests/SqlListenerTests.cs
+++ test/Sentry.DiagnosticSource.IntegrationTests/SqlListenerTests.cs
@@ -9,6 +9,7 @@ public class SqlListenerTests
 
     static SqlListenerTests()
     {
+        Skip.If(!RuntimeInformation.IsOSPlatform(OSPlatform.Windows));
         sqlInstance = new SqlInstance(
             name: "SqlListenerTests" + Namer.RuntimeAndVersion,
             buildTemplate: TestDbBuilder.CreateTable);
@@ -17,8 +18,6 @@ public class SqlListenerTests
     [SkippableFact]
     public async Task RecordsSql()
     {
-        Skip.If(!RuntimeInformation.IsOSPlatform(OSPlatform.Windows));
-
         var transport = new RecordingTransport();
         var options = new SentryOptions
         {
@@ -45,19 +44,41 @@ public class SqlListenerTests
             .SelectMany(x => x.Items)
             .Select(x => x.Payload)
             .ToList();
+
         await Verify(payloads)
-            .ModifySerialization(
-                p =>
-                {
-                    p.IgnoreMembersWithType<Contexts>();
-                    p.IgnoreMembersWithType<SdkVersion>();
-                    p.IgnoreMembersWithType<DateTimeOffset>();
-                    p.IgnoreMembersWithType<SpanId>();
-                    p.IgnoreMembersWithType<SentryId>();
-                    p.IgnoreMembers<SentryEvent>(e => e.Modules, e => e.Release);
-                    p.IgnoreMembers<Transaction>(t => t.Release);
-                    p.IgnoreMembers<SentryException>(e => e.Module, e => e.ThreadId);
-                });
+            .IgnoreStandardSentryMembers();
     }
 
+    [SkippableFact]
+    public async Task RecordsEfAsync()
+    {
+        var transport = new RecordingTransport();
+        var options = new SentryOptions
+        {
+            TracesSampleRate = 1,
+            Transport = transport,
+            Dsn = DsnSamples.ValidDsnWithoutSecret,
+            DiagnosticLevel = SentryLevel.Debug
+        };
+
+        options.AddIntegration(new SentryDiagnosticListenerIntegration());
+
+        using var database = await sqlInstance.Build();
+        using (var hub = new Hub(options))
+        {
+            var transaction = hub.StartTransaction("my transaction", "my operation");
+            hub.ConfigureScope(scope => scope.Transaction = transaction);
+            hub.CaptureException(new Exception("my exception"));
+            await TestDbBuilder.AddEfData(database);
+            await TestDbBuilder.GetEfData(database);
+            transaction.Finish();
+        }
+
+        var payloads = transport.Envelopes
+            .SelectMany(x => x.Items)
+            .Select(x => x.Payload)
+            .ToList();
+        await Verify(payloads)
+            .IgnoreStandardSentryMembers();
+    }
 }
---
+++ test/Sentry.DiagnosticSource.IntegrationTests/VerifyExtensions.cs
@@ -0,0 +1,18 @@
+﻿static class VerifyExtensions{
+
+    public static SettingsTask IgnoreStandardSentryMembers(this SettingsTask settings)
+    {
+        return settings .ModifySerialization(
+            p =>
+            {
+                p.IgnoreMembersWithType<Contexts>();
+                p.IgnoreMembersWithType<SdkVersion>();
+                p.IgnoreMembersWithType<DateTimeOffset>();
+                p.IgnoreMembersWithType<SpanId>();
+                p.IgnoreMembersWithType<SentryId>();
+                p.IgnoreMembers<SentryEvent>(e => e.Modules, e => e.Release);
+                p.IgnoreMembers<Transaction>(t => t.Release);
+                p.IgnoreMembers<SentryException>(e => e.Module, e => e.ThreadId);
+            });
+    }
+}
