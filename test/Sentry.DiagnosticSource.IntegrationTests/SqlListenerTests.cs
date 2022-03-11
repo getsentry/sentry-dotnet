cb2f6dc2255be906dfdf56627f6a6ad8c9b58e61
Simon Cropp <simon.cropp@gmail.com>
2022-03-11 18:38:59 +1100

    Update SqlListenerTests.cs

--- test/Sentry.DiagnosticSource.IntegrationTests/SqlListenerTests.cs
+++ test/Sentry.DiagnosticSource.IntegrationTests/SqlListenerTests.cs
@@ -37,10 +37,6 @@ public class SqlListenerTests
             hub.ConfigureScope(scope => scope.Transaction = transaction);
             hub.CaptureException(new Exception("my exception"));
             await TestDbBuilder.AddData(database);
-            while (!transport.Envelopes.Any())
-            {
-                await Task.Delay(100);
-            }
             await TestDbBuilder.GetData(database);
             transaction.Finish();
         }
