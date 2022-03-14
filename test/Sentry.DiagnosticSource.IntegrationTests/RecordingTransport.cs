2cda47c23ca7dd307b95846f0feb89830ed81fca
Simon Cropp <simon.cropp@gmail.com>
2022-03-14 11:56:18 +1100

    Update RecordingTransport.cs

--- test/Sentry.DiagnosticSource.IntegrationTests/RecordingTransport.cs
+++ test/Sentry.DiagnosticSource.IntegrationTests/RecordingTransport.cs
@@ -1,15 +1,14 @@
-﻿internal class RecordingTransport : ITransport
+﻿using System.Collections.Concurrent;
+
+internal class RecordingTransport : ITransport
 {
-    private List<Envelope> _envelopes = new();
+    private ConcurrentBag<Envelope> _envelopes = new();
 
     public IEnumerable<Envelope> Envelopes => _envelopes;
 
     public Task SendEnvelopeAsync(Envelope envelope, CancellationToken cancellationToken = default)
     {
-        lock (_envelopes)
-        {
-            _envelopes.Add(envelope);
-        }
+        _envelopes.Add(envelope);
         return Task.CompletedTask;
     }
 
