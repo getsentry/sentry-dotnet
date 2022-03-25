8d76fe16e72a10df2f0c3250c319c06dc4046f5b
Ivan Dlugos <dlugos.ivan@gmail.com>
2022-03-25 11:34:05 +0100

    refactor: review changes

--- src/Sentry/PlatformAbstractions/RuntimeInfo.cs
+++ src/Sentry/PlatformAbstractions/RuntimeInfo.cs
@@ -102,20 +102,22 @@ namespace Sentry.PlatformAbstractions
 
         internal static Runtime? GetFromRuntimeInformation()
         {
+            string frameworkDescription;
             try
             {
                 // Preferred API: netstandard2.0
                 // https://github.com/dotnet/corefx/blob/master/src/System.Runtime.InteropServices.RuntimeInformation/src/System/Runtime/InteropServices/RuntimeInformation/RuntimeInformation.cs
                 // https://github.com/mono/mono/blob/90b49aa3aebb594e0409341f9dca63b74f9df52e/mcs/class/corlib/System.Runtime.InteropServices.RuntimeInformation/RuntimeInformation.cs
                 // e.g: .NET Framework 4.7.2633.0, .NET Native, WebAssembly
-                var frameworkDescription = RuntimeInformation.FrameworkDescription;
-
-                return Parse(frameworkDescription);
+                // Note: this throws on some Unity IL2CPP versions
+                frameworkDescription = RuntimeInformation.FrameworkDescription;
             }
             catch
             {
                 return null;
             }
+
+            return Parse(frameworkDescription);
         }
 
         internal static Runtime? GetFromMonoRuntime()
