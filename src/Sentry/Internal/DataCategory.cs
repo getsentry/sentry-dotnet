6059f5c0faa9d0b8cd1d6ec16f0e632547e2a1a5
Matt Johnson-Pint <matt.johnson-pint@sentry.io>
2022-04-08 19:06:04 -0700

    Sort enums

--- src/Sentry/Internal/DataCategory.cs
+++ src/Sentry/Internal/DataCategory.cs
@@ -2,13 +2,13 @@ namespace Sentry.Internal
 {
     internal record DataCategory : Enumeration
     {
+        public static DataCategory Attachment = new("attachment");
         public static DataCategory Default = new("default");
         public static DataCategory Error = new("error");
-        public static DataCategory Transaction = new("transaction");
-        public static DataCategory Security = new("security");
-        public static DataCategory Attachment = new("attachment");
-        public static DataCategory Session = new("session");
         public static DataCategory Internal = new("internal");
+        public static DataCategory Security = new("security");
+        public static DataCategory Session = new("session");
+        public static DataCategory Transaction = new("transaction");
 
         private DataCategory(string value) : base(value)
         {
