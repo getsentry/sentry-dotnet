e61eb12455ae09480907e3a8b37fc333b9982c02
Simon Cropp <simon.cropp@gmail.com>
2022-04-21 09:05:14 +1000

    .

--- build.ps1
+++ build.ps1
@@ -1,8 +1,7 @@
 $ErrorActionPreference = "Stop"
 
-$testLogger = if ($env:GITHUB_ACTIONS -eq "true") {"GitHubActions;report-warnings=false"} else {"console"}
-
-dotnet test SentryNoSamples.slnf -c Release -l $testLogger `
+dotnet test SentryNoSamples.slnf `
+    -c Release `
     /p:CopyLocalLockFileAssemblies=true `
     /p:Exclude='"""[Sentry.Protocol.Test*]*,[xunit.*]*,[System.*]*,[Microsoft.*]*,[Sentry.Test*]*\"""'
 if ($LASTEXITCODE -ne 0) { exit 1 }
--- build.sh
+++ build.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
 set -e
 
-if [ "$GITHUB_ACTIONS" == "true" ]
-    then
-        testLogger="GitHubActions;report-warnings=false"
-    else
-        testLogger="console"
-fi
-
-dotnet test SentryNoSamples.slnf -c Release -l $testLogger \
+dotnet test SentryNoSamples.slnf \
+    -c Release \
     /p:CopyLocalLockFileAssemblies=true \
     /p:Exclude=\"[Sentry.Protocol.Test*]*,[xunit.*]*,[System.*]*,[Microsoft.*]*,[Sentry.Test*]*\" \
     /p:UseSourceLink=true
