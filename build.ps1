b8f07b3544394e4d8838ae19c15cb02c4a222e6f
Simon Cropp <simon.cropp@gmail.com>
2022-04-22 05:59:04 +1000

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -3,7 +3,7 @@ $ErrorActionPreference = "Stop"
 $testLogger = if ($env:GITHUB_ACTIONS -eq "true") {"GitHubActions;report-warnings=false"} else {"console"}
 
 dotnet test SentryNoSamples.slnf -c Release -l $testLogger `
-    /p:CopyLocalLockFileAssemblies=true `
+    /p:CopyLocalLockFileAssemblies=true
 if ($LASTEXITCODE -ne 0) { exit 1 }
 
 dotnet pack SentryNoSamples.slnf -c Release /p:ContinuousIntegrationBuild=true
