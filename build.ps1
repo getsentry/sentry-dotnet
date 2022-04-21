6ae750b9327a2a98cfb72fc7f522ecc455ddf8d8
Simon Cropp <simon.cropp@gmail.com>
2022-04-22 05:47:13 +1000

    remove code coverage exclude from build

--- build.ps1
+++ build.ps1
@@ -4,7 +4,6 @@ $testLogger = if ($env:GITHUB_ACTIONS -eq "true") {"GitHubActions;report-warning
 
 dotnet test SentryNoSamples.slnf -c Release -l $testLogger `
     /p:CopyLocalLockFileAssemblies=true `
-    /p:Exclude='"""[Sentry.Protocol.Test*]*,[xunit.*]*,[System.*]*,[Microsoft.*]*,[Sentry.Test*]*\"""'
 if ($LASTEXITCODE -ne 0) { exit 1 }
 
 dotnet pack SentryNoSamples.slnf -c Release /p:ContinuousIntegrationBuild=true
