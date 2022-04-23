def623108063996cd86facb3bef43998cb2b8867
Simon Cropp <simon.cropp@gmail.com>
2022-04-23 14:57:22 +1000

    move to explicit yml for build (#1582)

--- build.ps1
+++ build.ps1
@@ -1,10 +1,8 @@
 $ErrorActionPreference = "Stop"
 
-$testLogger = if ($env:GITHUB_ACTIONS -eq "true") {"GitHubActions;report-warnings=false"} else {"console"}
-
-dotnet test SentryNoSamples.slnf -c Release -l $testLogger `
+dotnet test SentryNoSamples.slnf -c Release -l "GitHubActions;report-warnings=false" `
     /p:CopyLocalLockFileAssemblies=true
 if ($LASTEXITCODE -ne 0) { exit 1 }
 
-dotnet pack SentryNoSamples.slnf -c Release /p:ContinuousIntegrationBuild=true
+dotnet pack SentryNoSamples.slnf -c Release --no-build /p:ContinuousIntegrationBuild=true
 if ($LASTEXITCODE -ne 0) { exit 1 }
--- build.sh
+++ build.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 set -e
 
-if [ "$GITHUB_ACTIONS" == "true" ]
-    then
-        testLogger="GitHubActions;report-warnings=false"
-    else
-        testLogger="console"
-fi
-
-dotnet test SentryNoSamples.slnf -c Release -l $testLogger \
-    /p:CopyLocalLockFileAssemblies=true \
-    /p:UseSourceLink=true
+dotnet test SentryNoSamples.slnf \
+    -c Release \
+    /p:CopyLocalLockFileAssemblies=true
