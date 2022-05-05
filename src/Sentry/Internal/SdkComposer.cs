4671aa77d3d66aa0e816be8b4c79c5bd220fc5e7
Matt Johnson-Pint <matt.johnson-pint@sentry.io>
2022-05-05 16:51:13 -0700

    Remove old BlockCacheFlush

--- src/Sentry/Internal/SdkComposer.cs
+++ src/Sentry/Internal/SdkComposer.cs
@@ -26,9 +26,7 @@ namespace Sentry.Internal
             // When a cache directory path is given, wrap the transport in a caching transport.
             if (!string.IsNullOrWhiteSpace(_options.CacheDirectoryPath))
             {
-                var cachingTransport = CachingTransport.Create(transport, _options);
-                BlockCacheFlush(cachingTransport);
-                transport = cachingTransport;
+                transport = CachingTransport.Create(transport, _options);
             }
 
             // Always persist the transport on the options, so other places can pick it up where necessary.
@@ -50,53 +48,6 @@ namespace Sentry.Internal
             var httpClient = httpClientFactory.Create(_options);
 
             return new HttpTransport(_options, httpClient);
-        }
-
-        internal void BlockCacheFlush(CachingTransport transport)
-        {
-            // If configured, flush existing cache
-            if (_options.InitCacheFlushTimeout > TimeSpan.Zero)
-            {
-                _options.LogDebug(
-                    "Flushing existing cache during transport activation up to {0}.",
-                    _options.InitCacheFlushTimeout);
-
-                try
-                {
-                    // Flush cache but block on it only for a limited amount of time.
-                    // If we don't flush it in time, then let it continue to run on the
-                    // background but don't block the calling thread any more than set timeout.
-#if NET6_0_OR_GREATER
-                    transport.FlushAsync().WaitAsync(_options.InitCacheFlushTimeout)
-                        // Block calling thread (Init) until either Flush or Timeout is reached
-                        .GetAwaiter().GetResult();
-#else
-                    var timeoutTask = Task.Delay(_options.InitCacheFlushTimeout);
-                    var flushTask = transport.FlushAsync();
-
-                    // If flush finished in time, finalize the task by awaiting it to
-                    // propagate potential exceptions.
-                    if (Task.WhenAny(timeoutTask, flushTask).GetAwaiter().GetResult() == flushTask)
-                    {
-                        flushTask.GetAwaiter().GetResult();
-                    }
-                    // If flush timed out, log and continue
-                    else
-                    {
-                        _options.LogInfo(
-                            "Cache flushing is taking longer than the configured timeout of {0}. " +
-                            "Continuing without waiting for the task to finish.",
-                            _options.InitCacheFlushTimeout);
-                    }
-#endif
-                }
-                catch (Exception ex)
-                {
-                    _options.LogError(
-                        "Cache flushing failed.",
-                        ex);
-                }
-            }
         }
 
         public IBackgroundWorker CreateBackgroundWorker()
