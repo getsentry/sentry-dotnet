b4739b3de46861a01bf029ddc88baaa4bbda362a
Simon Cropp <simon.cropp@gmail.com>
2022-05-06 15:46:08 +1000

    .

--- src/Sentry.AspNetCore/SentryWebHostBuilderExtensions.cs
+++ src/Sentry.AspNetCore/SentryWebHostBuilderExtensions.cs
@@ -1,9 +1,11 @@
 using System.ComponentModel;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Logging.Configuration;
 using Microsoft.Extensions.Options;
 using Sentry.AspNetCore;
+using Sentry.AspNetCore.Tunnel;
 
 // ReSharper disable once CheckNamespace
 namespace Microsoft.AspNetCore.Hosting;
@@ -95,4 +97,21 @@ public static class SentryWebHostBuilderExtensions
 
         return builder;
     }
+
+    /// <summary>
+    /// Adds and configures the Sentry tunneling middleware.
+    /// </summary>
+    /// <param name="services"></param>
+    /// <param name="hostnames">The extra hostnames to be allowed for the tunneling. sentry.io is allowed by default; add your own Sentry domain if you use a self-hosted Sentry or Relay.</param>
+    public static void AddSentryTunneling(this IServiceCollection services, params string[] hostnames) =>
+        services.AddScoped(_ => new SentryTunnelMiddleware(hostnames));
+
+    /// <summary>
+    /// Adds the <see cref="SentryTunnelMiddleware"/> to the pipeline.
+    /// </summary>
+    /// <param name="builder">The app builder.</param>
+    /// <param name="path">The path to listen for Sentry envelopes.</param>
+    public static void UseSentryTunneling(this IApplicationBuilder builder, string path = "/tunnel") =>
+        builder.Map(path, applicationBuilder =>
+            applicationBuilder.UseMiddleware<SentryTunnelMiddleware>());
 }
