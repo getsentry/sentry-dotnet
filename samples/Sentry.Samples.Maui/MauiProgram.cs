bc63f470acc10239a1ccadb73543af94506551af
Matt Johnson-Pint <matt.johnson-pint@sentry.io>
2022-05-19 16:07:22 -0400

    Update tests and sample

--- samples/Sentry.Samples.Maui/MauiProgram.cs
+++ samples/Sentry.Samples.Maui/MauiProgram.cs
@@ -2,18 +2,17 @@
 
 public static class MauiProgram
 {
-    public static MauiApp CreateMauiApp()
-    {
-        var builder = MauiApp.CreateBuilder();
-        builder
+    public static MauiApp CreateMauiApp() =>
+        MauiApp.CreateBuilder()
             .UseMauiApp<App>()
-            .UseSentry()
+            .UseSentry(options =>
+            {
+                options.Dsn = "https://eb18e953812b41c3aeb042e666fd3b5c@o447951.ingest.sentry.io/5428537";
+            })
             .ConfigureFonts(fonts =>
             {
                 fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                 fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-            });
-
-        return builder.Build();
-    }
+            })
+            .Build();
 }
