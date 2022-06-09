ead5f9a842324659c42696843b3b842d04bacab3
Simon Cropp <simon.cropp@gmail.com>
2022-06-10 07:28:39 +1000

    .

--- samples/Sentry.Samples.AspNetCore.Blazor.Server/Program.cs
+++ samples/Sentry.Samples.AspNetCore.Blazor.Server/Program.cs
@@ -1,31 +1,45 @@
-using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Web;
 using Sentry.Samples.AspNetCore.Blazor.Server.Data;
 
-var builder = WebApplication.CreateBuilder(args);
+// Capture blazor bootstrapping errors
+using var sdk = SentrySdk.Init(o =>
+{
+    o.Dsn = "https://eb18e953812b41c3aeb042e666fd3b5c@o447951.ingest.sentry.io/5428537";
+    o.Debug = true;
+});
+try
+{
+    var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddRazorPages();
-builder.Services.AddServerSideBlazor();
-builder.Services.AddSingleton<WeatherForecastService>();
+    builder.Services.AddRazorPages();
+    builder.Services.AddServerSideBlazor();
+    builder.Services.AddSingleton<WeatherForecastService>();
+    builder.WebHost.UseSentry();
 
-var app = builder.Build();
+    var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    if (!app.Environment.IsDevelopment())
+    {
+        app.UseExceptionHandler("/Error");
+        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+        app.UseHsts();
+    }
+
+    app.UseHttpsRedirection();
+
+    app.UseStaticFiles();
+
+    app.UseRouting();
+
+    app.MapBlazorHub();
+    app.MapFallbackToPage("/_Host");
+
+    app.Run();
 }
-
-app.UseHttpsRedirection();
-
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.MapBlazorHub();
-app.MapFallbackToPage("/_Host");
-
-app.Run();
+catch (Exception e)
+{
+    SentrySdk.CaptureException(e);
+    await SentrySdk.FlushAsync(TimeSpan.FromSeconds(2));
+    throw;
+}
