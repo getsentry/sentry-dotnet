2beaffbe8e4e83da143da379e71964d4cfbdd0ac
Matt Johnson-Pint <matt.johnson-pint@sentry.io>
2022-06-09 10:10:25 -0700

    Refactor and update Android options

---
+++ src/Sentry/Android/Extensions/BreadcrumbExtensions.cs
@@ -0,0 +1,39 @@
+namespace Sentry.Android.Extensions;
+
+internal static class BreadcrumbExtensions
+{
+    public static Breadcrumb ToBreadcrumb(this Java.Breadcrumb breadcrumb)
+    {
+        var data = breadcrumb.Data.ToDictionary(x => x.Key, x => x.Value?.ToString() ?? "");
+
+        return new(breadcrumb.Timestamp.ToDateTimeOffset(),
+            breadcrumb.Message,
+            breadcrumb.Type,
+            data,
+            breadcrumb.Category,
+            breadcrumb.Level?.ToBreadcrumbLevel() ?? default);
+    }
+
+    public static Java.Breadcrumb ToJavaBreadcrumb(this Breadcrumb breadcrumb)
+    {
+        var javaBreadcrumb = new Java.Breadcrumb(breadcrumb.Timestamp.ToJavaDate())
+        {
+            Message = breadcrumb.Message,
+            Type = breadcrumb.Type,
+            Category = breadcrumb.Category,
+            Level = breadcrumb.Level.ToJavaSentryLevel()
+        };
+
+        if (breadcrumb.Data is { } data)
+        {
+            var javaData = javaBreadcrumb.Data;
+            foreach (var item in data)
+            {
+                var value = item.Value ?? "";
+                javaData.Add(item.Key, value!);
+            }
+        }
+
+        return javaBreadcrumb;
+    }
+}
---
+++ src/Sentry/Android/Extensions/SentryEventExtensions.cs
@@ -0,0 +1,46 @@
+using Sentry.Extensibility;
+
+namespace Sentry.Android.Extensions;
+
+internal static class SentryEventExtensions
+{
+    /*
+     * These methods map between a SentryEvent and it's Java counterpart by serializing as JSON into memory on one side,
+     * then deserializing back to an object on the other side.  It is not expected to be performant, as this code is only
+     * used when a BeforeSend option is set, and then only when an event is captured by the Java SDK (which should be
+     * relatively rare).
+     * 
+     * This approach avoids having to write to/from methods for the entire object graph.  However, it's also important to
+     * recognize that there's not necessarily a one-to-one mapping available on all objects (even through serialization)
+     * between the two SDKs, so some optional details may be lost when roundtripping.  That's generally OK, as this is
+     * still better than nothing.  If a specific part of the object graph becomes important to roundtrip, we can consider
+     * updating the objects on either side.
+     */
+
+    public static SentryEvent ToSentryEvent(this Java.SentryEvent sentryEvent, Java.SentryOptions javaOptions)
+    {
+        using var stream = new MemoryStream();
+        using var streamWriter = new JavaOutputStreamWriter(stream);
+        using var jsonWriter = new Java.JsonObjectWriter(streamWriter, javaOptions.MaxDepth);
+        sentryEvent.Serialize(jsonWriter, javaOptions.Logger);
+        jsonWriter.Flush();
+        stream.Seek(0, SeekOrigin.Begin);
+
+        using var json = JsonDocument.Parse(stream);
+        return SentryEvent.FromJson(json.RootElement, sentryEvent.Throwable);
+    }
+
+    public static Java.SentryEvent ToJavaSentryEvent(this SentryEvent sentryEvent, SentryOptions options, Java.SentryOptions javaOptions)
+    {
+        using var stream = new MemoryStream();
+        using var jsonWriter = new Utf8JsonWriter(stream);
+        sentryEvent.WriteTo(jsonWriter, options.DiagnosticLogger);
+        jsonWriter.Flush();
+        stream.Seek(0, SeekOrigin.Begin);
+
+        using var streamReader = new JavaInputStreamReader(stream);
+        using var jsonReader = new Java.JsonObjectReader(streamReader);
+        using var deserializer = new Java.SentryEvent.Deserializer();
+        return deserializer.Deserialize(jsonReader, javaOptions.Logger);
+    }
+}
