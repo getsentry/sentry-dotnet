2019d9b6ca4e041ef60309ae5f4a5c55d1f1cff7
Matt Johnson-Pint <matt.johnson-pint@sentry.io>
2022-06-14 15:21:21 -0700

    Fix unit test

--- src/Sentry.Maui/Internal/SentryMauiEventProcessor.cs
+++ src/Sentry.Maui/Internal/SentryMauiEventProcessor.cs
@@ -4,11 +4,18 @@ namespace Sentry.Maui.Internal;
 
 internal class SentryMauiEventProcessor : ISentryEventProcessor
 {
+    private readonly SentryMauiOptions _options;
+
+    public SentryMauiEventProcessor(SentryMauiOptions options)
+    {
+        _options = options;
+    }
+
     public SentryEvent Process(SentryEvent @event)
     {
         @event.Sdk.Name = Constants.SdkName;
         @event.Sdk.Version = Constants.SdkVersion;
-        @event.Contexts.Device.ApplyMauiDeviceData();
+        @event.Contexts.Device.ApplyMauiDeviceData(_options.DiagnosticLogger);
 
         return @event;
     }
