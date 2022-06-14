2457fb22a22aa894ba09dd36d94b5cbc4467dd58
Simon Cropp <simon.cropp@gmail.com>
2022-06-14 21:31:12 +1000

    .

--- test/Sentry.AspNetCore.Tests/VersioningTests.cs
+++ test/Sentry.AspNetCore.Tests/VersioningTests.cs
@@ -2,6 +2,8 @@
 #if NET6_0
 using System.Net.Http;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
 
 public class VersioningTests
 {
@@ -9,15 +11,29 @@ public class VersioningTests
     public async Task Foo()
     {
         var builder = WebApplication.CreateBuilder();
-        var app = builder.Build();
+       // builder.Services.AddControllers();
+        await using var app = builder.Build();
+      //  app.MapControllers();
 
-        app.MapGet("/", () => "Hello World!");
-
-        var task = app.StartAsync();
+        app.MapGet("/", () => "a");
+        var task = app.RunAsync();
 
         var httpClient = new HttpClient();
-        var message = await httpClient.GetAsync(app.Urls.First());
+        var uri = $"{app.Urls.First()}";
+        var message = await httpClient.GetStringAsync(uri);
+        Assert.Equal("Hello world",message);
         await app.StopAsync();
+    }
+
+    [ApiController]
+    [Route("Controller")]
+    public class MyController : ControllerBase
+    {
+        [HttpGet]
+        public string Method()
+        {
+            return "Hello world";
+        }
     }
 }
 
