da48075694590d04699d2a6941ee5ae6355be7c5
Bruno Garcia <bruno@brunogarcia.com>
2022-06-16 09:06:17 -0400

    sample and ios only code and msbuild props

---
+++ samples/Sentry.Samples.iOS/Main.cs
@@ -0,0 +1,13 @@
+using Sentry.Samples.iOS;
+
+SentrySdk.Init(this, o =>
+{
+    o.Debug = true;
+    o.Dsn = "https://eb18e953812b41c3aeb042e666fd3b5c@o447951.ingest.sentry.io/5428537";
+});
+
+
+// This is the main entry point of the application.
+// If you want to use a different Application Delegate class from "AppDelegate"
+// you can specify it here.
+UIApplication.Main(args, null, typeof (AppDelegate));
