6d6032dc8e3d2c5969420a10139d5c0fb159a8f2
Bruno Garcia <bruno@brunogarcia.com>
2022-06-18 12:58:34 -0400

    init with instance

--- src/Sentry/iOS/SentrySdk.cs
+++ src/Sentry/iOS/SentrySdk.cs
@@ -6,11 +6,17 @@ public static partial class SentrySdk
 {
     static partial void InitNative(SentryOptions options)
     {
-        SentrySDK.StartWithConfigureOptions(o =>
-        {
-            o.Dsn = options.Dsn;
-            o.Debug = options.Debug;
-        });
+        var o = new Sentry.iOS.SentryOptions();
+        o.Dsn = options.Dsn;
+        o.Debug = options.Debug;
+        SentrySDK.StartWithOptionsObject(o);
+
+        // Blocked not getting executed:
+        // SentrySDK.StartWithConfigureOptions(o =>
+        // {
+        //     o.Dsn = options.Dsn;
+        //     o.Debug = options.Debug;
+        // });
 
         SentrySDK.CaptureMessage("Testing binding to iOS");
     }
