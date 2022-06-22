7c43c6bc25218b9f8ec261304393d229d0f44f88
Simon Cropp <simon.cropp@gmail.com>
2022-06-22 21:07:55 +1000

    add AttributeReader

---
+++ src/Sentry/AttributeReader.cs
@@ -0,0 +1,29 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Reflection;
+
+namespace Sentry
+{
+    internal static class AttributeReader
+    {
+        public static bool TryGetProjectDirectory([NotNullWhen(true)] out string? projectDirectory) =>
+            TryGetProjectDirectory(Assembly.GetCallingAssembly(), out projectDirectory);
+
+        public static bool TryGetProjectDirectory(Assembly assembly, [NotNullWhen(true)] out string? projectDirectory) =>
+            TryGetValue(assembly, "Sentry.ProjectDirectory", out projectDirectory);
+
+        public static bool TryGetSolutionDirectory([NotNullWhen(true)] out string? solutionDirectory) =>
+            TryGetSolutionDirectory(Assembly.GetCallingAssembly(), out solutionDirectory);
+
+        public static bool TryGetSolutionDirectory(Assembly assembly, [NotNullWhen(true)] out string? solutionDirectory) =>
+            TryGetValue(assembly, "Sentry.SolutionDirectory", out solutionDirectory);
+
+        private static bool TryGetValue(Assembly assembly, string key, [NotNullWhen(true)] out string? value)
+        {
+            value = assembly.GetCustomAttributes<AssemblyMetadataAttribute>()
+                .SingleOrDefault(x => x.Key == key)
+                ?.Value;
+            return value != null;
+        }
+    }
+}
---
+++ test/Sentry.Tests/AttributeReaderTests.cs
@@ -0,0 +1,14 @@
+﻿using AttributeReader = Sentry.AttributeReader;
+
+public class AttributeReaderTests
+{
+    [Fact]
+    public void Simple()
+    {
+        var assembly = typeof(AttributeReaderTests).Assembly;
+        Assert.True(AttributeReader.TryGetProjectDirectory(assembly, out var projectDirectory));
+        Assert.NotNull(projectDirectory);
+        Assert.True(AttributeReader.TryGetSolutionDirectory(assembly, out var solutionDirectory));
+        Assert.NotNull(solutionDirectory);
+    }
+}
