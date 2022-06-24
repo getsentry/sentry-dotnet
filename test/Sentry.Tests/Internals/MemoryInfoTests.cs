5c894699150af6a0bd57e92601a504de736369ea
Matt Johnson-Pint <matt.johnson-pint@sentry.io>
2022-06-23 18:10:11 -0700

    Use Verify on MemoryInfo tests

--- test/Sentry.Tests/Internals/MemoryInfoTests.cs
+++ test/Sentry.Tests/Internals/MemoryInfoTests.cs
@@ -1,27 +1,24 @@
 #if NETCOREAPP3_1_OR_GREATER
-using System.Text.Json;
 
+namespace Sentry.Tests.Internals;
+
+[UsesVerify]
 public class MemoryInfoTests
 {
     [Fact]
-    public void WriteTo()
+    public Task WriteTo()
     {
 #if NET5_0_OR_GREATER
-        var info = new MemoryInfo(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, true, false, new[] { TimeSpan.FromSeconds(1) });
+        var info = new MemoryInfo(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, true, false, new[] {TimeSpan.FromSeconds(1)});
 #else
         var info = new MemoryInfo(1, 2, 3, 4, 5, 6);
 #endif
+        var json = info.ToJsonString();
 
-        var stream = new MemoryStream();
-        var writer = new Utf8JsonWriter(stream, new JsonWriterOptions { Indented = true });
-        info.WriteTo(writer, null);
-        writer.Flush();
-        var json = Encoding.UTF8.GetString(stream.ToArray());
-        Assert.NotNull(json);
-        Assert.NotEmpty(json);
-        //Validate json
-        var serializer = new Newtonsoft.Json.JsonSerializer();
-        serializer.Deserialize(new Newtonsoft.Json.JsonTextReader(new StringReader(json)));
+        var settings = new VerifySettings();
+        settings.UniqueForTargetFrameworkAndVersion();
+        return VerifyJson(json, settings);
     }
 }
+
 #endif
