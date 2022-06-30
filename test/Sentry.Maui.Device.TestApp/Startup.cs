885262dfc8638fd83ae51b03628719910c4056ed
Ivan Dlugos <dlugos.ivan@gmail.com>
2022-06-30 17:08:45 +0200

    test: try to integrate Maui.Tests firsts

--- test/Sentry.Maui.Device.TestApp/Startup.cs
+++ test/Sentry.Maui.Device.TestApp/Startup.cs
@@ -13,7 +13,8 @@ namespace Sentry.Maui.Device.TestApp
                     Assemblies =
                     {
                         typeof(MauiProgram).Assembly,
-                        typeof(Sentry.Tests.SentrySdkTests).Assembly
+                        // typeof(Sentry.Tests.SentrySdkTests).Assembly
+                        typeof(Sentry.Maui.Tests.ApiApprovalTests).Assembly
                     },
                 })
                 .UseHeadlessRunner(new HeadlessRunnerOptions
