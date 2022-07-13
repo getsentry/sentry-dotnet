9de23f740bf8061615cf57d6f5a4cdefe8f0b58e
Simon Cropp <simon.cropp@gmail.com>
2022-07-14 09:19:50 +1000

    Update SentryDiagnosticListenerTests.cs

--- test/Sentry.DiagnosticSource.Tests/Integration/SQLite/SentryDiagnosticListenerTests.cs
+++ test/Sentry.DiagnosticSource.Tests/Integration/SQLite/SentryDiagnosticListenerTests.cs
@@ -131,7 +131,7 @@ public class SentryDiagnosticListenerTests
             commands.Add(i * 2);
         }
         // Save before the Transaction creation to avoid storing junk.
-        await context.SaveChangesAsync();
+        context.SaveChanges();
 
         var hub = _fixture.Hub;
         var transaction = hub.StartTransaction("test", "test");
