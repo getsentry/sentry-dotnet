1d2b77f94fbd114dd8b6bb47d46a349158c5d57e
Matt Johnson-Pint <matt.johnson-pint@sentry.io>
2022-07-22 10:26:31 -0700

    Update versioning test

--- test/Sentry.AspNetCore.Tests/VersioningTests.cs
+++ test/Sentry.AspNetCore.Tests/VersioningTests.cs
@@ -1,47 +1,54 @@
 #if NET6_0
-using System.Net.Http;
-using System.Runtime.InteropServices;
-using DiffEngine;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.TestHost;
 using Microsoft.Extensions.DependencyInjection;
+
+namespace Sentry.AspNetCore.Tests;
 
 [UsesVerify]
 public class VersioningTests
 {
-    [SkippableFact]
+    [Fact]
     public async Task Simple()
     {
-        //this test times out on mac on build server
-        Skip.If(RuntimeInformation.IsOSPlatform(OSPlatform.OSX) && BuildServerDetector.Detected);
+        // Arrange
         var transport = new RecordingTransport();
-        var builder = WebApplication.CreateBuilder();
-        builder.WebHost.UseSentry(_ =>
-        {
-            _.TracesSampleRate = 1;
-            _.Transport = transport;
-            _.Dsn = ValidDsn;
-        });
-        var services = builder.Services;
-        var controllers = services.AddControllers();
-        controllers.UseSpecificControllers(typeof(TargetController));
-        services.AddApiVersioning(_ =>
-        {
-            _.DefaultApiVersion = new ApiVersion(1, 0);
-            _.AssumeDefaultVersionWhenUnspecified = true;
-            _.ReportApiVersions = true;
-        });
+        using var server = new TestServer(new WebHostBuilder()
+            .UseSentry(o =>
+            {
+                o.Dsn = ValidDsn;
+                o.TracesSampleRate = 1;
+                o.Transport = transport;
+            })
+            .ConfigureServices(services =>
+            {
+                services.AddRouting();
 
-        await using var app = builder.Build();
-        app.UseSentryTracing();
-        app.MapControllers();
+                var controllers = services.AddControllers();
+                controllers.UseSpecificControllers(typeof(TargetController));
 
-        await app.StartAsync();
+                services.AddApiVersioning(versioningOptions =>
+                {
+                    versioningOptions.DefaultApiVersion = new ApiVersion(1, 0);
+                    versioningOptions.AssumeDefaultVersionWhenUnspecified = true;
+                    versioningOptions.ReportApiVersions = true;
+                });
+            })
+            .Configure(app =>
+            {
+                app.UseRouting();
+                app.UseSentryTracing();
+                app.UseEndpoints(routeBuilder => routeBuilder.MapControllers());
+            }));
 
-        using var client = new HttpClient();
-        var result = await client.GetStringAsync($"{app.Urls.First()}/v1.1/Target");
+        var client = server.CreateClient();
 
+        // Act
+        var result = await client.GetStringAsync("/v1.1/Target");
+
+        // Assert
         var payloads = transport.Envelopes
             .SelectMany(x => x.Items)
             .Select(x => x.Payload)
@@ -51,8 +58,6 @@ public class VersioningTests
             .IgnoreStandardSentryMembers()
             .ScrubLinesContaining("Message: Executed action ")
             .IgnoreMembers("ConnectionId", "RequestId");
-
-        await app.StopAsync();
     }
 
     [ApiController]
