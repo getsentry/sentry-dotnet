4db08701ca73124e3efe68e0780e47f81e563d24
Matt Johnson-Pint <matt.johnson-pint@sentry.io>
2022-07-25 11:26:40 -0700

    Update init-cache-flush test to use fake filesystem

---
+++ test/Sentry.Testing/FakeFileSystem.cs
@@ -0,0 +1,50 @@
+using MockFileSystem = System.IO.Abstractions.TestingHelpers.MockFileSystem;
+
+namespace Sentry.Testing;
+
+public class FakeFileSystem : IFileSystem
+{
+    private readonly MockFileSystem _mockFileSystem = new();
+
+    public IEnumerable<string> EnumerateFiles(string path) => _mockFileSystem.Directory.EnumerateFiles(path);
+
+    public IEnumerable<string> EnumerateFiles(string path, string searchPattern) =>
+        _mockFileSystem.Directory.EnumerateFiles(path, searchPattern);
+
+    public void CreateDirectory(string path) => _mockFileSystem.Directory.CreateDirectory(path);
+
+    public void DeleteDirectory(string path, bool recursive = false) =>
+        _mockFileSystem.Directory.Delete(path, recursive);
+
+    public bool DirectoryExists(string path) => _mockFileSystem.Directory.Exists(path);
+
+    public bool FileExists(string path) => _mockFileSystem.File.Exists(path);
+
+    public void MoveFile(string sourceFileName, string destFileName, bool overwrite = false)
+    {
+#if NET5_0_OR_GREATER
+        _mockFileSystem.File.Move(sourceFileName, destFileName, overwrite);
+#else
+        if (overwrite)
+        {
+            _mockFileSystem.File.Copy(sourceFileName, destFileName, overwrite: true);
+            _mockFileSystem.File.Delete(sourceFileName);
+        }
+        else
+        {
+            _mockFileSystem.File.Move(sourceFileName, destFileName);
+        }
+#endif
+    }
+
+    public void DeleteFile(string path) => _mockFileSystem.File.Delete(path);
+
+    public DateTimeOffset GetFileCreationTime(string path) =>
+        _mockFileSystem.FileInfo.FromFileName(path).CreationTimeUtc;
+
+    public string ReadAllTextFromFile(string file) => _mockFileSystem.File.ReadAllText(file);
+
+    public Stream OpenFileForReading(string path) => _mockFileSystem.File.OpenRead(path);
+
+    public Stream CreateFileForWriting(string path) => _mockFileSystem.File.Create(path);
+}
