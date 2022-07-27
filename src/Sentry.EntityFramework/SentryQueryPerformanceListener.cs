ee6e205860b4eb4603c331852a96129f994ba026
Simon Cropp <simon.cropp@gmail.com>
2022-07-28 09:57:17 +1000

    Update SentryQueryPerformanceListener.cs

--- src/Sentry.EntityFramework/SentryQueryPerformanceListener.cs
+++ src/Sentry.EntityFramework/SentryQueryPerformanceListener.cs
@@ -12,8 +12,8 @@ internal class SentryQueryPerformanceListener : IDbCommandInterceptor
     internal const string DbNonQueryKey = "db.execute";
     internal const string DbScalarKey = "db.query.scalar";
 
-    private SentryOptions _options { get; }
-    private IHub _hub { get; }
+    private SentryOptions _options;
+    private IHub _hub;
 
     internal SentryQueryPerformanceListener(IHub hub, SentryOptions options)
     {
