369e6472b3d10823e901bc014b37cc9ed5602490
Simon Cropp <simon.cropp@gmail.com>
2022-07-29 06:46:26 +1000

    Update DbInterceptionIntegrationTests.cs

--- test/Sentry.EntityFramework.Tests/DbInterceptionIntegrationTests.cs
+++ test/Sentry.EntityFramework.Tests/DbInterceptionIntegrationTests.cs
@@ -1,28 +1,27 @@
 using Sentry.Testing;
 
-namespace Sentry.EntityFramework.Tests
+namespace Sentry.EntityFramework.Tests;
+
+public class DbInterceptionIntegrationTests
 {
-    public class DbInterceptionIntegrationTests
+    [Fact]
+    public void Register_TraceSAmpleRateZero_IntegrationNotRegistered()
     {
-        [Fact]
-        public void Register_TraceSAmpleRateZero_IntegrationNotRegistered()
+        // Arrange
+        var logger = Substitute.For<ITestOutputHelper>();
+        var options = new SentryOptions
         {
-            // Arrange
-            var logger = Substitute.For<ITestOutputHelper>();
-            var options = new SentryOptions
-            {
-                Debug = true,
-                DiagnosticLogger = new TestOutputDiagnosticLogger(logger),
-                TracesSampleRate = 0
-            };
-            var integration = new DbInterceptionIntegration();
+            Debug = true,
+            DiagnosticLogger = new TestOutputDiagnosticLogger(logger),
+            TracesSampleRate = 0
+        };
+        var integration = new DbInterceptionIntegration();
 
-            // Act
-            integration.Register(Substitute.For<IHub>(), options);
+        // Act
+        integration.Register(Substitute.For<IHub>(), options);
 
-            // Assert
-            logger.Received(1).WriteLine(Arg.Is<string>(message => message.Contains(DbInterceptionIntegration.SampleRateDisabledMessage)));
-            Assert.Null(integration.SqlInterceptor);
-        }
+        // Assert
+        logger.Received(1).WriteLine(Arg.Is<string>(message => message.Contains(DbInterceptionIntegration.SampleRateDisabledMessage)));
+        Assert.Null(integration.SqlInterceptor);
     }
 }
