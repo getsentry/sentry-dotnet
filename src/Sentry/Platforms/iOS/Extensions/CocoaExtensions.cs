171567064fe2fbfbac85da982d2cb2451b3e9e77
Matt Johnson-Pint <matt.johnson-pint@sentry.io>
2022-07-30 15:00:29 -0700

    Fix date conversion

--- src/Sentry/Platforms/iOS/Extensions/CocoaExtensions.cs
+++ src/Sentry/Platforms/iOS/Extensions/CocoaExtensions.cs
@@ -4,5 +4,5 @@ internal static class CocoaExtensions
 {
     public static DateTimeOffset ToDateTimeOffset(this NSDate timestamp) => new((DateTime)timestamp);
 
-    public static NSDate ToNSDate(this DateTimeOffset timestamp) => (NSDate)timestamp.DateTime;
+    public static NSDate ToNSDate(this DateTimeOffset timestamp) => (NSDate)timestamp.UtcDateTime;
 }
