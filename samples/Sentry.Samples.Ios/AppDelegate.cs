a17184184b6c1d7aa86bc4ccf1022c5e5ed0fb33
Matt Johnson-Pint <matt.johnson-pint@sentry.io>
2022-07-30 14:36:48 -0700

    Init and test from the sample

--- samples/Sentry.Samples.Ios/AppDelegate.cs
+++ samples/Sentry.Samples.Ios/AppDelegate.cs
@@ -9,7 +9,14 @@ public class AppDelegate : UIApplicationDelegate {
 
 	public override bool FinishedLaunching (UIApplication application, NSDictionary launchOptions)
 	{
-		// create a new window instance based on the screen size
+        // Init the Sentry SDK
+        SentrySdk.Init(o =>
+        {
+            o.Debug = true;
+            o.Dsn = "https://eb18e953812b41c3aeb042e666fd3b5c@o447951.ingest.sentry.io/5428537";
+        });
+
+        // create a new window instance based on the screen size
 		Window = new UIWindow (UIScreen.MainScreen.Bounds);
 
 		// create a UIViewController with a single UILabel
@@ -24,6 +31,11 @@ public class AppDelegate : UIApplicationDelegate {
 
 		// make the window visible
 		Window.MakeKeyAndVisible ();
+
+
+        // Try out the Sentry SDK
+        SentrySdk.CaptureMessage("From iOS");
+
 
 		return true;
 	}
