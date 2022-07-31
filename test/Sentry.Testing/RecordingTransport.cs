ed3bbd223c3de0c78851fbd73dc03c7c7a15ea8c
Simon Cropp <simon.cropp@gmail.com>
2022-07-31 22:22:54 +1000

    Update RecordingTransport.cs

--- test/Sentry.Testing/RecordingTransport.cs
+++ test/Sentry.Testing/RecordingTransport.cs
@@ -1,12 +1,16 @@
 public class RecordingTransport : ITransport
 {
-    private ConcurrentBag<Envelope> _envelopes = new();
+    private List<Envelope> _envelopes = new();
 
     public IEnumerable<Envelope> Envelopes => _envelopes;
 
     public Task SendEnvelopeAsync(Envelope envelope, CancellationToken cancellationToken = default)
     {
-        _envelopes.Add(envelope);
+        lock (_envelopes)
+        {
+            _envelopes.Add(envelope);
+        }
+
         return Task.CompletedTask;
     }
 }
