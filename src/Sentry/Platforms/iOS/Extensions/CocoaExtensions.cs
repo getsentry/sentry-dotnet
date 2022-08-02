7770f7312c5b99566f026245f1b59be6179e0c87
Matt Johnson-Pint <matt.johnson-pint@sentry.io>
2022-08-02 10:07:58 -0700

    Add and use serialization and dictionary extensions

--- src/Sentry/Platforms/iOS/Extensions/CocoaExtensions.cs
+++ src/Sentry/Platforms/iOS/Extensions/CocoaExtensions.cs
@@ -1,3 +1,6 @@
+using Sentry.Extensibility;
+using SentryCocoa;
+
 namespace Sentry.iOS.Extensions;
 
 internal static class CocoaExtensions
@@ -5,4 +8,96 @@ internal static class CocoaExtensions
     public static DateTimeOffset ToDateTimeOffset(this NSDate timestamp) => new((DateTime)timestamp);
 
     public static NSDate ToNSDate(this DateTimeOffset timestamp) => (NSDate)timestamp.UtcDateTime;
+
+    public static string? ToJsonString(this NSObject? obj, IDiagnosticLogger? logger = null)
+    {
+        if (obj == null)
+        {
+            return null;
+        }
+
+        if (obj is ISentrySerializable serializable)
+        {
+            // For types that implement Sentry Cocoa's SentrySerializable protocol (interface),
+            // We should call that first, and then serialize the result to JSON later.
+            obj = serializable.Serialize();
+        }
+
+        // Now we will use Apple's JSON Serialization functions.
+        // See https://developer.apple.com/documentation/foundation/nsjsonserialization
+
+        if (!NSJsonSerialization.IsValidJSONObject(obj))
+        {
+            logger?.LogWarning("Cannot serialize a {0} directly to JSON", obj.GetType().Name);
+            return null;
+        }
+
+        try
+        {
+            using var data = NSJsonSerialization.Serialize(obj, 0, out _);
+            return data.ToString();
+        }
+        catch (Exception ex)
+        {
+            logger?.LogError("Error serializing {0} to JSON", ex, obj.GetType().Name);
+            return null;
+        }
+    }
+
+    public static Dictionary<string, string> ToStringDictionary<TValue>(
+        this NSDictionary<NSString, TValue>? dict,
+        IDiagnosticLogger? logger = null)
+        where TValue : NSObject
+    {
+        if (dict == null)
+        {
+            return new Dictionary<string, string>();
+        }
+
+        var result = new Dictionary<string, string>(capacity: (int)dict.Count);
+        foreach (var key in dict.Keys)
+        {
+            var value = dict[key];
+            if (value is NSString s)
+            {
+                result.Add(key, s);
+            }
+            else if (value.ToJsonString(logger) is { } json)
+            {
+                result.Add(key, json);
+            }
+
+            // Skip null values, including anything that couldn't be serialized
+        }
+
+        return result;
+    }
+
+    public static Dictionary<string, string>? ToNullableStringDictionary<TValue>(
+        this NSDictionary<NSString, TValue>? dict,
+        IDiagnosticLogger? logger = null)
+        where TValue : NSObject
+    {
+        var d = dict?.ToStringDictionary(logger);
+        return d?.Count is null or 0 ? null : d;
+    }
+
+    public static NSDictionary<NSString, NSObject> ToNSDictionary<TValue>(
+        this IEnumerable<KeyValuePair<string, TValue>> dict)
+    {
+        var result = new NSDictionary<NSString, NSObject>();
+        foreach (var item in dict)
+        {
+            result[item.Key] = NSObject.FromObject(item.Value);
+        }
+
+        return result;
+    }
+
+    public static NSDictionary<NSString, NSObject>? ToNullableNSDictionary<TValue>(
+        this IEnumerable<KeyValuePair<string, TValue>> dict)
+    {
+        var d = dict.ToNSDictionary();
+        return d.Count == 0 ? null : d;
+    }
 }
--- src/Sentry/Platforms/iOS/Extensions/UserExtensions.cs
+++ src/Sentry/Platforms/iOS/Extensions/UserExtensions.cs
@@ -1,71 +1,27 @@
-using System.Collections.ObjectModel;
-using System.Text.Json;
+using Sentry.Extensibility;
 
 namespace Sentry.iOS.Extensions;
 
 internal static class UserExtensions
 {
-    private static readonly IDictionary<string, string> EmptyDictionary =
-        new ReadOnlyDictionary<string, string>(new Dictionary<string, string>());
-
-    public static User ToUser(this SentryCocoa.SentryUser user)
-    {
-        IDictionary<string, string>? otherData;
-        if (user.Data is not IDictionary<NSString, NSObject> data)
-        {
-            otherData = EmptyDictionary;
-        }
-        else
-        {
-            otherData = data
-                .ToDictionary(
-                    x => (string)x.Key,
-                    x =>
-                    {
-                        try
-                        {
-                            return JsonSerializer.Serialize(x.Value);
-                        }
-                        catch
-                        {
-                            // TODO: log error
-                            return "";
-                        }
-                    });
-        }
-
-        return new User
+    public static User ToUser(this SentryCocoa.SentryUser user, IDiagnosticLogger? logger = null) =>
+        new()
         {
             Email = user.Email,
             Id = user.UserId,
             IpAddress = user.IpAddress,
             Username = user.Username,
-            Other = otherData
+            Other = user.Data.ToStringDictionary(logger)
         };
-    }
 
     public static SentryCocoa.SentryUser ToCocoaUser(this User user)
     {
-        NSDictionary<NSString, NSObject>? userData;
-        if (user.Other.Count == 0)
-        {
-            userData = null;
-        }
-        else
-        {
-            userData = new NSDictionary<NSString, NSObject>();
-            foreach (var item in user.Other)
-            {
-                userData[item.Key] = NSObject.FromObject(item.Value);
-            }
-        }
-
         var cocoaUser = new SentryCocoa.SentryUser
         {
             Email = user.Email,
             IpAddress = user.IpAddress,
             Username = user.Username,
-            Data = userData
+            Data = user.Other.ToNullableNSDictionary()
         };
 
         // Leave a null User ID uninitialized since it is optional.
