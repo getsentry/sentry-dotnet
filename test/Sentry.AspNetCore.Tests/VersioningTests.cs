36e83184a7328804d6322dfcbbe8322a69f47237
Lucas Zimerman Fraulob <lucas-zimerman1@hotmail.com>
2022-08-11 12:03:19 +0900

    Add sample for issue with NET CORE 3.1

--- test/Sentry.AspNetCore.Tests/VersioningTests.cs
+++ test/Sentry.AspNetCore.Tests/VersioningTests.cs
@@ -1,4 +1,5 @@
-#if NET6_0
+#if NET6_0 || NETCOREAPP3_1
+using System.Net.Http;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -40,6 +41,7 @@ public class VersioningTests
                 services.AddRouting();
 
                 var controllers = services.AddControllers();
+#if NET6_0_OR_GREATER
                 controllers.UseSpecificControllers(typeof(TargetController));
 
                 services.AddApiVersioning(versioningOptions =>
@@ -48,6 +50,7 @@ public class VersioningTests
                     versioningOptions.AssumeDefaultVersionWhenUnspecified = true;
                     versioningOptions.ReportApiVersions = true;
                 });
+#endif
             })
             .Configure(app =>
             {
@@ -75,14 +78,84 @@ public class VersioningTests
             .IgnoreMembers("ConnectionId", "RequestId")
             .ScrubLinesWithReplace(_=>_.Split(new []{" (Sentry.AspNetCore.Tests) "},StringSplitOptions.None)[0]);
     }
+    [Fact]
+
+    public async Task SimpleOptionRoute()
+    {
+        // Arrange
+        var transport = new RecordingTransport();
+        using var server = new TestServer(new WebHostBuilder()
+            .UseSentry(o =>
+            {
+                o.Dsn = ValidDsn;
+                o.TracesSampleRate = 1;
+                o.Transport = transport;
+                o.Debug = true;
+                o.DiagnosticLogger = _logger;
+
+                // Disable process exit flush to resolve "There is no currently active test." errors.
+                o.DisableAppDomainProcessExitFlush();
+            })
+            .ConfigureServices(services =>
+            {
+                services.AddRouting();
+
+                var controllers = services.AddControllers();
+#if NET6_0_OR_GREATER
+                controllers.UseSpecificControllers(typeof(TargetController));
+
+                services.AddApiVersioning(versioningOptions =>
+                {
+                    versioningOptions.DefaultApiVersion = new ApiVersion(1, 0);
+                    versioningOptions.AssumeDefaultVersionWhenUnspecified = true;
+                    versioningOptions.ReportApiVersions = true;
+                });
+#endif
+            })
+            .Configure(app =>
+            {
+                app.UseRouting();
+                app.UseSentryTracing();
+                app.UseEndpoints(routeBuilder => routeBuilder.MapControllers());
+            }));
+
+        var client = server.CreateClient();
+        var request = new HttpRequestMessage(HttpMethod.Options, "/v1.1/Target");
+
+        // Act
+        var result = await client.SendAsync(request);
+
+        // dispose will ultimately trigger the background worker to flush
+        server.Dispose();
+
+        // Assert
+        var payloads = transport.Envelopes
+            .SelectMany(x => x.Items)
+            .Select(x => x.Payload)
+            .ToList();
+
+        await Verify(new { result, payloads })
+            .IgnoreStandardSentryMembers()
+            .IgnoreMembers("ConnectionId", "RequestId")
+            .ScrubLinesWithReplace(_ => _.Split(new[] { " (Sentry.AspNetCore.Tests) " }, StringSplitOptions.None)[0]);
+    }
 
     [ApiController]
     [Route("v{version:apiVersion}/Target")]
+#if NET6_0_OR_GREATER
     [ApiVersion("1.1")]
+#endif
     public class TargetController : ControllerBase
     {
         [HttpGet]
         public string Method()
+        {
+            return "Hello world";
+        }
+
+
+        [HttpOptions]
+        public string MethodOption()
         {
             return "Hello world";
         }
