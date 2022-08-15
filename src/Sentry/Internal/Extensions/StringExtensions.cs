403c10e30562f95cfff85aff293124caabbd4d36
Matt Johnson-Pint <matt.johnson-pint@sentry.io>
2022-08-15 14:04:09 -0700

    Add String.NullIfWhitespace extension method

--- src/Sentry/Internal/Extensions/StringExtensions.cs
+++ src/Sentry/Internal/Extensions/StringExtensions.cs
@@ -7,5 +7,11 @@ namespace Sentry.Internal.Extensions
         // Used to convert enum value into snake case, which is how Sentry represents them
         public static string ToSnakeCase(this string str) =>
             Regex.Replace(str, @"(\p{Ll})(\p{Lu})", "$1_$2").ToLowerInvariant();
+
+        /// <summary>
+        /// Returns <c>null</c> if <paramref name="str"/> is <c>null</c> or contains only whitespace.
+        /// Otherwise, returns <paramref name="str"/>.
+        /// </summary>
+        public static string? NullIfWhitespace(this string? str) => string.IsNullOrWhiteSpace(str) ? null : str;
     }
 }
