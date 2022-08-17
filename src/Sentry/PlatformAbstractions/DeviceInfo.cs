d09fa5f5784c8ca0c647c5d62af74dc4775c7a9a
Matt Johnson-Pint <matt.johnson-pint@sentry.io>
2022-08-17 15:23:43 -0700

    indent

--- src/Sentry/PlatformAbstractions/DeviceInfo.cs
+++ src/Sentry/PlatformAbstractions/DeviceInfo.cs
@@ -3,11 +3,11 @@ namespace Sentry.PlatformAbstractions
     internal static class DeviceInfo
     {
 #if ANDROID
-    public const string PlatformName = "Android";
+        public const string PlatformName = "Android";
 #elif IOS
-    public const string PlatformName = "iOS";
+        public const string PlatformName = "iOS";
 #elif MACCATALYST
-    public const string PlatformName = "Mac Catalyst";
+        public const string PlatformName = "Mac Catalyst";
 #endif
     }
 }
