f154b077f0ce4f755a172049b1203eee0a8b49b2
Simon Cropp <simon.cropp@gmail.com>
2022-08-19 09:16:06 +1000

    .

--- src/Sentry.AspNetCore/SentryAspNetCoreOptionsSetup.cs
+++ src/Sentry.AspNetCore/SentryAspNetCoreOptionsSetup.cs
@@ -2,6 +2,14 @@ using Microsoft.Extensions.Logging.Configuration;
 using Microsoft.Extensions.Options;
 using Sentry.Extensions.Logging;
 
+#if NETSTANDARD2_0
+using Microsoft.AspNetCore.Hosting;
+using IHostingEnvironment = Microsoft.AspNetCore.Hosting.IHostingEnvironment;
+#else
+using Microsoft.Extensions.Hosting;
+using IHostingEnvironment = Microsoft.AspNetCore.Hosting.IWebHostEnvironment;
+#endif
+
 namespace Sentry.AspNetCore
 {
     /// <summary>
@@ -19,11 +27,23 @@ namespace Sentry.AspNetCore
         }
 
         /// <summary>
+        /// Creates a new instance of <see cref="SentryAspNetCoreOptionsSetup"/>.
+        /// </summary>
+        [Obsolete("Use constructor with no IHostingEnvironment")]
+        public SentryAspNetCoreOptionsSetup(
+            ILoggerProviderConfiguration<SentryAspNetCoreLoggerProvider> providerConfiguration,
+            IHostingEnvironment hostingEnvironment)
+            : base(providerConfiguration.Configuration)
+        {
+        }
+
+        /// <summary>
         /// Configures the <see cref="SentryAspNetCoreOptions"/>.
         /// </summary>
         public override void Configure(SentryAspNetCoreOptions options)
         {
             base.Configure(options);
+
             options.AddLogEntryFilter((category, _, eventId, _)
                 // https://github.com/aspnet/KestrelHttpServer/blob/0aff4a0440c2f393c0b98e9046a8e66e30a56cb0/src/Kestrel.Core/Internal/Infrastructure/KestrelTrace.cs#L33
                 // 13 = Application unhandled exception, which is captured by the middleware so the LogError of kestrel ends up as a duplicate with less info
