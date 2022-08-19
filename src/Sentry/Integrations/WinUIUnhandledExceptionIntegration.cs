efe0387353efc24bb801d9e8b2cd620162c699e9
Matt Johnson-Pint <matt.johnson-pint@sentry.io>
2022-08-18 18:04:37 -0700

    Move WinUI 3 exception handler to Sentry core

---
+++ src/Sentry/Integrations/WinUIUnhandledExceptionIntegration.cs
@@ -0,0 +1,133 @@
+#if NET5_0_OR_GREATER 
+using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+namespace Sentry.Integrations
+{
+    // This integration hooks unhandled exceptions in WinUI 3.
+    // The primary hook is Microsoft.UI.Xaml.Application.Current.UnhandledException
+    //
+    // There are some quirks to be aware of:
+    //
+    // - By default, important details (message, stack trace, etc.) are stripped away.
+    //   We can work around this by catching first-chance exceptions.
+    //   See: https://github.com/microsoft/microsoft-ui-xaml/issues/7160
+    //
+    // - Exceptions from background threads are not caught here.
+    //   However, they are caught by System.AppDomain.CurrentDomain.UnhandledException,
+    //   which we already hook in our AppDomainUnhandledExceptionIntegration
+    //   See: https://github.com/microsoft/microsoft-ui-xaml/issues/5221
+    //
+    // Note that we use reflection in this integration to get at WinUI code.
+    // If we ever add a Windows platform target (net6.0-windows, etc.), we could refactor to avoid reflection.
+    //
+    // This integration is for WinUI 3.  It does NOT work for UWP (WinUI 2).
+    // For UWP, the calling application will need to hook the event handler.
+    // See https://docs.sentry.io/platforms/dotnet/guides/uwp/
+    // (We can't do it automatically without a separate UWP class library,
+    // due to a security exception when attempting to attach the event dynamically.)
+
+    internal class WinUIUnhandledExceptionIntegration : ISdkIntegration
+    {
+        private static readonly Assembly? WinUIAssembly = GetWinUIAssembly();
+
+        private Exception? _lastFirstChanceException;
+        private IHub _hub = null!;
+        private SentryOptions _options = null!;
+
+        // Constants used for reflection
+        private const string WinUIAssemblyName = "Microsoft.WinUI";
+        private const string WinUiAssemblyPublicKeyToken = "de31ebe4ad15742b";
+        private const string WinUINamespace = "Microsoft.UI.Xaml";
+        private const string ApplicationTypeName = "Application";
+        private const string UnhandledExceptionEventName = "UnhandledException";
+        private const string CurrentPropertyName = "Current";
+        private const string HandledPropertyName = "Handled";
+        private const string ExceptionPropertyName = "Exception";
+        private const string ApplicationTypeFullName = WinUINamespace + "." + ApplicationTypeName;
+        private const string Mechanism = WinUINamespace + "." + UnhandledExceptionEventName;
+
+        public static bool IsApplicable => WinUIAssembly != null;
+
+        public void Register(IHub hub, SentryOptions options)
+        {
+            if (!IsApplicable)
+            {
+                return;
+            }
+
+            _hub = hub;
+            _options = options;
+
+            // Hook the main event handler
+            AttachEventHandler();
+
+            // First part of workaround for https://github.com/microsoft/microsoft-ui-xaml/issues/7160
+            AppDomain.CurrentDomain.FirstChanceException += (_, e) => _lastFirstChanceException = e.Exception;
+        }
+
+        private static Assembly? GetWinUIAssembly()
+        {
+            if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                // Not running on Windows
+                return null;
+            }
+
+            // Attempt to locate the Microsoft.WinUI assembly from the AppDomain
+            var assemblies = AppDomain.CurrentDomain.GetAssemblies();
+            return Array.Find(assemblies, x =>
+            {
+                // first check by name
+                var assemblyName = x.GetName();
+                if (assemblyName.Name != WinUIAssemblyName)
+                {
+                    return false;
+                }
+
+                // check the public key token also
+                var token = assemblyName.GetPublicKeyToken();
+                return token != null && string.Equals(Convert.ToHexString(token), WinUiAssemblyPublicKeyToken, StringComparison.OrdinalIgnoreCase);
+            });
+        }
+
+        private void AttachEventHandler()
+        {
+            // Reflection equivalent of:
+            //   Microsoft.UI.Xaml.Application.Current.UnhandledException += WinUIUnhandledExceptionHandler;
+            //
+            EventHandler handler = WinUIUnhandledExceptionHandler!;
+            var applicationType = WinUIAssembly!.GetType(ApplicationTypeFullName)!;
+            var application = applicationType.GetProperty(CurrentPropertyName)!.GetValue(null);
+            var eventInfo = applicationType.GetEvent(UnhandledExceptionEventName)!;
+            var typedHandler = Delegate.CreateDelegate(eventInfo.EventHandlerType!, handler.Target, handler.Method);
+            eventInfo.AddEventHandler(application, typedHandler);
+        }
+
+        private void WinUIUnhandledExceptionHandler(object sender, object e)
+        {
+            var eventArgsType = e.GetType();
+            var handled = (bool)eventArgsType.GetProperty(HandledPropertyName)!.GetValue(e)!;
+            var exception = (Exception)eventArgsType.GetProperty(ExceptionPropertyName)!.GetValue(e)!;
+
+            // Second part of workaround for https://github.com/microsoft/microsoft-ui-xaml/issues/7160
+            if (exception.StackTrace is null)
+            {
+                exception = _lastFirstChanceException!;
+            }
+
+            // Set some useful data and capture the exception
+            exception.Data[Protocol.Mechanism.HandledKey] = handled;
+            exception.Data[Protocol.Mechanism.MechanismKey] = Mechanism;
+            _hub.CaptureException(exception);
+
+            if (!handled)
+            {
+                // We're crashing, so flush events to Sentry right away
+                _hub.FlushAsync(_options.ShutdownTimeout).GetAwaiter().GetResult();
+            }
+        }
+    }
+}
+#endif
