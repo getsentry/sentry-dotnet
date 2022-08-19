62e621987a3074592bbd6b41b605db5bbf39ad1e
Matt Johnson-Pint <matt.johnson-pint@sentry.io>
2022-08-19 13:56:06 -0700

    Adjust minimum platform versions

--- samples/Sentry.Samples.Ios/AppDelegate.cs
+++ samples/Sentry.Samples.Ios/AppDelegate.cs
@@ -23,11 +23,18 @@ public class AppDelegate : UIApplicationDelegate
         // create a new window instance based on the screen size
         Window = new UIWindow(UIScreen.MainScreen.Bounds);
 
+        // determine the background color for the view (SystemBackground requires iOS >= 13.0)
+        var backgroundColor = UIDevice.CurrentDevice.CheckSystemVersion(13, 0)
+#pragma warning disable CA1416
+            ? UIColor.SystemBackground
+#pragma warning restore CA1416
+            : UIColor.White;
+
         // create a UIViewController with a single UILabel
         var vc = new UIViewController();
         vc.View!.AddSubview(new UILabel(Window!.Frame)
         {
-            BackgroundColor = UIColor.SystemBackground,
+            BackgroundColor = backgroundColor,
             TextAlignment = UITextAlignment.Center,
             Text = "Hello, iOS!",
             AutoresizingMask = UIViewAutoresizing.All,
