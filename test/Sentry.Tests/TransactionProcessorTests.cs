85e25207201326fe3ce7bbd7404cdf33739b0d50
Simon Cropp <simon.cropp@gmail.com>
2022-08-22 13:04:46 +1000

    .

--- test/Sentry.Tests/TransactionProcessorTests.cs
+++ test/Sentry.Tests/TransactionProcessorTests.cs
@@ -7,13 +7,7 @@ public class TransactionProcessorTests
     public async Task Simple()
     {
         var transport = new RecordingTransport();
-        var options = new SentryOptions
-        {
-            TracesSampleRate = 1,
-            Debug = true,
-            Transport = transport,
-            Dsn = ValidDsn,
-        };
+        var options = Options(transport);
 
         options.AddTransactionProcessor(new TheProcessor());
         var hub = SentrySdk.InitHub(options);
@@ -32,9 +26,43 @@ public class TransactionProcessorTests
 
     public class TheProcessor : ISentryTransactionProcessor
     {
-        public void Process(Transaction transaction)
-        {
+        public void Process(Transaction transaction) =>
             transaction.Contexts["key"] = "value";
-        }
     }
+
+    [Fact]
+    public async Task Discard()
+    {
+        var transport = new RecordingTransport();
+        var options = Options(transport);
+
+        options.AddTransactionProcessor(new DiscardProcessor());
+        var hub = SentrySdk.InitHub(options);
+        using (SentrySdk.UseHub(hub))
+        {
+            var transaction = hub.StartTransaction("my transaction", "my operation");
+            hub.ConfigureScope(scope => scope.Transaction = transaction);
+            hub.CaptureMessage("TheMessage");
+            transaction.Finish();
+            await hub.FlushAsync(TimeSpan.FromSeconds(1));
+        }
+
+        await Verify(transport.Envelopes)
+            .IgnoreStandardSentryMembers();
+    }
+
+    public class DiscardProcessor : ISentryTransactionProcessor
+    {
+        public void Process(Transaction transaction) =>
+            transaction.IsSampled = false;
+    }
+
+    private static SentryOptions Options(RecordingTransport transport) =>
+        new()
+        {
+            TracesSampleRate = 1,
+            Debug = true,
+            Transport = transport,
+            Dsn = ValidDsn,
+        };
 }
