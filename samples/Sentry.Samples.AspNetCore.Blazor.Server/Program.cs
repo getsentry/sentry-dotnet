a5e5bfa25ce84aa0e8e5f9e0d31d8d7b5b9cbaee
Simon Cropp <simon.cropp@gmail.com>
2022-08-29 16:23:53 +1000

    Update Program.cs

--- samples/Sentry.Samples.AspNetCore.Blazor.Server/Program.cs
+++ samples/Sentry.Samples.AspNetCore.Blazor.Server/Program.cs
@@ -15,11 +15,13 @@ try
 
     var app = builder.Build();
 
+    // Enable Sentry performance monitoring
+    app.UseSentryTracing();
+
     // Configure the HTTP request pipeline.
     if (!app.Environment.IsDevelopment())
     {
         app.UseExceptionHandler("/Error");
-        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
         app.UseHsts();
     }
 
