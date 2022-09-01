18b55ffbacb97f4817bbecd5c3aa5394d353415a
Simon Cropp <simon.cropp@gmail.com>
2022-09-02 06:53:01 +1000

    Update Program.cs
    Update Program.cs

--- samples/Sentry.Samples.AspNetCore.Blazor.Server/Program.cs
+++ samples/Sentry.Samples.AspNetCore.Blazor.Server/Program.cs
@@ -1,41 +1,27 @@
 // Capture blazor bootstrapping errors
 
-var configureSentry = new Action<SentryOptions>(
-    options =>
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.WebHost.UseSentry(options =>
 {
     options.Dsn = "https://eb18e953812b41c3aeb042e666fd3b5c@o447951.ingest.sentry.io/5428537";
     options.Debug = true;
 });
-using var sdk = SentrySdk.Init(configureSentry);
-try
-{
-    var builder = WebApplication.CreateBuilder(args);
 
-    builder.Logging.AddSentry(configureSentry);
-    // Add services to the container.
-    builder.Services.AddRazorPages();
-    builder.Services.AddServerSideBlazor();
-    builder.WebHost.UseSentry();
+var app = builder.Build();
 
-    var app = builder.Build();
+// Enable Sentry performance monitoring
+app.UseSentryTracing();
 
-    // Enable Sentry performance monitoring
-    app.UseSentryTracing();
+app.UseHttpsRedirection();
 
-    app.UseHttpsRedirection();
+app.UseStaticFiles();
 
-    app.UseStaticFiles();
+app.UseRouting();
 
-    app.UseRouting();
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
 
-    app.MapBlazorHub();
-    app.MapFallbackToPage("/_Host");
-
-    app.Run();
-}
-catch (Exception e)
-{
-    SentrySdk.CaptureException(e);
-    await SentrySdk.FlushAsync(TimeSpan.FromSeconds(2));
-    throw;
-}
+app.Run();
