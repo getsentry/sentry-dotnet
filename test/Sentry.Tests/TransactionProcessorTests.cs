18cc322024df0cfd41b6fdc2ef8f7739111941c9
Simon Cropp <simon.cropp@gmail.com>
2022-09-02 07:30:15 +1000

    SampledOut

--- test/Sentry.Tests/TransactionProcessorTests.cs
+++ test/Sentry.Tests/TransactionProcessorTests.cs
@@ -34,6 +34,39 @@ public class TransactionProcessorTests
     }
 
     [Fact]
+    public async Task SampledOut()
+    {
+        var transport = new RecordingTransport();
+        var options = Options(transport);
+        options.SampleRate = float.Epsilon;
+        options.AddTransactionProcessor(new TheProcessor());
+        var hub = SentrySdk.InitHub(options);
+        using (SentrySdk.UseHub(hub))
+        {
+            var transaction = hub.StartTransaction("my transaction", "my operation");
+            hub.ConfigureScope(scope => scope.Transaction = transaction);
+            hub.CaptureMessage("TheMessage");
+            transaction.Finish();
+            await hub.FlushAsync(TimeSpan.FromSeconds(1));
+        }
+
+        Assert.False(TrackingProcessor.Called);
+        await Verify(transport.Envelopes)
+            .IgnoreStandardSentryMembers();
+    }
+
+    public class TrackingProcessor : ISentryTransactionProcessor
+    {
+        public Transaction Process(Transaction transaction)
+        {
+            Called = true;
+            return transaction;
+        }
+
+        public static bool Called;
+    }
+
+    [Fact]
     public async Task Discard()
     {
         var transport = new RecordingTransport();
