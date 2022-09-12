26c3dbbf210fa88edf6db7f0bc43603a0861c0b9
Matt Johnson-Pint <matt.johnson-pint@sentry.io>
2022-09-12 07:58:29 -0700

    Update scripts

--- scripts/run-device-tests.cmd
+++ scripts/run-device-tests.cmd
@@ -3,16 +3,17 @@ setlocal
 
 pushd %~dp0
 
-dotnet build -f net6.0-android ..\test\Sentry.Maui.Device.TestApp
-
 where xharness >nul 2>nul
 if %ERRORLEVEL% NEQ 0 dotnet tool install Microsoft.DotNet.XHarness.CLI --global --version "1.*-*"
 
-if exist ..\test_output rmdir /q /s ..\test_output
+dotnet build -f net6.0-android ..\test\Sentry.Maui.Device.TestApp
+if %ERRORLEVEL% NEQ 0 goto end
 
+if exist ..\test_output rmdir /q /s ..\test_output
 xharness android test ^
     --app=..\test\Sentry.Maui.Device.TestApp\bin\Debug\net6.0-android\io.sentry.dotnet.maui.device.testapp-Signed.apk ^
     --package-name=io.sentry.dotnet.maui.device.testapp ^
     --output-directory=..\test_output
 
+:end
 popd
--- scripts/run-device-tests.sh
+++ scripts/run-device-tests.sh
@@ -2,18 +2,20 @@
 
 pushd "$(dirname "$0")" > /dev/null
 
-dotnet build -f net6.0-android ../test/Sentry.Maui.Device.TestApp
-
 if ! command -v xharness &> /dev/null
 then
     dotnet tool install Microsoft.DotNet.XHarness.CLI --global --version "1.*-*"
 fi
 
-rm -rf ../test_output
+dotnet build -f net6.0-android ../test/Sentry.Maui.Device.TestApp
 
-xharness android test \
-    --app=../test/Sentry.Maui.Device.TestApp/bin/Debug/net6.0-android/io.sentry.dotnet.maui.device.testapp-Signed.apk \
-    --package-name=io.sentry.dotnet.maui.device.testapp \
-    --output-directory=../test_output
+if [ $? -eq 0 ]
+then
+    rm -rf ../test_output
+    xharness android test \
+        --app=../test/Sentry.Maui.Device.TestApp/bin/Debug/net6.0-android/io.sentry.dotnet.maui.device.testapp-Signed.apk \
+        --package-name=io.sentry.dotnet.maui.device.testapp \
+        --output-directory=../test_output
+fi
 
 popd > /dev/null
