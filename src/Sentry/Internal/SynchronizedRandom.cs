106900e690c7edd6b7f943218f9152a2846aca35
Matt Johnson-Pint <matt.johnson-pint@sentry.io>
2022-09-12 17:14:44 -0700

    Update SynchronizedRandom implementation

--- src/Sentry/Internal/SynchronizedRandom.cs
+++ src/Sentry/Internal/SynchronizedRandom.cs
@@ -1,5 +1,9 @@
 using System;
 
+#if !NET6_0_OR_GREATER
+using System.Threading;
+#endif
+
 namespace Sentry.Internal
 {
     internal static class SynchronizedRandom
@@ -17,40 +21,13 @@ namespace Sentry.Internal
         public static double NextDouble() => Random.Shared.NextDouble();
         public static void NextBytes(byte[] bytes) => Random.Shared.NextBytes(bytes);
 #else
-        // TODO: ThreadLocal instance would avoid contention
-        private static readonly Random Random = new();
+        private static readonly AsyncLocal<Random> LocalRandom = new();
+        private static Random Random => LocalRandom.Value ??= new Random();
 
-        public static void NextBytes(byte[] bytes)
-        {
-            lock (Random)
-            {
-                Random.NextBytes(bytes);
-            }
-        }
-
-        public static double NextDouble()
-        {
-            lock (Random)
-            {
-                return Random.NextDouble();
-            }
-        }
-
-        public static int Next(int minValue, int maxValue)
-        {
-            lock (Random)
-            {
-                return Random.Next(minValue, maxValue);
-            }
-        }
-
-        public static int Next()
-        {
-            lock (Random)
-            {
-                return Random.Next();
-            }
-        }
+        public static void NextBytes(byte[] bytes) => Random.NextBytes(bytes);
+        public static double NextDouble() => Random.NextDouble();
+        public static int Next(int minValue, int maxValue) => Random.Next(minValue, maxValue);
+        public static int Next() => Random.Next();
 #endif
     }
 }
