f6184b2c242477aacf5a6aeff4e5788224106429
Matt Johnson-Pint <matt.johnson-pint@sentry.io>
2022-09-12 17:01:51 -0700

    Improve SynchronizedRandom

--- src/Sentry/Internal/SynchronizedRandom.cs
+++ src/Sentry/Internal/SynchronizedRandom.cs
@@ -1,4 +1,9 @@
+#if ANDROID
+using Java.Util.Concurrent;
+#else
 using System;
+using System.Threading;
+#endif
 
 namespace Sentry.Internal
 {
@@ -11,46 +16,26 @@ namespace Sentry.Internal
             _ => NextDouble() < rate
         };
 
-#if NET6_0_OR_GREATER
+#if ANDROID
+        // We use the Java implementation because there were device test failures with the .NET one
+        // related to uneven distribution when run on Android
+        public static int Next(int minValue, int maxValue) => ThreadLocalRandom.Current()!.NextInt(minValue, maxValue);
+        public static int Next() => ThreadLocalRandom.Current()!.NextInt();
+        public static double NextDouble() => ThreadLocalRandom.Current()!.NextDouble();
+        public static void NextBytes(byte[] bytes) => ThreadLocalRandom.Current()!.NextBytes(bytes);
+#elif NET6_0_OR_GREATER
         public static int Next(int minValue, int maxValue) => Random.Shared.Next(minValue, maxValue);
         public static int Next() => Random.Shared.Next();
         public static double NextDouble() => Random.Shared.NextDouble();
         public static void NextBytes(byte[] bytes) => Random.Shared.NextBytes(bytes);
 #else
-        // TODO: ThreadLocal instance would avoid contention
-        private static readonly Random Random = new();
+        private static readonly AsyncLocal<Random> LocalRandom = new();
+        private static Random Random => LocalRandom.Value ??= new Random();
 
-        public static void NextBytes(byte[] bytes)
-        {
-            lock (Random)
-            {
-                Random.NextBytes(bytes);
-            }
-        }
-
-        public static double NextDouble()
-        {
-            lock (Random)
-            {
-                return Random.NextDouble();
-            }
-        }
-
-        public static int Next(int minValue, int maxValue)
-        {
-            lock (Random)
-            {
-                return Random.Next(minValue, maxValue);
-            }
-        }
-
-        public static int Next()
-        {
-            lock (Random)
-            {
-                return Random.Next();
-            }
-        }
+        public static void NextBytes(byte[] bytes) => Random.NextBytes(bytes);
+        public static double NextDouble() => Random.NextDouble();
+        public static int Next(int minValue, int maxValue) => Random.Next(minValue, maxValue);
+        public static int Next() => Random.Next();
 #endif
     }
 }
