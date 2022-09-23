3bbbb0b2275d3feb7d86bd1287e5819d8c270207
Ivan Dlugos <dlugos.ivan@gmail.com>
2022-09-23 17:50:03 +0200

    x

--- build.sh
+++ build.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 set -e
 
-if [ "$GITHUB_ACTIONS" == "true" ]
-    then
-        testLogger="GitHubActions;report-warnings=false"
-    else
-        testLogger="console"
+if [ "$GITHUB_ACTIONS" == "true" ]; then
+    testLogger="GitHubActions;report-warnings=false"
+else
+    testLogger="console"
 fi
 
 dotnet test -c Release -l $testLogger \
     /p:CopyLocalLockFileAssemblies=true
 
-// test
+// test 2
