4df24953d7d813b0e652c21167b5b1e14dd06d99
Matt Johnson-Pint <matt.johnson-pint@sentry.io>
2022-11-09 10:19:48 -0800

    Fix integration disabled incorrectly

--- src/Sentry.DiagnosticSource/Internal/DiagnosticSource/SentryDiagnosticListenerIntegration.cs
+++ src/Sentry.DiagnosticSource/Internal/DiagnosticSource/SentryDiagnosticListenerIntegration.cs
@@ -11,9 +11,9 @@ internal class SentryDiagnosticListenerIntegration : ISdkIntegration
 
     public void Register(IHub hub, SentryOptions options)
     {
-        if (options.TracesSampleRate == 0)
+        if (options.TracesSampleRate == 0 && options.TracesSampler == null)
         {
-            options.Log(SentryLevel.Info, "DiagnosticSource Integration is now disabled due to TracesSampleRate being set to zero.");
+            options.Log(SentryLevel.Info, "DiagnosticSource Integration is now disabled due to TracesSampleRate being set to zero, and no TracesSampler set.");
             options.DisableDiagnosticSourceIntegration();
             return;
         }
