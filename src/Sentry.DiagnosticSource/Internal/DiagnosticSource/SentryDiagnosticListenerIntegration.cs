b8e01bf4c183b3727b54380bf677324bb94ea932
Matt Johnson-Pint <matt.johnson-pint@sentry.io>
2022-11-10 05:09:34 -0800

    Fix DiagnosticSource integration disabled incorrectly with TracesSampler (#2039)

--- src/Sentry.DiagnosticSource/Internal/DiagnosticSource/SentryDiagnosticListenerIntegration.cs
+++ src/Sentry.DiagnosticSource/Internal/DiagnosticSource/SentryDiagnosticListenerIntegration.cs
@@ -6,19 +6,16 @@ namespace Sentry.Internal.DiagnosticSource;
 
 internal class SentryDiagnosticListenerIntegration : ISdkIntegration
 {
-    private SentryDiagnosticSubscriber? _subscriber;
-    private IDisposable? _diagnosticListener;
-
     public void Register(IHub hub, SentryOptions options)
     {
-        if (options.TracesSampleRate == 0)
+        if (options.TracesSampleRate == 0 && options.TracesSampler == null)
         {
-            options.Log(SentryLevel.Info, "DiagnosticSource Integration is now disabled due to TracesSampleRate being set to zero.");
+            options.Log(SentryLevel.Info, "DiagnosticSource Integration is now disabled due to TracesSampleRate being set to zero, and no TracesSampler set.");
             options.DisableDiagnosticSourceIntegration();
             return;
         }
 
-        _subscriber = new SentryDiagnosticSubscriber(hub, options);
-        _diagnosticListener = DiagnosticListener.AllListeners.Subscribe(_subscriber);
+        var subscriber = new SentryDiagnosticSubscriber(hub, options);
+        DiagnosticListener.AllListeners.Subscribe(subscriber);
     }
 }
