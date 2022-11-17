ea91f58b992095dad5c8dc92ca7313da8efe4739
Ivan Dlugos <dlugos.ivan@gmail.com>
2022-11-17 15:18:07 +0100

    test: script to update verifier expected files

---
+++ scripts/accept-verifier-changes.ps1
@@ -0,0 +1,23 @@
+# Similar to `dotnet verify accept` but doesn't create new runtime-specific ".verified" files if a common one exists.
+param([switch] $DryRun)
+
+Set-StrictMode -Version Latest
+$ErrorActionPreference = "Stop"
+
+$receivedFiles = Get-ChildItem . -Recurse -File -Include "*.received.txt"
+
+$receivedFiles | ForEach-Object {
+    $nameWithRuntime = ($_.BaseName.Split(".") | Select-Object -SkipLast 1) -join "."
+    $nameWithoutRuntime = ($_.BaseName.Split(".") | Select-Object -SkipLast 2) -join "."
+    $ext = ".verified.txt"
+
+    $targetFile = (Test-Path "$($_.Directory)/$nameWithRuntime$ext") `
+        ? "$($_.Directory)/$nameWithRuntime$ext" `
+        : "$($_.Directory)/$nameWithoutRuntime$ext"
+
+    Write-Host "Updating $($targetFile.Replace((Get-Item .).FullName, ''))"
+    if (-not ($DryRun))
+    {
+        Move-Item $_ $targetFile -Force
+    }
+}
