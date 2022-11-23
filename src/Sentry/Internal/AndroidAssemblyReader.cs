73e8c02e002f6148084f33b4522b264ab26f7189
Ivan Dlugos <dlugos.ivan@gmail.com>
2022-11-23 17:14:31 +0100

    feat: assembly decompression

--- src/Sentry/Internal/AndroidAssemblyReader.cs
+++ src/Sentry/Internal/AndroidAssemblyReader.cs
@@ -1,6 +1,9 @@
 using System.IO.Compression;
 using System.Reflection.PortableExecutable;
 using Sentry.Extensibility;
+using K4os.Compression.LZ4;
+using System.Diagnostics;
+using System.Buffers;
 
 namespace Sentry.Internal;
 
@@ -14,7 +17,9 @@ internal sealed class AndroidAssemblyReaderFactory
     public static IAndroidAssemblyReader Open(string apkPath, IList<string> supportedAbis, IDiagnosticLogger? logger)
     {
         logger?.LogDebug("Opening APK: {0}", apkPath);
-        var zipArchive = ZipFile.OpenRead(apkPath);
+        // Note: We open in Update mode so that the streams returned by ZipArchiveEntry.Open() support seeking.
+        // We don't actually do any updates though.
+        var zipArchive = ZipFile.Open(apkPath, ZipArchiveMode.Update);
 
         if (zipArchive.GetEntry("assemblies/assemblies.manifest") is not null)
         {
@@ -31,9 +36,10 @@ internal sealed class AndroidAssemblyReaderFactory
 
 internal class AndroidAssemblyReader : IDisposable
 {
-    protected IDiagnosticLogger? _logger;
-    protected ZipArchive _zipArchive;
-    protected IList<string> _supportedAbis;
+    protected readonly IDiagnosticLogger? _logger;
+    protected readonly ZipArchive _zipArchive;
+    protected readonly IList<string> _supportedAbis;
+    private static readonly ArrayPool<byte> bytePool = ArrayPool<byte>.Shared;
 
     public AndroidAssemblyReader(ZipArchive zip, IList<string> supportedAbis, IDiagnosticLogger? logger)
     {
@@ -45,6 +51,68 @@ internal class AndroidAssemblyReader : IDisposable
     public void Dispose()
     {
         _zipArchive.Dispose();
+    }
+
+    protected PEReader CreatePEReader(string assemblyName, Stream assemblyDataStream)
+    {
+        var decompressedStream = TryDecompressLZ4(assemblyName, assemblyDataStream);
+        // Use the decompressed stream, or if null, i.e. it wasn't compressed, use the original.
+        return new(decompressedStream ?? assemblyDataStream);
+    }
+
+    /// <summary>
+    /// The DLL may be LZ4 compressed, see https://github.com/xamarin/xamarin-android/pull/4686
+    /// The format is:
+    ///    [ 4 byte magic header ] (XALZ)
+    ///    [ 4 byte header index ]
+    ///    [ 4 byte uncompressed payload length ]
+    ///    [rest: lz4 compressed payload]
+    /// </summary>
+    /// <seealso href="https://github.com/xamarin/xamarin-android/blob/c92702619f5fabcff0ed88e09160baf9edd70f41/tools/decompress-assemblies/main.cs#L26" />
+    private Stream? TryDecompressLZ4(string assemblyName, Stream inputStream)
+    {
+        const uint CompressedDataMagic = 0x5A4C4158; // 'XALZ', little-endian
+        var reader = new BinaryReader(inputStream);
+        if (reader.ReadUInt32() != CompressedDataMagic)
+        {
+            // Restore the input stream to the begininng if we're not decompressing.
+            inputStream.Position = 0;
+            return null;
+        }
+        reader.ReadUInt32(); // ignore descriptor index, we don't need it
+        var decompressedLength = reader.ReadInt32();
+
+        _logger?.LogDebug("Decompressing assembly ({0} bytes uncompressed) using LZ4", decompressedLength);
+
+        // copy the compressed data
+        Debug.Assert(inputStream.Position == 12);
+        var inputLength = (int)(inputStream.Length - 12);
+        var sourceBytes = bytePool.Rent(inputLength);
+        try
+        {
+            reader.Read(sourceBytes, 0, inputLength);
+
+            // We can release the reader & the input stream now.
+            reader.Dispose();
+            inputStream.Dispose();
+
+            var outputStream = new MemoryStream(decompressedLength);
+
+            // we're writing to the underlying array manually
+            outputStream.SetLength(decompressedLength);
+            var outputBuffer = outputStream.GetBuffer();
+
+            var decoded = LZ4Codec.Decode(sourceBytes, 0, inputLength, outputBuffer, 0, decompressedLength);
+            if (decoded != decompressedLength)
+            {
+                throw new Exception($"Failed to decompress LZ4 data of assembly {assemblyName} - decoded {decoded} instead of expected {decompressedLength} bytes");
+            }
+            return outputStream;
+        }
+        finally
+        {
+            bytePool.Return(sourceBytes);
+        }
     }
 }
 
@@ -77,23 +145,7 @@ internal sealed class AndroidAssemblyDirectoryReader : AndroidAssemblyReader, IA
         }
 
         _logger?.LogDebug("Resolved assembly {0} in the APK at {1}", name, zipEntry.FullName);
-
-        using var zipStream = zipEntry.Open();
-
-        // The DLL may be LZ4 compressed in addition to the APK being zipped.
-        // See https://github.com/xamarin/xamarin-android/pull/4686
-        // The format is:
-        //    [ 4 byte magic header ] (XALZ)
-        //    [ 4 byte header index ]
-        //    [ 4 byte uncompressed payload length ]
-        //    [rest: lz4 compressed payload]
-
-        // Unfortunately, we can't just return `new PEReader(zipEntry.Open())` because it requires a seekable stream.
-        // Therefore, we are making an in-memory copy of the DLL.
-        var memStream = new MemoryStream((int)zipEntry.Length);
-        zipStream.CopyTo(memStream);
-        memStream.Position = 0;
-        return new(memStream);
+        return CreatePEReader(name, zipEntry.Open());
     }
 
     private ZipArchiveEntry? FindAssembly(string name)
--- test/Sentry.Tests/Internals/AndroidAssemblyReaderTests.cs
+++ test/Sentry.Tests/Internals/AndroidAssemblyReaderTests.cs
@@ -53,17 +53,17 @@ public class AndroidAssemblyReaderTests
     }
 
     [Theory]
-    // [InlineData(false, true, "Mono.Android.dll")]
+    [InlineData(false, true, "Mono.Android.dll")]
     [InlineData(false, false, "Mono.Android.dll")]
-    // [InlineData(false, true, "System.Threading.dll")]
-    // [InlineData(false, false, "System.Threading.dll")]
+    [InlineData(false, true, "System.Threading.dll")]
+    [InlineData(false, false, "System.Threading.dll")]
     // [InlineData(true, true, "Mono.Android.dll")]
     // [InlineData(true, false, "Mono.Android.dll")]
     // [InlineData(true, true, "System.Threading.dll")]
     // [InlineData(true, false, "System.Threading.dll")]
-    public void ReadsAssembly(bool isAssemblyStore, bool compressed, string assemblyName)
+    public void ReadsAssembly(bool isAssemblyStore, bool isCompressed, string assemblyName)
     {
-        using var sut = GetSut(isAssemblyStore, compressed, supportedAbis: new() { "x86_64" });
+        using var sut = GetSut(isAssemblyStore, isCompressed, supportedAbis: new() { "x86_64" });
 
         var peReader = sut.TryReadAssembly(assemblyName);
         Assert.NotNull(peReader);
