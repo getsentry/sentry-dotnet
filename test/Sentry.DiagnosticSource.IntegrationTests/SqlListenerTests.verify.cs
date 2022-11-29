db0a8dc87a7dae1e2722e610cd102a4985326be8
Matt Johnson-Pint <matt.johnson-pint@sentry.io>
2022-11-29 12:18:43 -0800

    Capture db connection in integration tests

--- test/Sentry.DiagnosticSource.IntegrationTests/SqlListenerTests.verify.cs
+++ test/Sentry.DiagnosticSource.IntegrationTests/SqlListenerTests.verify.cs
@@ -37,8 +37,13 @@ public class SqlListenerTests : IClassFixture<LocalDbFixture>
             var transaction = hub.StartTransaction("my transaction", "my operation");
             hub.ConfigureScope(scope => scope.Transaction = transaction);
             hub.CaptureException(new("my exception"));
-            await TestDbBuilder.AddDataAsync(database);
-            await TestDbBuilder.GetDataAsync(database);
+
+            await using (var connection = await database.OpenNewConnection())
+            {
+                await TestDbBuilder.AddDataAsync(connection);
+                await TestDbBuilder.GetDataAsync(connection);
+            }
+
             transaction.Finish();
         }
 
@@ -70,44 +75,41 @@ public class SqlListenerTests : IClassFixture<LocalDbFixture>
         var options = new SentryLoggingOptions();
         ApplyOptions(options);
 
-        var loggerFactory = LoggerFactory.Create(_ => _.AddSentry(ApplyOptions));
-
-#if NET6_0_OR_GREATER
         await using var database = await _fixture.SqlInstance.Build();
-#else
-        using var database = await _fixture.SqlInstance.Build();
-#endif
-        var builder = new DbContextOptionsBuilder<TestDbContext>();
-        builder.UseSqlServer(database);
-        builder.UseLoggerFactory(loggerFactory);
-        builder.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
 
-        await using var dbContext = new TestDbContext(builder.Options);
-        dbContext.Add(new TestEntity
+        await using (var dbContext = TestDbBuilder.GetDbContext(database.Connection))
         {
-            Property = "Value"
-        });
-
-        await dbContext.SaveChangesAsync();
-
-        using (var hub = new Hub(options))
-        {
-            var transaction = hub.StartTransaction("my transaction", "my operation");
-            hub.ConfigureScope(scope => scope.Transaction = transaction);
-
             dbContext.Add(new TestEntity
             {
                 Property = "Value"
             });
 
-            try
+            await dbContext.SaveChangesAsync();
+        }
+
+        var loggerFactory = LoggerFactory.Create(_ => _.AddSentry(ApplyOptions));
+        using (var hub = new Hub(options))
+        {
+            var transaction = hub.StartTransaction("my transaction", "my operation");
+            hub.ConfigureScope(scope => scope.Transaction = transaction);
+
+            await using (var connection = await database.OpenNewConnection())
+            await using (var dbContext = TestDbBuilder.GetDbContext(connection, loggerFactory))
             {
-                await dbContext.SaveChangesAsync();
-            }
-            catch
-            {
-                // Suppress the exception so we can test that we received the error through logging.
-                // Note, this uses the Sentry.Extensions.Logging integration.
+                dbContext.Add(new TestEntity
+                {
+                    Property = "Value"
+                });
+
+                try
+                {
+                    await dbContext.SaveChangesAsync();
+                }
+                catch
+                {
+                    // Suppress the exception so we can test that we received the error through logging.
+                    // Note, this uses the Sentry.Extensions.Logging integration.
+                }
             }
 
             transaction.Finish();
@@ -191,8 +193,17 @@ public class SqlListenerTests : IClassFixture<LocalDbFixture>
             var transaction = hub.StartTransaction("my transaction", "my operation");
             hub.ConfigureScope(scope => scope.Transaction = transaction);
             hub.CaptureException(new("my exception"));
-            await TestDbBuilder.AddEfDataAsync(database);
-            await TestDbBuilder.GetEfDataAsync(database);
+
+#if NETCOREAPP
+            await using (var connection = await database.OpenNewConnection())
+#else
+            using (var connection = await database.OpenNewConnection())
+#endif
+            {
+                await TestDbBuilder.AddEfDataAsync(connection);
+                await TestDbBuilder.GetEfDataAsync(connection);
+            }
+
             transaction.Finish();
         }
 
