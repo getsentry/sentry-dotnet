c050bd3745a16fe9223a25db08f04d82344305e2
Matt Johnson-Pint <matt.johnson-pint@sentry.io>
2022-12-20 13:26:41 -0800

    Fix hash case

--- src/Sentry/Internal/Extensions/HashExtensions.cs
+++ src/Sentry/Internal/Extensions/HashExtensions.cs
@@ -2,11 +2,11 @@ namespace Sentry.Internal.Extensions;
 
 internal static class HashExtensions
 {
-    public static string GetHashString(this string str)
+    public static string GetHashString(this string str, bool upperCase = true)
     {
         var bytes = Encoding.UTF8.GetBytes(str);
         using var sha = SHA1.Create();
         var hash = sha.ComputeHash(bytes);
-        return hash.ToHexString();
+        return hash.ToHexString(upperCase);
     }
 }
