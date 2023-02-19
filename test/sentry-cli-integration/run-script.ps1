ac8d2692c91f1ea2d575eef067fcf82b1b0bde0e
Ivan Dlugos <dlugos.ivan@gmail.com>
2023-02-19 21:09:04 +0100

    fix: symbol upoad test

--- test/sentry-cli-integration/run-script.ps1
+++ test/sentry-cli-integration/run-script.ps1
@@ -1,1 +1,7 @@
-dotnet build samples/Sentry.Samples.Maui/Sentry.Samples.Maui.csproj -c Release --no-restore --nologo /p:CopyLocalLockFileAssemblies=true
+param(
+    [Parameter(Mandatory = $true)][string] $ServerUri
+)
+
+$env:SENTRY_URL = $ServerUri
+dotnet build samples/Sentry.Samples.Maui/Sentry.Samples.Maui.csproj -c Release --no-restore --nologo `
+    /p:CopyLocalLockFileAssemblies=true /p:SentryOrg=org /p:SentryProject=project
