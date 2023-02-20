a63dd506d64829fc34a4bf3cfefaaa972618c179
Ivan Dlugos <dlugos.ivan@gmail.com>
2023-02-20 17:32:11 +0100

    chore: fixup integration test

--- test/sentry-cli-integration/run-script.ps1
+++ test/sentry-cli-integration/run-script.ps1
@@ -2,6 +2,23 @@ param(
     [Parameter(Mandatory = $true)][string] $ServerUri
 )
 
-$env:SENTRY_URL = $ServerUri
-dotnet build samples/Sentry.Samples.Maui/Sentry.Samples.Maui.csproj -c Release --no-restore --nologo `
-    /p:CopyLocalLockFileAssemblies=true /p:SentryOrg=org /p:SentryProject=project
+$sample = 'Sentry.Samples.Console.Basic'
+$rootDir = "$(Get-Item $PSScriptRoot/../../)"
+
+dotnet build "samples/$sample/$sample.csproj" -c Release --framework net6.0 --no-restore --nologo `
+    /p:CopyLocalLockFileAssemblies=true `
+    /p:SentryOrg=org `
+    /p:SentryProject=project `
+    /p:SentryUrl=$ServerUri `
+    /p:SentryAuthToken=dummy `
+| ForEach-Object {
+    if ($_ -match "^Time Elapsed ")
+    {
+        "Time Elapsed [value removed]"
+    }
+    else
+    {
+        "$_".Replace($rootDir, '').Replace('\', '/')
+    }
+}
+
