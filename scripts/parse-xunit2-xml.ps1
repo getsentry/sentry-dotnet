1fb0d2c6fe4b19c5a5c21984d9dbe7570abbcae6
Matt Johnson-Pint <matt.johnson-pint@sentry.io>
2023-02-26 11:53:41 -0800

    Just warn if file not found

--- scripts/parse-xunit2-xml.ps1
+++ scripts/parse-xunit2-xml.ps1
@@ -2,6 +2,14 @@ param([string] $File)
 
 Set-StrictMode -Version Latest
 
+if (!Test-Path($File))
+{
+    Write-Warning "$File was not found."
+
+    # Return success exit code so that GitHub Actions highlights the failure in the test run, rather than in this script.
+    return
+}
+
 [xml]$xml = Get-Content $File
 
 function ElementText([System.Xml.XmlElement] $element)
