7627ad2390bb3c7933f40f2db0f48d7afd0d23e1
Matt Johnson-Pint <matt.johnson-pint@sentry.io>
2023-02-27 18:24:53 -0800

    Fix build failures

--- src/Sentry.EntityFramework/ErrorProcessors/DbEntityValidationExceptionProcessor.cs
+++ src/Sentry.EntityFramework/ErrorProcessors/DbEntityValidationExceptionProcessor.cs
@@ -8,7 +8,7 @@ public class DbEntityValidationExceptionProcessor : SentryEventExceptionProcesso
     /// <summary>
     /// Extracts details from <see cref="DbEntityValidationException"/> into the <see cref="SentryEvent"/>.
     /// </summary>
-    protected override void ProcessException(DbEntityValidationException exception, SentryEvent sentryEvent)
+    protected internal override void ProcessException(DbEntityValidationException exception, SentryEvent sentryEvent)
     {
         var errorList = new Dictionary<string, List<string>>();
         foreach (var error in exception.EntityValidationErrors.SelectMany(x => x.ValidationErrors))
