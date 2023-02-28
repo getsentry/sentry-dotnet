16984f66602e129168ca93ad209598272661c619
Ivan Dlugos <dlugos.ivan@gmail.com>
2023-02-28 13:05:55 +0100

    feat: internal array types

---
+++ src/Sentry/Internal/SparseArray.cs
@@ -0,0 +1,100 @@
+namespace Sentry.Internal;
+
+/// <summary>
+/// Sparse array for scalars (value types). You must provide the uninitialized value that will be used for new unused elements.
+/// </summary>
+/// <typeparam name="T"></typeparam>
+internal sealed class SparseScalarArray<T> where T : IEquatable<T>
+{
+    private GrowableArray<T> _items;
+    private T _uninitializedValue;
+
+    public SparseScalarArray(T uninitializedValue)
+    {
+        _items = new GrowableArray<T>();
+        _uninitializedValue = uninitializedValue;
+    }
+
+    public SparseScalarArray(T uninitializedValue, int capacity)
+    {
+        _items = new GrowableArray<T>(capacity);
+        _uninitializedValue = uninitializedValue;
+    }
+
+    public T this[int index]
+    {
+        get
+        {
+            return _items[index];
+        }
+        set
+        {
+            // Increase the capacity of the sparse array so that the key can fit.
+            while (_items.Count <= index)
+            {
+                _items.Add(_uninitializedValue);
+            }
+            _items[index] = value;
+        }
+    }
+
+    public bool ContainsKey(int key)
+    {
+        return key > 0 && key < _items.Count && !_uninitializedValue.Equals(_items[key]);
+    }
+}
+
+/// <summary>
+/// Sparse array for objects. Null value is considered a missing item.
+/// </summary>
+/// <typeparam name="T"></typeparam>
+internal sealed class SparseObjectArray<T> where T : class
+{
+    private GrowableArray<T?> _items;
+
+    public SparseObjectArray()
+    {
+        _items = new GrowableArray<T?>();
+    }
+
+    public SparseObjectArray(int capacity)
+    {
+        _items = new GrowableArray<T?>(capacity);
+    }
+
+    public T? this[int index]
+    {
+        get
+        {
+            return _items[index];
+        }
+        set
+        {
+            // Increase the capacity of the sparse array so that the key can fit.
+            while (_items.Count <= index)
+            {
+                _items.Add(null);
+            }
+            _items[index] = value;
+        }
+    }
+
+    public bool ContainsKey(int key)
+    {
+        return key > 0 && key < _items.Count && _items[key] is not null;
+    }
+
+    /// <summary>
+    /// Executes 'func(key, value)' for each element present.
+    /// </summary>
+    public void Foreach(Action<int, T> func)
+    {
+        for (int i = 0; i < _items.Count; i++)
+        {
+            if (_items[i] is { } value)
+            {
+                func(i, value);
+            }
+        }
+    }
+}
