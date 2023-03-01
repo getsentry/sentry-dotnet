f6a13b799ae96733f47a45321b7da900908a68bb
Matt Johnson-Pint <matt.johnson-pint@sentry.io>
2023-03-01 10:51:16 -0800

    add comment

--- src/Sentry.Bindings.Cocoa/PrivateApiDefinitions.cs
+++ src/Sentry.Bindings.Cocoa/PrivateApiDefinitions.cs
@@ -4,6 +4,8 @@ using ObjCRuntime;
 
 namespace Sentry.CocoaSdk;
 
+// SentryScope.ApplyToEvent was made private in 8.x, but we use it in our IosEventProcessor class.
+// TODO: Find a better way than using the private API.
 partial interface SentryScope
 {
     // -(SentryEvent * _Nullable)applyToEvent:(SentryEvent * _Nonnull)event maxBreadcrumb:(NSUInteger)maxBreadcrumbs;
