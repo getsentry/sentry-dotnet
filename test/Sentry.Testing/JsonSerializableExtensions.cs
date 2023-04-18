e4ebf99c146536f3dd0644df58feb07e9b4b9780
Matt Johnson-Pint <matt.johnson-pint@sentry.io>
2023-04-17 21:22:23 -0700

    Support indenting JSON strings

--- test/Sentry.Testing/JsonSerializableExtensions.cs
+++ test/Sentry.Testing/JsonSerializableExtensions.cs
@@ -1,18 +1,23 @@
 internal static class JsonSerializableExtensions
 {
-    public static string ToJsonString(this IJsonSerializable serializable, IDiagnosticLogger logger = null) =>
-        WriteToJsonString(writer => writer.WriteSerializableValue(serializable, logger));
+    public static string ToJsonString(this IJsonSerializable serializable, IDiagnosticLogger logger = null, bool indented = false) =>
+        WriteToJsonString(writer => writer.WriteSerializableValue(serializable, logger), indented);
 
-    public static string ToJsonString(this object @object, IDiagnosticLogger logger = null) =>
-        WriteToJsonString(writer => writer.WriteDynamicValue(@object, logger));
+    public static string ToJsonString(this object @object, IDiagnosticLogger logger = null, bool indented = false) =>
+        WriteToJsonString(writer => writer.WriteDynamicValue(@object, logger), indented);
 
-    private static string WriteToJsonString(Action<Utf8JsonWriter> writeAction)
+    private static string WriteToJsonString(Action<Utf8JsonWriter> writeAction, bool indented)
     {
+        var options = new JsonWriterOptions
+        {
+            Indented = indented
+        };
+
 #if NETCOREAPP3_0_OR_GREATER
         // This implementation is better, as it uses fewer allocations
         var buffer = new ArrayBufferWriter<byte>();
 
-        using var writer = new Utf8JsonWriter(buffer);
+        using var writer = new Utf8JsonWriter(buffer, options);
         writeAction(writer);
         writer.Flush();
 
@@ -21,7 +26,7 @@ internal static class JsonSerializableExtensions
         // This implementation is compatible with older targets
         using var stream = new MemoryStream();
 
-        using var writer = new Utf8JsonWriter(stream);
+        using var writer = new Utf8JsonWriter(stream, options);
         writeAction(writer);
         writer.Flush();
 
