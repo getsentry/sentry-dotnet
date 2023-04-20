38ea9cc27f5103c2246b00896abaceab31ae921f
Matt Johnson-Pint <matt.johnson-pint@sentry.io>
2023-04-19 17:36:13 -0700

    Remove duplicate DSN attributes

--- samples/Sentry.Samples.Console.Profiling/Program.cs
+++ samples/Sentry.Samples.Console.Profiling/Program.cs
@@ -4,10 +4,6 @@ using Sentry;
 using Sentry.Extensibility;
 using Sentry.Profiling;
 
-// One of the ways to set your DSN is via an attribute:
-// It could be set via AssemblyInfo.cs and patched via CI.
-// Other ways are via environment variable, configuration files and explicitly via parameter to Init
-[assembly: Dsn(Program.DefaultDsn)]
 // Tracks the release which sent the event and enables more features: https://docs.sentry.io/learn/releases/
 // Much like the DSN above, this is only one of the ways to define the release.
 // If not set here, it can also be defined via appsettings.json, environment variable 'SENTRY_RELEASE' and AssemblyVersion
@@ -16,10 +12,6 @@ using Sentry.Profiling;
 
 internal static class Program
 {
-    public const string DefaultDsn = "https://eb18e953812b41c3aeb042e666fd3b5c@o447951.ingest.sentry.io/5428537";
-    // A different DSN for a section of the app (i.e: admin)
-    public const string AdminDsn = "https://f670c444cca14cf2bb4bfc403525b6a3@sentry.io/259314";
-
     private static async Task Main()
     {
         // When the SDK is disabled, no callback is executed:
@@ -34,6 +26,11 @@ internal static class Program
         // Enable the SDK
         using (SentrySdk.Init(o =>
         {
+            // A Sentry Data Source Name (DSN) is required.
+            // See https://docs.sentry.io/product/sentry-basics/dsn-explainer/
+            // You can set it in the SENTRY_DSN environment variable, or you can set it in code here.
+            // o.Dsn = "... Your DSN ...";
+
             // Send stack trace for events that were not created from an exception
             // e.g: CaptureMessage, log.LogDebug, log.LogInformation ...
             o.AttachStacktrace = true;
@@ -200,7 +197,8 @@ internal static class Program
             evt.Level = SentryLevel.Debug;
             SentrySdk.CaptureEvent(evt);
 
-            // Using a different DSN:
+            // Using a different DSN for a section of the app (i.e: admin)
+            const string AdminDsn = "https://f670c444cca14cf2bb4bfc403525b6a3@sentry.io/259314";
             using (var adminClient = new SentryClient(new SentryOptions { Dsn = AdminDsn }))
             {
                 // Make believe web framework middleware
