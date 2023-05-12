4e285e1778ba5bb6bceb600a0a6a34929e20ac01
Sean Feldman <feldman.sean@gmail.com>
2023-05-12 02:42:50 -0600

    Remove commented out code

--- src/Sentry.AzureFunctions.Worker/SentryFunctionsWorkerMiddleware.cs
+++ src/Sentry.AzureFunctions.Worker/SentryFunctionsWorkerMiddleware.cs
@@ -25,16 +25,6 @@ internal class SentryFunctionsWorkerMiddleware : IFunctionsWorkerMiddleware
             {
                 scope.Transaction = transaction;
 
-                // TODO: remove when SentryLogger supports filtering out tags option
-                // https://github.com/getsentry/sentry-dotnet/pull/2346#discussion_r1188143329
-                // AzureFunctions_FunctionName and AzureFunctions_InvocationId are already set by the time we get here.
-                // Clear those and replace with "function" scope context.
-                // scope.UnsetTag("AzureFunctions_FunctionName");
-                // scope.UnsetTag("AzureFunctions_InvocationId");
-
-                // TODO: consider adding "functionName" single tag
-                // scope.SetTag("functionName", context.FunctionDefinition.Name);
-
                 scope.Contexts["function"] = new Dictionary<string, string>
                 {
                     { "name", context.FunctionDefinition.Name },
