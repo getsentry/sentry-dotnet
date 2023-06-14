d9aa461df21d7620e0407ad271ee8b432b7b8cd2
James Crosswell <jamescrosswell@users.noreply.github.com>
2023-06-14 14:22:47 +1200

    WIP - pushing to try on Windows instead.

---
+++ samples/Sentry.Samples.SingleFileApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using Sentry;
+using System.Diagnostics;
+
+// This gives us a chance to attach a debugger to the process when running the published app from the Debug directory.
+// When doing that, you probably want to build and run the app in Debug mode from the command line:
+//   dotnet publish -c Debug
+//   ./bin/Debug/net7.0/osx-arm64/publish/Sentry.Samples.SingleFileApp
+Console.WriteLine("Current process ID: " + Process.GetCurrentProcess().Id);
+Console.WriteLine("You can take our lives, but you can never take...");
+Console.ReadKey();
+
+try
+{
+    Console.WriteLine("OUR SYMBOLICATION!");
+    throw new Exception("Line numbers please.");
+}
+catch (Exception ex)
+{
+    SentrySdk.CaptureException(ex);
+}
+finally
+{
+    SentrySdk.FlushAsync(TimeSpan.FromSeconds(2)).Wait();
+}
