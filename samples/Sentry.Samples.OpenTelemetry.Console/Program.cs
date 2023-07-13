a023841a5fccc36fc95fba6f67c2084110f0282b
bitsandfoxes <reg@bitfox.at>
2023-07-13 11:10:17 +0200

    added simple console project

---
+++ samples/Sentry.Samples.OpenTelemetry.Console/Program.cs
@@ -0,0 +1,33 @@
+﻿/*
+ * This sample demonstrates how to initialize and enable Open Telemetry with Sentry
+ * in a console application.
+ * For using Open Telemetry and Sentry in ASP.NET, see Sentry.Samples.OpenTelemetry.AspNet.
+ * For using Open Telemetry and Sentry in ASP.NET Core, see Sentry.Samples.OpenTelemetry.AspNetCore.
+ */
+
+using OpenTelemetry;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+using Sentry;
+using Sentry.OpenTelemetry;
+
+var serviceName = "Sentry.Samples.OpenTelemetry.Console";
+var serviceVersion = "1.0.0";
+
+using var tracerProvider = Sdk.CreateTracerProviderBuilder()
+    .AddSource(serviceName)
+    .ConfigureResource(resource =>
+        resource.AddService(
+            serviceName: serviceName,
+            serviceVersion: serviceVersion))
+    .AddSentry()    // <-- Configure OpenTelemetry to send traces to Sentry
+    .Build();
+
+
+SentrySdk.Init(o =>
+{
+    // options.Dsn = "... Your DSN ...";
+    o.UseOpenTelemetry(); // <-- Configure Sentry to use OpenTelemetry trace information
+});
+
+//
