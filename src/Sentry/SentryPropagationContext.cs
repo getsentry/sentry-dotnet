3ec17d73476501258044e2211f2d1d7e7035bde1
bitsandfoxes <reg@bitfox.at>
2023-07-18 18:40:10 +0200

    added propagation context

---
+++ src/Sentry/SentryPropagationContext.cs
@@ -0,0 +1,45 @@
+namespace Sentry;
+
+internal class SentryPropagationContext
+{
+    public SentryId TraceId { get; }
+    public SpanId SpanId { get; }
+    public SpanId? ParentSpanId { get; }
+    public DynamicSamplingContext? DynamicSamplingContext { get; }
+
+    private SentryPropagationContext(
+        SentryTraceHeader traceHeader,
+        SpanId? parentSpanId,
+        DynamicSamplingContext? dynamicSamplingContext)
+    {
+        TraceId = traceHeader.TraceId;
+        SpanId = traceHeader.SpanId;
+        ParentSpanId = parentSpanId;
+        DynamicSamplingContext = dynamicSamplingContext;
+    }
+
+    private SentryPropagationContext()
+    {
+        TraceId = new SentryId();
+        SpanId = new SpanId();
+    }
+
+    public static SentryPropagationContext CreateFromHeaders(string? sentryTraceHeader, string? baggageHeadersString)
+    {
+        if (sentryTraceHeader == null)
+        {
+            return new SentryPropagationContext();
+        }
+
+        var traceHeader = SentryTraceHeader.Parse(sentryTraceHeader);
+
+        DynamicSamplingContext? dynamicSamplingContext = null;
+        if (baggageHeadersString is not null)
+        {
+            var baggageHeader = BaggageHeader.TryParse(baggageHeadersString);
+            dynamicSamplingContext = baggageHeader?.CreateDynamicSamplingContext();
+        }
+
+        return new SentryPropagationContext(traceHeader, null, dynamicSamplingContext);
+    }
+}
