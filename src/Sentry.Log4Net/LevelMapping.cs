51437be6398c9426a8a44fa76a97145bef78799f
Seweryn Presnal <IhateTrains@users.noreply.github.com>
2023-07-29 11:58:57 +0200

    Convert log4net Level.All to BreadcrumbLevel.Debug

--- src/Sentry.Log4Net/LevelMapping.cs
+++ src/Sentry.Log4Net/LevelMapping.cs
@@ -20,12 +20,12 @@ internal static class LevelMapping
     {
         return loggingLevel.Level switch
         {
-            var l when l == Level.Fatal || l == Level.Emergency || l == Level.All => BreadcrumbLevel.Critical,
+            var l when l == Level.Fatal || l == Level.Emergency => BreadcrumbLevel.Critical,
             var l when l == Level.Alert || l == Level.Critical || l == Level.Severe || l == Level.Error => BreadcrumbLevel.Error,
             var l when l == Level.Warn => BreadcrumbLevel.Warning,
             var l when l == Level.Notice || l == Level.Info => BreadcrumbLevel.Info,
             var l when l == Level.Debug || l == Level.Verbose || l == Level.Trace || l == Level.Finer || l == Level.Finest ||
-                       l == Level.Fine => BreadcrumbLevel.Debug,
+                       l == Level.Fine || l == Level.All => BreadcrumbLevel.Debug,
             _ => null
         };
     }
