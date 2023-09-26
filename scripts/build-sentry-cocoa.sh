a8a46e48f206c132a068d90dc27142985e7a6a40
bitsandfoxes <reg@bitfox.at>
2023-09-26 18:52:21 +0200

    updated the build cocoa script

--- scripts/build-sentry-cocoa.sh
+++ scripts/build-sentry-cocoa.sh
@@ -1,77 +1,67 @@
 #!/bin/bash
 
 pushd "$(dirname "$0")" > /dev/null
-cd ../modules
-
-# We need a custom build of Carthage that supports building XCFrameworks for Mac Catalyst,
-# so we'll bring it in as a submodule and build it here.
-# See https://github.com/getsentry/sentry-cocoa/issues/2031
-# and https://github.com/Carthage/Carthage/pull/3235
-
-CARTHAGE=Carthage/.build/debug/carthage
-
-if [ ! -f $CARTHAGE ]; then
-    cd Carthage
-    echo "---------- Building Carthage ---------- "
-    make all
-    echo ""
-    cd ..
-fi
-
-rollbackSchemes() {
-    mv Sentry.xcodeproj/xcshareddata/xcschemes/SentryPrivate.xcscheme.bak Sentry.xcodeproj/xcshareddata/xcschemes/SentryPrivate.xcscheme
-    mv Sentry.xcodeproj/xcshareddata/xcschemes/SentrySwiftUI.xcscheme.bak Sentry.xcodeproj/xcshareddata/xcschemes/SentrySwiftUI.xcscheme
-}
+cd ../modules/sentry-cocoa
 
 if [ $? -eq 0 ]
 then
-    cd sentry-cocoa
-
-    # Keep track of the submodule's SHA so we only build when we need to
     SHA=$(git rev-parse HEAD)
-    SHAFILE=Carthage/.built-from-sha
+    SHAFILE=./Carthage/.built-from-sha
     [ -f $SHAFILE ] && SHAFROMFILE=$(<$SHAFILE)
     VERSION="$(git describe --tags) ($(git rev-parse --short HEAD))"
 
     if [ "$SHA" == "$SHAFROMFILE" ]; then
         echo "Sentry Cocoa SDK $VERSION was already built"
     else
-        echo "---------- Building Sentry Cocoa SDK $VERSION ---------- "
+        echo "Building Sentry Cocoa SDK $VERSION"
         rm -rf Carthage
 
-        # Delete SentryPrivate and SentrySwiftUI schemes
-        # we dont want to build them
-
-        mv Sentry.xcodeproj/xcshareddata/xcschemes/SentryPrivate.xcscheme Sentry.xcodeproj/xcshareddata/xcschemes/SentryPrivate.xcscheme.bak
-        mv Sentry.xcodeproj/xcshareddata/xcschemes/SentrySwiftUI.xcscheme Sentry.xcodeproj/xcshareddata/xcschemes/SentrySwiftUI.xcscheme.bak
-
-        trap 'rollbackSchemes; exit 1' SIGINT
+        # Grabbing the first SDK versions
+        sdks=$(xcodebuild -showsdks)
+        ios_sdk=$(echo "$sdks" | awk '/iOS SDKs/{getline; print $NF}')
+        ios_simulator_sdk=$(echo "$sdks" | awk '/iOS Simulator SDKs/{getline; print $NF}')
 
         # Note - We keep the build output in separate directories so that .NET
         # bundles iOS with net6.0-ios and Mac Catalyst with net6.0-maccatalyst.
         # The lack of symlinks in the ios builds, means we should also be able
         # to use the package on Windows with "Pair to Mac".
 
-        # Build for iOS.  We'll get both ios and ios-simulator from this.
-        ../$CARTHAGE build --use-xcframeworks --no-skip-current --platform ios
-        mv Carthage/Build Carthage/Build-ios
+        # Build for iOS and iOS simulator.
+        xcodebuild -project Sentry.xcodeproj \
+            -scheme Sentry \
+            -configuration Release \
+            -sdk "$ios_sdk" \
+            -derivedDataPath ./Carthage/output-ios
+        xcodebuild -project Sentry.xcodeproj \
+            -scheme Sentry \
+            -configuration Release \
+            -sdk "$ios_simulator_sdk" \
+            -derivedDataPath ./Carthage/output-ios
+        xcodebuild -create-xcframework \
+            -framework ./Carthage/output-ios/Build/Products/Release-iphoneos/Sentry.framework \
+            -framework ./Carthage/output-ios/Build/Products/Release-iphonesimulator/Sentry.framework \
+            -output ./Carthage/Build-ios/Sentry.xcframework
 
-        # Separately, build for Mac Catalyst in its own directory.
-        ../$CARTHAGE build --use-xcframeworks --no-skip-current --platform macCatalyst
-        mv Carthage/Build Carthage/Build-maccatalyst
+        # Separately, build for Mac Catalyst
+        xcodebuild -project Sentry.xcodeproj \
+            -scheme Sentry \
+            -configuration Release \
+            -destination 'platform=macOS,variant=Mac Catalyst' \
+            -derivedDataPath ./Carthage/output-maccatalyst
+        xcodebuild -create-xcframework \
+            -framework ./Carthage/output-maccatalyst/Build/Products/Release-maccatalyst/Sentry.framework \
+            -output ./Carthage/Build-maccatalyst/Sentry.xcframework
 
         # Copy headers - used for generating bindings
         mkdir Carthage/Headers
         find Carthage/Build-ios/Sentry.xcframework/ios-arm64 -name '*.h' -exec cp {} Carthage/Headers \;
 
-        echo $SHA > $SHAFILE
+        echo "$SHA" > "$SHAFILE"
         echo ""
     fi
 
     # Remove anything we don't want to bundle in the nuget package.
     find Carthage/Build* \( -name Headers -o -name PrivateHeaders -o -name Modules \) -exec rm -rf {} +
-
-    rollbackSchemes
 fi
 
 popd > /dev/null
