3a91104a6ca53cc44601c2ecf2f96d7f70c5b0d6
James Crosswell <jamescrosswell@users.noreply.github.com>
2023-10-12 11:11:10 +1300

    Made ISentryUserFactory public

--- src/Sentry/ISentryUserFactory.cs
+++ src/Sentry/ISentryUserFactory.cs
@@ -1,6 +1,13 @@
 namespace Sentry;
 
-internal interface ISentryUserFactory
+/// <summary>
+/// Sentry User Factory
+/// </summary>
+public interface ISentryUserFactory
 {
+    /// <summary>
+    /// Creates a Sentry <see cref="User"/> representing the current principal.
+    /// </summary>
+    /// <returns>The protocol user</returns>
     public User? Create();
 }
