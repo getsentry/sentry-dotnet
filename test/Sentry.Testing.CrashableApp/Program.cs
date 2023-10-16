146f2a0d3b1414f414d6767061240b7509274850
Ivan Dlugos <dlugos.ivan@gmail.com>
2023-10-16 14:06:43 +0200

    chore: remove unused code

--- test/Sentry.Testing.CrashableApp/Program.cs
+++ test/Sentry.Testing.CrashableApp/Program.cs
@@ -6,18 +6,7 @@ public static class Program
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine($"Running CrashableApp with {args[0]}");
         var crashType = (CrashType)Enum.Parse(typeof(CrashType), args[0]);
-
-        // // Enable the SDK
-        // using var _ = SentrySdk.Init(options =>
-        // {
-        //     options.Dsn = "https://eb18e953812b41c3aeb042e666fd3b5c@o447951.ingest.sentry.io/5428537";
-        //     options.Debug = true;
-        //     options.IsGlobalModeEnabled = true;
-        // });
-
-        Console.WriteLine($"Crashing with {crashType}");
         SentrySdk.CauseCrash(crashType);
     }
 }
