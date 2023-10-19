7150f8c8488e85ed7e53225bf6cefa184ae58283
Ivan Dlugos <dlugos.ivan@gmail.com>
2023-10-19 20:02:48 +0200

    support solution filters in dotnet workload restore

--- scripts/dotnet-workload-restore.ps1
+++ scripts/dotnet-workload-restore.ps1
@@ -1,6 +1,5 @@
 # `dotnet workload restore` sometimes doesn't fetch all workloads on the first run
 # This script runs the same command until it stops fetching new workloads
-# Note: the parameter cannot be a solution filter - it's not supported by `dotnet workload restore`.
 param([string] $ProjectOrSolution = 'Sentry.sln')
 
 Set-StrictMode -Version latest
@@ -8,13 +7,14 @@ $ErrorActionPreference = "Stop"
 
 
 # Restore workloads and return the list of installed ones.
-$dotnetWorkloadRestore = {
+function DotnetWorkloadRestore([string] $target)
+{
     $matchText = "Successfully installed workload\(s\) "
-    Write-Host "::group::Restoring workloads for $ProjectOrSolution"
+    Write-Host "::group::Restoring workloads for $target"
     try
     {
         $tempArg = (Test-Path env:RUNNER_TEMP) ? @("--temp-dir", $env:RUNNER_TEMP) : ''
-        dotnet workload restore $ProjectOrSolution --from-rollback-file rollback.json $tempArg --nologo | ForEach-Object {
+        dotnet workload restore $target --from-rollback-file rollback.json $tempArg --nologo | ForEach-Object {
             Write-Host "  $_"
             if ($_ -match $matchText)
             {
@@ -32,24 +32,35 @@ $dotnetWorkloadRestore = {
     }
 }
 
-$maxRuns = 5
-$prevInstalled = @();
-while ($true)
+# Solution filters are not accepted by `dotnet workload restore`. See https://github.com/dotnet/sdk/issues/36277
+$projects = @($ProjectOrSolution)
+if ($ProjectOrSolution -match "\.slnf$")
 {
-    $installed = $dotnetWorkloadRestore.Invoke()
-    if ("$installed" -eq "")
+    $slnf = Get-Content -Raw .\Sentry-CI-Build-Windows.slnf | ConvertFrom-Json
+    $projects = $slnf.solution.projects 
+}
+
+$prevInstalled = @();
+$projects | ForEach-Object {
+    $project = $_
+    $maxRuns = 5
+    while ($true)
     {
-        throw "Installation error"
-    }
-    elseif ("$installed" -eq "$prevInstalled")
-    {
-        Write-Host "No new packages installed since the previous run, stopping."
-        break
-    }
-    $prevInstalled = $installed
+        $installed = DotnetWorkloadRestore $project
+        if ("$installed" -eq "")
+        {
+            throw "Installation error"
+        }
+        elseif ("$installed" -eq "$prevInstalled")
+        {
+            Write-Host "No new packages installed since the previous run, stopping."
+            break
+        }
+        $prevInstalled = $installed
     
-    if (--$maxRuns -le 0)
-    {
-        throw "Too many retries to install all workloads"
+        if (--$maxRuns -le 0)
+        {
+            throw "Too many retries to install all workloads"
+        }
     }
 }
