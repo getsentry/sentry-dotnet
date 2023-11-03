7aeecfcbd9ad3518d742192cb2550ef34ec9956a
Ivan Dlugos <dlugos.ivan@gmail.com>
2023-11-03 09:25:19 +0100

    build sentry-native in CI

---
+++ scripts/build-sentry-native.ps1
@@ -0,0 +1,70 @@
+param([switch] $Clean)
+Set-StrictMode -Version Latest
+$ErrorActionPreference = "Stop"
+
+Push-Location $PSScriptRoot/..
+try
+{
+    $submodule = 'modules/sentry-native'
+    $outDir = 'src/Sentry/Platforms/Native/runtimes'
+    $buildDir = "$submodule/build"
+
+    $additionalArgs = @()
+    $libPrefix = 'lib'
+    $libExtension = '.a'
+    if ($IsMacOS)
+    {
+        $outDir += '/osx/native'
+        $additionalArgs += @('-D', 'CMAKE_OSX_ARCHITECTURES=arm64;x86_64')
+    }
+    elseif ($IsWindows)
+    {
+        $outDir += '/win-x64/native'
+        $additionalArgs += @('-C', 'src/Sentry/Platforms/Native/windows-config.cmake')
+        $libPrefix = ''
+        $libExtension = '.lib'
+    }
+    elseif ($IsLinux)
+    {
+        $outDir += '/linux-x64/native'
+    }
+    else
+    {
+        throw "Unsupported platform"
+    }
+
+    git submodule update --init --recursive $submodule
+
+    if ($Clean)
+    {
+        rm -rf $buildDir
+    }
+
+    cmake `
+        -S $submodule `
+        -B $buildDir `
+        -D CMAKE_BUILD_TYPE=RelWithDebInfo `
+        -D SENTRY_SDK_NAME=sentry.native.dotnet `
+        -D SENTRY_BUILD_SHARED_LIBS=0 `
+        -D SENTRY_BACKEND=none `
+        -D SENTRY_TRANSPORT=none `
+        $additionalArgs
+
+    cmake `
+        --build $buildDir `
+        --target sentry `
+        --config RelWithDebInfo `
+        --parallel
+
+    $srcFile = "$buildDir/${libPrefix}sentry$libExtension"
+    $outFile = "$outDir/${libPrefix}sentry-native$libExtension"
+    Write-Host "Copying $srcFile to $outFile"
+    Copy-Item -Force -Path $srcFile -Destination $outFile
+
+    # Touch the file to mark it as up-to-date for MSBuild
+    (Get-Item $outFile).LastWriteTime = Get-Date
+}
+finally
+{
+    Pop-Location
+}
