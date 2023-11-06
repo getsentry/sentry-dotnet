70f7706825bce49c47d9aa3c1d6db9575ffe55eb
Ivan Dlugos <dlugos.ivan@gmail.com>
2023-11-06 13:21:07 +0100

    integration test fixes

--- test/aot-integration.Tests.ps1
+++ test/aot-integration.Tests.ps1
@@ -1,38 +1,7 @@
 # This file contains test cases for https://pester.dev/
 Set-StrictMode -Version Latest
 $ErrorActionPreference = 'Stop'
-
-BeforeAll {
-    $env:SENTRY_LOG_LEVEL = 'debug';
-    . $PSScriptRoot/test-functions.ps1
-    function GetSentryPackageVersion()
-    {
-        (Select-Xml -Path "$PSScriptRoot/../Directory.Build.props" -XPath "/Project/PropertyGroup/Version").Node.InnerText
-    }
-
-    $packageVersion = GetSentryPackageVersion
-    $packagePath = "src/Sentry/bin/Release/Sentry.$packageVersion.nupkg"
-    if (-not (Test-Path env:CI))
-    {
-        Write-Host "Packaging $packagePath"
-        dotnet pack src/Sentry -c Release --nologo -p:Version=$packageVersion | ForEach-Object { Write-Host $_ }
-        if ($LASTEXITCODE -ne 0)
-        {
-            throw "Failed to package sentry."
-        }
-    }
-    Write-Host "Using package $packagePath - $((Get-Item $packagePath).Length) bytes"
-    Remove-Item -Path ./temp/packages -Recurse -Force -ErrorAction SilentlyContinue
-
-    nuget add $packagePath -source ./temp/packages | ForEach-Object { Write-Host $_ }
-    if ($LASTEXITCODE -ne 0)
-    {
-        throw "Failed to add package sentry to a local nuget source."
-    }
-
-    # We need to remove the package from cache or it won't re resolved properly
-    Remove-Item -Path ~/.nuget/packages/sentry/$packageVersion -Recurse -Force -ErrorAction SilentlyContinue
-}
+. $PSScriptRoot/integration-test-setup.ps1
 
 Describe 'Console app (<framework>)' -ForEach @(
     @{ framework = "net7.0" },
@@ -40,12 +9,7 @@ Describe 'Console app (<framework>)' -ForEach @(
 ) {
     BeforeAll {
         $path = './temp/console-app'
-        Remove-Item -Path $path -Recurse -Force
-        dotnet new console --framework $framework --output $path
-        if ($LASTEXITCODE -ne 0)
-        {
-            throw "Failed to create the test app from template."
-        }
+        DotnetNew 'console' $path $framework
         @"
 using Sentry;
 using Sentry.Extensibility;
@@ -69,29 +33,7 @@ internal class FakeTransport : ITransport
         return Task.CompletedTask;
     }
 }
-
 "@ | Out-File $path/Program.cs
-        @"
-<Project>
-  <PropertyGroup>
-    <PublishAot>true</PublishAot>
-  </PropertyGroup>
-</Project>
-"@ | Out-File $path/Directory.build.props
-        Push-Location $path
-        try
-        {
-            $packageVersion = GetSentryPackageVersion
-            dotnet add package sentry --source ../packages --version $packageVersion | ForEach-Object { Write-Host $_ }
-            if ($LASTEXITCODE -ne 0)
-            {
-                throw "Failed to add package dependency to the test app project."
-            }
-        }
-        finally
-        {
-            Pop-Location
-        }
 
         # Publish once, then run the executable in actual tests.
         dotnet publish temp/console-app -c Release --nologo --framework $framework | ForEach-Object { Write-Host $_ }
@@ -129,15 +71,6 @@ internal class FakeTransport : ITransport
             }
         }
     }
-
-    # TODO migrate CLI integration tests to run on blank template app, such as the following code.
-    # As is, this does an additional publish which we don't need to run app-specific integration tests.
-    # It "uploads symbols and sources during build" {
-    #     $result = RunDotnet 'publish' 'temp/console-app' $True $True $framework
-    #     $result.UploadedDebugFiles() | Sort-Object -Unique | Should -Be @('console-app.pdb')
-    #     $result.ScriptOutput | Should -AnyElementMatch 'Found 1 debug information file'
-    #     $result.ScriptOutput | Should -AnyElementMatch 'Resolved source code for 1 debug information file'
-    # }
 
     It "sends native debug images" {
         runConsoleApp | Should -AnyElementMatch '"debug_meta":{"images":\[{"type":"pe","image_addr":"0x[a-f0-9]+","image_size":[0-9]+,"debug_id":"[a-f0-9\-]+"'
---
+++ test/cli-integration.Tests.ps1
@@ -0,0 +1,150 @@
+# This file contains test cases for https://pester.dev/
+Set-StrictMode -Version Latest
+$ErrorActionPreference = 'Stop'
+. $PSScriptRoot/integration-test-setup.ps1
+
+Describe 'Console apps (<framework>) - normal build' -ForEach @(
+    @{ framework = "net7.0" }
+) {
+    BeforeAll {
+        DotnetNew 'console' './temp/console-app' $framework
+    }
+
+    BeforeEach {
+        Remove-Item "./temp/console-app/bin/Release/$framework/*.src.zip" -ErrorAction SilentlyContinue
+    }
+
+    It "uploads symbols and sources" {
+        $result = RunDotnet 'build' './temp/console-app' $True $True $framework
+        $result.UploadedDebugFiles() | Sort-Object -Unique | Should -Be @('console-app.pdb')
+        $result.ScriptOutput | Should -AnyElementMatch 'Found 1 debug information file \(1 with embedded sources\)'
+        $result.ScriptOutput | Should -AnyElementMatch 'Resolved source code for 0 debug information files'
+    }
+
+    It "uploads symbols" {
+        $result = RunDotnet 'build' './temp/console-app' $True $False $framework
+        $result.UploadedDebugFiles() | Sort-Object -Unique | Should -Be @('console-app.pdb')
+        $result.ScriptOutput | Should -AnyElementMatch 'Found 1 debug information file \(1 with embedded sources\)'
+    }
+
+    It "uploads sources" {
+        $result = RunDotnet 'build' './temp/console-app' $False $True $framework
+        $result.ScriptOutput | Should -AnyElementMatch 'Skipping embedded source file: .*/temp/console-app/Program.cs'
+        $result.UploadedDebugFiles() | Should -BeNullOrEmpty
+    }
+
+    It "uploads nothing when disabled" {
+        $result = RunDotnet 'build' './temp/console-app' $False $False $framework
+        $result.UploadedDebugFiles() | Should -BeNullOrEmpty
+    }
+}
+
+Describe 'Console apps (<framework>) - native AOT publish' -ForEach @(
+    @{ framework = "net7.0" },
+    @{ framework = "net8.0" }
+) {
+    BeforeAll {
+        DotnetNew 'console' './temp/console-app' $framework
+    }
+
+    BeforeEach {
+        Remove-Item "./temp/console-app/bin/Release/$framework/publish" -ErrorAction SilentlyContinue
+    }
+
+    It "uploads symbols and sources" -Skip:($IsMacOS -and $framework -eq 'net7.0') {
+        $result = RunDotnet 'publish' './temp/console-app' $True $True $framework
+        $result.ScriptOutput | Should -AnyElementMatch "Preparing upload to Sentry for project 'console-app'"
+        if ($IsWindows -or ($IsLinux -and $framework -eq 'net7.0'))
+        {
+            $extension = $IsLinux ? '' : '.pdb'
+            $result.UploadedDebugFiles() | Sort-Object -Unique | Should -Be @("console-app$extension")
+            $result.ScriptOutput | Should -AnyElementMatch 'Found 1 debug information file'
+            $result.ScriptOutput | Should -AnyElementMatch 'Resolved source code for 1 debug information file'
+        }
+        else
+        {
+            # On macOS, only the dwarf is uploaded from dSYM so it has the same name as the actual executable.
+            $debugExtension = $IsLinux ? '.dbg' : ''
+            $result.UploadedDebugFiles() | Sort-Object -Unique | Should -Be (@(
+                    'console-app', "console-app$debugExtension") | Sort-Object -Unique)
+            $result.ScriptOutput | Should -AnyElementMatch 'Found 2 debug information files'
+            $result.ScriptOutput | Should -AnyElementMatch 'Resolved source code for 1 debug information file'
+        }
+    }
+
+    It "uploads symbols" -Skip:($IsMacOS -and $framework -eq 'net7.0') {
+        $result = RunDotnet 'publish' './temp/console-app' $True $False $framework
+        $result.ScriptOutput | Should -AnyElementMatch "Preparing upload to Sentry for project 'console-app'"
+        if ($IsWindows -or ($IsLinux -and $framework -eq 'net7.0'))
+        {
+            $extension = $IsLinux ? '' : '.pdb'
+            $result.UploadedDebugFiles() | Sort-Object -Unique | Should -Be @("console-app$extension")
+            $result.ScriptOutput | Should -AnyElementMatch 'Found 1 debug information file'
+        }
+        else
+        {
+            # On macOS, only the dwarf is uploaded from dSYM so it has the same name as the actual executable.
+            $debugExtension = $IsLinux ? '.dbg' : ''
+            $result.UploadedDebugFiles() | Sort-Object -Unique | Should -Be (@(
+                    'console-app', "console-app$debugExtension") | Sort-Object -Unique)
+            $result.ScriptOutput | Should -AnyElementMatch 'Found 2 debug information files'
+        }
+    }
+
+    It "uploads sources" -Skip:($IsMacOS -and $framework -eq 'net7.0') {
+        $result = RunDotnet 'publish' './temp/console-app' $False $True $framework
+        $result.ScriptOutput | Should -AnyElementMatch "Preparing upload to Sentry for project 'console-app'"
+        $sourceBundle = 'console-app.src.zip'
+        if ($IsMacOS -or ($IsLinux -and $framework -eq 'net7.0'))
+        {
+            $sourceBundle = 'console-app.src.zip'
+        }
+        $result.UploadedDebugFiles() | Sort-Object -Unique | Should -Be @($sourceBundle)
+    }
+
+    It "uploads nothing when disabled" -Skip:($IsMacOS -and $framework -eq 'net7.0') {
+        $result = RunDotnet 'publish' './temp/console-app' $False $False $framework
+        $result.UploadedDebugFiles() | Should -BeNullOrEmpty
+    }
+}
+
+Describe 'MAUI' {
+    BeforeAll {
+        CreateSentryPackage 'Sentry.Android.AssemblyReader'
+        CreateSentryPackage 'Sentry.Bindings.Android'
+        CreateSentryPackage 'Sentry.Extensions.Logging'
+        CreateSentryPackage 'Sentry.Maui'
+        DotnetNew 'maui' './temp/maui-app' 'net7.0'
+    }
+
+    It "uploads symbols and sources for an Android build" {
+        $result = RunDotnet 'build' './temp/maui-app' $True $True 'net7.0-android'
+        $result.UploadedDebugFiles() | Sort-Object -Unique | Should -Be @('maui-app.pdb')
+        $result.ScriptOutput | Should -AnyElementMatch 'Found 6 debug information files \(6 with embedded sources\)'
+    }
+
+    It "uploads symbols and sources for an iOS build" -Skip:(!$IsMacOS) {
+        $result = RunDotnet 'build' './temp/maui-app' $True $True 'net8.0-ios'
+        $result.UploadedDebugFiles() | Sort-Object -Unique | Should -Be @(
+            'libmono-component-debugger.dylib',
+            'libmono-component-diagnostics_tracing.dylib',
+            'libmono-component-hot_reload.dylib',
+            'libmonosgen-2.0.dylib',
+            'libSystem.IO.Compression.Native.dylib',
+            'libSystem.Native.dylib',
+            'libSystem.Net.Security.Native.dylib',
+            'libSystem.Security.Cryptography.Native.Apple.dylib',
+            'libxamarin-dotnet-debug.dylib',
+            'libxamarin-dotnet.dylib',
+            'Sentry',
+            'Sentry.Bindings.Cocoa.pdb',
+            'Sentry.Extensions.Logging.pdb',
+            'Sentry.Maui.pdb',
+            'Sentry.pdb',
+            'maui-app',
+            'maui-app.pdb'
+        )
+        $nonZeroNumberRegex = '[1-9][0-9]*';
+        $result.ScriptOutput | Should -AnyElementMatch "Found $nonZeroNumberRegex debug information files \($nonZeroNumberRegex with embedded sources\)"
+    }
+}
