a5ce6d0bc6a1d29fdc6042fc68390a1524225a93
Ivan Dlugos <dlugos.ivan@gmail.com>
2023-11-06 14:50:00 +0100

    suppress aot warning

--- src/Sentry/Internal/AotHelper.cs
+++ src/Sentry/Internal/AotHelper.cs
@@ -11,12 +11,14 @@ internal static class AotHelper
 
     internal static bool IsAot { get; }
 
+    [UnconditionalSuppressMessage("Trimming", "IL2026:Members annotated with 'RequiresUnreferencedCodeAttribute' require dynamic access otherwise can break functionality when trimming application code", Justification = AotHelper.SuppressionJustification)]
     static AotHelper()
     {
-        IsAot = false;
 #if NET6_0_OR_GREATER   // TODO NET7 once we target it
         var stackTrace = new StackTrace(false);
         IsAot = stackTrace.GetFrame(0)?.GetMethod() is null;
+#else
+        IsAot = false;
 #endif
     }
 }
