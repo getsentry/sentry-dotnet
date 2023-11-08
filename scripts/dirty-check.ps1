c173f1cc9433fb2491442e78539ea65dc2bb90a7
Ivan Dlugos <6349682+vaind@users.noreply.github.com>
2023-11-08 11:17:14 +0100

    ci: run solution filters tests separately (#2806)

--- scripts/dirty-check.ps1
+++ scripts/dirty-check.ps1
@@ -8,14 +8,12 @@ $ErrorActionPreference = "Stop"
 
 # Any value will be truthy in PS so if our check returns something, we've got tracked changes
 $changes = git diff --name-only $PathToCheck
-if($changes){
-    Write-Debug "Path: $PathToCheck"
-    Write-Debug "Changes:`n$changes"
-    Write-Error "$GuidanceOnFailure" `
-        -CategoryActivity Error -ErrorAction Stop
+if ($changes)
+{
+    git diff $PathToCheck
+    Write-Error "$GuidanceOnFailure"
 }
 else
 {
-    Write-Debug '$PathToCheck matches HEAD.'
+    Write-Host "$PathToCheck matches HEAD."
 }
-
