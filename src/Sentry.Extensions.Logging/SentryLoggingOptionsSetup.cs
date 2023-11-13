55e10a8e23fad79d7701b50f1030b350e173ba7e
James Crosswell <jamescrosswell@users.noreply.github.com>
2023-11-13 17:32:19 +1300

    WIP

--- src/Sentry.Extensions.Logging/SentryLoggingOptionsSetup.cs
+++ src/Sentry.Extensions.Logging/SentryLoggingOptionsSetup.cs
@@ -1,3 +1,27 @@
+#if NET6_0_OR_GREATER
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging.Configuration;
+using Microsoft.Extensions.Options;
+
+namespace Sentry.Extensions.Logging;
+
+internal class SentryLoggingOptionsSetup : IConfigureOptions<SentryLoggingOptions>
+{
+    private readonly IConfiguration _config;
+
+    public SentryLoggingOptionsSetup(ILoggerProviderConfiguration<SentryLoggerProvider> config)
+    {
+        ArgumentNullException.ThrowIfNull(config);
+        _config = config.Configuration;
+    }
+
+    public virtual void Configure(SentryLoggingOptions options)
+    {
+        ArgumentNullException.ThrowIfNull(options);
+        _config.Bind(options);
+    }
+}
+#else
 using Microsoft.Extensions.Logging.Configuration;
 using Microsoft.Extensions.Options;
 
@@ -10,3 +34,4 @@ internal class SentryLoggingOptionsSetup : ConfigureFromConfigurationOptions<Sen
         : base(providerConfiguration.Configuration)
     { }
 }
+#endif
