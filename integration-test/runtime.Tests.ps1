6460c489070e1fe89d1b3dc3497b742e475a4e1a
Ivan Dlugos <dlugos.ivan@gmail.com>
2023-11-16 10:21:31 +0100

    skip the test if  not on Windows

--- integration-test/runtime.Tests.ps1
+++ integration-test/runtime.Tests.ps1
@@ -104,7 +104,7 @@ internal class FakeTransport : ITransport
 }
 
 # This ensures we don't have a regression for https://github.com/getsentry/sentry-dotnet/issues/2825
-Describe 'Console app regression (missing System.Reflection.Metadata)' {
+Describe 'Console app regression (missing System.Reflection.Metadata)' -Skip:(!$IsWindows) {
     AfterAll {
         dotnet remove ./net4-console/console-app.csproj package Sentry
     }
