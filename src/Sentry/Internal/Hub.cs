c62a5a6b15ee36f0cd6f6739ee8b0b2930463b7a
bitsandfoxes <reg@bitfox.at>
2023-11-23 11:44:21 +0100

    hub passes 'GlobalSessionManager' to client

--- src/Sentry/Internal/Hub.cs
+++ src/Sentry/Internal/Hub.cs
@@ -47,9 +47,9 @@ internal class Hub : IHubEx, IDisposable
 
         _options = options;
         _randomValuesFactory = randomValuesFactory ?? new SynchronizedRandomValuesFactory();
-        _ownedClient = client ?? new SentryClient(options, randomValuesFactory: _randomValuesFactory);
-        _clock = clock ?? SystemClock.Clock;
         _sessionManager = sessionManager ?? new GlobalSessionManager(options);
+        _ownedClient = client ?? new SentryClient(options, randomValuesFactory: _randomValuesFactory, sessionManager: _sessionManager);
+        _clock = clock ?? SystemClock.Clock;
 
         ScopeManager = scopeManager ?? new SentryScopeManager(options, _ownedClient);
 
