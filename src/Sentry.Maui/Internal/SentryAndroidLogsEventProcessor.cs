e3dccd59455f49981db97e5d20ce3380dc95e718
Daniel Rusznyak <daniel@rusznyak.com>
2023-11-29 16:12:25 +0000

    Add better comments and disable the feature on Android 23, though realistically MAUI can't target version 23 anway

--- src/Sentry.Maui/Internal/SentryAndroidLogsEventProcessor.cs
+++ src/Sentry.Maui/Internal/SentryAndroidLogsEventProcessor.cs
@@ -15,25 +15,39 @@ internal class SentryAndroidLogsEventProcessor : ISentryEventProcessorWithHint
 
     public SentryEvent? Process(SentryEvent @event, Hint hint)
     {
+        // If sending has failed once, we have to disable this feature to prevent infinite loops and to allow the SDK to work otherwise
         if (!_sendLogcatLogs)
         {
             return @event;
         }
+
+        // The logcat command only works on Android API 23 and above
+        if (!OperatingSystem.IsAndroidVersionAtLeast(23))
+        {
+            _sendLogcatLogs = false;
+            return @event;
+        }
+
         try
         {
             if (@event.Exception is null)
                 return @event;
 
+            // Only send logcat logs if the event is unhandled if the integration is set to Unhandled
             if(_logCatIntegrationType == LogCatIntegrationType.Unhandled)
             {
                 if(!@event.HasTerminalException())
                     return @event;
             }
 
+            // We run the logcat command via the runtime
             var process = Runtime.GetRuntime()?.Exec("logcat -t 1000 *:I");
+
+            // Strangely enough, process.InputStream is the *output* of the command
             if (process?.InputStream is null)
                 return @event;
 
+            // We write the logcat logs to a file so we can attach it
             using var output = Android.App.Application.Context.OpenFileOutput("sentry_logcat.txt", FileCreationMode.Private);
             if (output is null)
                 return @event;
@@ -47,7 +61,10 @@ internal class SentryAndroidLogsEventProcessor : ISentryEventProcessorWithHint
         }
         catch (Exception e) // Catch all exceptions to prevent crashing the app during logging
         {
+            // Disable the feature if it fails once
             _sendLogcatLogs = false;
+
+            // Log the failure to Sentry
             SentrySdk.CaptureException(e);
             return @event;
         }
