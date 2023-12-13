475e8d650cb30280cbf5e88c7ec34a2c3447f84f
Giorgi <giorgi@giorgi.dev>
2023-12-13 13:00:23 +0400

    Add comment, skip on Windows

--- src/Sentry.Maui/Internal/ScreenshotAttachment.cs
+++ src/Sentry.Maui/Internal/ScreenshotAttachment.cs
@@ -25,13 +25,26 @@ internal class ScreenshotAttachmentContent : IAttachmentContent
 {
     public Stream GetStream()
     {
-        if (Screenshot.Default.IsCaptureSupported)
+        try
         {
-            var screen = Screenshot.Default.CaptureAsync().ConfigureAwait(false).GetAwaiter().GetResult();
+#if !WINDOWS
+            if (Screenshot.Default.IsCaptureSupported)
+            {
+                // This actually runs synchronously (returning Task.FromResult) on the following platforms:
+                // Android: https://github.com/dotnet/maui/blob/3c7b65264d2f341a48db32263a271fd8718cfd23/src/Essentials/src/Screenshot/Screenshot.android.cs#L49
+                // iOS: https://github.com/dotnet/maui/blob/3c7b65264d2f341a48db32263a271fd8718cfd23/src/Essentials/src/Screenshot/Screenshot.ios.cs#L49
+                var screen = Screenshot.Default.CaptureAsync().ConfigureAwait(false).GetAwaiter().GetResult();
 
-            var stream = screen.OpenReadAsync(ScreenshotFormat.Jpeg).ConfigureAwait(false).GetAwaiter().GetResult();
+                var stream = screen.OpenReadAsync(ScreenshotFormat.Jpeg).ConfigureAwait(false).GetAwaiter().GetResult();
 
-            return stream;
+                return stream;
+            }
+#endif
+        }
+        //In some cases screen capture can throw, for example on Android if the activity is marked as secure.
+        catch (Exception)
+        {
+            return MemoryStream.Null;
         }
 
         return MemoryStream.Null;
