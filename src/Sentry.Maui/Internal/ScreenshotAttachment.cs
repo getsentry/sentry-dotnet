28fca1fc4b8bc9f7b3d477a7e8198c1584ce2768
Giorgi <giorgi@giorgi.dev>
2023-12-15 20:52:11 +0400

    Capture screen on UI thread.

--- src/Sentry.Maui/Internal/ScreenshotAttachment.cs
+++ src/Sentry.Maui/Internal/ScreenshotAttachment.cs
@@ -32,32 +32,33 @@ internal class ScreenshotAttachmentContent : IAttachmentContent
 
     public Stream GetStream()
     {
-// Not including this on Windows specific build because on WinUI this can deadlock.
+        // Not including this on Windows specific build because on WinUI this can deadlock.
 #if !WINDOWS
-        try
+        var stream = MainThread.InvokeOnMainThreadAsync(async () =>
         {
-            if (Screenshot.Default.IsCaptureSupported)
+            try
             {
-                // This actually runs synchronously (returning Task.FromResult) on the following platforms:
-                // Android: https://github.com/dotnet/maui/blob/3c7b65264d2f341a48db32263a271fd8718cfd23/src/Essentials/src/Screenshot/Screenshot.android.cs#L49
-                // iOS: https://github.com/dotnet/maui/blob/3c7b65264d2f341a48db32263a271fd8718cfd23/src/Essentials/src/Screenshot/Screenshot.ios.cs#L49
-                var screen = Screenshot.Default.CaptureAsync().ConfigureAwait(false).GetAwaiter().GetResult();
+                if (Screenshot.Default.IsCaptureSupported)
+                {
+                    var screen = await Screenshot.Default.CaptureAsync().ConfigureAwait(true);
 
-                var stream = screen.OpenReadAsync(ScreenshotFormat.Jpeg).ConfigureAwait(false).GetAwaiter().GetResult();
+                    var stream = await screen.OpenReadAsync(ScreenshotFormat.Jpeg).ConfigureAwait(true);
 
-                return stream;
+                    return stream;
+                }
+                else
+                {
+                    _options.DiagnosticLogger?.Log(SentryLevel.Warning, "Capturing screenshot not supported");
+                    return Stream.Null;
+                }
             }
-            else
+            //In some cases screen capture can throw, for example on Android if the activity is marked as secure.
+            catch (Exception ex)
             {
-                _options.DiagnosticLogger?.Log(SentryLevel.Warning, "Capturing screenshot not supported");
+                _options.DiagnosticLogger?.Log(SentryLevel.Error, "Error capturing screenshot", ex);
+                return Stream.Null;
             }
-        }
-        //In some cases screen capture can throw, for example on Android if the activity is marked as secure.
-        catch (Exception ex)
-        {
-            _options.DiagnosticLogger?.Log(SentryLevel.Error, "Error capturing screenshot", ex);
-            return Stream.Null;
-        }
+        }).ConfigureAwait(false).GetAwaiter().GetResult();
 #endif
 
         return Stream.Null;
