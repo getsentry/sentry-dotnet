43515be02965987d8459f9b987d916019a059897
Bruno Garcia <bruno@brunogarcia.com>
2023-12-22 16:57:00 +0100

    binding

--- src/Sentry.AspNetCore/BindableSentryAspNetCoreOptions.cs
+++ src/Sentry.AspNetCore/BindableSentryAspNetCoreOptions.cs
@@ -20,6 +20,7 @@ internal class BindableSentryAspNetCoreOptions : BindableSentryLoggingOptions
     public bool? FlushBeforeRequestCompleted { get; set; }
     public bool? AdjustStandardEnvironmentNameCasing { get; set; }
     public bool? AutoRegisterTracing { get; set; }
+    public bool? CaptureBlockingCalls { get; set; }
 
     public void ApplyTo(SentryAspNetCoreOptions options)
     {
@@ -30,5 +31,6 @@ internal class BindableSentryAspNetCoreOptions : BindableSentryLoggingOptions
         options.FlushBeforeRequestCompleted = FlushBeforeRequestCompleted ?? options.FlushBeforeRequestCompleted;
         options.AdjustStandardEnvironmentNameCasing = AdjustStandardEnvironmentNameCasing ?? options.AdjustStandardEnvironmentNameCasing;
         options.AutoRegisterTracing = AutoRegisterTracing ?? options.AutoRegisterTracing;
+        options.CaptureBlockingCalls = CaptureBlockingCalls ?? options.CaptureBlockingCalls;
     }
 }
