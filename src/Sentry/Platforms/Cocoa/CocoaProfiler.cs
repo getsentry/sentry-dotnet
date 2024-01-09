a210a6716b66a9e5d2e916d3abd18f9ccdfb9800
Ivan Dlugos <dlugos.ivan@gmail.com>
2024-01-09 11:10:18 +0100

    and also remove type cast

--- src/Sentry/Platforms/Cocoa/CocoaProfiler.cs
+++ src/Sentry/Platforms/Cocoa/CocoaProfiler.cs
@@ -36,7 +36,7 @@ internal class CocoaProfiler : ITransactionProfiler
 
     public ISerializable? Collect(Transaction transaction)
     {
-        var payload = SentryCocoaHybridSdk.CollectProfileBetween(_startTimeNs, _endTimeNs, _cocoaTraceId) as NSMutableDictionary;
+        var payload = SentryCocoaHybridSdk.CollectProfileBetween(_startTimeNs, _endTimeNs, _cocoaTraceId);
         if (payload is null)
         {
             _options.LogWarning("Trace {0} collected profile payload is null", _traceId);
