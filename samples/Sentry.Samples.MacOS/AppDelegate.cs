80f2b6819a5873fd5c3de0970e21ae8dd5de403c
Ivan Dlugos <dlugos.ivan@gmail.com>
2024-01-25 19:37:27 +0100

    fixup

---
+++ samples/Sentry.Samples.MacOS/AppDelegate.cs
@@ -0,0 +1,16 @@
+namespace Sentry.Samples.MacOS;
+
+[Register("AppDelegate")]
+public class AppDelegate : NSApplicationDelegate
+{
+    public override void DidFinishLaunching(NSNotification notification)
+    {
+        // Init the Sentry SDK
+        SentrySdk.Init(o =>
+        {
+            o.Debug = true;
+            o.Dsn = "https://eb18e953812b41c3aeb042e666fd3b5c@o447951.ingest.sentry.io/5428537";
+            o.TracesSampleRate = 1.0;
+        });
+    }
+}
