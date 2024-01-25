0930374eb2df41474b518cc2f6b214c413a77817
bitsandfoxes <reg@bitfox.at>
2024-01-25 13:10:05 +0100

    wip

--- samples/Sentry.Samples.Hangfire/Program.cs
+++ samples/Sentry.Samples.Hangfire/Program.cs
@@ -1,27 +1,78 @@
 using Hangfire;
 using Hangfire.MemoryStorage;
+using Sentry.Hangfire;
 
-var builder = WebApplication.CreateBuilder(args);
+namespace Sentry.Samples.Hangfire;
 
-builder.Services.AddHangfire(configuration => configuration
-    .SetDataCompatibilityLevel(CompatibilityLevel.Version_180)
-    .UseSimpleAssemblyNameTypeSerializer()
-    .UseRecommendedSerializerSettings()
-    .UseMemoryStorage(new MemoryStorageOptions()));
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        builder.Services.AddHangfire(configuration => configuration
+            .SetDataCompatibilityLevel(CompatibilityLevel.Version_180)
+            .UseSimpleAssemblyNameTypeSerializer()
+            .UseRecommendedSerializerSettings()
+            .UseMemoryStorage(new MemoryStorageOptions())
+            .UseSentry()
+        );
 
-builder.Services.AddHangfireServer();
+        builder.Services.AddSingleton<IHostedService, MyJobStarter>();
+        builder.Services.AddHangfireServer();
 
-var app = builder.Build();
+        var app = builder.Build();
 
-app.UseRouting();
+        app.UseRouting();
 
 #pragma warning disable ASP0014
-app.UseEndpoints(endpoints =>
-{
-    endpoints.MapHangfireDashboard();
-});
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapHangfireDashboard();
+        });
 #pragma warning restore ASP0014
 
-app.UseHttpsRedirection();
+        app.UseHttpsRedirection();
 
-app.Run();
+        app.Run();
+    }
+}
+
+public class MyJobStarter : IHostedService
+{
+    public Task StartAsync(CancellationToken cancellationToken)
+    {
+        Console.WriteLine("MyJobStarter is starting...");
+
+        var jobId = BackgroundJob.Enqueue<MyBackgroundJob>(x => x.Execute());
+        Console.WriteLine($"Job Enqueued. JobId: {jobId}");
+
+        RecurringJob.AddOrUpdate<MyBackgroundJob>(
+            "my_recurring_job",
+            x => x.Execute(),
+            Cron.Minutely);
+
+        for (var i = 0; i < 100; i++)
+        {
+            var job = BackgroundJob.Schedule<MyBackgroundJob>(
+                x => x.Execute(),
+                TimeSpan.FromSeconds(new Random().Next(1, 100)));
+        }
+
+        return Task.CompletedTask;
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+        return Task.CompletedTask;
+    }
+}
+
+public class MyBackgroundJob
+{
+    public void Execute()
+    {
+        Console.WriteLine($"Background task executed at: {DateTime.Now}");
+        Task.Delay(2000).Wait();
+        Console.WriteLine($"Background task finished at: {DateTime.Now}");
+    }
+}
---
+++ src/Sentry.Hangfire/GlobalConfigurationExtensions.cs
@@ -0,0 +1,38 @@
+﻿using Hangfire;
+using Hangfire.Server;
+
+namespace Sentry.Hangfire;
+
+/// <summary>
+/// Hangfire Extensions for <see cref="SentryOptions"/>.
+/// </summary>
+public static class GlobalConfigurationExtensions
+{
+    /// <summary>
+    /// Uses Sentry
+    /// </summary>
+    /// <param name="configuration"></param>
+    /// <returns></returns>
+    public static IGlobalConfiguration UseSentry(this IGlobalConfiguration configuration)
+    {
+        configuration.UseFilter(new SentryJobFilter());
+        return configuration;
+    }
+}
+
+internal class SentryJobFilter : IServerFilter
+{
+    public void OnPerforming(PerformingContext context)
+    {
+        // Checkin: In Progress
+    }
+
+    public void OnPerformed(PerformedContext context)
+    {
+        // Checkin: Finished (or not)
+
+        // context.Result
+        // context.Canceled
+        // context.Exception
+    }
+}
