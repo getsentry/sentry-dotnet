30fc215967c0e240aa9bb1cd45d02674fdc546b5
James Crosswell <jamescrosswell@users.noreply.github.com>
2024-01-25 15:42:39 +1300

    Updated Asp.NET Core MVC sample to use newest template

--- samples/Sentry.Samples.AspNetCore.Mvc/Models/ErrorViewModel.cs
+++ samples/Sentry.Samples.AspNetCore.Mvc/Models/ErrorViewModel.cs
@@ -1,8 +1,8 @@
-namespace Samples.AspNetCore.Mvc.Models;
+namespace Sentry.Samples.AspNetCore.Mvc.Models;
 
 public class ErrorViewModel
 {
-    public string RequestId { get; set; }
+    public string? RequestId { get; set; }
 
     public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 }
