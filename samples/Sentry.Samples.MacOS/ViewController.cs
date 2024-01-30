f4a5a2dfc1613fda6aad14f6add053a8da17df64
James Crosswell <jamescrosswell@users.noreply.github.com>
2024-01-30 16:13:14 +1300

    Update samples/Sentry.Samples.MacOS/ViewController.cs
    Co-authored-by: Stefan Jandl <reg@bitfox.at>

--- samples/Sentry.Samples.MacOS/ViewController.cs
+++ samples/Sentry.Samples.MacOS/ViewController.cs
@@ -1,27 +1,24 @@
-using ObjCRuntime;
+public partial class ViewController : NSViewController
+{
+    protected ViewController (NativeHandle handle) : base (handle)
+    {
+        // This constructor is required if the view controller is loaded from a xib or a storyboard.
+        // Do not put any initialization here, use ViewDidLoad instead.
+    }
 
-namespace Sentry.Samples.MacOS;
+    public override void ViewDidLoad ()
+    {
+        base.ViewDidLoad ();
 
-public partial class ViewController : NSViewController {
-	protected ViewController (NativeHandle handle) : base (handle)
-	{
-		// This constructor is required if the view controller is loaded from a xib or a storyboard.
-		// Do not put any initialization here, use ViewDidLoad instead.
-	}
+        // Do any additional setup after loading the view.
+    }
 
-	public override void ViewDidLoad ()
-	{
-		base.ViewDidLoad ();
+    public override NSObject RepresentedObject {
+        get => base.RepresentedObject;
+        set {
+            base.RepresentedObject = value;
 
-		// Do any additional setup after loading the view.
-	}
-
-	public override NSObject RepresentedObject {
-		get => base.RepresentedObject;
-		set {
-			base.RepresentedObject = value;
-
-			// Update the view, if already loaded.
-		}
-	}
+            // Update the view, if already loaded.
+        }
+    }
 }
