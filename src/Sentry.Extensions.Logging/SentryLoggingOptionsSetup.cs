87259e06484d4afeac5e5411dafd5382b243e621
bitsandfoxes <reg@bitfox.at>
2024-01-31 11:12:24 +0100

    forgot that one

--- src/Sentry.Extensions.Logging/SentryLoggingOptionsSetup.cs
+++ src/Sentry.Extensions.Logging/SentryLoggingOptionsSetup.cs
@@ -15,8 +15,6 @@ internal class SentryLoggingOptionsSetup : IConfigureOptions<SentryLoggingOption
         _config = config.Configuration;
     }
 
-    [RequiresDynamicCode("Calls Microsoft.Extensions.Configuration.ConfigurationBinder.Bind(Object)")]
-    [RequiresUnreferencedCode("Calls Microsoft.Extensions.Configuration.ConfigurationBinder.Bind(Object)")]
     public void Configure(SentryLoggingOptions options)
     {
         // ArgumentNullException.ThrowIfNull(options);
