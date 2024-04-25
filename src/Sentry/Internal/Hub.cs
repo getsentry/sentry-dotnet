af2dd7214bf60eb73630f040c7a75e992c4e2d66
James Crosswell <jamescrosswell@users.noreply.github.com>
2024-04-25 21:34:37 +1200

    Allow OpenTelemetry child spans for Sentry Transactions (#3288)

--- src/Sentry/Internal/Hub.cs
+++ src/Sentry/Internal/Hub.cs
@@ -119,16 +119,6 @@ internal class Hub : IHub, IMetricHub, IDisposable
         IReadOnlyDictionary<string, object?> customSamplingContext,
         DynamicSamplingContext? dynamicSamplingContext)
     {
-        var instrumenter = (context as SpanContext)?.Instrumenter;
-        if (instrumenter != _options.Instrumenter)
-        {
-            _options.LogWarning(
-                $"Attempted to start a transaction via {instrumenter} instrumentation when the SDK is" +
-                $" configured for {_options.Instrumenter} instrumentation.  The transaction will not be created.");
-
-            return NoOpTransaction.Instance;
-        }
-
         var transaction = new TransactionTracer(this, context);
 
         // If the hub is disabled, we will always sample out.  In other words, starting a transaction
