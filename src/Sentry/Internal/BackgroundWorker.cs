ee3b6c60ed1c9b38f527eed44dce6cb3a4b05e8e
James Crosswell <jamescrosswell@users.noreply.github.com>
2024-05-08 13:44:09 +1200

    Update BackgroundWorker.cs

--- src/Sentry/Internal/BackgroundWorker.cs
+++ src/Sentry/Internal/BackgroundWorker.cs
@@ -9,7 +9,8 @@ internal class BackgroundWorker : IBackgroundWorker, IDisposable
 {
     private readonly ITransport _transport;
     private readonly SentryOptions _options;
-    private readonly ConcurrentQueue<Envelope> _queue;
+    // private readonly ConcurrentQueue<Envelope> _queue;
+    private readonly ConcurrentQueueLite<Envelope> _queue;
     private readonly int _maxItems;
     private readonly CancellationTokenSource _shutdownSource;
     private readonly SemaphoreSlim _queuedEnvelopeSemaphore;
@@ -31,7 +32,8 @@ internal class BackgroundWorker : IBackgroundWorker, IDisposable
     {
         _transport = transport;
         _options = options;
-        _queue = queue ?? new ConcurrentQueue<Envelope>();
+        // _queue = queue ?? new ConcurrentQueue<Envelope>();
+        _queue = new ConcurrentQueueLite<Envelope>();
         _maxItems = options.MaxQueueItems;
         _shutdownSource = shutdownSource ?? new CancellationTokenSource();
         _queuedEnvelopeSemaphore = new SemaphoreSlim(0, _maxItems);
@@ -370,5 +372,51 @@ internal class BackgroundWorker : IBackgroundWorker, IDisposable
             // Dispose the transport if needed
             (_transport as IDisposable)?.Dispose();
         }
+    }
+}
+
+/// <summary>
+/// This class is purely for testing purposes. It's been hacked together in a short amount of time. Performance is no
+/// doubt terrible and it should in no way be used in production code. It does confirm we have a memory issue with the
+/// <see cref="ConcurrentQueue{T}"/> class however. See https://github.com/getsentry/sentry-dotnet/issues/2516
+/// </summary>
+internal class ConcurrentQueueLite<T>
+{
+    private readonly List<T> _queue = new();
+    private int _listCounter = 0;
+
+    public void Enqueue(T item)
+    {
+        lock (_queue) {
+            _queue.Add (item);
+            _listCounter++;
+        }
+    }
+    public bool TryDequeue([NotNullWhen(true)] out T? item)
+    {
+        item = default;
+        lock (_queue) {
+            if (_listCounter > 0) {
+                item = _queue [0];
+                _queue.RemoveAt (0);
+                _listCounter--;
+            }
+        }
+        return item != null;
+    }
+
+    public int Count => _listCounter;
+
+    public bool IsEmpty => _listCounter == 0;
+
+    public bool TryPeek([NotNullWhen(true)] out T? item)
+    {
+        item = default;
+        lock (_queue) {
+            if (_listCounter > 0) {
+                item = _queue [0];
+            }
+        }
+        return item != null;
     }
 }
