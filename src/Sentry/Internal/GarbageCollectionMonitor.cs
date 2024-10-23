87b76d8c376a74ebd4dfa393bc9a85d725db650b
James Crosswell <jamescrosswell@users.noreply.github.com>
2024-10-23 14:56:03 +1300

    Refactored error handling on garbage collection monitor to use a Continuation

--- src/Sentry/Internal/GarbageCollectionMonitor.cs
+++ src/Sentry/Internal/GarbageCollectionMonitor.cs
@@ -10,41 +10,33 @@ internal sealed class GarbageCollectionMonitor
     private const int MaxGenerationThreshold = 10;
     private const int LargeObjectHeapThreshold = 10;
 
-    public static Task Start(SentryOptions options, Action onGarbageCollected, CancellationToken cancellationToken) =>
-        Task.Run(() => MonitorGarbageCollection(options, onGarbageCollected, cancellationToken), cancellationToken);
+    public static Task Start(Action onGarbageCollected, CancellationToken cancellationToken) =>
+        Task.Run(() => MonitorGarbageCollection(onGarbageCollected, cancellationToken), cancellationToken);
 
-    private static void MonitorGarbageCollection(SentryOptions options, Action onGarbageCollected, CancellationToken cancellationToken)
+    private static void MonitorGarbageCollection(Action onGarbageCollected, CancellationToken cancellationToken)
     {
+        GC.RegisterForFullGCNotification(MaxGenerationThreshold, LargeObjectHeapThreshold);
         try
         {
-            GC.RegisterForFullGCNotification(MaxGenerationThreshold, LargeObjectHeapThreshold);
-            try
+            while (!cancellationToken.IsCancellationRequested)
             {
-                while (!cancellationToken.IsCancellationRequested)
-                {
 #if NET8_0_OR_GREATER
-                    if (GC.WaitForFullGCComplete(TimeSpan.FromSeconds(1)) == GCNotificationStatus.Succeeded)
+                if (GC.WaitForFullGCComplete(TimeSpan.FromSeconds(1)) == GCNotificationStatus.Succeeded)
 #else
-                    if (GC.WaitForFullGCComplete(1000) == GCNotificationStatus.Succeeded)
+                if (GC.WaitForFullGCComplete(1000) == GCNotificationStatus.Succeeded)
 #endif
-                    {
-                        onGarbageCollected?.Invoke();
-                    }
+                {
+                    onGarbageCollected?.Invoke();
                 }
-            }
-            finally
-            {
-                GC.CancelFullGCNotification();
             }
         }
         catch (OperationCanceledException) when (cancellationToken.IsCancellationRequested)
         {
             // Ignore
         }
-        catch (Exception e)
+        finally
         {
-            // This might happen if concurrent garbage collection is enabled.
-            options.LogError(e, "Garbage collection monitor failed");
+            GC.CancelFullGCNotification();
         }
     }
 }
