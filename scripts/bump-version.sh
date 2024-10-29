19eca4e97383433a01249abaa2a867b905bb1ce8
James Crosswell <jamescrosswell@users.noreply.github.com>
2024-10-29 14:30:56 +1300

    Removed unecessary files

--- scripts/bump-version.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -eux
-
-# Requires powershell: `brew install powershell`
-# craft executes this file by convension, passing the new version as the second argument:
-pwsh ./scripts/bump-version.ps1 $2
--- scripts/update-java.ps1
+++
@@ -1,5 +0,0 @@
-$file = 'src/Sentry.Bindings.Android/Sentry.Bindings.Android.csproj'
-$property = 'SentryAndroidSdkVersion'
-$repo = 'https://github.com/getsentry/sentry-java'
-
-. "$PSScriptRoot/update-project-xml.ps1" @args
