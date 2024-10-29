10d612c636d45d018acdbc4d89fa2852ad1544c7
James Crosswell <jamescrosswell@users.noreply.github.com>
2024-10-29 14:34:51 +1300

    Removed modules folder

--- modules/make-internal.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-
-find Ben.Demystifier/src -name \*.cs -print0 | xargs -0 sed -E -i '' 's/public static class/internal static class/g'
-find Ben.Demystifier/src -name \*.cs -print0 | xargs -0 sed -E -i '' 's/public partial class/internal partial class/g'
-find Ben.Demystifier/src -name \*.cs -print0 | xargs -0 sed -E -i '' 's/public class/internal class/g'
-find Ben.Demystifier/src -name \*.cs -print0 | xargs -0 sed -E -i '' 's/public struct/internal struct/g'
