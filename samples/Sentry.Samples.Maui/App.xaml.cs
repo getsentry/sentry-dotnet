6cdb228e078f1b6034dab2148eb760e891bd9b5b
James Crosswell <jamescrosswell@users.noreply.github.com>
2024-11-01 10:28:18 +1300

    Fixed alignment in libsentrysupplemental.so

--- samples/Sentry.Samples.Maui/App.xaml.cs
+++ samples/Sentry.Samples.Maui/App.xaml.cs
@@ -5,7 +5,11 @@ public partial class App
     public App()
     {
         InitializeComponent();
+    }
 
-        MainPage = new AppShell();
+    protected override Window CreateWindow(IActivationState activationState)
+    {
+        Windows[0].Page = new AppShell();
+        return base.CreateWindow(activationState);
     }
 }
