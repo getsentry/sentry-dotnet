0528ac682d9f7bc023501a92e7c3d56417451ae4
Brice Friha <37577669+bricefriha@users.noreply.github.com>
2024-11-06 15:03:24 +0000

    Cocoa/SentryOptions.cs | Update code doc
    Co-authored-by: Bruno Garcia <bruno@brunogarcia.com>

--- src/Sentry/Platforms/Cocoa/SentryOptions.cs
+++ src/Sentry/Platforms/Cocoa/SentryOptions.cs
@@ -189,9 +189,9 @@ public partial class SentryOptions
         /// default value: true
         /// </summary>
         /// <remarks>
-        /// We recommend to set EnableMarshalManagedException to false for NativeAOT to avoid the used of unsupported marshelling behaviour
-        /// for more, see: https://github.com/getsentry/sentry-dotnet/pull/3729
+        /// We recommend to set EnableMarshalManagedException to false for NativeAOT to avoid the used of unsupported marshelling behaviour.
         /// </remarks>
+        /// <seealso href="https://github.com/xamarin/xamarin-macios/issues/15252#issuecomment-2349301905" />
         public bool EnableMarshalManagedException { get; set; } = true;
         internal List<string>? InAppExcludes { get; private set; }
         internal List<string>? InAppIncludes { get; private set; }
