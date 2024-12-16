f67bd1458351e221ae1533007e81733300ab7b29
James Crosswell <jamescrosswell@users.noreply.github.com>
2024-12-16 16:26:41 +1300

    Update MainPage.xaml.cs

--- test/Sentry.MauiTrimTest/MainPage.xaml.cs
+++ test/Sentry.MauiTrimTest/MainPage.xaml.cs
@@ -1,14 +1,7 @@
-namespace Sentry.MauiTrimTest;
+﻿namespace Sentry.MauiTrimTest;
 
 public partial class MainPage : ContentPage
 {
-
-    /* Unmerged change from project 'Sentry.MauiTrimTest(net9.0-ios18.0)'
-    Before:
-        int count = 0;
-    After:
-        private int count = 0;
-    */
     private int count = 0;
 
     public MainPage()
