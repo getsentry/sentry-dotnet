96845d170fd15bd149053d523fcfa68fba71976b
James Crosswell <jamescrosswell@users.noreply.github.com>
2025-01-30 15:58:34 +1300

    Added separate Managed and Native crash buttons to the iOS sample

--- samples/Sentry.Samples.Ios/AppDelegate.cs
+++ samples/Sentry.Samples.Ios/AppDelegate.cs
@@ -1,3 +1,5 @@
+using ObjCRuntime;
+
 namespace Sentry.Samples.Ios;
 
 [Register("AppDelegate")]
@@ -23,77 +25,108 @@ public class AppDelegate : UIApplicationDelegate
             // https://docs.sentry.io/platforms/apple/guides/ios/configuration/
             // Enable Native iOS SDK App Hangs detection
             options.Native.EnableAppHangTracking = true;
+
+            options.CacheDirectoryPath = Path.Combine(Path.GetTempPath(), "test12");
         });
 
         // create a new window instance based on the screen size
         Window = new UIWindow(UIScreen.MainScreen.Bounds);
 
-        // determine the background color for the view (SystemBackground requires iOS >= 13.0)
+        // determine control colours (SystemBackground requires iOS >= 13.0)
         var backgroundColor = UIDevice.CurrentDevice.CheckSystemVersion(13, 0)
 #pragma warning disable CA1416
             ? UIColor.SystemBackground
 #pragma warning restore CA1416
             : UIColor.White;
+        var buttonConfig = UIButtonConfiguration.TintedButtonConfiguration;
 
-        // create a UIViewController with a single UILabel
         var vc = new UIViewController();
-        vc.View!.AddSubview(new UILabel(Window!.Frame)
+
+        var label = new UILabel
         {
             BackgroundColor = backgroundColor,
             TextAlignment = UITextAlignment.Center,
             Text = "Hello, iOS!",
+            AutoresizingMask = UIViewAutoresizing.All
+        };
+
+        // UIButton for managed crash
+        var managedCrashButton = new UIButton(UIButtonType.RoundedRect)
+        {
             AutoresizingMask = UIViewAutoresizing.All,
-        });
+            Configuration = buttonConfig
+        };
+        managedCrashButton.SetTitle("Managed Crash", UIControlState.Normal);
+        managedCrashButton.TouchUpInside += delegate
+        {
+            Console.WriteLine("Managed Crash button clicked!");
+            try
+            {
+                string s = null!;
+                Console.WriteLine("Length: {0}", s.Length);
+            }
+            catch (Exception e)
+            {
+                SentrySdk.CaptureException(e);
+            }
+        };
+
+        // UIButton for native crash
+        var nativeCrashButton = new UIButton(UIButtonType.System)
+        {
+            Configuration = buttonConfig
+        };
+        nativeCrashButton.SetTitle("Native Crash", UIControlState.Normal);
+        nativeCrashButton.TouchUpInside += delegate
+        {
+            Console.WriteLine("Native Crash button clicked!");
+#pragma warning disable CS0618 // Type or member is obsolete
+            SentrySdk.CauseCrash(CrashType.Native);
+#pragma warning restore CS0618 // Type or member is obsolete
+        };
+
+        // create a UIStackView to hold the label and buttons
+        var stackView = new UIStackView(new UIView[] { label, managedCrashButton, nativeCrashButton })
+        {
+            Axis = UILayoutConstraintAxis.Vertical,
+            Distribution = UIStackViewDistribution.FillEqually,
+            Alignment = UIStackViewAlignment.Center,
+            Spacing = 10,
+            TranslatesAutoresizingMaskIntoConstraints = false,
+        };
+
+        // add the stack view to the view controller's view
+        vc.View!.BackgroundColor = backgroundColor;
+        vc.View.AddSubview(stackView);
+
+        // set constraints for the stack view
+        NSLayoutConstraint.ActivateConstraints([
+            stackView.CenterXAnchor.ConstraintEqualTo(vc.View.CenterXAnchor),
+            stackView.CenterYAnchor.ConstraintEqualTo(vc.View.CenterYAnchor),
+            stackView.WidthAnchor.ConstraintEqualTo(vc.View.WidthAnchor, 0.8f),
+            stackView.HeightAnchor.ConstraintEqualTo(vc.View.HeightAnchor, 0.5f)
+        ]);
+
         Window.RootViewController = vc;
 
         // make the window visible
         Window.MakeKeyAndVisible();
 
-
-        // Try out the Sentry SDK
-        SentrySdk.CaptureMessage("From iOS");
-
-        // Uncomment to try these
-        // throw new Exception("Test Unhandled Managed Exception");
-        // SentrySdk.CauseCrash(CrashType.Native);
-
+        AppDomain.CurrentDomain.UnhandledException += (_, _) =>
         {
-            var tx = SentrySdk.StartTransaction("app", "run");
-            var count = 10;
-            for (var i = 0; i < count; i++)
-            {
-                FindPrimeNumber(100000);
-            }
+            Console.WriteLine("In UnhandledException Handler");
+        };
 
-            tx.Finish();
-        }
+        Runtime.MarshalManagedException += (_, _) =>
+        {
+            Console.WriteLine("In MarshalManagedException Handler");
+        };
+
+        Runtime.MarshalObjectiveCException += (_, _) =>
+        {
+            Console.WriteLine("In MarshalObjectiveCException Handler");
+        };
 
         return true;
-    }
-
-    private static long FindPrimeNumber(int n)
-    {
-        int count = 0;
-        long a = 2;
-        while (count < n)
-        {
-            long b = 2;
-            int prime = 1;// to check if found a prime
-            while (b * b <= a)
-            {
-                if (a % b == 0)
-                {
-                    prime = 0;
-                    break;
-                }
-                b++;
-            }
-            if (prime > 0)
-            {
-                count++;
-            }
-            a++;
-        }
-        return (--a);
     }
 }
