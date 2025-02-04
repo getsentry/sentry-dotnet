ffef4eaaff1adc76d4ad68544d04688ce7902bed
Allan Ritchie <allan.ritchie@gmail.com>
2025-02-04 14:34:42 -0500

    Initial manual testing seems to remove the concurrency issue.

--- src/Sentry/Internal/Extensions/HashExtensions.cs
+++ src/Sentry/Internal/Extensions/HashExtensions.cs
@@ -5,8 +5,13 @@ internal static class HashExtensions
     public static string GetHashString(this string str, bool upperCase = true)
     {
         var bytes = Encoding.UTF8.GetBytes(str);
-        using var sha = SHA1.Create();
+        #if NET9_0 && ANDROID
+        var hash = SHA1.HashData(bytes);
+        return hash.ToHexString(upperCase);
+        #else
+        var sha = SHA1.Create();
         var hash = sha.ComputeHash(bytes);
         return hash.ToHexString(upperCase);
+        #endif
     }
 }
