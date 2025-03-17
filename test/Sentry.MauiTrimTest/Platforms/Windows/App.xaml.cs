ac4f837508d996608df6b77102b1afd7ab1e7221
Alberto Aldegheri <albyrock87@gmail.com>
2025-03-17 05:42:10 +0100

    Fix missing MAUI `Shell` navigation breadcrumbs on iOS (#4006)
    ---------
    
    Co-authored-by: James Crosswell <jamescrosswell@users.noreply.github.com>

--- test/Sentry.MauiTrimTest/Platforms/Windows/App.xaml.cs
+++ test/Sentry.MauiTrimTest/Platforms/Windows/App.xaml.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -10,15 +10,15 @@ namespace Sentry.MauiTrimTest.WinUI;
 /// </summary>
 public partial class App : MauiWinUIApplication
 {
-	/// <summary>
-	/// Initializes the singleton application object.  This is the first line of authored code
-	/// executed, and as such is the logical equivalent of main() or WinMain().
-	/// </summary>
-	public App()
-	{
-		this.InitializeComponent();
-	}
+    /// <summary>
+    /// Initializes the singleton application object.  This is the first line of authored code
+    /// executed, and as such is the logical equivalent of main() or WinMain().
+    /// </summary>
+    public App()
+    {
+        this.InitializeComponent();
+    }
 
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
 }
 
