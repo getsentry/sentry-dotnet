c0f55b84d7e9d4f39b59c1028c13e5d79f3dbce5
Max Katz <maxkatz6@outlook.com>
2025-04-12 18:39:52 -0700

    Add SentryNative.IsEnabled boolean

--- src/Sentry/Platforms/Native/SentryNative.cs
+++ src/Sentry/Platforms/Native/SentryNative.cs
@@ -7,11 +7,13 @@ namespace Sentry;
 internal static class SentryNative
 {
 #if NET8_0_OR_GREATER
+    internal static bool IsEnabled => !AppContext.TryGetSwitch("Sentry.Native.IsEnabled", out var isEnabled) || isEnabled;
+
     internal static bool IsAvailable { get; }
 
     static SentryNative()
     {
-        IsAvailable = AotHelper.IsTrimmed && !SentryRuntime.Current.IsBrowserWasm();
+        IsAvailable = IsEnabled && AotHelper.IsTrimmed && !SentryRuntime.Current.IsBrowserWasm();
     }
 #else
     // This is a compile-time const so that the irrelevant code is removed during compilation.
