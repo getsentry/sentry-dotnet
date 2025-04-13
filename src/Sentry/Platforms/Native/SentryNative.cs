6eb1020f4ab66499d708cf2477641d17a344dcb5
Max Katz <maxkatz6@outlook.com>
2025-04-12 18:44:28 -0700

    Update for better maintability

--- src/Sentry/Platforms/Native/SentryNative.cs
+++ src/Sentry/Platforms/Native/SentryNative.cs
@@ -7,18 +7,23 @@ namespace Sentry;
 internal static class SentryNative
 {
 #if NET8_0_OR_GREATER
+    // Should be in-sync with Sentry.Native.targets const.
+    private const string SentryNativeIsEnabledSwitchName = "Sentry.Native.IsEnabled";
+
+    private static readonly bool IsAvailableCore;
+
 #if NET9_0_OR_GREATER
     // FeatureSwitchDefinition should help with trimming disabled code.
     // This way, `SentryNative.IsEnabled` should be treated as a compile-time constant for trimmed apps.
-    [FeatureSwitchDefinition("Sentry.Native.IsEnabled")]
+    [FeatureSwitchDefinition(SentryNativeIsEnabledSwitchName)]
 #endif
-    internal static bool IsEnabled => !AppContext.TryGetSwitch("Sentry.Native.IsEnabled", out var isEnabled) || isEnabled;
+    private static bool IsEnabled => !AppContext.TryGetSwitch(SentryNativeIsEnabledSwitchName, out var isEnabled) || isEnabled;
 
-    internal static bool IsAvailable { get; }
+    internal static bool IsAvailable => IsEnabled && IsAvailableCore;
 
     static SentryNative()
     {
-        IsAvailable = IsEnabled && AotHelper.IsTrimmed && !SentryRuntime.Current.IsBrowserWasm();
+        IsAvailableCore = AotHelper.IsTrimmed && !SentryRuntime.Current.IsBrowserWasm();
     }
 #else
     // This is a compile-time const so that the irrelevant code is removed during compilation.
