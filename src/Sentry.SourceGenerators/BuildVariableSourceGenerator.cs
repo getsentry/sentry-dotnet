5320e8a5ada548fa7f24b32c96dffcba96c74d61
Allan Ritchie <allan.ritchie@gmail.com>
2025-04-24 13:57:07 -0400

    add tests - having tooling issues lighting this up - trying in CI

--- src/Sentry.SourceGenerators/BuildVariableSourceGenerator.cs
+++ src/Sentry.SourceGenerators/BuildVariableSourceGenerator.cs
@@ -1,18 +1,19 @@
+using System;
+using System.Linq;
 using Microsoft.CodeAnalysis;
 
-namespace Sentry.Analyzers;
+namespace Sentry.SourceGenerators;
 
 
 /// <summary>
 /// Generates the necessary msbuild variables
 /// </summary>
 [Generator(LanguageNames.CSharp)]
-public class BuildVariableSourceGenerator : ISourceGenerator
+public class BuildPropertySourceGenerator : ISourceGenerator
 {
     /// <summary>
     /// Initialize the source gen
     /// </summary>
-    /// <param name="context"></param>
     public void Initialize(GeneratorInitializationContext context)
     {
     }
@@ -20,16 +21,18 @@ public class BuildVariableSourceGenerator : ISourceGenerator
     /// <summary>
     /// Execute the source gen
     /// </summary>
-    /// <param name="context"></param>
     public void Execute(GeneratorExecutionContext context)
     {
+        // we'll wrap code in ifdef as it is safer than checking all of the various pieces involved here
+        // ((CSharpCompilation)context.Compilation).LanguageVersion == LanguageVersion.CSharp9
+
         var opts = context.AnalyzerConfigOptions.GlobalOptions;
         var properties = opts.Keys.Where(x => x.StartsWith("build_property.")).ToList();
         if (properties.Count == 0)
             return;
 
-        if (opts.TryGetValue("build_property.DisableSentrySourceGenerator", out var _))
-            return;
+        if (!opts.TryGetValue("build_property.debugpropertycapture", out var debug) && (debug?.Equals("true", StringComparison.InvariantCultureIgnoreCase) ?? false))
+            Debugger.Break();
 
         // we only want to generate code where host setup takes place
         if (!opts.TryGetValue("build_property.outputtype", out var outputType))
@@ -40,6 +43,10 @@ public class BuildVariableSourceGenerator : ISourceGenerator
 
         var sb = new StringBuilder();
         sb
+            .AppendLine("#if NET8_0_OR_GREATER")
+            .AppendLine("using System;")
+            .AppendLine("using System.Collections.Generic;")
+            .AppendLine()
             .AppendLine("namespace Sentry;")
             .AppendLine()
             .AppendLine("[global::System.Runtime.CompilerServices.CompilerGenerated]")
@@ -54,8 +61,8 @@ public class BuildVariableSourceGenerator : ISourceGenerator
         {
             if (opts.TryGetValue(property, out var value))
             {
-                var pn = property.Replace("build_property.", "");
-                var ev = value.Replace("\\", "\\\\");
+                var pn = EscapeString(property.Replace("build_property.", ""));
+                var ev = EscapeString(value);
                 sb
                     .Append("\t\t\t{")
                     .Append($"\"{pn}\", \"{ev}\"")
@@ -66,8 +73,12 @@ public class BuildVariableSourceGenerator : ISourceGenerator
         sb
             .AppendLine("\t\t});") // close dictionary
             .AppendLine("\t}")
-            .AppendLine("}");
+            .AppendLine("}")
+            .AppendLine("#endif");
 
         context.AddSource("__BuildVariables.g.cs", sb.ToString());
     }
+
+
+    private static string EscapeString(string value) => value.Replace("\\", "\\\\");
 }
---
+++ test/Sentry.Tests/BuildPropertySourceGeneratorTests.cs
@@ -0,0 +1,102 @@
+#if NET8_0_OR_GREATER && !__ANDROID__ && !__IOS__
+#nullable enable
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+using Sentry.SourceGenerators;
+
+namespace Sentry.Tests;
+
+
+public class BuildPropertySourceGeneratorTests
+{
+    [Fact]
+    public Task Driver_Success()
+    {
+        var driver = BuildDriver(typeof(Program).Assembly);
+        return Verify(driver);
+    }
+
+
+    [Fact]
+    public Task RunResult_Success()
+    {
+        var driver = BuildDriver(typeof(Program).Assembly, ("PublishAot", "false"), ("OutputType", "exe"));
+        var result = driver.GetRunResult().Results.FirstOrDefault();
+        result.Exception.Should().BeNull();
+        result.GeneratedSources.Length.Should().Be(1);
+        result.GeneratedSources.First().HintName.Should().Be("__BuildVariables.g.cs");
+        return Verify(result);
+    }
+
+
+    [Fact]
+    public Task RunResult_BadStrings()
+    {
+        // we're hijacking PublishAot to make life easy
+        var driver = BuildDriver(typeof(Program).Assembly, ("My\\Key", "test\\test"), ("OutputType", "exe"));
+        var result = driver.GetRunResult().Results.FirstOrDefault();
+        result.Exception.Should().BeNull();
+        result.GeneratedSources.Length.Should().Be(1);
+        result.GeneratedSources.First().HintName.Should().Be("__BuildVariables.g.cs");
+        return Verify(result);
+    }
+
+
+    [Fact]
+    public Task RunResult_Publish_AotTrue()
+    {
+        var driver = BuildDriver(typeof(Program).Assembly, ("PublishAot", "true"), ("OutputType", "exe"));
+        var result = driver.GetRunResult().Results.FirstOrDefault();
+        result.Exception.Should().BeNull();
+        result.GeneratedSources.Length.Should().Be(1);
+        result.GeneratedSources.First().HintName.Should().Be("__BuildVariables.g.cs");
+        return Verify(result);
+    }
+
+
+    [Fact]
+    public Task RunResult_Expect_None()
+    {
+        var driver = BuildDriver(typeof(Program).Assembly, ("PublishAot", "false"));
+        var result = driver.GetRunResult().Results.FirstOrDefault();
+        result.Exception.Should().BeNull();
+        result.GeneratedSources.Length.Should().Be(0);
+
+        return Verify(result);
+    }
+
+
+    static GeneratorDriver BuildDriver(Assembly metadataAssembly, params IEnumerable<(string Key, string Value)> buildProperties)
+    {
+        var metadataReference = MetadataReference.CreateFromFile(metadataAssembly.Location);
+        var options = new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary);
+        var compilation = CSharpCompilation.Create("TestAssembly", [], [metadataReference], options);
+        var generator = new BuildPropertySourceGenerator();
+
+        var dict = buildProperties.ToDictionary(x => "build_property." + x.Key, x => x.Value, comparer: StringComparer.InvariantCultureIgnoreCase);
+        var provider = new MockAnalyzerConfigOptionsProvider(dict);
+
+        var driver = CSharpGeneratorDriver.Create([generator], optionsProvider: provider);
+        return driver.RunGenerators(compilation);
+    }
+}
+
+file class MockAnalyzerConfigOptionsProvider(Dictionary<string, string> buildProperties) : AnalyzerConfigOptionsProvider
+{
+    readonly MockAnalyzerConfigOptions options = new (buildProperties);
+
+    public override AnalyzerConfigOptions GetOptions(SyntaxTree tree) => this.options;
+    public override AnalyzerConfigOptions GetOptions(AdditionalText textFile)  => this.options;
+    public override AnalyzerConfigOptions GlobalOptions => this.options;
+}
+
+file class MockAnalyzerConfigOptions(Dictionary<string, string> values) : AnalyzerConfigOptions
+{
+    public override bool TryGetValue(string key, [NotNullWhen(true)] out string? value)
+        => values.TryGetValue(key, out value);
+
+    public override IEnumerable<string> Keys => values.Keys;
+}
+#endif
