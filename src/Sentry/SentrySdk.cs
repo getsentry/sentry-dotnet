6f71d500ea815a0be81ec5cb7788802cca74c8fc
Allan Ritchie <allan.ritchie@gmail.com>
2025-04-30 09:33:45 -0400

    Update SentrySdk.cs

--- src/Sentry/SentrySdk.cs
+++ src/Sentry/SentrySdk.cs
@@ -1,7 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Threading;
-using System.Threading.Tasks;
 using Sentry.Extensibility;
 using Sentry.Infrastructure;
 using Sentry.Internal;
