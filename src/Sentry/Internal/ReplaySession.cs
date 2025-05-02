39e56fdcb42d072301a141d3210ef21cfe85507c
James Crosswell <jamescrosswell@users.noreply.github.com>
2025-05-02 18:04:28 +1200

    Update ReplaySession.cs

--- src/Sentry/Internal/ReplaySession.cs
+++ src/Sentry/Internal/ReplaySession.cs
@@ -4,12 +4,6 @@ using Sentry.Android.Extensions;
 
 namespace Sentry.Internal;
 
-// TODO: This static class is pretty ugly... let's refactor it into an IReplaySession interface so that we can
-// inject a mock in unit tests. If no IReplaySession is provided to the various classes that need it then we can
-// fall back to a singleton instance of this class.
-//
-// We should be able to remove the ReplayFixture then as well (which is ugly - it forces us to initialise the test
-// replay id for all tests in a Test class... which makes it difficult to test alternate scenarios).
 internal interface IReplaySession
 {
     public SentryId? ActiveReplayId { get; }
