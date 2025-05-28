30b92d84a43b0a2ce568f1400e18afb0b8ab671f
J-P Nurmi <jpnurmi@gmail.com>
2025-05-28 15:00:37 +0200

    add missing parentheses

--- integration-test/cli.Tests.ps1
+++ integration-test/cli.Tests.ps1
@@ -107,7 +107,7 @@ Describe 'Console apps (<framework>) - native AOT publish' -ForEach @(
 
 Describe 'MAUI' -ForEach @(
     @{ framework = "net8.0" }
-) -Skip:$env:NO_MOBILE -eq "true" {
+) -Skip:($env:NO_MOBILE -eq "true") {
     BeforeAll {
         RegisterLocalPackage 'Sentry.Android.AssemblyReader'
         RegisterLocalPackage 'Sentry.Bindings.Android'
