88516fb44ef6932455cde2d5a8d8442b3869c921
James Crosswell <jamescrosswell@users.noreply.github.com>
2025-06-11 16:51:12 +1200

    Revert "Bump framework targets from net462 to net48"
    This reverts commit 613ebf24cb0f87c5da65536da91e957433cddf50.

--- src/Sentry/Internal/RandomValuesFactory.cs
+++ src/Sentry/Internal/RandomValuesFactory.cs
@@ -7,7 +7,7 @@ internal abstract class RandomValuesFactory
     public abstract double NextDouble();
     public abstract void NextBytes(byte[] bytes);
 
-#if !(NETSTANDARD2_0 || NET48X)
+#if !(NETSTANDARD2_0 || NET462)
     public abstract void NextBytes(Span<byte> bytes);
 #endif
 
