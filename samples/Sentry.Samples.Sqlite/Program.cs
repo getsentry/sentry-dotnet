cc22c0fde2cf085227af919b28e842b87c3d6298
James Crosswell <jamescrosswell@users.noreply.github.com>
2025-06-12 11:46:00 +1200

    Experimental SqlLite integration
    Relates to #1837:
    - https://github.com/getsentry/sentry-dotnet/issues/1837
    
    This is just mucking around for a couple of hours to see how such an integration might be pieced together. If we want to proceed, there is some complexity around the `SqliteDataReader`. This only has an internal constructor, so we'd have to wrap it via a proxy class rather than inheritance....  which would doubtless involve a bunch of boilerplate grunt work.
    
    We'd also have to make decisions about what the hang the spans off. Ideally these DB spans would be children of any active transaction (e.g. the transactions created by the Mvvm integration)... which means storing/retrieving those transaction on the Scope - and as @bruno-garcia recently discovered in the SymbolCollector project, that's fiddly business in GlobalMode (particularly for DB stuff that is likely to be running on a background thread).

---
+++ samples/Sentry.Samples.Sqlite/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.IO;
+using Microsoft.Data.Sqlite;
+using Sentry.Sqlite;
+
+SentrySdk.Init(options =>
+{
+#if !SENTRY_DSN_DEFINED_IN_ENV
+    // A DSN is required. You can set here in code, or you can set it in the SENTRY_DSN environment variable.
+    // See https://docs.sentry.io/product/sentry-basics/dsn-explainer/
+    options.Dsn = SamplesShared.Dsn;
+#endif
+    options.Debug = false;
+    options.TracesSampleRate = 1.0;
+});
+
+using (var connection = new SentrySqliteConnection("Data Source=hello.db"))
+{
+    connection.Open();
+
+    var command = connection.CreateCommand();
+    command.CommandText =
+    @"
+        CREATE TABLE user (
+            id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+            name TEXT NOT NULL
+        );
+
+        INSERT INTO user
+        VALUES (1, 'Brice'),
+               (2, 'Alexander'),
+               (3, 'Nate');
+    ";
+    command.ExecuteNonQuery();
+
+    Console.Write("Name: ");
+    var name = Console.ReadLine();
+
+    #region snippet_Parameter
+    command.CommandText =
+    @"
+        INSERT INTO user (name)
+        VALUES ($name)
+    ";
+    command.Parameters.AddWithValue("$name", name);
+    #endregion
+    command.ExecuteNonQuery();
+
+    command.CommandText =
+    @"
+        SELECT last_insert_rowid()
+    ";
+    var newId = (long)command.ExecuteScalar()!;
+
+    Console.WriteLine($"Your new user ID is {newId}.");
+}
+
+Console.Write("User ID: ");
+var id = int.Parse(Console.ReadLine()!);
+
+#region snippet_HelloWorld
+using (var connection = new SqliteConnection("Data Source=hello.db"))
+{
+    connection.Open();
+
+    var command = connection.CreateCommand();
+    command.CommandText =
+    @"
+        SELECT name
+        FROM user
+        WHERE id = $id
+    ";
+    command.Parameters.AddWithValue("$id", id);
+
+    using (var reader = command.ExecuteReader())
+    {
+        while (reader.Read())
+        {
+            var name = reader.GetString(0);
+
+            Console.WriteLine($"Hello, {name}!");
+        }
+    }
+}
+#endregion
+
+// Clean up
+SqliteConnection.ClearAllPools();
+File.Delete("hello.db");
---
+++ src/Sentry.Sqlite/SentrySqliteCommand.cs
@@ -0,0 +1,37 @@
+using Microsoft.Data.Sqlite;
+
+namespace Sentry.Sqlite;
+
+/// <inheritdoc cref="SqliteCommand"/>
+public class SentrySqliteCommand : SqliteCommand
+{
+    internal ITransactionTracer? TransactionTracer { get; set; } = null;
+
+    /// <inheritdoc cref="SqliteCommand.ExecuteNonQuery"/>
+    public override int ExecuteNonQuery()
+    {
+        var span = TransactionTracer?.StartChild("db.query", this.CommandText);
+        try
+        {
+            return base.ExecuteNonQuery();
+        }
+        finally
+        {
+            span?.Finish();
+        }
+    }
+
+    /// <inheritdoc cref="SqliteCommand.ExecuteScalar"/>
+    public override object? ExecuteScalar()
+    {
+        var span = TransactionTracer?.StartChild("db.query", this.CommandText);
+        try
+        {
+            return base.ExecuteScalar();
+        }
+        finally
+        {
+            span?.Finish();
+        }
+    }
+}
