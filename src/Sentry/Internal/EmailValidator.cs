2448c9eae2cd9ef9673870937ade64bff9a0c1b0
James Crosswell <jamescrosswell@users.noreply.github.com>
2025-06-18 14:45:14 +1200

    Made EmailValidator more generic / rigorous

--- src/Sentry/Internal/EmailValidator.cs
+++ src/Sentry/Internal/EmailValidator.cs
@@ -25,13 +25,5 @@ internal static partial class EmailValidator
     /// </summary>
     /// <param name="email">The email address to validate.</param>
     /// <returns>True if the email is valid, false otherwise.</returns>
-    public static bool IsValidEmail(string? email)
-    {
-        if (string.IsNullOrEmpty(email))
-        {
-            return true;
-        }
-
-        return Email.IsMatch(email);
-    }
+    public static bool IsValidEmail(string email) =>  Email.IsMatch(email);
 }
