b0f890a19164a737ee2691f69c71ab07cbc7c24b
bitsandfoxes <reg@bitfox.at>
2025-07-21 13:22:33 +0200

    made nullattachment ctor private

--- test/Sentry.Testing/NullAttachmentContent.cs
+++ test/Sentry.Testing/NullAttachmentContent.cs
@@ -5,4 +5,6 @@ internal sealed class NullAttachmentContent : IAttachmentContent
     public static NullAttachmentContent Instance { get; } = new();
 
     public Stream GetStream() => Stream.Null;
+
+    private NullAttachmentContent() { }
 }
