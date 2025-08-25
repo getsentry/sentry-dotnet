f05de68761d31032398a8bd09b5f61f3c753e3fb
Stefan Pölz <38893694+Flash0ver@users.noreply.github.com>
2025-08-25 16:29:04 +0200

    demo: reproduction

--- samples/Sentry.Samples.Azure.Functions.Worker/Program.cs
+++ samples/Sentry.Samples.Azure.Functions.Worker/Program.cs
@@ -1,19 +1,21 @@
+using Microsoft.Azure.Functions.Worker.Builder;
 using Microsoft.Extensions.Hosting;
 using Sentry.Azure.Functions.Worker;
 
-var host = new HostBuilder()
-    .ConfigureFunctionsWorkerDefaults((host, builder) =>
-    {
-        builder.UseSentry(host, options =>
-        {
+
+
+var builder = FunctionsApplication.CreateBuilder(args);
+builder.UseSentry(options =>
+{
 #if !SENTRY_DSN_DEFINED_IN_ENV
             // A DSN is required. You can set here in code, or you can set it in the SENTRY_DSN environment variable.
             options.Dsn = SamplesShared.Dsn;
 #endif
-            options.TracesSampleRate = 1.0;
-            options.Debug = true;
-        });
-    })
-    .Build();
+    options.TracesSampleRate = 1.0;
+    options.Debug = true;
+});
+builder.ConfigureFunctionsWebApplication();
+
+var host = builder.Build();
 
 await host.RunAsync();
