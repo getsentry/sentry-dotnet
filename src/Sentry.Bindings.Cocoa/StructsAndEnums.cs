f05a3aa5bada1ef9adce2bfb7d135b94a211899e
J-P Nurmi <jpnurmi@gmail.com>
2025-09-02 17:39:59 +0200

    WIP: Generate bindings from Sentry-Swift.h

--- src/Sentry.Bindings.Cocoa/StructsAndEnums.cs
+++ src/Sentry.Bindings.Cocoa/StructsAndEnums.cs
@@ -66,3 +66,84 @@ internal enum SentrySpanStatus : ulong
     OutOfRange,
     DataLoss
 }
+
+[Native]
+internal enum SentryANRType : long
+{
+    FatalFullyBlocking = 0,
+    FatalNonFullyBlocking = 1,
+    FullyBlocking = 2,
+    NonFullyBlocking = 3,
+    Unknown = 4
+}
+
+[Native]
+internal enum SentryFeedbackSource : long
+{
+    Widget = 0,
+    Custom = 1
+}
+
+[Native]
+internal enum SentryLevel : ulong
+{
+    None = 0,
+    Debug = 1,
+    Info = 2,
+    Warning = 3,
+    Error = 4,
+    Fatal = 5
+}
+
+[Native]
+internal enum SentryStructuredLogLevel : long
+{
+    Trace = 0,
+    Debug = 1,
+    Info = 2,
+    Warn = 3,
+    Error = 4,
+    Fatal = 5
+}
+
+[Native]
+internal enum SentryProfileLifecycle : long
+{
+    Manual = 0,
+    Trace = 1
+}
+
+[Native]
+internal enum SentryReplayQuality : long
+{
+    Low = 0,
+    Medium = 1,
+    High = 2
+}
+
+[Native]
+internal enum SentryReplayType : long
+{
+    Session = 0,
+    Buffer = 1
+}
+
+[Native]
+internal enum SentrySessionStatus : ulong
+{
+    Ok = 0,
+    Exited = 1,
+    Crashed = 2,
+    Abnormal = 3
+}
+
+[Native]
+internal enum SentryTransactionNameSource : long
+{
+    Custom = 0,
+    Url = 1,
+    Route = 2,
+    View = 3,
+    Component = 4,
+    Task = 5
+}
