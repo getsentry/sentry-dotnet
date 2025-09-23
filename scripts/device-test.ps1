6792c2e64f8f5d99b261321214b613dd3afbcdca
J-P Nurmi <jpnurmi@gmail.com>
2025-09-23 12:28:51 +0200

    bump Microsoft.DotNet.XHarness.CLI to version 10.0.0-prerelease.25466.1

--- scripts/device-test.ps1
+++ scripts/device-test.ps1
@@ -87,7 +87,7 @@ try
         if (!(Get-Command xharness -ErrorAction SilentlyContinue))
         {
             Push-Location ($CI ? $env:RUNNER_TEMP : $IsWindows ? $env:TMP : $IsMacos ? $env:TMPDIR : '/tmp')
-            dotnet tool install Microsoft.DotNet.XHarness.CLI --global --version '10.0.0-prerelease.25412.1' `
+            dotnet tool install Microsoft.DotNet.XHarness.CLI --global --version '10.0.0-prerelease.25466.1' `
                 --add-source https://pkgs.dev.azure.com/dnceng/public/_packaging/dotnet-eng/nuget/v3/index.json
             Pop-Location
         }
