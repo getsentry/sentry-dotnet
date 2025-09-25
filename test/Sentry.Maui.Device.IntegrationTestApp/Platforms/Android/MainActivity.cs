d5083594fcd1c31549a8b53f7ce0ca8783fb1ded
J-P Nurmi <jpnurmi@gmail.com>
2025-09-25 15:06:57 +0200

    Prepare predictable Activity name on Android

--- test/Sentry.Maui.Device.IntegrationTestApp/Platforms/Android/MainActivity.cs
+++ test/Sentry.Maui.Device.IntegrationTestApp/Platforms/Android/MainActivity.cs
@@ -4,7 +4,13 @@ using Android.OS;
 
 namespace Sentry.Maui.Device.IntegrationTestApp;
 
-[Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
+[Activity(
+    Name = "io.sentry.dotnet.maui.device.integrationtestapp.MainActivity",
+    Theme = "@style/Maui.SplashTheme",
+    MainLauncher = true,
+    LaunchMode = LaunchMode.SingleTop,
+    ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density
+)]
 public class MainActivity : MauiAppCompatActivity
 {
 }
