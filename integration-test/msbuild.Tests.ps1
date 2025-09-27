328dfb929e376502081f9307e5b01c4b01b36ac9
J-P Nurmi <jpnurmi@gmail.com>
2025-09-27 12:39:43 +0200

    Conditional net5.0 (unusable on linux-musl, linux-arm64, macos)

--- integration-test/msbuild.Tests.ps1
+++ integration-test/msbuild.Tests.ps1
@@ -9,6 +9,7 @@ Describe 'MSBuild app (<framework>)' -ForEach @(
     @{ framework = 'net9.0' }
 ) {
     BeforeAll {
+        Write-Host "::group::Create app"
         DotnetNew 'console' 'msbuild-app' $framework
         Push-Location msbuild-app
         @'
@@ -22,8 +23,11 @@ SentrySdk.Init(options =>
 
 SentrySdk.CaptureMessage("Hello from MSBuild app");
 '@ | Out-File Program.cs
+        Write-Host "::endgroup::"
 
-        if (Test-Path variable:sdk) {
+        Write-Host "::group::Setup .NET SDK"
+        if (Test-Path variable:sdk)
+        {
             @"
 {
     "sdk": {
@@ -36,30 +40,45 @@ SentrySdk.CaptureMessage("Hello from MSBuild app");
 
         dotnet --version | ForEach-Object { Write-Host $_ }
         dotnet msbuild -version | ForEach-Object { Write-Host $_ }
+        $hasDotnetSdk = $LASTEXITCODE -eq 0
+        Write-Host "::endgroup::"
     }
 
     AfterAll {
         Pop-Location
-        #Remove-Item -Recurse -Force msbuild-app
+        Remove-Item -Recurse -Force msbuild-app
     }
 
     It 'builds without warnings and is able to capture a message' {
-        if (!(Test-Path variable:config)) {
+        if (!$hasDotnetSdk)
+        {
+            Set-ItResult -Skipped -Because "$framework is not installed"
+            return
+        }
+
+        if (!(Test-Path variable:config))
+        {
             $config = "$PSScriptRoot/nuget.config"
         }
+        Write-Host "::group::Restore packages"
         dotnet restore msbuild-app.csproj --configfile $config | ForEach-Object { Write-Host $_ }
         $LASTEXITCODE | Should -Be 0
+        Write-Host "::endgroup::"
 
+        Write-Host "::group::Build app"
         # TODO: pass -p:TreatWarningsAsErrors=true after #4554 is fixed
-        dotnet msbuild msbuild-app.csproj -t:Build -p:Configuration=Release -p:TreatWarningsAsErrors=true | ForEach-Object { Write-Host $_ }
+        dotnet msbuild msbuild-app.csproj -t:Build -p:Configuration=Release -p:TreatWarningsAsErrors=false | ForEach-Object { Write-Host $_ }
         $LASTEXITCODE | Should -Be 0
+        Write-Host "::endgroup::"
 
+        Write-Host "::group::Test app"
         $result = Invoke-SentryServer {
-            Param([string]$url)
+            param([string]$url)
             $dsn = $url.Replace('http://', 'http://key@') + '/0'
             dotnet msbuild msbuild-app.csproj -t:Run -p:Configuration=Release -p:RunArguments=$dsn | ForEach-Object { Write-Host $_ }
             $LASTEXITCODE | Should -Be 0
         }
+        Write-Host "::endgroup::"
         $result.HasErrors() | Should -BeFalse
         $result.Envelopes() | Should -AnyElementMatch "`"message`":`"Hello from MSBuild app`""
     }
