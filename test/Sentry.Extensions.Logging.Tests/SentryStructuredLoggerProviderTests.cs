90a38233ff3dd727069b40b7d184d7f943b8e07e
James Crosswell <jamescrosswell@users.noreply.github.com>
2025-10-16 22:12:37 +1300

    Merge version 5.16.1 into version6 (#4634)
    Co-authored-by: J-P Nurmi <jpnurmi@gmail.com>

--- test/Sentry.Extensions.Logging.Tests/SentryStructuredLoggerProviderTests.cs
+++ test/Sentry.Extensions.Logging.Tests/SentryStructuredLoggerProviderTests.cs
@@ -91,6 +91,9 @@ public class SentryStructuredLoggerProviderTests
 
         capturedLog.TryGetAttribute("sentry.sdk.version", out object? version).Should().BeTrue();
         version.Should().Be(SentryLoggerProvider.NameAndVersion.Version);
+
+        capturedLog.TryGetAttribute("sentry.origin", out object? origin).Should().BeTrue();
+        origin.Should().Be("auto.log.extensions_logging");
     }
 
     [Fact]
