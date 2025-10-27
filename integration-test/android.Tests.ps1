af57caa8ca4292f9501c45660439c7be49859340
J-P Nurmi <jpnurmi@gmail.com>
2025-10-27 12:29:20 +0100

    feat: allow configuring native signal handler strategy on Android

--- integration-test/android.Tests.ps1
+++ integration-test/android.Tests.ps1
@@ -169,12 +169,7 @@ Describe 'MAUI app (<tfm>, <configuration>)' -ForEach @(
         Dump-ServerErrors -Result $result
         $result.HasErrors() | Should -BeFalse
         $result.Envelopes() | Should -AnyElementMatch "`"type`":`"System.NullReferenceException`""
-        # TODO: fix redundant SIGSEGV in Release (#3954)
-        if ($configuration -eq "Release") {
-            { $result.Envelopes() | Should -Not -AnyElementMatch "`"type`":`"SIGSEGV`"" } | Should -Throw
-        } else {
-            $result.Envelopes() | Should -Not -AnyElementMatch "`"type`":`"SIGSEGV`""
-            $result.Envelopes() | Should -HaveCount 1
-        }
+        $result.Envelopes() | Should -Not -AnyElementMatch "`"type`":`"SIGSEGV`""
+        $result.Envelopes() | Should -HaveCount 1
     }
 }
