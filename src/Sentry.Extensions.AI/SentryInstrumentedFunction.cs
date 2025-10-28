a26501639fc28586d910d8aa44175e30825878a6
Alex Sohn <alex.sohn@sentry.io>
2025-10-28 14:00:19 -0400

    Fix concurrency issue when multiple GetResponseAsync is called

--- src/Sentry.Extensions.AI/SentryInstrumentedFunction.cs
+++ src/Sentry.Extensions.AI/SentryInstrumentedFunction.cs
@@ -3,29 +3,17 @@ using Sentry.Extensibility;
 
 namespace Sentry.Extensions.AI;
 
-internal sealed class SentryInstrumentedFunction(AIFunction innerFunction, ChatOptions? options = null)
+internal sealed class SentryInstrumentedFunction(AIFunction innerFunction, ChatOptions options)
     : DelegatingAIFunction(innerFunction)
 {
-    private readonly HubAdapter _hub = HubAdapter.Instance;
+    private static readonly HubAdapter Hub = HubAdapter.Instance;
 
     protected override async ValueTask<object?> InvokeCoreAsync(
         AIFunctionArguments arguments,
         CancellationToken cancellationToken)
     {
-        const string operation = "gen_ai.execute_tool";
-        var spanName = $"execute_tool {Name}";
-        var currSpan = SentryChatClient.RootSpan == null ?
-            _hub.StartSpan(operation, spanName) :
-            SentryChatClient.RootSpan.StartChild(operation, spanName);
-
-        currSpan.SetData("gen_ai.request.model", options?.ModelId);
-
-        currSpan.SetData("gen_ai.operation.name", "execute_tool");
-        currSpan.SetData("gen_ai.tool.name", Name);
-        currSpan.SetData("gen_ai.tool.description", Description);
-
-        currSpan.SetData("gen_ai.tool.input", arguments);
-
+        var currSpan = InitToolSpan(arguments);
+        RemoveSentryArgs(ref arguments);
         try
         {
             var result = await base.InvokeCoreAsync(arguments, cancellationToken).ConfigureAwait(false);
@@ -43,5 +31,38 @@ internal sealed class SentryInstrumentedFunction(AIFunction innerFunction, ChatO
             currSpan.Finish(ex);
             throw;
         }
+    }
+
+    private ISpan InitToolSpan(AIFunctionArguments arguments)
+    {
+        const string operation = "gen_ai.execute_tool";
+        var spanName = $"execute_tool {Name}";
+        ISpan currSpan;
+
+        if (arguments.TryGetValue(SentryAIConstants.KeyMessageFunctionArgumentDictKey,
+                out var keyMessage)
+            && keyMessage is ChatMessage message
+            && SentryChatClient.GetMessageToSpanDict(options).TryGetValue(message, out var agentSpan))
+        {
+            currSpan = agentSpan.StartChild(operation, spanName);
+        }
+        else
+        {
+            // If we couldn't find the agent span, just attach it to the hub's current scope
+            currSpan = Hub.StartSpan(operation, spanName);
+        }
+
+        currSpan.SetData("gen_ai.request.model", options?.ModelId);
+        currSpan.SetData("gen_ai.operation.name", "execute_tool");
+        currSpan.SetData("gen_ai.tool.name", Name);
+        currSpan.SetData("gen_ai.tool.description", Description);
+        currSpan.SetData("gen_ai.tool.input", arguments);
+
+        return currSpan;
+    }
+
+    private static void RemoveSentryArgs(ref AIFunctionArguments arguments)
+    {
+        arguments.Remove(SentryAIConstants.KeyMessageFunctionArgumentDictKey);
     }
 }
