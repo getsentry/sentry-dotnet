81adbdf4f4d0723fbc7b7c869d661a018d07f6b5
Alex Sohn <alex.sohn@sentry.io>
2025-10-31 17:07:22 -0400

    fix broken tool spans + cleanup unused methods

--- src/Sentry.Extensions.AI/SentryAIConstants.cs
+++ src/Sentry.Extensions.AI/SentryAIConstants.cs
@@ -5,27 +5,6 @@ namespace Sentry.Extensions.AI;
 internal static class SentryAIConstants
 {
     /// <summary>
-    /// <para>
-    /// Sentry will add a <see cref="ISpan"/> to AdditionalAttribute in <see cref="ChatOptions"/>.
-    /// </para>
-    /// <para>
-    /// This constant represents the string key to get the span which represents the agent span.
-    /// </para>
-    /// </summary>
-    internal const string OptionsAdditionalAttributeAgentSpanName = "SentryChatMessageAgentSpan";
-
-    /// <summary>
-    /// <para>
-    /// When an LLM uses a tool, Sentry will add an argument to <see cref="AIFunctionArguments"/>.
-    /// The additional argument will contain the request <see cref="ChatMessage"/> which initialized the tool call.
-    /// </para>
-    /// <para>
-    /// This constant represents the string key to get the message.
-    /// </para>
-    /// </summary>
-    internal const string KeyMessageFunctionArgumentDictKey = "SentrySpanToMessageDictKey";
-
-    /// <summary>
     /// The list of strings which FunctionInvokingChatClient(FICC) uses to start the tool call <see cref="Activity"/>.
     /// </summary>
     internal static readonly string[] FICCActivityNames = ["orchestrate_tools", "FunctionInvokingChatClient.GetResponseAsync", "FunctionInvokingChatClient"];
