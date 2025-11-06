017ff79f0237bcc53884daf7bd5966bc7b46b143
Alex Sohn <alex.sohn@sentry.io>
2025-11-06 15:21:17 -0500

    Fix SentryInstrumentedFunction initialization

--- src/Sentry.Extensions.AI/Extensions/SentryAIExtensions.cs
+++ src/Sentry.Extensions.AI/Extensions/SentryAIExtensions.cs
@@ -14,7 +14,7 @@ public static class SentryAIExtensions
     /// <param name="options">The <see cref="ChatOptions"/> that contains the <see cref="AIFunction"/> to instrument</param>
     public static ChatOptions AddSentryToolInstrumentation(this ChatOptions options)
     {
-        if (options.Tools is { Count: > 0 })
+        if (options.Tools is { Count: 0 })
         {
             return options;
         }
