d058e600afdf2bd050a34af9865d29eb92f2fec3
bitsandfoxes <reg@bitfox.at>
2025-11-06 16:17:27 +0100

    Log only when setting up

--- src/Sentry/SentryOptions.cs
+++ src/Sentry/SentryOptions.cs
@@ -1793,14 +1793,14 @@ public class SentryOptions
                 DiagnosticLogger = new ConsoleDiagnosticLogger(DiagnosticLevel);
                 DiagnosticLogger.LogDebug("Logging enabled with ConsoleDiagnosticLogger and min level: {0}",
                     DiagnosticLevel);
-            }
 
-            if (SettingLocator.GetEnvironment().Equals("production", StringComparison.OrdinalIgnoreCase))
-            {
-                DiagnosticLogger.LogWarning("Sentry option 'Debug' is set to true while Environment is production. " +
-                                            "Be aware this can cause performance degradation and is not advised. " +
-                                            "See https://docs.sentry.io/platforms/dotnet/configuration/diagnostic-logger " +
-                                            "for more information");
+                if (SettingLocator.GetEnvironment().Equals("production", StringComparison.OrdinalIgnoreCase))
+                {
+                    DiagnosticLogger.LogWarning("Sentry option 'Debug' is set to true while Environment is production. " +
+                                                "Be aware this can cause performance degradation and is not advised. " +
+                                                "See https://docs.sentry.io/platforms/dotnet/configuration/diagnostic-logger " +
+                                                "for more information");
+                }
             }
         }
         else
