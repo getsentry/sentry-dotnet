b97a6bddbb294a7f43ff3dcf753b119bb3e9ed36
Eric Sampson <esampson@eaze.com>
2025-11-11 14:11:10 -0600

    Enhance the HttpFailedRequestHandler so that it captures a full exception stack trace where possible, to improve Issue grouping.

--- src/Sentry/SentryHttpFailedRequestHandler.cs
+++ src/Sentry/SentryHttpFailedRequestHandler.cs
@@ -21,61 +21,78 @@ internal class SentryHttpFailedRequestHandler : SentryFailedRequestHandler
         }
 
         // Capture the event
+
+        var statusCode = (int)response.StatusCode;
+        // Match behavior of HttpResponseMessage.EnsureSuccessStatusCode
+        if (statusCode >= 200 && statusCode <= 299)
+        {
+            return;
+        }
+
+        var exception = new HttpRequestException(
+            string.Format(
+                System.Globalization.CultureInfo.InvariantCulture,
+                "Response status code does not indicate success: {0}",
+                statusCode)
+            );
+
+#if NET6_0_OR_GREATER
+        // Add a full stack trace into the exception to improve Issue grouping,
+        // see https://github.com/getsentry/sentry-dotnet/issues/3582
+        ExceptionDispatchInfo.SetRemoteStackTrace(exception, Environment.StackTrace);
+#else
+        // Where SetRemoteStackTrace is not available, throw and catch to get a basic stack trace
         try
         {
-#if NET5_0_OR_GREATER
-            response.EnsureSuccessStatusCode();
-#else
-            // Use our own implementation of EnsureSuccessStatusCode because older implementations of
-            // EnsureSuccessStatusCode disposes the content.
-            // See https://github.com/dotnet/runtime/issues/24845
-            response.StatusCode.EnsureSuccessStatusCode();
-#endif
+            throw exception;
         }
-        catch (HttpRequestException exception)
+        catch (HttpRequestException ex)
         {
-            exception.SetSentryMechanism(MechanismType);
-
-            var @event = new SentryEvent(exception);
-            var hint = new SentryHint(HintTypes.HttpResponseMessage, response);
-
-            var uri = response.RequestMessage?.RequestUri;
-            var sentryRequest = new SentryRequest
-            {
-                QueryString = uri?.Query,
-                Method = response.RequestMessage?.Method.Method.ToUpperInvariant()
-            };
-
-            var responseContext = new Response
-            {
-                StatusCode = (short)response.StatusCode,
-#if NET5_0_OR_GREATER
-                // Starting with .NET 5, the content and headers are guaranteed to not be null.
-                BodySize = response.Content.Headers.ContentLength
-#else
-                // The ContentLength might be null (but that's ok).
-                // See https://github.com/dotnet/runtime/issues/16162
-                BodySize = response.Content?.Headers?.ContentLength
-#endif
-            };
-
-            if (!Options.SendDefaultPii)
-            {
-                sentryRequest.Url = uri?.HttpRequestUrl();
-            }
-            else
-            {
-                sentryRequest.Url = uri?.AbsoluteUri;
-                sentryRequest.Cookies = request.Headers.GetCookies();
-                sentryRequest.AddHeaders(request.Headers);
-                responseContext.Cookies = response.Headers.GetCookies();
-                responseContext.AddHeaders(response.Headers);
-            }
-
-            @event.Request = sentryRequest;
-            @event.Contexts[Response.Type] = responseContext;
-
-            Hub.CaptureEvent(@event, hint: hint);
+            exception = ex;
         }
+#endif
+
+        exception.SetSentryMechanism(MechanismType);
+
+        var @event = new SentryEvent(exception);
+        var hint = new SentryHint(HintTypes.HttpResponseMessage, response);
+
+        var uri = response.RequestMessage?.RequestUri;
+        var sentryRequest = new SentryRequest
+        {
+            QueryString = uri?.Query,
+            Method = response.RequestMessage?.Method.Method.ToUpperInvariant()
+        };
+
+        var responseContext = new Response
+        {
+            StatusCode = (short)response.StatusCode,
+#if NET5_0_OR_GREATER
+            // Starting with .NET 5, the content and headers are guaranteed to not be null.
+            BodySize = response.Content.Headers.ContentLength
+#else
+            // The ContentLength might be null (but that's ok).
+            // See https://github.com/dotnet/runtime/issues/16162
+            BodySize = response.Content?.Headers?.ContentLength
+#endif
+        };
+
+        if (!Options.SendDefaultPii)
+        {
+            sentryRequest.Url = uri?.HttpRequestUrl();
+        }
+        else
+        {
+            sentryRequest.Url = uri?.AbsoluteUri;
+            sentryRequest.Cookies = request.Headers.GetCookies();
+            sentryRequest.AddHeaders(request.Headers);
+            responseContext.Cookies = response.Headers.GetCookies();
+            responseContext.AddHeaders(response.Headers);
+        }
+
+        @event.Request = sentryRequest;
+        @event.Contexts[Response.Type] = responseContext;
+
+        Hub.CaptureEvent(@event, hint: hint);
     }
 }
