570eae6bb33b9f252d6b84a83265a84934b2eadd
James Crosswell <jamescrosswell@users.noreply.github.com>
2025-11-12 21:28:23 +1300

    Implement process and instance isolation (#4498)

--- src/Sentry/SentrySdk.cs
+++ src/Sentry/SentrySdk.cs
@@ -30,6 +30,8 @@ static partial class SentrySdk
 
     internal static IHub InitHub(SentryOptions options)
     {
+        options.InitCounter.Increment();
+
         options.SetupLogging();
 
         ProcessInfo.Instance ??= new ProcessInfo(options);
