35b4a8f09cc2ae0554688edec8400d96672449a6
Stefan Pölz <38893694+Flash0ver@users.noreply.github.com>
2025-11-28 13:35:57 +0100

    Update samples/Sentry.Samples.Google.Cloud.Functions/Function.cs
    Co-authored-by: James Crosswell <jamescrosswell@users.noreply.github.com>

--- samples/Sentry.Samples.Google.Cloud.Functions/Function.cs
+++ samples/Sentry.Samples.Google.Cloud.Functions/Function.cs
@@ -16,6 +16,7 @@ public class Function : IHttpFunction
 
     public Task HandleAsync(HttpContext context)
     {
+        // Configure structured logging via appsettings.json (Logging:Sentry:LogLevel:)
         _logger.LogTrace("LogLevel.Trace is not configured to be sent as structured log");
         _logger.LogInformation("Useful info that is added to the breadcrumb list.");
         throw new Exception("Bad function");
