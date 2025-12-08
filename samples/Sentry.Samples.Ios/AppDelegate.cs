67226439d40a8c36e96be3fd1c307f829bbd62d8
James Crosswell <jamescrosswell@users.noreply.github.com>
2025-12-08 13:17:27 +1300

    Testing something insane...

--- samples/Sentry.Samples.Ios/AppDelegate.cs
+++ samples/Sentry.Samples.Ios/AppDelegate.cs
@@ -58,7 +58,8 @@ public class AppDelegate : UIApplicationDelegate
 #pragma warning disable CA1422 // Validate platform compatibility
         // create a new window instance based on the screen size
         Window = new UIWindow(UIScreen.MainScreen.Bounds);
-#pragma warning restore CA1422
+// TODO: Undomment this line... just testing this comment: https://github.com/getsentry/sentry-dotnet/pull/4782#issuecomment-3623881935
+// #pragma warning restore CA1422
 
         // determine control colours (SystemBackground requires iOS >= 13.0)
         var backgroundColor = UIDevice.CurrentDevice.CheckSystemVersion(13, 0)
