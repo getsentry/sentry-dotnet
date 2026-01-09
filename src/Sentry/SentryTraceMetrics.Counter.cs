9485ca4b73336348ae513a3fdca50a95bd73f742
Stefan Pölz <38893694+Flash0ver@users.noreply.github.com>
2026-01-09 19:11:19 +0100

    more public overloads for convenience

---
+++ src/Sentry/SentryTraceMetrics.Counter.cs
@@ -0,0 +1,53 @@
+namespace Sentry;
+
+public abstract partial class SentryTraceMetrics
+{
+    /// <summary>
+    /// Increment a counter.
+    /// </summary>
+    /// <param name="name">The name of the metric.</param>
+    /// <param name="value">The value of the metric.</param>
+    /// <typeparam name="T">The numeric type of the metric.</typeparam>
+    public void AddCounter<T>(string name, T value) where T : struct
+    {
+        CaptureMetric(SentryMetricType.Counter, name, value, null, [], null);
+    }
+
+    /// <summary>
+    /// Increment a counter.
+    /// </summary>
+    /// <param name="name">The name of the metric.</param>
+    /// <param name="value">The value of the metric.</param>
+    /// <param name="scope">The scope to capture the metric with.</param>
+    /// <typeparam name="T">The numeric type of the metric.</typeparam>
+    public void AddCounter<T>(string name, T value, Scope? scope) where T : struct
+    {
+        CaptureMetric(SentryMetricType.Counter, name, value, null, [], scope);
+    }
+
+    /// <summary>
+    /// Increment a counter.
+    /// </summary>
+    /// <param name="name">The name of the metric.</param>
+    /// <param name="value">The value of the metric.</param>
+    /// <param name="attributes">A dictionary of attributes (key-value pairs with type information).</param>
+    /// <param name="scope">The scope to capture the metric with.</param>
+    /// <typeparam name="T">The numeric type of the metric.</typeparam>
+    public void AddCounter<T>(string name, T value, IEnumerable<KeyValuePair<string, object>>? attributes, Scope? scope = null) where T : struct
+    {
+        CaptureMetric(SentryMetricType.Counter, name, value, null, attributes, scope);
+    }
+
+    /// <summary>
+    /// Increment a counter.
+    /// </summary>
+    /// <param name="name">The name of the metric.</param>
+    /// <param name="value">The value of the metric.</param>
+    /// <param name="attributes">A dictionary of attributes (key-value pairs with type information).</param>
+    /// <param name="scope">The scope to capture the metric with.</param>
+    /// <typeparam name="T">The numeric type of the metric.</typeparam>
+    public void AddCounter<T>(string name, T value, ReadOnlySpan<KeyValuePair<string, object>> attributes, Scope? scope = null) where T : struct
+    {
+        CaptureMetric(SentryMetricType.Counter, name, value, null, attributes, scope);
+    }
+}
---
+++ src/Sentry/SentryTraceMetrics.Gauge.cs
@@ -0,0 +1,68 @@
+namespace Sentry;
+
+public abstract partial class SentryTraceMetrics
+{
+    /// <summary>
+    /// Set a gauge value.
+    /// </summary>
+    /// <param name="name">The name of the metric.</param>
+    /// <param name="value">The value of the metric.</param>
+    /// <typeparam name="T">The numeric type of the metric.</typeparam>
+    public void RecordGauge<T>(string name, T value) where T : struct
+    {
+        CaptureMetric(SentryMetricType.Gauge, name, value, null, [], null);
+    }
+
+    /// <summary>
+    /// Set a gauge value.
+    /// </summary>
+    /// <param name="name">The name of the metric.</param>
+    /// <param name="value">The value of the metric.</param>
+    /// <param name="unit">The unit of measurement.</param>
+    /// <typeparam name="T">The numeric type of the metric.</typeparam>
+    public void RecordGauge<T>(string name, T value, string? unit) where T : struct
+    {
+        CaptureMetric(SentryMetricType.Gauge, name, value, unit, [], null);
+    }
+
+    /// <summary>
+    /// Set a gauge value.
+    /// </summary>
+    /// <param name="name">The name of the metric.</param>
+    /// <param name="value">The value of the metric.</param>
+    /// <param name="unit">The unit of measurement.</param>
+    /// <param name="scope">The scope to capture the metric with.</param>
+    /// <typeparam name="T">The numeric type of the metric.</typeparam>
+    public void RecordGauge<T>(string name, T value, string? unit, Scope? scope) where T : struct
+    {
+        CaptureMetric(SentryMetricType.Gauge, name, value, unit, [], scope);
+    }
+
+    /// <summary>
+    /// Set a gauge value.
+    /// </summary>
+    /// <param name="name">The name of the metric.</param>
+    /// <param name="value">The value of the metric.</param>
+    /// <param name="unit">The unit of measurement.</param>
+    /// <param name="attributes">A dictionary of attributes (key-value pairs with type information).</param>
+    /// <param name="scope">The scope to capture the metric with.</param>
+    /// <typeparam name="T">The numeric type of the metric.</typeparam>
+    public void RecordGauge<T>(string name, T value, string? unit, IEnumerable<KeyValuePair<string, object>>? attributes, Scope? scope = null) where T : struct
+    {
+        CaptureMetric(SentryMetricType.Gauge, name, value, unit, attributes, scope);
+    }
+
+    /// <summary>
+    /// Set a gauge value.
+    /// </summary>
+    /// <param name="name">The name of the metric.</param>
+    /// <param name="value">The value of the metric.</param>
+    /// <param name="unit">The unit of measurement.</param>
+    /// <param name="attributes">A dictionary of attributes (key-value pairs with type information).</param>
+    /// <param name="scope">The scope to capture the metric with.</param>
+    /// <typeparam name="T">The numeric type of the metric.</typeparam>
+    public void RecordGauge<T>(string name, T value, string? unit, ReadOnlySpan<KeyValuePair<string, object>> attributes, Scope? scope = null) where T : struct
+    {
+        CaptureMetric(SentryMetricType.Gauge, name, value, unit, attributes, scope);
+    }
+}
