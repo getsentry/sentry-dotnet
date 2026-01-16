32caf06bd729212b33d54096d82f7295be8c9fc4
Stefan Pölz <38893694+Flash0ver@users.noreply.github.com>
2026-01-16 14:52:50 +0100

    Update SentryUnits.cs

--- src/Sentry/SentryUnits.cs
+++ src/Sentry/SentryUnits.cs
@@ -1,33 +1,37 @@
 namespace Sentry;
 
 /// <summary>
-/// Provides constants for metric units supported by Sentry.
+/// Supported units by Relay and Sentry.
+/// Applies to <see cref="SentryMetric{T}.Unit"/>, as well as attributes of <see cref="SentryLog"/> and attributes of <see cref="SentryMetric{T}"/>.
 /// </summary>
 /// <remarks>
-/// While the SDK accepts any string value for metric units, these constants provide
-/// type-safe access to units that are officially supported by Sentry's Relay and platform.
+/// Contains the units currently <see href="https://getsentry.github.io/relay/relay_metrics/enum.MetricUnit.html">supported by Relay</see> and Sentry.
+/// For <see href="https://develop.sentry.dev/sdk/telemetry/metrics/">Metrics</see> and <see href="https://develop.sentry.dev/sdk/telemetry/attributes/">Attributes</see>, currently, custom units are not allowed and will be set to "None" by Relay.
 /// </remarks>
-/// <seealso href="https://develop.sentry.dev/sdk/telemetry/metrics/"/>
 /// <seealso href="https://develop.sentry.dev/sdk/telemetry/attributes/#units"/>
-public static class SentryMetricUnits
+/// <seealso href="https://github.com/getsentry/sentry-conventions/"/>
+public static class SentryUnits
 {
     /// <summary>
-    /// Duration units for time measurements.
+    /// Duration Units.
     /// </summary>
     public static class Duration
     {
         /// <summary>
         /// Nanosecond unit (ns).
+        /// 10^-9 seconds.
         /// </summary>
         public static string Nanosecond { get; } = "nanosecond";
 
         /// <summary>
         /// Microsecond unit (μs).
+        /// 10^-6 seconds.
         /// </summary>
         public static string Microsecond { get; } = "microsecond";
 
         /// <summary>
         /// Millisecond unit (ms).
+        /// 10^-3 seconds.
         /// </summary>
         public static string Millisecond { get; } = "millisecond";
 
@@ -38,113 +42,136 @@ public static class SentryMetricUnits
 
         /// <summary>
         /// Minute unit (min).
+        /// 60 seconds.
         /// </summary>
         public static string Minute { get; } = "minute";
 
         /// <summary>
         /// Hour unit (h).
+        /// 3_600 seconds.
         /// </summary>
         public static string Hour { get; } = "hour";
 
         /// <summary>
         /// Day unit (d).
+        /// 86_400 seconds.
         /// </summary>
         public static string Day { get; } = "day";
 
         /// <summary>
         /// Week unit (wk).
+        /// 604_800 seconds.
         /// </summary>
         public static string Week { get; } = "week";
     }
 
     /// <summary>
-    /// Information units for data storage and transfer measurements.
+    /// Information Units.
     /// </summary>
+    /// <remarks>
+    /// Note that there are computer systems with a different number of bits per byte.
+    /// </remarks>
     public static class Information
     {
         /// <summary>
         /// Bit unit (b).
+        /// 1/8 of a byte.
         /// </summary>
         public static string Bit { get; } = "bit";
 
         /// <summary>
         /// Byte unit (B).
+        /// 8 bits.
         /// </summary>
         public static string Byte { get; } = "byte";
 
         /// <summary>
-        /// Kilobyte unit (KB) - 1,000 bytes (decimal).
+        /// Kilobyte unit (kB).
+        /// 10^3 bytes = 1_000 bytes (decimal).
         /// </summary>
         public static string Kilobyte { get; } = "kilobyte";
 
         /// <summary>
-        /// Kibibyte unit (KiB) - 1,024 bytes (binary).
+        /// Kibibyte unit (KiB).
+        /// 2^10 bytes = 1_024 bytes (binary).
         /// </summary>
         public static string Kibibyte { get; } = "kibibyte";
 
         /// <summary>
-        /// Megabyte unit (MB) - 1,000,000 bytes (decimal).
+        /// Megabyte unit (MB).
+        /// 10^6 bytes = 1_000_000 bytes (decimal).
         /// </summary>
         public static string Megabyte { get; } = "megabyte";
 
         /// <summary>
-        /// Mebibyte unit (MiB) - 1,048,576 bytes (binary).
+        /// Mebibyte unit (MiB).
+        /// 2^20 bytes = 1_048_576 bytes (binary).
         /// </summary>
         public static string Mebibyte { get; } = "mebibyte";
 
         /// <summary>
-        /// Gigabyte unit (GB) - 1,000,000,000 bytes (decimal).
+        /// Gigabyte unit (GB).
+        /// 10^9 bytes = 1_000_000_000 bytes (decimal).
         /// </summary>
         public static string Gigabyte { get; } = "gigabyte";
 
         /// <summary>
-        /// Gibibyte unit (GiB) - 1,073,741,824 bytes (binary).
+        /// Gibibyte unit (GiB).
+        /// 2^30 bytes = 1_073_741_824 bytes (binary).
         /// </summary>
         public static string Gibibyte { get; } = "gibibyte";
 
         /// <summary>
-        /// Terabyte unit (TB) - 1,000,000,000,000 bytes (decimal).
+        /// Terabyte unit (TB).
+        /// 10^12 bytes = 1_000_000_000_000 bytes (decimal).
         /// </summary>
         public static string Terabyte { get; } = "terabyte";
 
         /// <summary>
-        /// Tebibyte unit (TiB) - 1,099,511,627,776 bytes (binary).
+        /// Tebibyte unit (TiB).
+        /// 2^40 bytes = 1_099_511_627_776 bytes (binary).
         /// </summary>
         public static string Tebibyte { get; } = "tebibyte";
 
         /// <summary>
-        /// Petabyte unit (PB) - 1,000,000,000,000,000 bytes (decimal).
+        /// Petabyte unit (PB).
+        /// 10^15 bytes = 1_000_000_000_000_000 bytes (decimal).
         /// </summary>
         public static string Petabyte { get; } = "petabyte";
 
         /// <summary>
-        /// Pebibyte unit (PiB) - 1,125,899,906,842,624 bytes (binary).
+        /// Pebibyte unit (PiB).
+        /// 2^50 bytes = 1_125_899_906_842_624 bytes (binary).
         /// </summary>
         public static string Pebibyte { get; } = "pebibyte";
 
         /// <summary>
-        /// Exabyte unit (EB) - 1,000,000,000,000,000,000 bytes (decimal).
+        /// Exabyte unit (EB).
+        /// 10^18 bytes = 1_000_000_000_000_000_000 bytes (decimal).
         /// </summary>
         public static string Exabyte { get; } = "exabyte";
 
         /// <summary>
-        /// Exbibyte unit (EiB) - 1,152,921,504,606,846,976 bytes (binary).
+        /// Exbibyte unit (EiB).
+        /// 2^60 bytes = 1_152_921_504_606_846_976 bytes (binary).
         /// </summary>
         public static string Exbibyte { get; } = "exbibyte";
     }
 
     /// <summary>
-    /// Fraction units for ratio and percentage measurements.
+    /// Fraction Units.
     /// </summary>
     public static class Fraction
     {
         /// <summary>
-        /// Ratio unit (unitless fraction).
+        /// Ratio unit.
+        /// Floating point fraction of 1.
         /// </summary>
         public static string Ratio { get; } = "ratio";
 
         /// <summary>
         /// Percent unit (%).
+        /// Ratio expressed as a fraction of 100. 100% equals a ratio of 1.0.
         /// </summary>
         public static string Percent { get; } = "percent";
     }
