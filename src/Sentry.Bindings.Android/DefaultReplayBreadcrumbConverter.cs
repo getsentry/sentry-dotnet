cabaa599a7343cd4ef00fce723c4404e7929fb7e
James Crosswell <jamescrosswell@users.noreply.github.com>
2026-01-27 11:49:14 +1300

    Fix issues building against local maven repository

--- src/Sentry.Bindings.Android/DefaultReplayBreadcrumbConverter.cs
+++ src/Sentry.Bindings.Android/DefaultReplayBreadcrumbConverter.cs
@@ -4,7 +4,8 @@ using Sentry.JavaSdk;
 // ReSharper disable once CheckNamespace - match generated code namespace
 namespace Sentry.JavaSdk.Android.Replay;
 
-// This file used to try to "patch" the generated bindings via a partial class.
-// It caused build breaks when the generated Java binding shape changed.
-// Binding fixes should be done via Transforms/Metadata.xml instead.
+// Add IReplayBreadcrumbConverter interface to DefaultReplayBreadcrumbConverter, to work source generator issue
+internal partial class DefaultReplayBreadcrumbConverter  : IReplayBreadcrumbConverter
+{
+}
 #endif
