704d35f3714e0b3110aa854abede1551133a5888
James Crosswell <jamescrosswell@users.noreply.github.com>
2026-02-12 13:06:36 +1300

    Try another tack

--- test/Sentry.Tests/AttributeReaderTests.cs
+++ test/Sentry.Tests/AttributeReaderTests.cs
@@ -6,8 +6,6 @@ public class AttributeReaderTests
     public void Simple()
     {
         var assembly = typeof(AttributeReaderTests).Assembly;
-#pragma warning disable IDE0055 // Disable formatting - see https://github.com/getsentry/sentry-dotnet/pull/4911#discussion_r2795717887
         Assert.NotNull(AttributeReader.TryGetProjectDirectory(assembly));
-#pragma warning restore IDE0055 // Restore formatting        
     }
 }
