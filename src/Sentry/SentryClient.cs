4789ad8a942a67ea1fc09741ffb166f3c84d9d7f
bitsandfoxes <reg@bitfox.at>
2026-03-19 10:28:59 +0100

    removed redundant enrichting

--- src/Sentry/SentryClient.cs
+++ src/Sentry/SentryClient.cs
@@ -110,7 +110,6 @@ public class SentryClient : ISentryClient, IDisposable
         // Evaluate and copy before invoking the callback
         scope.Evaluate();
         scope.Apply(evt);
-        _enricher.Apply(evt);
 
         if (scope.Level != null && scope.Level != SentryLevel.Info)
         {
