cd8b52b9d6608fc5cdaf731e2a556b9b4f7db881
James Crosswell <jamescrosswell@users.noreply.github.com>
2026-04-22 15:15:00 +1200

    Revert unintentional changes to Shell breadcrumb tests

--- test/Sentry.Maui.Tests/MauiEventsBinderTests.Shell.cs
+++ test/Sentry.Maui.Tests/MauiEventsBinderTests.Shell.cs
@@ -6,7 +6,7 @@ namespace Sentry.Maui.Tests;
 public partial class MauiEventsBinderTests
 {
     [Fact]
-    public void OnShellOnNavigating_AddsBreadcrumb()
+    public void Shell_Navigating_AddsBreadcrumb()
     {
         // Arrange
         var shell = new Shell
@@ -32,6 +32,32 @@ public partial class MauiEventsBinderTests
         crumb.Data.Should().Contain($"from", "foo");
         crumb.Data.Should().Contain($"to", "bar");
         crumb.Data.Should().Contain($"Source", "Push");
+    }
+
+    [Fact]
+    public void Shell_UnbindNavigating_DoesNotAddBreadcrumb()
+    {
+        // Arrange
+        var shell = new Shell
+        {
+            StyleId = "shell"
+        };
+        _fixture.Binder.HandleShellEvents(shell);
+
+        var current = new ShellNavigationState("foo");
+        var target = new ShellNavigationState("bar");
+        const ShellNavigationSource source = ShellNavigationSource.Push;
+
+        shell.RaiseEvent(nameof(Shell.Navigating), new ShellNavigatingEventArgs(current, target, source, false));
+        Assert.Single(_fixture.Scope.Breadcrumbs); // Sanity check
+
+        _fixture.Binder.HandleShellEvents(shell, bind: false);
+
+        // Act
+        shell.RaiseEvent(nameof(Shell.Navigating), new ShellNavigatingEventArgs(target, current, source, false));
+
+        // Assert
+        Assert.Single(_fixture.Scope.Breadcrumbs);
     }
 
     [Fact]
@@ -120,7 +146,7 @@ public partial class MauiEventsBinderTests
     }
 
     [Fact]
-    public void OnShellOnNavigated_AddsBreadcrumb()
+    public void Shell_Navigated_AddsBreadcrumb()
     {
         // Arrange
         var shell = new Shell
@@ -146,6 +172,32 @@ public partial class MauiEventsBinderTests
         crumb.Data.Should().Contain($"from", "foo");
         crumb.Data.Should().Contain($"to", "bar");
         crumb.Data.Should().Contain($"Source", "Push");
+    }
+
+    [Fact]
+    public void Shell_UnbindNavigated_DoesNotAddBreadcrumb()
+    {
+        // Arrange
+        var shell = new Shell
+        {
+            StyleId = "shell"
+        };
+        _fixture.Binder.HandleShellEvents(shell);
+
+        var previous = new ShellNavigationState("foo");
+        var current = new ShellNavigationState("bar");
+        const ShellNavigationSource source = ShellNavigationSource.Push;
+
+        shell.RaiseEvent(nameof(Shell.Navigated), new ShellNavigatedEventArgs(previous, current, source));
+        Assert.Single(_fixture.Scope.Breadcrumbs); // Sanity check
+
+        _fixture.Binder.HandleShellEvents(shell, bind: false);
+
+        // Act
+        shell.RaiseEvent(nameof(Shell.Navigated), new ShellNavigatedEventArgs(current, previous, source));
+
+        // Assert
+        Assert.Single(_fixture.Scope.Breadcrumbs);
     }
 
     [Fact]
