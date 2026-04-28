861ee9351813e6d68bb7fb9d287f89e8da554fc5
James Crosswell <jamescrosswell@users.noreply.github.com>
2026-04-28 14:17:04 +1200

    Tests for UseOtlp

---
+++ test/Sentry.OpenTelemetry.Exporter.OpenTelemetryProtocol.Tests/SentryOptionsExtensionsTests.cs
@@ -0,0 +1,122 @@
+using OpenTelemetry.Context.Propagation;
+using OpenTelemetry.Trace;
+
+namespace Sentry.OpenTelemetry.Exporter.OpenTelemetryProtocol.Tests;
+
+public class SentryOptionsExtensionsTests
+{
+    [Fact]
+    public void UseOtlp_SetsInstrumenterToOpenTelemetry()
+    {
+        // Arrange
+        var options = new SentryOptions { Dsn = ValidDsn };
+
+        // Act
+        options.UseOtlp();
+
+        // Assert
+        options.Instrumenter.Should().Be(Instrumenter.OpenTelemetry);
+    }
+
+    [Fact]
+    public void UseOtlp_DisablesSentryTracing()
+    {
+        // Arrange
+        var options = new SentryOptions { Dsn = ValidDsn };
+
+        // Act
+        options.UseOtlp();
+
+        // Assert
+        options.DisableSentryTracing.Should().BeTrue();
+    }
+
+    [Fact]
+    public void UseOtlp_SetsPropagationContextFactoryToOtelPropagationContext()
+    {
+        // Arrange
+        var options = new SentryOptions { Dsn = ValidDsn };
+
+        // Act
+        options.UseOtlp();
+
+        // Assert
+        options.PropagationContextFactory.Should().NotBeNull();
+        options.PropagationContextFactory!(null).Should().BeOfType<OtelPropagationContext>();
+    }
+
+    [Theory]
+    [InlineData(null)]
+    [InlineData("")]
+    [InlineData("   ")]
+    public void UseOtlp_WithTracerProviderBuilder_MissingDsn_ThrowsArgumentException(string dsn)
+    {
+        // Arrange
+        var options = new SentryOptions { Dsn = dsn };
+        var tracerProviderBuilder = Substitute.For<TracerProviderBuilder>();
+
+        // Act
+        var act = () => options.UseOtlp(tracerProviderBuilder);
+
+        // Assert
+        act.Should().Throw<ArgumentException>();
+    }
+
+    [Fact]
+    public void UseOtlp_WithTracerProviderBuilder_SetsInstrumenterToOpenTelemetry()
+    {
+        // Arrange
+        var options = new SentryOptions { Dsn = ValidDsn };
+        var tracerProviderBuilder = Substitute.For<TracerProviderBuilder>();
+
+        // Act
+        options.UseOtlp(tracerProviderBuilder);
+
+        // Assert
+        options.Instrumenter.Should().Be(Instrumenter.OpenTelemetry);
+    }
+
+    [Fact]
+    public void UseOtlp_WithTracerProviderBuilder_DisablesSentryTracing()
+    {
+        // Arrange
+        var options = new SentryOptions { Dsn = ValidDsn };
+        var tracerProviderBuilder = Substitute.For<TracerProviderBuilder>();
+
+        // Act
+        options.UseOtlp(tracerProviderBuilder);
+
+        // Assert
+        options.DisableSentryTracing.Should().BeTrue();
+    }
+
+    [Fact]
+    public void UseOtlp_WithTracerProviderBuilder_SetsPropagationContextFactoryToOtelPropagationContext()
+    {
+        // Arrange
+        var options = new SentryOptions { Dsn = ValidDsn };
+        var tracerProviderBuilder = Substitute.For<TracerProviderBuilder>();
+
+        // Act
+        options.UseOtlp(tracerProviderBuilder);
+
+        // Assert
+        options.PropagationContextFactory.Should().NotBeNull();
+        options.PropagationContextFactory!(null).Should().BeOfType<OtelPropagationContext>();
+    }
+
+    [Fact]
+    public void UseOtlp_WithCustomPropagator_DoesNotThrow()
+    {
+        // Arrange
+        var options = new SentryOptions { Dsn = DsnSamples.ValidDsn };
+        var tracerProviderBuilder = Substitute.For<TracerProviderBuilder>();
+        var customPropagator = Substitute.For<TextMapPropagator>();
+
+        // Act
+        var act = () => options.UseOtlp(tracerProviderBuilder, customPropagator);
+
+        // Assert
+        act.Should().NotThrow();
+    }
+}
