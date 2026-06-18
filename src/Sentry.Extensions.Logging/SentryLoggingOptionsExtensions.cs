74efd6f552f6f15cd0e7dbca57032d99c7a913fe
Stefan Pölz <38893694+Flash0ver@users.noreply.github.com>
2026-06-18 14:17:35 +0200

    docs: add XML comments for boolean return of ILogEntryFilter

--- src/Sentry.Extensions.Logging/SentryLoggingOptionsExtensions.cs
+++ src/Sentry.Extensions.Logging/SentryLoggingOptionsExtensions.cs
@@ -9,26 +9,28 @@ namespace Sentry.Extensions.Logging;
 public static class SentryLoggingOptionsExtensions
 {
     /// <summary>
-    /// Add an log event filter
+    /// Add a log event filter.
     /// </summary>
     /// <remarks>
     /// Filters are called before sending an event.
     /// This allows the filter to decide whether the log message should not be recorded at all.
+    /// The log entry is neither captured as <see cref="SentryEvent"/> nor added as <see cref="Breadcrumb"/> when any filter returns <see langword="true"/>.
     /// </remarks>
     /// <param name="options">The <see cref="SentryLoggingOptions"/> to hold the filter.</param>
-    /// <param name="filter">The filter.</param>
+    /// <param name="filter">The <see cref="ILogEntryFilter"/> filter.</param>
     public static void AddLogEntryFilter(this SentryLoggingOptions options, ILogEntryFilter filter)
         => options.Filters = options.Filters.Concat(new[] { filter }).ToArray();
 
     /// <summary>
-    /// Add an log event filter
+    /// Add a log event filter.
     /// </summary>
     /// <remarks>
     /// Filters are called before sending an event.
     /// This allows the filter to decide whether the log message should not be recorded at all.
+    /// The log entry is neither captured as <see cref="SentryEvent"/> nor added as <see cref="Breadcrumb"/> when any filter returns <see langword="true"/>.
     /// </remarks>
     /// <param name="options">The <see cref="SentryLoggingOptions"/> to hold the filter.</param>
-    /// <param name="filter">The filter.</param>
+    /// <param name="filter">The filter <see langword="delegate"/>. Return <see langword="true"/> if the log entry should be filtered.</param>
     public static void AddLogEntryFilter(
         this SentryLoggingOptions options,
         Func<string, LogLevel, EventId, Exception?, bool> filter)
