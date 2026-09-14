17df6e812013ca50b629df8c730a281301299a59
Michael Mairegger <michael.mairegger@yahoo.de>
2026-09-14 08:58:27 +0200

    refactor: migrate extension methods to extension block

--- src/Sentry.Quartz/IQuartzBuilderExtensions.cs
+++ src/Sentry.Quartz/IQuartzBuilderExtensions.cs
@@ -10,50 +10,49 @@ namespace Sentry.Quartz;
 /// </summary>
 public static class IQuartzBuilderExtensions
 {
-    /// <summary>
-    /// Adds middleware to track CRON jobs to Sentry
-    /// </summary>
-    /// <param name="configuration"></param>
-    /// <param name="configure">Configures the options</param>
-    /// <returns></returns>
-    public static IQuartzBuilder AddSentryCronJobs(this IQuartzBuilder configuration, Action<SentryCronJobOptions>? configure = null)
+    extension(IQuartzBuilder configuration)
     {
-        return configuration.AddJobMiddleware<SentryCronJobMiddleware>().ConfigureOptions(configure);
-    }
+        /// <summary>
+        /// Adds middleware to track CRON jobs to Sentry
+        /// </summary>
+        /// <param name="configure">Configures the options</param>
+        /// <returns></returns>
+        public IQuartzBuilder AddSentryCronJobs(Action<SentryCronJobOptions>? configure = null)
+        {
+            return configuration.AddJobMiddleware<SentryCronJobMiddleware>().ConfigureOptions(configure);
+        }
 
-    /// <summary>
-    /// Adds middleware to track job execution duration metrics
-    /// </summary>
-    /// <param name="configuration"></param>
-    /// <param name="configure">Configures the options</param>
-    /// <returns></returns>
-    public static IQuartzBuilder AddSentryMetrics(this IQuartzBuilder configuration, Action<SentryMetricsOptions>? configure = null)
-    {
-        return configuration.AddJobMiddleware<SentryMetricsMiddleware>().ConfigureOptions(configure);
-    }
+        /// <summary>
+        /// Adds middleware to track job execution duration metrics
+        /// </summary>
+        /// <param name="configure">Configures the options</param>
+        /// <returns></returns>
+        public IQuartzBuilder AddSentryMetrics(Action<SentryMetricsOptions>? configure = null)
+        {
+            return configuration.AddJobMiddleware<SentryMetricsMiddleware>().ConfigureOptions(configure);
+        }
 
-    /// <summary>
-    /// Adds middleware that pushes a scope to sentry before job execution
-    /// </summary>
-    /// <param name="configuration"></param>
-    /// <param name="configure">Configures the options</param>
-    /// <returns></returns>
-    public static IQuartzBuilder AddSentryScope(this IQuartzBuilder configuration, Action<SentryScopeMiddlewareOptions>? configure = null)
-    {
-        return configuration.AddJobMiddleware<SentryScopeMiddleware>().ConfigureOptions(configure);
-    }
+        /// <summary>
+        /// Adds middleware that pushes a scope to sentry before job execution
+        /// </summary>
+        /// <param name="configure">Configures the options</param>
+        /// <returns></returns>
+        public IQuartzBuilder AddSentryScope(Action<SentryScopeMiddlewareOptions>? configure = null)
+        {
+            return configuration.AddJobMiddleware<SentryScopeMiddleware>().ConfigureOptions(configure);
+        }
 
-    /// <summary>
-    /// For testing
-    /// </summary>
-    /// <param name="configuration"></param>
-    /// <param name="options"></param>
-    /// <param name="hub"></param>
-    /// <param name="logger"></param>
-    /// <returns></returns>
-    internal static IQuartzBuilder AddSentryCronJobs(this IQuartzBuilder configuration, IOptions<SentryCronJobOptions> options, IHub hub, ILogger<SentryCronJobMiddleware> logger)
-    {
-        configuration.AddJobMiddleware(new SentryCronJobMiddleware(hub, options, logger));
-        return configuration;
+        /// <summary>
+        /// For testing
+        /// </summary>
+        /// <param name="options"></param>
+        /// <param name="hub"></param>
+        /// <param name="logger"></param>
+        /// <returns></returns>
+        internal IQuartzBuilder AddSentryCronJobs(IOptions<SentryCronJobOptions> options, IHub hub, ILogger<SentryCronJobMiddleware> logger)
+        {
+            configuration.AddJobMiddleware(new SentryCronJobMiddleware(hub, options, logger));
+            return configuration;
+        }
     }
 }
