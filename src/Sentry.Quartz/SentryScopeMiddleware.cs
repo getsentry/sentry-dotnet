0d32eb9e3dabfa9e9678b862de4e7968a0faea92
Michael Mairegger <michael.mairegger@yahoo.de>
2026-09-14 09:07:55 +0200

    fix: use JobDetail.Key.ToString()

--- src/Sentry.Quartz/SentryScopeMiddleware.cs
+++ src/Sentry.Quartz/SentryScopeMiddleware.cs
@@ -17,12 +17,23 @@ internal sealed class SentryScopeMiddleware : IJobExecutionMiddleware
     public async ValueTask Invoke(IJobExecutionContext context, JobExecutionDelegate next, CancellationToken cancellationToken)
     {
         using var _ = _sentryHub.PushScope();
-        _sentryHub.ConfigureScope(scope => scope.SetTag(_options.ScopeTagName, context.JobDetail.Key.Name));
+        _sentryHub.ConfigureScope(scope => scope.SetTag(_options.ScopeTagName, context.JobDetail.Key.ToString()));
         await next(context, cancellationToken).ConfigureAwait(false);
     }
 }
 
+/// <summary>
+/// Options for configuring the behavior of the SentryScopeMiddleware.
+/// </summary>
 public class SentryScopeMiddlewareOptions
 {
+    /// <summary>
+    /// Gets or sets the name of the tag that will be added to the Sentry scope.
+    /// </summary>
+    /// <remarks>
+    /// The value of this property is used as the key for a tag in the Sentry scope
+    /// to allow associating metadata about the current Quartz job execution context.
+    /// By default, it is set to "quartz.job".
+    /// </remarks>
     public string ScopeTagName { get; set; } = "quartz.job";
 }
